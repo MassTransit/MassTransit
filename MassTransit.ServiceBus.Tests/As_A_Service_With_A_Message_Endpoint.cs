3c5e308a6f5d1a954ceb04a9668b51c907b01123
Chris Patterson <chris@phatboyg.com>
2007-12-27 19:26:36 +0000

    Fixed some tests
    Former-commit-id: 287e8e1df8493714131542412ca8656c4f493c32

--- MassTransit.ServiceBus.Tests/As_A_Service_With_A_Message_Endpoint.cs
+++ MassTransit.ServiceBus.Tests/As_A_Service_With_A_Message_Endpoint.cs
@@ -4,26 +4,25 @@ using NUnit.Framework.SyntaxHelpers;
 
 namespace MassTransit.ServiceBus.Tests
 {
-    [TestFixture]
-    public class As_A_Service_With_A_Message_Endpoint :
-        ServiceBusSetupFixture
-    {
+	[TestFixture]
+	public class As_A_Service_With_A_Message_Endpoint :
+		ServiceBusSetupFixture
+	{
+		[Test]
+		public void I_Want_To_Be_Able_To_Register_An_Event_Handler_For_Messages()
+		{
+			_serviceBus.Endpoint<MyUpdateMessage>().MessageReceived += MyUpdateMessage_Received;
 
-        [Test]
-        public void I_Want_To_Be_Able_To_Register_An_Event_Handler_For_Messages()
-        {
-            _serviceBus.Endpoint<MyUpdateMessage>().MessageReceived += As_A_Service_With_A_Message_Endpoint_MessageReceived;
+			Assert.That(_serviceBus.Endpoint<MyUpdateMessage>(), Is.Not.Null);
+		}
 
-            Assert.That(_serviceBus.Endpoint<MyUpdateMessage>(), Is.Not.Null);
-        }
+		private static void MyUpdateMessage_Received(object sender, MessageReceivedEventArgs<MyUpdateMessage> e)
+		{
+		}
 
-        void As_A_Service_With_A_Message_Endpoint_MessageReceived(object sender, MessageReceivedEventArgs<MyUpdateMessage> e)
-        {
-                
-        }
-
-        [Serializable]
-        public class MyUpdateMessage : IMessage {}
-        
-    }
+		[Serializable]
+		public class MyUpdateMessage : IMessage
+		{
+		}
+	}
 }
--- MassTransit.ServiceBus.Tests/When_A_Message_Is_Received_On_The_Service_Bus.cs
+++ MassTransit.ServiceBus.Tests/When_A_Message_Is_Received_On_The_Service_Bus.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Threading;
 using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
 
 namespace MassTransit.ServiceBus.Tests
 {
@@ -6,38 +9,26 @@ namespace MassTransit.ServiceBus.Tests
     public class When_A_Message_Is_Received_On_The_Service_Bus :
         ServiceBusSetupFixture
     {
-        //[Test]
-        //public void The_Message_Should_Be_Matched_To_The_Appropriate_Message_Handler()
-        //{
-        //    IMessageEndpoint<PingMessage> pingEndpoint = new PingMessageHandler(_serviceBus);
-
-        //    _serviceBus.MessageHandlers.Add(pingEndpoint);
-
-        //    PingMessage pm = new PingMessage();
-
-        //    IList<Type> handlerTypes = _serviceBus.MessageHandlers.Find(pm);
-
-        //    Assert.That(handlerTypes, Is.Not.Null);
-
-        //    Assert.That(handlerTypes.Count, Is.EqualTo(1));
-
-        //    Assert.That(handlerTypes[0], Is.EqualTo(pingEndpoint.GetType()));
-        //}
-
         [Test]
-        public void An_Event_Handler_Should_Be_Registered_On_The_Message_Endpoint()
+        public void An_Event_Handler_Should_Be_Called()
         {
-            _serviceBus.Endpoint<PingMessage>().MessageReceived += PingReceived;
+        	bool _received = false;
+        	ManualResetEvent _receivedEvent = new ManualResetEvent(false);
+
+			EventHandler<MessageReceivedEventArgs<PingMessage>> handler = delegate
+        	{
+        		_received = true;
+        		_receivedEvent.Set();
+        	};
+			
+        	_serviceBus.Endpoint<PingMessage>().MessageReceived += handler;
 
             PingMessage pm = new PingMessage();
-
             _serviceBus.Send(pm);
 
-        }
+        	Assert.That(_receivedEvent.WaitOne(TimeSpan.FromSeconds(5), true), Is.True);
 
-        private void PingReceived(object sender, MessageReceivedEventArgs<PingMessage> e)
-        {
-            
+        	Assert.That(_received, Is.True);
         }
     }
 }
