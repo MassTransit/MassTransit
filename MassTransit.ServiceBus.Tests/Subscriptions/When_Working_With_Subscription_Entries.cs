548a3f49331aa548079550180dd30aec4c7a392a
Chris Patterson <chris@phatboyg.com>
2007-12-28 01:03:14 +0000

    missing test file
    Former-commit-id: 5d33e0d8dc0c8e5afdab79ac0259c91d1ed7fdcc

---
+++ MassTransit.ServiceBus.Tests/Subscriptions/When_Working_With_Subscription_Entries.cs
@@ -0,0 +1,24 @@
+using MassTransit.ServiceBus.Subscriptions;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace MassTransit.ServiceBus.Tests.Subscriptions
+{
+	[TestFixture]
+	public class When_Working_With_Subscription_Entries :
+		ServiceBusSetupFixture
+	{
+		[Test]
+		public void Comparing_Two_Entries_Should_Return_True()
+		{
+			MessageQueueEndpoint leftEndpoint = _testEndPoint.Transport.Address;
+			MessageQueueEndpoint rightEndpoint = _testEndPoint.Transport.Address;
+
+			SubscriptionCacheEntry left = new SubscriptionCacheEntry(leftEndpoint);
+			SubscriptionCacheEntry right = new SubscriptionCacheEntry(rightEndpoint);
+
+			Assert.That(left, Is.EqualTo(right));
+		}
+		
+	}
+}
