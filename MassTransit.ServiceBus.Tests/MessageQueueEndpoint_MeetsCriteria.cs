b82ad6e9174e3199620b8f37a5d7f43657575e4c
Chris Patterson <chris@phatboyg.com>
2008-01-06 03:44:22 +0000

    Wrote new unit tests for message consumer, more likely needed, but the last one I just added found a bug in the criteria checking code!! Yeah unit tests rock!
    Former-commit-id: ea1c94866b4fa22c97f0cfa46e376786d1eba053

--- MassTransit.ServiceBus.Tests/MessageQueueEndpoint_MeetsCriteria.cs
+++ MassTransit.ServiceBus.Tests/MessageQueueEndpoint_MeetsCriteria.cs
@@ -1,43 +1,89 @@
+using MassTransit.ServiceBus.Tests.Messages;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
 namespace MassTransit.ServiceBus.Tests
 {
-    using System.Messaging;
-    using Messages;
-    using NUnit.Framework;
-    using Rhino.Mocks;
-
     [TestFixture]
     public class MessageQueueEndpoint_MeetsCriteria
+        : ServiceBusSetupFixture
     {
-        private MockRepository mocks;
-        private MessageQueueEndpoint _endpoint;
-        private IEnvelopeConsumer _consumer;
-        private PingMessage _msg;
-        private Message msmqMsg = new Message();
-
-        [SetUp]
-        public void SetUp()
+        [Test]
+        public void The_Service_Bus_Should_Return_False_If_The_Message_Will_Not_Be_Handled()
         {
-            mocks = new MockRepository();
-            _msg = new PingMessage();
-            _endpoint = new MessageQueueEndpoint(@".\private$\test");
-            _consumer = mocks.CreateMock<IEnvelopeConsumer>();
-            _endpoint.SerializeMessages(msmqMsg.BodyStream, new IMessage[] {_msg});
-        }
+            _serviceBus.Subscribe<PingMessage>(
+                delegate { },
+                delegate { return false; });
 
-        [TearDown]
-        public void TearDown()
-        {
-            mocks = null;
-            _endpoint = null;
-            _consumer = null;
+            IEnvelopeConsumer consumer = (IEnvelopeConsumer) _serviceBus;
+
+            IEnvelope envelope = new Envelope(new PingMessage());
+
+            Assert.That(consumer.MeetsCriteria(envelope), Is.False);
         }
 
         [Test]
-        public void Should_Hit_The_Second_Delegate()
+        public void The_Service_Bus_Should_Return_True_If_The_Message_Will_Be_Handled()
         {
-            _endpoint.Subscribe(_consumer);
+            _serviceBus.Subscribe<PingMessage>(
+                delegate { },
+                delegate { return true; });
 
-            _endpoint.ProcessMessage(msmqMsg);
+            IEnvelopeConsumer consumer = (IEnvelopeConsumer) _serviceBus;
+
+            IEnvelope envelope = new Envelope(new PingMessage());
+
+            Assert.That(consumer.MeetsCriteria(envelope), Is.True);
         }
+
+        [Test]
+        public void The_Service_Bus_Should_Return_True_If_The_Message_Will_Be_Handled_By_One_Of_multiple_handlers()
+        {
+            _serviceBus.Subscribe<PingMessage>(
+                delegate { },
+                delegate { return false; });
+
+            _serviceBus.Subscribe<PingMessage>(
+                delegate { },
+                delegate { return true; });
+
+            IEnvelopeConsumer consumer = (IEnvelopeConsumer) _serviceBus;
+
+            IEnvelope envelope = new Envelope(new PingMessage());
+
+            Assert.That(consumer.MeetsCriteria(envelope), Is.True);
+        }
+
+        //private MockRepository mocks;
+        //private MessageQueueEndpoint _endpoint;
+        //private IEnvelopeConsumer _consumer;
+        //private PingMessage _msg;
+        //private Message msmqMsg = new Message();
+
+        //[SetUp]
+        //public void SetUp()
+        //{
+        //    mocks = new MockRepository();
+        //    _msg = new PingMessage();
+        //    _endpoint = new MessageQueueEndpoint(@".\private$\test");
+        //    _consumer = mocks.CreateMock<IEnvelopeConsumer>();
+        //    _endpoint.SerializeMessages(msmqMsg.BodyStream, new IMessage[] {_msg});
+        //}
+
+        //[TearDown]
+        //public void TearDown()
+        //{
+        //    mocks = null;
+        //    _endpoint = null;
+        //    _consumer = null;
+        //}
+
+        //[Test]
+        //public void Should_Hit_The_Second_Delegate()
+        //{
+        //    _endpoint.Subscribe(_consumer);
+
+        //    _endpoint.ProcessMessage(msmqMsg);
+        //}
     }
 }
