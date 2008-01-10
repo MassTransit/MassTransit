3f0674c637ab7860df68afd74b072181f24e6e84
Chris Patterson <chris@phatboyg.com>
2008-01-10 21:51:46 +0000

    Some documentation added
    Former-commit-id: 6471c058bc98a5ebcb67c7336edd3cc94c50abf4

--- MassTransit.ServiceBus/IEndpoint.cs
+++ MassTransit.ServiceBus/IEndpoint.cs
@@ -2,9 +2,15 @@ using System;
 
 namespace MassTransit.ServiceBus
 {
+	/// <summary>
+	/// IEndpoint is implemented by an endpoint. An endpoint is an addressable location on the network.
+	/// </summary>
     public interface IEndpoint :
         IDisposable
     {
+		/// <summary>
+		/// The address of the endpoint, in URI format
+		/// </summary>
 		Uri Uri { get; }
     }
 }
--- MassTransit.ServiceBus/IMessageQueueEndpoint.cs
+++ MassTransit.ServiceBus/IMessageQueueEndpoint.cs
@@ -1,10 +1,14 @@
 namespace MassTransit.ServiceBus
 {
+	/// <summary>
+	/// An extension of the IEndpoint interface for the additional support of Message Queue backed endpoints
+	/// </summary>
     public interface IMessageQueueEndpoint :
         IEndpoint
     {
         /// <summary>
-        /// Returns a MessageQueue name for use with MessageQueue.Open
+        /// The path of the message queue for the endpoint. Suitable for use with <c ref="MessageQueue" />.Open
+        /// to access a message queue.
         /// </summary>
         string QueueName { get; }
     }
