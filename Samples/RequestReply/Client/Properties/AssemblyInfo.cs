c9357ca4cd065675f1434d2c4217341559bab5e3
Dru Sellers <dru@drusellers.com>
2008-01-13 02:24:20 +0000

    added asminfo to some of the projects
    Former-commit-id: 99a72dc4d648ed38e84797b154bb1cc786caae8e

--- Samples/RequestReply/Client/Properties/AssemblyInfo.cs
+++ Samples/RequestReply/Client/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,26 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DDS")]
 [assembly: AssemblyProduct("Client")]
-[assembly: AssemblyCopyright("Copyright © DDS 2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9838ad50-9188-40f1-8e2e-254075928c8c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
