d6206e415f45f72cd2bfca7b0d03df771c0f1eb0
Dru Sellers <dru@drusellers.com>
2008-01-13 16:58:34 +0000

    added test to try and get msmq storage idempotent
    Former-commit-id: f349dc5f235ce0aa3975e64c07143d4a4375ea2e

--- MassTransit.ServiceBus.Tests/IntegrationTests/MsmqStorage_Idempotent_Tests.cs
+++ MassTransit.ServiceBus.Tests/IntegrationTests/MsmqStorage_Idempotent_Tests.cs
@@ -1,5 +1,7 @@
 namespace MassTransit.ServiceBus.Tests.IntegrationTests
 {
+    using System;
+    using System.Threading;
     using MassTransit.ServiceBus.Subscriptions;
     using NUnit.Framework;
     using NUnit.Framework.SyntaxHelpers;
@@ -14,8 +16,19 @@ namespace MassTransit.ServiceBus.Tests.IntegrationTests
             IMessageQueueEndpoint ep = new MessageQueueEndpoint("msmq://localhost/test_subscriptions");
             ISubscriptionStorage cache = new LocalSubscriptionCache();
             MsmqSubscriptionStorage store = new MsmqSubscriptionStorage(ep, cache);
-            
+            Thread.Sleep(10000);
             Assert.That(store.List().Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Idempontent_Add()
+        {
+            IMessageQueueEndpoint ep = new MessageQueueEndpoint("msmq://localhost/test_subscriptions");
+            ISubscriptionStorage cache = new LocalSubscriptionCache();
+            MsmqSubscriptionStorage store = new MsmqSubscriptionStorage(ep, cache);
+            store.Add(typeof(PingMessage).FullName, new Uri("msmq://localhost/test_servicebus"));
+            store.Add(typeof(PingMessage).FullName, new Uri("msmq://localhost/test_servicebus"));
+            Assert.That(store.List().Count, Is.EqualTo(1));
         }
     }
 }
