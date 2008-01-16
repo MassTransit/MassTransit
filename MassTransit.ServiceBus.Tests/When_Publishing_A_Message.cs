cb56ef363e65e422ad593a82ddb556b8c695b088
Dru Sellers <dru@drusellers.com>
2008-01-16 16:50:36 +0000

    cleaning up tests
    Former-commit-id: 91f071e8370a99d6eed19b394836df381b2c3559

--- MassTransit.ServiceBus.Tests/When_Publishing_A_Message.cs
+++ MassTransit.ServiceBus.Tests/When_Publishing_A_Message.cs
@@ -1,36 +1,79 @@
+using System;
+using System.Threading;
 using NUnit.Framework;
+using Rhino.Mocks;
 
 namespace MassTransit.ServiceBus.Tests
 {
 	[TestFixture]
-	public class When_Publishing_A_Message :
-		ServiceBusSetupFixture
+	public class When_Publishing_A_Message
 	{
-		//[Test]
-		//public void Poison_Letters_Should_Be_Moved_To_A_Poison_Queue()
-		//{
-		//    ManualResetEvent updateEvent = new ManualResetEvent(false);
+	    private IServiceBus _serviceBus;
+	    private IMessageQueueEndpoint _busEndpoint;
+	    private ISubscriptionStorage _subs;
+	    private MockRepository mocks;
+
+	    private string queueName = @".\private$\test";
+	    private Uri queueUri = new Uri("msmq://" + Environment.MachineName + "/test");
 
 
-		//    //this ends up in a seperate thread and I am therefore unable to figure out how to test
-		//    _serviceBus.Subscribe<PoisonMessage>(delegate(MessageContext<PoisonMessage> cxt)
-		//                                                               {
-		//                                                                   try
-		//                                                                   {
-		//                                                                       cxt.Message.ThrowException();
-		//                                                                   }
-		//                                                                   catch(Exception)
-		//                                                                   {
-		//                                                                       cxt.MarkPoison();
-		//                                                                   }
-		//                                                               }
 
-		//        );
 
-		//   _serviceBus.Publish(new PoisonMessage());
+	    [SetUp]
+	    public void SetUp()
+	    {
+            mocks = new MockRepository();
+	        _busEndpoint = mocks.CreateMock<IMessageQueueEndpoint>();
+	        _subs = mocks.CreateMock<ISubscriptionStorage>();
+	        ServiceBusSetupFixture.ValidateAndPurgeQueue(queueName);
+	    }
 
-		//    updateEvent.WaitOne(TimeSpan.FromSeconds(3), true);
-		//    VerifyMessageInQueue(_serviceBus.Endpoint.PoisonEndpoint.Address, new PoisonMessage());
-		//}
+	    [TearDown]
+	    public void TearDown()
+	    {
+	        mocks = null;
+	        _busEndpoint = null;
+	        _subs = null;
+	        _serviceBus = null;
+	    }
+
+        [Test]
+        [Ignore("working on it")]
+        public void Poison_Letters_Should_Be_Moved_To_A_Poison_Queue()
+        {
+            using(mocks.Record())
+            {
+                Expect.Call(_busEndpoint.QueueName).Return(queueName);
+                Expect.Call(_busEndpoint.Uri).Return(queueUri);
+                Expect.Call(_busEndpoint.QueueName).Return(queueName);
+                //Expect.Call(_busEndpoint.Uri).Return(queueUri);
+            }
+            using (mocks.Playback())
+            {
+                _serviceBus = new ServiceBus(_busEndpoint, _subs);
+
+                //ManualResetEvent updateEvent = new ManualResetEvent(false);
+                //MessageQueueEndpoint poisonEndpoint = new MessageQueueEndpoint(_serviceBus.PoisonEndpoint.Uri);
+
+                //this ends up in a seperate thread and I am therefore unable to figure out how to test
+                //_serviceBus.Subscribe<PoisonMessage>(delegate(MessageContext<PoisonMessage> cxt)
+                //                                         {
+                //                                             try
+                //                                             {
+                //                                                 cxt.Message.ThrowException();
+                //                                             }
+                //                                             catch (Exception)
+                //                                             {
+                //                                                 cxt.MarkPoison();
+                //                                             }
+                //                                         }
+                //    );
+
+                //_serviceBus.Publish(new PoisonMessage());
+
+                //updateEvent.WaitOne(TimeSpan.FromSeconds(3), true);
+                //ServiceBusSetupFixture.VerifyMessageInQueue(poisonEndpoint, new PoisonMessage());
+            }
+        }
 	}
 }
