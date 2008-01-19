89103c626733d3ba735af619d9e742a9d12d6156
Dru Sellers <dru@drusellers.com>
2008-01-19 12:28:36 +0000

    fixed some tests. the new factory is making testing a lot easier.!
    Former-commit-id: c18bed3fbe823ed91d853f1bd96cf58827f19f76

--- MassTransit.ServiceBus.Tests/MessageContext_Should_Help.cs
+++ MassTransit.ServiceBus.Tests/MessageContext_Should_Help.cs
@@ -1,10 +1,8 @@
+using NUnit.Framework;
+using Rhino.Mocks;
+
 namespace MassTransit.ServiceBus.Tests
 {
-    using System;
-    using NUnit.Framework;
-    using Rhino.Mocks;
-    using Util;
-
     [TestFixture]
     public class MessageContext_Should_Help
     {
@@ -14,10 +12,11 @@ namespace MassTransit.ServiceBus.Tests
         private IEnvelope mockEnvelope;
         private IEndpoint mockPoisonEndpoint;
         private IMessageQueueEndpoint mockEndpoint;
-        private IMessageSenderFactory factory;
+        private IMessageSenderFactory mockFactory;
 
         private PingMessage requestMessage = new PingMessage();
         private PongMessage replyMessage = new PongMessage();
+        private IMessageSender mockMessageSender;
 
         #region SetUp / TearDown
         [SetUp]
@@ -29,6 +28,8 @@ namespace MassTransit.ServiceBus.Tests
             mockEnvelope = mocks.CreateMock<IEnvelope>();
             mockEndpoint = mocks.CreateMock<IMessageQueueEndpoint>();
             mockPoisonEndpoint = mocks.CreateMock<IEndpoint>();
+            mockFactory = mocks.CreateMock<IMessageSenderFactory>();
+            mockMessageSender = mocks.CreateMock<IMessageSender>();
         }
 
         [TearDown]
@@ -40,22 +41,24 @@ namespace MassTransit.ServiceBus.Tests
             mockEnvelope = null;
             mockEndpoint = null;
             mockPoisonEndpoint = null;
+            mockFactory = null;
+            mockMessageSender = null;
         }
         #endregion
 
         [Test]
-        [Ignore("MessageSender.Using is killing the mock test approach")]
         public void With_Replies()
         {
-            //TODO: this test is now hitting the machine
-            MessageContext<PingMessage> cxt = new MessageContext<PingMessage>(mockBus, mockEnvelope, requestMessage, factory);
+            MessageContext<PingMessage> cxt = new MessageContext<PingMessage>(mockBus, mockEnvelope, requestMessage, mockFactory);
 
             using (mocks.Record())
             {
                 Expect.Call(mockEnvelope.ReturnEndpoint).Return(mockEndpoint);
                 Expect.Call(mockBus.Endpoint).Return(mockBusEndpoint);
                 Expect.Call(mockEnvelope.Id).Return("");
-                Expect.Call(mockEndpoint.QueueName).Return(@".\private$\test_client");
+                Expect.Call(mockFactory.Using(mockEndpoint)).Return(mockMessageSender);
+                Expect.Call(delegate { mockMessageSender.Send(null); }).IgnoreArguments(); //ignoring arguments because we create a new envelope in the method
+
             }
 
             using (mocks.Playback())
@@ -68,25 +71,13 @@ namespace MassTransit.ServiceBus.Tests
         public void With_Handling_Later()
         {
 
-            MessageContext<PingMessage> cxt = new MessageContext<PingMessage>(mockBus, mockEnvelope, requestMessage, factory);
-            IMessage[] messages = new IMessage[1] { replyMessage };
+            MessageContext<PingMessage> cxt = new MessageContext<PingMessage>(mockBus, mockEnvelope, requestMessage, mockFactory);
 
             using (mocks.Record())
             {
-                //TODO: Can we better hide the mapper?
                 Expect.Call(mockBus.Endpoint).Return(mockEndpoint);
-                Expect.Call(mockEndpoint.QueueName).Return(@".\private$\test_client");
-                Expect.Call(mockEnvelope.Messages).Return(null);
-                Expect.Call(mockEnvelope.ReturnEndpoint).Return(mockEndpoint);
-                Expect.Call(mockEndpoint.QueueName).Return(@".\private$\test_client");
-                Expect.Call(mockEnvelope.TimeToBeReceived).Return(new TimeSpan(1, 0, 0));
-                Expect.Call(mockEnvelope.TimeToBeReceived).Return(new TimeSpan(1, 0, 0));
-                Expect.Call(mockEnvelope.Label).Return("bob");
-                Expect.Call(mockEnvelope.Label).Return("bob");
-                Expect.Call(mockEnvelope.Recoverable).Return(true);
-                Expect.Call(mockEnvelope.CorrelationId).Return(MessageId.Empty);
-                mockEnvelope.Id = "";
-                LastCall.IgnoreArguments();
+                Expect.Call(mockFactory.Using(mockEndpoint)).Return(mockMessageSender);
+                Expect.Call(delegate { mockMessageSender.Send(null); }).IgnoreArguments(); //ignoring arguments because we create a new envelope in the method
             }
 
             using (mocks.Playback())
@@ -96,18 +87,15 @@ namespace MassTransit.ServiceBus.Tests
         }
 
         [Test]
-        [Ignore("MessageSender.Using is killing the mock test approach")]
         public void With_Poison_Letters()
         {
-            MessageContext<PingMessage> cxt = new MessageContext<PingMessage>(mockBus, mockEnvelope, requestMessage, factory);
+            MessageContext<PingMessage> cxt = new MessageContext<PingMessage>(mockBus, mockEnvelope, requestMessage, mockFactory);
 
             using (mocks.Record())
             {
-                Expect.Call(mockEnvelope.Id).PropertyBehavior(); //stupid log4net
-                Expect.Call(mockEnvelope.Messages).PropertyBehavior(); //stupid log4net
-                Expect.Call(mockBus.Endpoint).Return(mockEndpoint);
                 Expect.Call(mockBus.PoisonEndpoint).Return(mockPoisonEndpoint);
-                Expect.Call(mockPoisonEndpoint.Uri).Return(new Uri("msmq://localhost/test_servicebus_poison"));
+                Expect.Call(mockFactory.Using(mockPoisonEndpoint)).Return(mockMessageSender);
+                Expect.Call(delegate { mockMessageSender.Send(null); }).IgnoreArguments(); //ignoring arguments because we create a new envelope in the method
             }
 
             using (mocks.Playback())
@@ -117,20 +105,17 @@ namespace MassTransit.ServiceBus.Tests
         }
 
         [Test]
-        [Ignore("MessageSender.Using is killing the mock test approach")]
         public void With_Poison_Letter()
         {
-            MessageContext<PingMessage> cxt = new MessageContext<PingMessage>(mockBus, mockEnvelope, requestMessage, factory);
+            MessageContext<PingMessage> cxt = new MessageContext<PingMessage>(mockBus, mockEnvelope, requestMessage, mockFactory);
 
             using (mocks.Record())
             {
-                Expect.Call(mockEnvelope.Id).PropertyBehavior(); //stupid log4net
-                Expect.Call(mockEnvelope.Messages).PropertyBehavior(); //stupid log4net
                 Expect.Call(mockEnvelope.Clone()).Return(mockEnvelope);
                 mockEnvelope.Messages = new IMessage[] { requestMessage };
-                Expect.Call(mockBus.Endpoint).Return(mockEndpoint);
                 Expect.Call(mockBus.PoisonEndpoint).Return(mockPoisonEndpoint);
-                Expect.Call(mockPoisonEndpoint.Uri).Return(new Uri("msmq://localhost/test_servicebus_poison"));
+                Expect.Call(mockFactory.Using(mockPoisonEndpoint)).Return(mockMessageSender);
+                Expect.Call(delegate { mockMessageSender.Send(null); }).IgnoreArguments(); //ignoring arguments because we create a new envelope in the method
             }
 
             using (mocks.Playback())
