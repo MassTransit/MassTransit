1edec82db70d422b0c4586f7c372a9fb2ef5fb40
Chris Patterson <chris@phatboyg.com>
2008-01-25 18:14:29 +0000

    Redesigned the message reception code to be more resilient over slow network links

--- MassTransit.ServiceBus/MessageQueueReceiver.cs
+++ MassTransit.ServiceBus/MessageQueueReceiver.cs
@@ -19,186 +19,165 @@ using MassTransit.ServiceBus.Exceptions;
 
 namespace MassTransit.ServiceBus
 {
-    /// <summary>
-    /// Receives envelopes from a message queue
-    /// </summary>
-    public class MessageQueueReceiver :
-        IMessageReceiver
-    {
-        private static readonly object _locker = new object();
-        private static readonly ILog _log = LogManager.GetLogger(typeof (MessageQueueReceiver));
-        private readonly IMessageQueueEndpoint _endpoint;
+	/// <summary>
+	/// Receives envelopes from a message queue
+	/// </summary>
+	public class MessageQueueReceiver :
+		IMessageReceiver
+	{
+		private static readonly object _locker = new object();
+		private static readonly ILog _log = LogManager.GetLogger(typeof (MessageQueueReceiver));
+		private readonly IMessageQueueEndpoint _endpoint;
 
-        private IEnvelopeConsumer _consumer;
+		private IEnvelopeConsumer _consumer;
 
-        private Cursor _cursor;
+		private MessageQueue _queue;
 
-        private MessageQueue _queue;
+		/// <summary>
+		/// Initializes a MessageQueueReceiver
+		/// </summary>
+		/// <param name="endpoint">The endpoint where the receiver should be attached</param>
+		public MessageQueueReceiver(IMessageQueueEndpoint endpoint)
+		{
+			_endpoint = endpoint;
+		}
 
-        /// <summary>
-        /// Initializes a MessageQueueReceiver
-        /// </summary>
-        /// <param name="endpoint">The endpoint where the receiver should be attached</param>
-        public MessageQueueReceiver(IMessageQueueEndpoint endpoint)
-        {
-            _endpoint = endpoint;
-        }
+		#region IMessageReceiver Members
 
-        #region IMessageReceiver Members
+		///<summary>
+		///Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		///</summary>
+		///<filterpriority>2</filterpriority>
+		public void Dispose()
+		{
+			Stop();
+		}
 
-        ///<summary>
-        ///Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        ///</summary>
-        ///<filterpriority>2</filterpriority>
-        public void Dispose()
-        {
-            Stop();
-        }
+		/// <summary>
+		/// Adds a consumer to the message receiver
+		/// </summary>
+		/// <param name="consumer">The consumer to add</param>
+		public void Subscribe(IEnvelopeConsumer consumer)
+		{
+			lock (_locker)
+			{
+				if (_consumer == null)
+				{
+					_consumer = consumer;
 
-        /// <summary>
-        /// Adds a consumer to the message receiver
-        /// </summary>
-        /// <param name="consumer">The consumer to add</param>
-        public void Subscribe(IEnvelopeConsumer consumer)
-        {
-            lock (_locker)
-            {
-                if (_consumer == null)
-                {
-                    _consumer = consumer;
+					Restart();
+				}
+				else if (_consumer != consumer)
+				{
+					throw new EndpointException(_endpoint, "Only one consumer can be registered for a message receiver");
+				}
+			}
+		}
 
-                    Restart();
-                }
-                else if (_consumer != consumer)
-                {
-                    throw new EndpointException(_endpoint, "Only one consumer can be registered for a message receiver");
-                }
-            }
-        }
+		#endregion
 
-        #endregion
+		private void Restart()
+		{
+			lock (this)
+			{
+				Stop();
 
-        private void Restart()
-        {
-            Stop();
+				Start();
+			}
+		}
 
-            Start();
-        }
+		private void Start()
+		{
+			_queue = _endpoint.Open(QueueAccessMode.SendAndReceive);
 
-        private void Start()
-        {
-            _queue = _endpoint.Open(QueueAccessMode.SendAndReceive);
+			ThreadPool.QueueUserWorkItem(MonitorQueue, null);
+		}
 
-            try
-            {
-                _cursor = _queue.CreateCursor();
-            }
-            catch (MessageQueueException ex)
-            {
-                if (_log.IsErrorEnabled)
-                    _log.Error("There is an issue with the queue " + _endpoint.Uri, ex);
+		private void Stop()
+		{
+			if (_queue != null)
+			{
+				_queue.Close();
+				_queue.Dispose();
+				_queue = null;
+			}
+		}
 
-                throw new EndpointException(_endpoint, string.Format("There are issues with the queue '{0}'", _endpoint.Uri), ex);
-            }
+		/// <summary>
+		/// Called by the thread pool to process a message that has been seen on the queue (via Peek)
+		/// 
+		/// The message is checked to see if it will handled by a consumer and if so, the message
+		/// is received from the queue and send to the consumer(s) that will handle it.
+		/// </summary>
+		/// <param name="obj">An instance of <c ref="Message" /> that was seen on the queue</param>
+		public void ProcessMessage(object obj)
+		{
+			IEnvelope e = obj as IEnvelope;
+			if (e == null)
+				return;
+			try
+			{
+				_consumer.Deliver(e);
+			}
+			catch (Exception ex)
+			{
+				if (_log.IsErrorEnabled)
+					_log.Error(string.Format("An exception occured delivering envelope {0}", e.Id), ex);
+			}
+		}
 
-            _queue.BeginPeek(TimeSpan.FromHours(24), _cursor, PeekAction.Current, this, Queue_PeekCompleted);
-        }
+		private void MonitorQueue(object obj)
+		{
+			MessageQueue queue = _queue;
+			if (queue == null)
+				return;
 
-        private void Stop()
-        {
-            if (_cursor != null)
-            {
-                _cursor.Dispose();
-                _cursor = null;
-            }
+			try
+			{
+				Cursor cursor = _queue.CreateCursor();
 
-            if (_queue != null)
-            {
-                _queue.Close();
-                _queue.Dispose();
-                _queue = null;
-            }
-        }
+				Message msg = queue.Peek(TimeSpan.FromSeconds(1), cursor, PeekAction.Current);
 
-        /// <summary>
-        /// Called by the thread pool to process a message that has been seen on the queue (via Peek)
-        /// 
-        /// The message is checked to see if it will handled by a consumer and if so, the message
-        /// is received from the queue and send to the consumer(s) that will handle it.
-        /// </summary>
-        /// <param name="obj">An instance of <c ref="Message" /> that was seen on the queue</param>
-        public void ProcessMessage(object obj)
-        {
-            Message msg = obj as Message;
-            if (msg == null)
-                return;
+				while (msg != null)
+				{
+					if (_log.IsDebugEnabled)
+						_log.DebugFormat("Queue: {0} Received Message Id {1}", _queue.Path, msg.Id);
 
-            if (_log.IsDebugEnabled)
-                _log.DebugFormat("Queue: {0} Received Message Id {1}", _queue.Path, msg.Id);
+					if (_consumer != null)
+					{
+						IEnvelope e = EnvelopeMessageMapper.MapFrom(msg);
 
-            if (_consumer != null)
-            {
-                IEnvelope e = EnvelopeMessageMapper.MapFrom(msg);
+						if (_consumer.IsHandled(e))
+						{
+							Message received = queue.Receive(TimeSpan.FromSeconds(3), cursor);
 
-                try
-                {
-                    if (_consumer.IsHandled(e))
-                    {
-                        //we have found someone that cares take it off the queue
-                        _queue.ReceiveById(msg.Id);
+							if (received.Id == msg.Id)
+							{
+								ThreadPool.QueueUserWorkItem(ProcessMessage, e);
+								break;
+							}
+						}
+					}
 
-                        if (_log.IsDebugEnabled)
-                            _log.DebugFormat("Delivering Envelope {0} by {1}", e.Id, GetHashCode());
+					msg = queue.Peek(TimeSpan.FromSeconds(1), cursor, PeekAction.Next);
+				}
+			}
+			catch (MessageQueueException ex)
+			{
+				if (ex.MessageQueueErrorCode == MessageQueueErrorCode.IOTimeout)
+				{
+				}
+				else
+				{
+					_log.Error("An error occured while communicating with the queue", ex);
+				}
+			}
+			catch(Exception ex)
+			{
+				_log.Error("An unknown exception occured", ex);
+			}
 
-                        _consumer.Deliver(e);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    if (_log.IsErrorEnabled)
-                        _log.Error("Envelope Exception", ex);
-
-                    throw;
-                }
-            }
-        }
-
-        private void Queue_PeekCompleted(IAsyncResult asyncResult)
-        {
-            try
-            {
-                if (_queue == null)
-                    return;
-
-                Message msg = _queue.EndPeek(asyncResult);
-
-                if (msg != null)
-                {
-                    ThreadPool.QueueUserWorkItem(ProcessMessage, msg);
-                }
-            }
-            catch (MessageQueueException ex)
-            {
-                if ((uint) ex.MessageQueueErrorCode == 0xC0000120 ||
-                    ex.MessageQueueErrorCode == MessageQueueErrorCode.IllegalCursorAction)
-                {
-                    if (_log.IsInfoEnabled)
-                        _log.InfoFormat("The queue '{0}' was closed during an asynchronous operation", _queue.QueueName);
-
-                    return;
-                }
-
-                if (ex.MessageQueueErrorCode != MessageQueueErrorCode.IOTimeout)
-                {
-                    if (_log.IsErrorEnabled)
-                        _log.ErrorFormat("Queue_PeekCompleted Exception ({0}) on '{1}: {2} ", ex.Message,
-                            _queue.QueueName, ex.MessageQueueErrorCode);
-
-                    return;
-                }
-            }
-
-            _queue.BeginPeek(TimeSpan.FromHours(24), _cursor, PeekAction.Next, this, Queue_PeekCompleted);
-        }
-    }
+			ThreadPool.QueueUserWorkItem(MonitorQueue, obj);
+		}
+	}
 }
