74cbb5c90464dc9168f90fe228f41c326a3ad42d
Chris Patterson <chris@phatboyg.com>
2008-02-05 05:22:29 +0000

    Changed sample to use new SubscriptionService and SubscriptionClient for remote subscription management
    Former-commit-id: 5e00f57d442452114fe512d3208dd27e6bc60306

--- Samples/RequestReply/Client/Program.cs
+++ Samples/RequestReply/Client/Program.cs
@@ -1,26 +1,25 @@
-using System;
-using MassTransit.ServiceBus;
-using MassTransit.ServiceBus.Subscriptions;
-using SecurityMessages;
-
 namespace Client
 {
-    using MassTransit.ServiceBus.SubscriptionsManager.Client;
+    using System;
+    using MassTransit.ServiceBus;
+    using MassTransit.ServiceBus.Subscriptions;
+    using SecurityMessages;
 
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             log4net.Config.XmlConfigurator.Configure();
 
-            IEndpoint clientEndpoint = new MessageQueueEndpoint("msmq://localhost/test_client");
-            IEndpoint wellKnown = new MessageQueueEndpoint("msmq://localhost/test_subscriptions");
+            IMessageQueueEndpoint clientEndpoint = new MessageQueueEndpoint("msmq://localhost/test_client");
+            IMessageQueueEndpoint subscriptionManagerEndpoint = new MessageQueueEndpoint("msmq://localhost/test_subscriptions");
 
             ISubscriptionStorage storage = new LocalSubscriptionCache();
 
             ServiceBus bus = new ServiceBus(clientEndpoint, storage);
-            ClientProxy proxy = new ClientProxy(wellKnown);
-            proxy.StartWatching(bus, bus.SubscriptionStorage);
+            SubscriptionManagerClient subscriptionClient = new SubscriptionManagerClient(bus, storage, subscriptionManagerEndpoint);
+
+            subscriptionClient.Start();
 
             bus.Subscribe<PasswordUpdateComplete>(Program_MessageReceived);
 
@@ -28,18 +27,19 @@ namespace Client
             Console.WriteLine("What would you like to set your new password to?");
             Console.Write("New Password:");
             string newPassword = Console.ReadLine();
-           
+
             bus.Publish(new RequestPasswordUpdate(newPassword));
 
             Console.WriteLine("Waiting For Reply");
-            Console.ReadKey(true);
+            Console.ReadKey();
+
+            bus.Dispose();
         }
 
-        static void Program_MessageReceived(IMessageContext<PasswordUpdateComplete> cxt)
+        private static void Program_MessageReceived(IMessageContext<PasswordUpdateComplete> cxt)
         {
             Console.WriteLine("Password Set");
             Console.WriteLine("Thank You. Press any key to exit");
-            Console.ReadKey(true);
         }
     }
 }
