b0571fbacfb14b562ab8871e6eee93d9ab6a6335
Chris Patterson <chris@phatboyg.com>
2008-02-06 17:17:30 +0000

    The ability to install and uninstall the service is now present. There are some tweaks that need to be done to make it work properly but for now it seems to be OK.
    Former-commit-id: 2ae03dca360e09ac0831b8f58ea9ded98a1e3b63

--- MassTransit.Host/Controller.cs
+++ MassTransit.Host/Controller.cs
@@ -1,75 +1,162 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Reflection;
+using MassTransit.ServiceBus;
+using nu.Utility;
+
 namespace MassTransit.Host
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Reflection;
-    using nu.Utility;
-    using ServiceBus;
+	public class Controller
+	{
+		private readonly IArgumentMapFactory _argumentMapFactory = new ArgumentMapFactory();
+		private readonly IArgumentParser _argumentParser = new ArgumentParser();
 
-    public class Controller
-    {
-        private readonly IArgumentParser _argumentParser = new ArgumentParser();
-        private IArgumentMapFactory _argumentMapFactory = new ArgumentMapFactory();
+		private string _assemblyName;
+		private string _endpointUri;
+		private bool _installService;
+		private string _subscriptionEndpoint;
+		private bool _uninstallService;
 
-        private string _assemblyName;
-        private string _endpointUri;
-        private string _subscriptionEndpoint;
+		[DefaultArgument(Required = true, AllowMultiple = true, Description = "The assemblies to load into the host process")]
+		public string AssemblyName
+		{
+			get { return _assemblyName; }
+			set { _assemblyName = value; }
+		}
 
-        [DefaultArgument(Required = true, AllowMultiple = true, Description = "The assemblies to load into the host process")]
-        public string AssemblyName
-        {
-            get { return _assemblyName; }
-            set { _assemblyName = value; }
-        }
+		[Argument(Key = "install", Description = "Install the host service")]
+		public bool InstallService
+		{
+			get { return _installService; }
+			set { _installService = value; }
+		}
 
-        [Argument(Key = "endpoint", Required = false, Description = "The endpoint to use for the service bus")]
-        public string EndpointUri
-        {
-            get { return _endpointUri; }
-            set { _endpointUri = value; }
-        }
+		[Argument(Key = "uninstall", Description = "Uninstall the host service")]
+		public bool UninstallService
+		{
+			get { return _uninstallService; }
+			set { _uninstallService = value; }
+		}
 
-        [Argument(Key = "subscription", Required = false, Description = "The remote subscription manager to use")]
-        public string SubscriptionEndpoint
-        {
-            get { return _subscriptionEndpoint; }
-            set { _subscriptionEndpoint = value; }
-        }
 
-        public void Start(string[] args)
-        {
-            IEnumerable<IArgument> arguments = _argumentParser.Parse(args);
+		[Argument(Key = "endpoint", Required = false, Description = "The endpoint to use for the service bus")]
+		public string EndpointUri
+		{
+			get { return _endpointUri; }
+			set { _endpointUri = value; }
+		}
 
-            IArgumentMap mapper = _argumentMapFactory.CreateMap(this);
+		[Argument(Key = "subscription", Required = false, Description = "The remote subscription manager to use")]
+		public string SubscriptionEndpoint
+		{
+			get { return _subscriptionEndpoint; }
+			set { _subscriptionEndpoint = value; }
+		}
 
-            mapper.ApplyTo(this, arguments);
+		public void Start(string[] args)
+		{
+			IEnumerable<IArgument> arguments = _argumentParser.Parse(args);
 
-            if (string.IsNullOrEmpty(_assemblyName))
-            {
-                Console.WriteLine("Usage: {0}", mapper.Usage);
-                return;
-            }
+			IArgumentMap mapper = _argumentMapFactory.CreateMap(this);
 
-            LoadAssembly();
-        }
+			mapper.ApplyTo(this, arguments);
 
-        private void LoadAssembly()
-        {
-            try
-            {
-                Assembly assembly = Assembly.Load(_assemblyName);
+			if (_installService)
+			{
+				RegisterService(arguments);
+				return;
+			}
 
-                Type[] types = assembly.GetTypes();
-                foreach (Type t in types)
-                {
-                    if (t.IsAssignableFrom(typeof (IAutoSubscriber)) && !t.IsAbstract)
-                        Console.WriteLine("Type: {0}", t.Name);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Exception Occurred: {0}", ex);
-            }
-        }
-    }
+			if (_uninstallService)
+			{
+				UnregisterService(arguments);
+				return;
+			}
+
+			if (string.IsNullOrEmpty(_assemblyName))
+			{
+				Console.WriteLine("Usage: {0}", mapper.Usage);
+				return;
+			}
+
+			LoadAssembly();
+		}
+
+		private void UnregisterService(IEnumerable<IArgument> arguments)
+		{
+			HostServiceInstaller installer = new HostServiceInstaller("MassTransitHost", "MassTransit Message Host", "Mass Transit Host");
+
+			IArgumentMap installerMap = _argumentMapFactory.CreateMap(installer);
+			IEnumerable<IArgument> remaining = installerMap.ApplyTo(installerMap, arguments);
+
+			if (!installer.IsInstalled())
+			{
+				Console.WriteLine("Service is not installed");
+				return;
+			}
+
+			using (TransactedInstaller ti = new TransactedInstaller())
+			{
+				ti.Installers.Add(installer);
+
+				string path = string.Format("/assemblypath={0}", Assembly.GetExecutingAssembly().Location);
+				string[] commandLine = { path };
+
+				InstallContext context = new InstallContext(null, commandLine);
+				ti.Context = context;
+
+				ti.Uninstall(null);
+			}
+		}
+
+		private void RegisterService(IEnumerable<IArgument> arguments)
+		{
+			HostServiceInstaller installer = new HostServiceInstaller("MassTransitHost", "MassTransit Message Host", "Mass Transit Host");
+
+			IArgumentMap installerMap = _argumentMapFactory.CreateMap(installer);
+			IEnumerable<IArgument> remaining = installerMap.ApplyTo(installerMap, arguments);
+
+			if (installer.IsInstalled())
+			{
+				Console.WriteLine("Service is already installed");
+				return;
+			}
+
+			using (TransactedInstaller ti = new TransactedInstaller())
+			{
+				ti.Installers.Add(installer);
+
+				string path = string.Format("/assemblypath={0}", Assembly.GetExecutingAssembly().Location);
+				string[] commandLine = {path};
+
+				InstallContext context = new InstallContext(null, commandLine);
+				ti.Context = context;
+
+				Hashtable savedState = new Hashtable();
+
+				ti.Install(savedState);
+			}
+		}
+
+		private void LoadAssembly()
+		{
+			try
+			{
+				Assembly assembly = Assembly.Load(_assemblyName);
+
+				Type[] types = assembly.GetTypes();
+				foreach (Type t in types)
+				{
+					if (t.IsAssignableFrom(typeof (IAutoSubscriber)) && !t.IsAbstract)
+						Console.WriteLine("Type: {0}", t.Name);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Exception Occurred: {0}", ex);
+			}
+		}
+	}
 }
---
+++ MassTransit.Host/HostServiceInstaller.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Configuration.Install;
+using System.ServiceProcess;
+using log4net;
+using Microsoft.Win32;
+using nu.Utility;
+
+namespace MassTransit.Host
+{
+	public class HostServiceInstaller :
+		Installer
+	{
+		private static readonly ILog _log = LogManager.GetLogger(typeof (HostServiceInstaller));
+		private readonly ServiceInstaller _serviceInstaller = new ServiceInstaller();
+		private readonly ServiceProcessInstaller _serviceProcessInstaller = new ServiceProcessInstaller();
+
+		public HostServiceInstaller(string serviceName, string description, string displayName)
+		{
+			_serviceInstaller.ServiceName = serviceName;
+			_serviceInstaller.Description = description;
+			_serviceInstaller.DisplayName = displayName;
+
+			Initialize();
+		}
+
+		[Argument(Key = "name", Description = "The name for the service", Required = false)]
+		public string Name
+		{
+			set { _serviceInstaller.ServiceName = value; }
+		}
+
+		[Argument(Key = "description", Description = "The description for the service", Required = false)]
+		public string Description
+		{
+			set { _serviceInstaller.Description = value; }
+		}
+
+		[Argument(Key = "displayname", Description = "The name to display for the service", Required = false)]
+		public string DisplayName
+
+		{
+			set { _serviceInstaller.DisplayName = value; }
+		}
+
+		[Argument(Key = "username", Description = "Username for service account", Required = false)]
+		public string Username
+		{
+			set
+			{
+				_serviceProcessInstaller.Username = value;
+				_serviceProcessInstaller.Account = ServiceAccount.User;
+			}
+		}
+
+		[Argument(Key = "password", Description = "Password for service account", Required = false)]
+		public string Password
+		{
+			set { _serviceProcessInstaller.Password = value; }
+		}
+
+		private void Initialize()
+		{
+			_serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+			_serviceProcessInstaller.Username = null;
+			_serviceProcessInstaller.Password = null;
+
+			_serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+			Installers.AddRange(new Installer[] {_serviceProcessInstaller, _serviceInstaller});
+		}
+
+		public bool IsInstalled()
+		{
+			foreach (ServiceController service in ServiceController.GetServices())
+			{
+				if (service.ServiceName == _serviceInstaller.ServiceName)
+					return true;
+			}
+
+			return false;
+		}
+
+		public override void Install(IDictionary stateSaver)
+		{
+			_log.InfoFormat("Installing Service {0}", _serviceInstaller.ServiceName);
+
+			base.Install(stateSaver);
+
+			_log.InfoFormat("Opening Registry");
+
+			using (RegistryKey system = Registry.LocalMachine.OpenSubKey("System"))
+			using (RegistryKey currentControlSet = system.OpenSubKey("CurrentControlSet"))
+			using (RegistryKey services = currentControlSet.OpenSubKey("Services"))
+			using (RegistryKey service = services.OpenSubKey(_serviceInstaller.ServiceName, true))
+			{
+				service.SetValue("Description", _serviceInstaller.Description);
+
+				string imagePath = (string) service.GetValue("ImagePath");
+
+				_log.InfoFormat("Service Path {0}", imagePath);
+
+				// TODO tweak the command line to include the service argument and any other useful stuff
+
+				imagePath += " -service";
+				service.SetValue("ImagePath", imagePath);
+
+				service.Close();
+				services.Close();
+				currentControlSet.Close();
+				system.Close();
+			}
+		}
+	}
+}
