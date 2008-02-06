11f1f11bb6b220b41caaa620487c749cc0bfd0ae
Chris Patterson <chris@phatboyg.com>
2008-02-06 03:01:18 +0000

    Some initial loading code, along with type discovery
    Former-commit-id: 7f909a92b5edaf347450843e430e739bf6559037

--- MassTransit.Host/Controller.cs
+++ MassTransit.Host/Controller.cs
@@ -2,7 +2,9 @@ namespace MassTransit.Host
 {
     using System;
     using System.Collections.Generic;
+    using System.Reflection;
     using nu.Utility;
+    using ServiceBus;
 
     public class Controller
     {
@@ -10,6 +12,9 @@ namespace MassTransit.Host
         private IArgumentMapFactory _argumentMapFactory = new ArgumentMapFactory();
 
         private string _assemblyName;
+        private string _endpointUri;
+        private string _subscriptionEndpoint;
+
         [DefaultArgument(Required = true, AllowMultiple = true, Description = "The assemblies to load into the host process")]
         public string AssemblyName
         {
@@ -17,6 +22,19 @@ namespace MassTransit.Host
             set { _assemblyName = value; }
         }
 
+        [Argument(Key = "endpoint", Required = false, Description = "The endpoint to use for the service bus")]
+        public string EndpointUri
+        {
+            get { return _endpointUri; }
+            set { _endpointUri = value; }
+        }
+
+        [Argument(Key = "subscription", Required = false, Description = "The remote subscription manager to use")]
+        public string SubscriptionEndpoint
+        {
+            get { return _subscriptionEndpoint; }
+            set { _subscriptionEndpoint = value; }
+        }
 
         public void Start(string[] args)
         {
@@ -26,13 +44,32 @@ namespace MassTransit.Host
 
             mapper.ApplyTo(this, arguments);
 
-            if(string.IsNullOrEmpty(_assemblyName))
+            if (string.IsNullOrEmpty(_assemblyName))
             {
                 Console.WriteLine("Usage: {0}", mapper.Usage);
                 return;
             }
 
+            LoadAssembly();
+        }
 
+        private void LoadAssembly()
+        {
+            try
+            {
+                Assembly assembly = Assembly.Load(_assemblyName);
+
+                Type[] types = assembly.GetTypes();
+                foreach (Type t in types)
+                {
+                    if (t.IsAssignableFrom(typeof (IAutoSubscriber)) && !t.IsAbstract)
+                        Console.WriteLine("Type: {0}", t.Name);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Occurred: {0}", ex);
+            }
         }
     }
 }
