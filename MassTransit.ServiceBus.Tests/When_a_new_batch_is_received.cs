85a42baef0aef812c2a6e3c3b1c1725cd6cea2e5
Chris Patterson <chris@phatboyg.com>
2008-02-10 17:04:17 +0000

    More flow on the batch handler
    Former-commit-id: d0f8d1cab4da13c2feba9bf2d81898adadce41f2

--- MassTransit.ServiceBus.Tests/When_a_new_batch_is_received.cs
+++ MassTransit.ServiceBus.Tests/When_a_new_batch_is_received.cs
@@ -34,106 +34,129 @@ namespace MassTransit.ServiceBus.Tests
             }
         }
 
-        public delegate void BatchControllerHandler<T, K>(BatchTransferController<T, K> controller) where T : BatchTransferMessage<K>;
-
-        [Serializable]
-        public abstract class BatchTransferMessage<K> :
-            IMessage
-        {
-            private K _batchId;
-            private long _batchLength;
-
-            /// <summary>
-            /// Identifies the batch containing this message
-            /// </summary>
-            public K BatchId
-            {
-                get { return _batchId; }
-                set { _batchId = value; }
-            }
-
-            /// <summary>
-            /// The number of messages in the batch
-            /// </summary>
-            public long BatchLength
-            {
-                get { return _batchLength; }
-                set { _batchLength = value; }
-            }
-        }
-
-        [Serializable]
-        public class BatchTransferController<T, K> :
-            IEnumerable<T>,
-            IMessage where T : BatchTransferMessage<K>
-        {
-            private readonly ManualResetEvent _complete = new ManualResetEvent(false);
-            private readonly BatchControllerHandler<T, K> _handler;
-            private readonly Semaphore _messageReady = new Semaphore(0, 0);
-            private readonly Queue<T> _messages = new Queue<T>();
-
-            private K _batchId;
-
-            public BatchTransferController(BatchControllerHandler<T, K> handler)
-            {
-                _handler = handler;
-            }
-
-            public K BatchId
-            {
-                get { return _batchId; }
-                set { _batchId = value; }
-            }
-
-            #region IEnumerable<T> Members
-
-            IEnumerator<T> IEnumerable<T>.GetEnumerator()
-            {
-                WaitHandle[] handles = new WaitHandle[] { _messageReady, _complete };
-                int waitResult;
-                while ((waitResult = WaitHandle.WaitAny(handles, TimeSpan.FromMinutes(10), true)) != 1)
-                {
-                    T message = _messages.Dequeue();
-
-                    yield return message;
-                }
-
-                if (waitResult == WaitHandle.WaitTimeout)
-                    throw new ApplicationException("Timeout waiting for batch to complete");
-            }
-
-            public IEnumerator GetEnumerator()
-            {
-                return ((IEnumerable<T>)this).GetEnumerator();
-            }
-
-            #endregion
-
-            public void HandleMessage(IMessageContext<T> context)
-            {
-                if (context.Message.BatchId.Equals(_batchId))
-                {
-                    // this is for our batch
-                }
-                else
-                {
-                    // this is a new batch, we need to create a new controller and subscribe with the appropriate predicate to handle it
-                }
-            }
-        }
-
-        [Serializable]
-        public class BatchMessage :
-            BatchTransferMessage<Guid>
-        {
-        }
-
         [Test]
         public void Notify_the_subscriber_with_a_batch_message()
         {
             _controller = new BatchTransferController<BatchMessage, Guid>(HandleBatch);
 
             _bus.Subscribe<BatchMessage>(_controller.HandleMessage);
+        }
+    }
+
+    public delegate void BatchControllerHandler<T, K>(BatchTransferController<T, K> controller) where T : BatchTransferMessage<K>;
+
+    [Serializable]
+    public class BatchMessage :
+        BatchTransferMessage<Guid>
+    {
+    }
+
+    public class BatchTransferController<T, K> :
+        IEnumerable<T>,
+        IMessage where T : BatchTransferMessage<K>
+    {
+        private readonly ManualResetEvent _complete = new ManualResetEvent(false);
+        private readonly BatchControllerHandler<T, K> _handler;
+        private readonly Semaphore _messageReady = new Semaphore(0, 0);
+        private readonly Queue<T> _messages = new Queue<T>();
+
+        private K _batchId;
+
+        public BatchTransferController(BatchControllerHandler<T, K> handler)
+        {
+            _handler = handler;
+        }
+
+        private BatchTransferController(BatchControllerHandler<T, K> handler, IMessageContext<T> context)
+        {
+            _handler = handler;
+            _batchId = context.Message.BatchId;
+
+            context.Bus.Subscribe<T>(HandleMessage, 
+                delegate(T message)
+                    {
+                        if (message.BatchId.Equals(_batchId))
+                            return true;
+
+                        return false;
+                    });
+
+            _messages.Enqueue(context.Message);
+            _messageReady.Release();
+        }
+
+        public K BatchId
+        {
+            get { return _batchId; }
+            set { _batchId = value; }
+        }
+
+        #region IEnumerable<T> Members
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            WaitHandle[] handles = new WaitHandle[] { _messageReady, _complete };
+            int waitResult;
+            while ((waitResult = WaitHandle.WaitAny(handles, TimeSpan.FromMinutes(10), true)) != 1)
+            {
+                T message = _messages.Dequeue();
+
+                yield return message;
+            }
+
+            if (waitResult == WaitHandle.WaitTimeout)
+                throw new ApplicationException("Timeout waiting for batch to complete");
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+
+        #endregion
+
+        public void HandleMessage(IMessageContext<T> context)
+        {
+            K batchId = context.Message.BatchId;
+
+            if (batchId.Equals(_batchId))
+            {
+                // this is a message for our batch, so add it to the queue and dispatch the enumerable handler
+                _messages.Enqueue(context.Message);
+                _messageReady.Release();
+            }
+            else
+            {
+                BatchTransferController<T,K> newController = new BatchTransferController<T, K>(_handler, context);
+
+                // this is a new batch, we need to create a new controller and subscribe with the appropriate predicate to handle it
+            }
+        }
+    }
+
+    [Serializable]
+    public abstract class BatchTransferMessage<K> :
+        IMessage
+    {
+        private K _batchId;
+        private long _batchLength;
+
+        /// <summary>
+        /// Identifies the batch containing this message
+        /// </summary>
+        public K BatchId
+        {
+            get { return _batchId; }
+            set { _batchId = value; }
+        }
+
+        /// <summary>
+        /// The number of messages in the batch
+        /// </summary>
+        public long BatchLength
+        {
+            get { return _batchLength; }
+            set { _batchLength = value; }
         }
     }
 }
