19fc567329a0cb8f04d88ee772033e22bca630a6
Chris Patterson <chris@phatboyg.com>
2008-02-22 05:16:19 +0000

    Wow, this is crazy now but it's working like a champ!
    Former-commit-id: 41acdcc0394785b49983ea65f0c9f301d3c8a48b

---
+++ MassTransit.Host.Tests/SimpleMessageServer.cs
@@ -0,0 +1,47 @@
+namespace MassTransit.Host.Tests
+{
+	using System;
+	using ServiceBus;
+
+	public class SimpleMessageServer :
+		IMessageService
+	{
+		private readonly IServiceBus _serviceBus;
+		private int _hitCount;
+
+		public SimpleMessageServer(IServiceBus serviceBus)
+		{
+			_serviceBus = serviceBus;
+		}
+
+		#region IMessageService Members
+
+		public void Start()
+		{
+			_serviceBus.Subscribe<MySimpleMessage>(HandleMySimpleMessage);
+		}
+
+		public void Stop()
+		{
+			_serviceBus.Unsubscribe<MySimpleMessage>(HandleMySimpleMessage);
+		}
+
+		public void Dispose()
+		{
+			_serviceBus.Dispose();
+		}
+
+		#endregion
+
+		private void HandleMySimpleMessage(IMessageContext<MySimpleMessage> ctx)
+		{
+			_hitCount++;
+		}
+	}
+
+	[Serializable]
+	public class MySimpleMessage :
+		IMessage
+	{
+	}
+}
