47cfabd19c0b1fb7f97d4713b2021be55aa6a894
Chris Patterson <chris@phatboyg.com>
2008-02-22 05:10:03 +0000

    Wow, this is crazy now but it's working like a champ!

--- MassTransit.Host/Controller.cs
+++ MassTransit.Host/Controller.cs
@@ -4,16 +4,19 @@ namespace MassTransit.Host
 	using System.Collections.Generic;
 	using System.Reflection;
 	using Config;
+	using log4net;
 	using MassTransit.Host.Config.Util.Arguments;
 	using ServiceBus;
 
 	public class Controller
 	{
+		private static readonly ILog _log = LogManager.GetLogger(typeof (Controller));
+
 		private readonly IArgumentMapFactory _argumentMapFactory = new ArgumentMapFactory();
 		private readonly IArgumentParser _argumentParser = new ArgumentParser();
 		private IHostConfigurator _configurator;
 
-		private string _ConfiguratorName;
+		private string _configuratorName;
 		private bool _installService;
 		private bool _isService;
 		private bool _uninstallService;
@@ -21,72 +24,106 @@ namespace MassTransit.Host
 		[Argument(Key = "config", Required = true, Description = "The configuration provider to use for the host")]
 		public string Configurator
 		{
-			get { return _ConfiguratorName; }
-			set { _ConfiguratorName = value; }
+			get { return _configuratorName; }
+			set
+			{
+				_configuratorName = value;
+				_log.InfoFormat("Configurator Name: {0}", value);
+			}
 		}
 
 		[Argument(Key = "install", Description = "Install the host service")]
 		public bool InstallService
 		{
 			get { return _installService; }
-			set { _installService = value; }
+			set
+			{
+				_installService = value;
+				_log.InfoFormat("Install: {0}", value);
+			}
 		}
 
 		[Argument(Key = "service", Description = "Set when starting as a service")]
 		public bool IsService
 		{
 			get { return _isService; }
-			set { _isService = value; }
+			set
+			{
+				_isService = value;
+				_log.InfoFormat("Service: {0}", value);
+			}
 		}
 
 		[Argument(Key = "uninstall", Description = "Uninstall the host service")]
 		public bool UninstallService
 		{
 			get { return _uninstallService; }
-			set { _uninstallService = value; }
+			set
+			{
+				_uninstallService = value;
+				_log.InfoFormat("Uninstall: {0}", value);
+			}
 		}
 
 		public void Start(string[] args)
 		{
-			IEnumerable<IArgument> arguments = _argumentParser.Parse(args);
-
-			IArgumentMap mapper = _argumentMapFactory.CreateMap(this);
-
-			IEnumerable<IArgument> remaining = mapper.ApplyTo(this, arguments);
-
-			if (string.IsNullOrEmpty(_ConfiguratorName))
+			try
 			{
-				Console.WriteLine("Usage: {0}", mapper.Usage);
-			}
-			else if (_installService)
-			{
-				RegisterService(arguments, true);
-			}
-			else if (_uninstallService)
-			{
-				RegisterService(arguments, false);
-			}
-			else if (_isService)
-			{
-				Console.WriteLine("Service not working yet.");
-			}
-			else
-			{
-				Console.WriteLine("Starting up as a console application");
+				_log.Info("Starting Host");
 
-				LoadConfiguration(remaining);
+				IEnumerable<IArgument> arguments = _argumentParser.Parse(args);
 
-				if (_configurator != null)
+				IArgumentMap mapper = _argumentMapFactory.CreateMap(this);
+
+				IEnumerable<IArgument> remaining = mapper.ApplyTo(this, arguments);
+
+				if (string.IsNullOrEmpty(_configuratorName))
 				{
-					foreach (IMessageService service in _configurator.Services)
-					{
-						service.Start();
-					}
-
-					Console.WriteLine("The service is running, press Control+C to exit.");
-
-					Console.ReadKey();
+					Console.WriteLine("Usage: {0}", mapper.Usage);
 				}
+				else if (_installService)
+				{
+					RegisterService(arguments, true);
+				}
+				else if (_uninstallService)
+				{
+					RegisterService(arguments, false);
+				}
+				else if (_isService)
+				{
+					Console.WriteLine("Service not working yet.");
+				}
+				else
+				{
+					Console.WriteLine("Starting up as a console application");
+
+					LoadConfiguration(remaining);
+
+					if (_configurator != null)
+					{
+						foreach (IMessageService service in _configurator.Services)
+						{
+							service.Start();
+						}
+
+						Console.WriteLine("The service is running, press Control+C to exit.");
+
+						Console.ReadKey();
+
+						Console.WriteLine("Exiting.");
+
+						foreach (IMessageService service in _configurator.Services)
+						{
+							service.Stop();
+						}
+
+						_configurator.Dispose();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				_log.Error("Controller caught exception", ex);
 			}
 		}
 
@@ -99,7 +136,7 @@ namespace MassTransit.Host
 
 			if (install)
 			{
-				Assembly assembly = Assembly.Load(_ConfiguratorName);
+				Assembly assembly = Assembly.Load(_configuratorName);
 
 				installer.Register(new Assembly[] {assembly});
 			}
@@ -111,33 +148,32 @@ namespace MassTransit.Host
 
 		private void LoadConfiguration(IEnumerable<IArgument> arguments)
 		{
-			try
+			Assembly assembly = Assembly.Load(_configuratorName);
+
+			Console.WriteLine("Loading assembly: {0}", assembly.FullName);
+
+			Type checkType = typeof (IHostConfigurator);
+
+			Type[] types = assembly.GetTypes();
+			foreach (Type t in types)
 			{
-				Assembly assembly = Assembly.Load(_ConfiguratorName);
+				_log.DebugFormat("Checking Type: {0}", t.FullName);
 
-				Type[] types = assembly.GetTypes();
-				foreach (Type t in types)
+				if (checkType.IsAssignableFrom(t) && !t.IsAbstract)
 				{
-					if (t.IsAssignableFrom(typeof (IHostConfigurator)) && !t.IsAbstract)
+					Console.WriteLine("Type: {0}", t.Name);
+
+					IHostConfigurator configurator = (IHostConfigurator) Activator.CreateInstance(t);
+					if (configurator != null)
 					{
-						Console.WriteLine("Type: {0}", t.Name);
+						IArgumentMap mapper = _argumentMapFactory.CreateMap(configurator);
 
-						IHostConfigurator configurator = (IHostConfigurator) Activator.CreateInstance(t);
-						if (configurator != null)
-						{
-							IArgumentMap mapper = _argumentMapFactory.CreateMap(configurator);
+						mapper.ApplyTo(configurator, arguments);
 
-							mapper.ApplyTo(configurator, arguments);
-
-							Configure(configurator);
-							return;
-						}
+						Configure(configurator);
+						return;
 					}
 				}
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine("Exception Occurred: {0}", ex);
 			}
 		}
 
--- MassTransit.ServiceBus/Subscriptions/Messages/CancelSubscriptionUpdates.cs
+++ MassTransit.ServiceBus/Subscriptions/Messages/CancelSubscriptionUpdates.cs
@@ -1,5 +1,8 @@
 namespace MassTransit.ServiceBus.Subscriptions.Messages
 {
+	using System;
+
+	[Serializable]
     public class CancelSubscriptionUpdates :
         IMessage
     {
