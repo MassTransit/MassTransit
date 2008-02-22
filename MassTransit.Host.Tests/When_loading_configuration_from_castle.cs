e13fe166384673a20b632b7a872bd066fc82e1eb
Chris Patterson <chris@phatboyg.com>
2008-02-22 01:52:59 +0000

    I think this is closer to reality for the host service. My guess is I broke something but we're going to clean it up tonight before its all over.
    Maybe want to create a MassTransit.Host.Castle assembly with the castle specific bits as well.
    
    
    
    Former-commit-id: f399bcd0f60c19b44b95e11b40f2558d1116e6fc

---
+++ MassTransit.Host.Tests/When_loading_configuration_from_castle.cs
@@ -0,0 +1,32 @@
+namespace MassTransit.Host.Tests
+{
+	using System.IO;
+	using System.Reflection;
+	using Config;
+	using NUnit.Framework;
+	using NUnit.Framework.SyntaxHelpers;
+	using ServiceBus;
+
+	[TestFixture]
+	public class When_loading_configuration_from_castle
+	{
+		[Test]
+		public void The_services_should_be_available()
+		{
+			string configFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "hostConfig.xml");
+			CastleConfigurator configurator = new CastleConfigurator();
+			configurator.ConfigFile = configFile;
+			configurator.Configure();
+
+			Assert.That(configurator.Services, Is.Not.Null);
+
+			int count = 0;
+			foreach (IMessageService service in configurator.Services)
+			{
+				count++;
+			}
+
+			Assert.That(count, Is.EqualTo(1));
+		}
+	}
+}
