6044136270b3554dbab406a16229201b674880f5
Chris Patterson <chris@phatboyg.com>
2008-02-28 19:08:12 +0000

    Opps.
    Former-commit-id: 036d5f80d7aaaa63cb8fa6d5db6b08eff6cb28eb

---
+++ MassTransit.Patterns/Batching/IBatchContext.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using MassTransit.ServiceBus;
+
+namespace MassTransit.Patterns.Batching
+{
+	public interface IBatchContext<T, K> :
+		IEnumerable<T>
+	{
+		K BatchId { get; }
+
+		IEndpoint ReturnEndpoint { get; }
+
+		IServiceBus Bus { get; }
+
+		bool IsComplete { get; }
+
+		void Enqueue(T message);
+	}
+}
