d7b480460c5f1bf65aa730c1d812127affb79b6e
Dru Sellers <dru@drusellers.com>
2008-03-01 17:31:25 +0000

    sender code clean up
    Former-commit-id: 6eedb7c67592b1f399bf560b1d90d556d3e2f461

--- MassTransit.ServiceBus.NMS/NmsMessageSender.cs
+++ MassTransit.ServiceBus.NMS/NmsMessageSender.cs
@@ -15,7 +15,9 @@ namespace MassTransit.ServiceBus.NMS
     using System;
     using Apache.NMS;
     using Apache.NMS.ActiveMQ;
+    using Exceptions;
     using Internal;
+    using log4net;
 
     public class NmsMessageSender :
         IMessageSender
@@ -23,8 +25,14 @@ namespace MassTransit.ServiceBus.NMS
         private IConnectionFactory _factory;
         private readonly string _queueName;
 
+        private static readonly ILog _log = LogManager.GetLogger(typeof(NmsMessageSender));
+        private static readonly ILog _messageLog = LogManager.GetLogger("MassTransit.Messages");
+        private IEndpoint _endpoint;
+
         public NmsMessageSender(IEndpoint endpoint)
         {
+            _endpoint = endpoint;
+
             UriBuilder queueUri = new UriBuilder("tcp", endpoint.Uri.Host, endpoint.Uri.Port);
 
             _queueName = endpoint.Uri.AbsolutePath.Substring(1);
@@ -44,43 +52,26 @@ namespace MassTransit.ServiceBus.NMS
 
                     using (IMessageProducer producer = session.CreateProducer(destination))
                     {
-                        producer.Send(msg);
-                        // TODO need to abstract IMessageId to support multiple platforms
+                        try
+                        {
+                            if (_messageLog.IsInfoEnabled)
+                                _messageLog.InfoFormat("Message {0} Sent To {1}", envelope.Messages[0].GetType(),
+                                                       _endpoint.Uri);
+
+                            producer.Send(msg);
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new EndpointException(_endpoint, "Problem with " + _endpoint.Uri, ex);
+                        }
+                        // TODO: need to abstract IMessageId to support multiple platforms
                         // envelope.Id = msg.NMSMessageId;
                     }
+                    if (_log.IsDebugEnabled)
+                        _log.DebugFormat("Message Sent: Id = {0}, Message Type = {1}", msg.NMSMessageId,
+                                         envelope.Messages != null ? envelope.Messages[0].GetType().ToString() : "");
                 }
-
-                //// lets consume a message
-                //ActiveMQTextMessage message = (ActiveMQTextMessage)consumer.Receive();
-                //if (message == null)
-                //{
-                //    Console.WriteLine("No message received!");
-                //}
-                //else
-                //{
-                //    Console.WriteLine("Received message with ID:   " + message.NMSMessageId);
-                //    Console.WriteLine("Received message with text: " + message.Text);
-                //}
             }
-            //Message msg = MessageQueueEnvelopeMapper.MapFrom(envelope);
-
-            //try
-            //{
-            //    if (_messageLog.IsInfoEnabled)
-            //        _messageLog.InfoFormat("Message {0} Sent To {1}", envelope.Messages[0].GetType(), _endpoint.Uri);
-
-            //    _queue.Send(msg);
-            //}
-            //catch (MessageQueueException ex)
-            //{
-            //    throw new EndpointException(_endpoint, "Problem with " + _endpoint.QueuePath, ex);
-            //}
-
-            //envelope.Id = msg.Id;
-
-            //if (_log.IsDebugEnabled)
-            //    _log.DebugFormat("Message Sent: Id = {0}, Message Type = {1}", msg.Id,
-            //                     envelope.Messages != null ? envelope.Messages[0].GetType().ToString() : "");
         }
 
         public void Dispose()
