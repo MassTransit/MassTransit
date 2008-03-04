7240b9757ec1008cceb105c64c1d7f842a462d3a
Dru Sellers <dru@drusellers.com>
2008-03-04 14:20:08 +0000

    whoot
    Former-commit-id: 88debc19969c5ca2f423a38f63ebc2e2868e0041

--- MassTransit.ServiceBus.Tests/Formatters/XmlMessageFormatterTests.cs
+++ MassTransit.ServiceBus.Tests/Formatters/XmlMessageFormatterTests.cs
@@ -8,42 +8,54 @@ namespace MassTransit.ServiceBus.Tests.Formatters
     [TestFixture]
     public class XmlMessageFormatterTests
     {
-        private MockRepository mocks = new MockRepository();
+        private MockRepository mocks;
+        private XmlMessageFormatter formatter;
+        IFormattedBody mockBody;
 
-        private string serialized = "<?xml version=\"1.0\"?>" + Environment.NewLine +
+        private readonly string serialized = "<?xml version=\"1.0\"?>" + Environment.NewLine +
                                     "<PingMessage xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />";
 
+        [SetUp]
+        public void SetUp()
+        {
+            mocks = new MockRepository();
+            formatter = new XmlMessageFormatter();
+            mockBody = mocks.CreateMock<IFormattedBody>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            mocks = null;
+            formatter = null;
+            mockBody = null;
+        }
 
         [Test]
         public void Serialize()
         {
             PingMessage msg = new PingMessage();
-            XmlMessageFormatter xmf = new XmlMessageFormatter();
-            IFormattedBody mockBody = mocks.CreateMock<IFormattedBody>();
-
+            
             using (mocks.Record())
             {
                 mockBody.Body = serialized;
             }
             using (mocks.Playback())
             {
-                xmf.Serialize(mockBody, msg);
+                formatter.Serialize(mockBody, msg);
             }
         }
 
         [Test]
         public void Deserialize()
         {
-            XmlMessageFormatter xmf = new XmlMessageFormatter();
-            IFormattedBody mockBody = mocks.CreateMock<IFormattedBody>();
-
             using (mocks.Record())
             {
                 Expect.Call(mockBody.Body).Return(serialized);
             }
             using (mocks.Playback())
             {
-                IMessage[] msgs = xmf.Deserialize(mockBody);
+                IMessage[] msgs = formatter.Deserialize(mockBody);
                 Assert.AreEqual(1, msgs.Length);
             }
         }
