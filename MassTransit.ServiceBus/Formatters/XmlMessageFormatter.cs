fc56566358aea15e04e9ee8d8a60f05ea0ada4da
Dru Sellers <dru@drusellers.com>
2008-03-05 20:20:42 +0000

    Former-commit-id: 18f002e4d32f1803c642686f0082b96b15412498

--- MassTransit.ServiceBus/Formatters/XmlMessageFormatter.cs
+++ MassTransit.ServiceBus/Formatters/XmlMessageFormatter.cs
@@ -8,46 +8,23 @@ namespace MassTransit.ServiceBus.Formatters
 		IMessageFormatter
 	{
 		private readonly XmlSerializer _serializer;
-        private readonly Dictionary<Type, XmlSerializer> _genericSerializers;
-
 
 		public XmlMessageFormatter()
 		{
-            MessageFinder.Initialize();
-			List<Type> types = MessageFinder.AllNonGenericMessageTypes();
+			List<Type> types = MessageFinder.AllMessageTypes();
 
 			_serializer = new XmlSerializer(typeof (object[]), types.ToArray());
-            _genericSerializers = new Dictionary<Type, XmlSerializer>();
 		}
 
 		#region IMessageFormatter Members
 
 		public void Serialize(IFormattedBody body, params IMessage[] messages)
 		{
-            Type type = messages[0].GetType();
-            if (type.IsGenericType)
-            {
-                SerializeGeneric(body, messages);
-            }
-            else
-            {
-                List<object> objects = new List<object>();
-                objects.AddRange(messages);
+			List<object> objects = new List<object>();
+			objects.AddRange(messages);
 
-                _serializer.Serialize(body.BodyStream, objects.ToArray());
-            }
+			_serializer.Serialize(body.BodyStream, objects.ToArray());
 		}
-
-        private void SerializeGeneric(IFormattedBody body, params IMessage[] messages)
-        {
-            Type type = messages[0].GetType();
-            if(!_genericSerializers.ContainsKey(type))
-            {
-                _genericSerializers.Add(type, new XmlSerializer(typeof (object[]), new Type[] {type}));
-            }
-
-            _genericSerializers[type].Serialize(body.BodyStream, messages);          
-        }
 
 		public IMessage[] Deserialize(IFormattedBody formattedBody)
 		{
@@ -65,11 +42,6 @@ namespace MassTransit.ServiceBus.Formatters
 
 			return messages.ToArray();
 		}
-        private IMessage[] DeserializeGeneric(IFormattedBody formattedBody)
-        {
-            //?
-            return null;
-        }
 
 		#endregion
 	}
