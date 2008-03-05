46200a5f35489b4ae31d0d8f5e9d6b52b0ef5076
Chris Patterson <chris@phatboyg.com>
2008-03-05 20:35:02 +0000

    No need to reflect every time unless we have to at some point.

--- MassTransit.ServiceBus/Formatters/MessageFinder.cs
+++ MassTransit.ServiceBus/Formatters/MessageFinder.cs
@@ -1,65 +1,47 @@
 namespace MassTransit.ServiceBus.Formatters
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Reflection;
+	using System;
+	using System.Collections.Generic;
+	using System.Reflection;
 
-    public class MessageFinder
-    {
-        private static readonly List<Type> _messageTypes = new List<Type>();
+	public class MessageFinder
+	{
+		private static readonly List<Type> _messageTypes = new List<Type>();
 
+		public static void Initialize()
+		{
+			lock (_messageTypes)
+			{
+				_messageTypes.Clear();
 
-        public static void Initialize()
-        {
-            lock (_messageTypes)
-            {
-                _messageTypes.Clear();
-               
-                Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
-                Type messageType = typeof (IMessage);
+				Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+				Type messageType = typeof (IMessage);
 
-                foreach (Assembly assembly in assemblies)
-                {
-                    if (DoesAssemblyReferenceMassTransit(assembly))
-                    {
-                        Type[] types = assembly.GetTypes();
+				foreach (Assembly assembly in assemblies)
+				{
+					Type[] types = assembly.GetTypes();
 
-                        foreach (Type type in types)
-                        {
-                            if (type.IsAbstract || type.IsInterface)
-                                continue;
+					foreach (Type type in types)
+					{
+						if (type.IsAbstract || type.IsInterface)
+							continue;
 
-							if (type.ContainsGenericParameters)
-								continue;
+						if (type.ContainsGenericParameters)
+							continue;
 
-                            if (messageType.IsAssignableFrom(type))
-                                _messageTypes.Add(type);
-                        }
-                    }
-                }
-            }
-        }
+						if (messageType.IsAssignableFrom(type))
+							_messageTypes.Add(type);
+					}
+				}
+			}
+		}
 
-        public static bool DoesAssemblyReferenceMassTransit(Assembly assembly)
-        {
-            bool result = false;
-            AssemblyName[] names = assembly.GetReferencedAssemblies();
+		public static List<Type> AllMessageTypes()
+		{
+			if (_messageTypes.Count == 0)
+				Initialize();
 
-            foreach (AssemblyName name in names)
-            {
-                if(name.Name.Contains("MassTransit"))
-                {
-                    result = true;
-                    break;
-                }
-            }
-
-            return result;
-        }
-        public static List<Type> AllMessageTypes()
-        {
-            Initialize();
-            return _messageTypes;
-        }
-    }
+			return _messageTypes;
+		}
+	}
 }
