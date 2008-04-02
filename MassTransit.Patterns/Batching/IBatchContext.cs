6ef3561dbfa610c9d9abb34680dadee3d5244dc3
Dru Sellers <dru@drusellers.com>
2008-04-02 00:42:58 +0000

    legal beagle
    Former-commit-id: 40eeb32553cb51ed86b885f673513e90cdb1f86a

--- MassTransit.Patterns/Batching/IBatchContext.cs
+++ MassTransit.Patterns/Batching/IBatchContext.cs
@@ -1,19 +1,32 @@
-using System.Collections.Generic;
-using MassTransit.ServiceBus;
+/// Copyright 2007-2008 The Apache Software Foundation.
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+/// this file except in compliance with the License. You may obtain a copy of the 
+/// License at 
+/// 
+///   http://www.apache.org/licenses/LICENSE-2.0 
+/// 
+/// Unless required by applicable law or agreed to in writing, software distributed 
+/// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+/// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+/// specific language governing permissions and limitations under the License.
 
 namespace MassTransit.Patterns.Batching
 {
-	public interface IBatchContext<T, K> :
-		IEnumerable<T>
-	{
-		K BatchId { get; }
+    using System.Collections.Generic;
+    using ServiceBus;
 
-		IEndpoint ReturnEndpoint { get; }
+    public interface IBatchContext<T, K> :
+        IEnumerable<T>
+    {
+        K BatchId { get; }
 
-		IServiceBus Bus { get; }
+        IEndpoint ReturnEndpoint { get; }
 
-		bool IsComplete { get; }
+        IServiceBus Bus { get; }
 
-		void Enqueue(T message);
-	}
+        bool IsComplete { get; }
+
+        void Enqueue(T message);
+    }
 }
