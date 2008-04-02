cce9656b0f91c7616fc03c0e9f63b60fc319ff0b
Chris Patterson <chris@phatboyg.com>
2008-04-02 02:01:51 +0000

    Changes to make it work

--- Samples/PublishSubscribe/SubMgr/Program.cs
+++ Samples/PublishSubscribe/SubMgr/Program.cs
@@ -1,60 +1,38 @@
 namespace SubMgr
 {
-    using System;
-    using log4net.Config;
-    using MassTransit.ServiceBus;
-    using MassTransit.ServiceBus.Subscriptions;
-    using MassTransit.ServiceBus.SubscriptionsManager;
-    using NHibernate;
-    using NHibernate.Cfg;
+
 
     internal class Program
     {
-        private static readonly IMessageQueueEndpoint _endpoint = new MessageQueueEndpoint("msmq://localhost/test_subscriptions");
-        private static IServiceBus _bus;
-        private static ISessionFactory _sessionFactory;
-        private static ISubscriptionStorage _subscriptionCache;
-        private static ISubscriptionStorage _subscriptionRepository;
-        private static SubscriptionService _subscriptionService;
+		//private static readonly IMessageQueueEndpoint _endpoint = new MessageQueueEndpoint("msmq://localhost/test_subscriptions");
+		//private static IServiceBus _bus;
+		//private static ISessionFactory _sessionFactory;
+		//private static ISubscriptionStorage _subscriptionCache;
+		//private static ISubscriptionStorage _subscriptionRepository;
+		//private static SubscriptionService _subscriptionService;
 
-        protected static void Initialize()
-        {
-            string connectionString = "Server=localhost;initial catalog=test;Trusted_Connection=yes";
+		//protected static void Initialize()
+		//{
+		//    string connectionString = "Server=localhost;initial catalog=test;Trusted_Connection=yes";
 
-            Configuration cfg = new Configuration();
+		//    Configuration cfg = new Configuration();
 
-            cfg.SetProperty("hibernate.connection.provider", "NHibernate.Connection.DriverConnectionProvider");
-            cfg.SetProperty("hibernate.connection.driver_class", "NHibernate.Driver.SqlClientDriver");
-            cfg.SetProperty("hibernate.connection.connection_string", connectionString);
-            cfg.SetProperty("hibernate.dialect", "NHibernate.Dialect.MsSql2005Dialect");
+		//    cfg.SetProperty("hibernate.connection.provider", "NHibernate.Connection.DriverConnectionProvider");
+		//    cfg.SetProperty("hibernate.connection.driver_class", "NHibernate.Driver.SqlClientDriver");
+		//    cfg.SetProperty("hibernate.connection.connection_string", connectionString);
+		//    cfg.SetProperty("hibernate.dialect", "NHibernate.Dialect.MsSql2005Dialect");
 
-            cfg.AddAssembly("MassTransit.ServiceBus.SubscriptionsManager");
+		//    cfg.AddAssembly("MassTransit.ServiceBus.SubscriptionsManager");
 
-            _sessionFactory = cfg.BuildSessionFactory();
+		//    _sessionFactory = cfg.BuildSessionFactory();
 
-            _subscriptionRepository = new PersistantSubscriptionStorage(_sessionFactory);
+		//    _subscriptionRepository = new PersistantSubscriptionStorage(_sessionFactory);
 
-            _subscriptionCache = new LocalSubscriptionCache();
+		//    _subscriptionCache = new LocalSubscriptionCache();
 
-            _bus = new ServiceBus(_endpoint, _subscriptionCache);
+		//    _bus = new ServiceBus(_endpoint, _subscriptionCache);
 
-            _subscriptionService = new SubscriptionService(_bus, _subscriptionCache, _subscriptionRepository);
+		//    _subscriptionService = new SubscriptionService(_bus, _subscriptionCache, _subscriptionRepository);
         }
-
-
-        private static void Main(string[] args)
-        {
-            XmlConfigurator.Configure();
-
-            Initialize();
-
-            _subscriptionService.Start(args);
-
-
-            Console.WriteLine("Started...");
-            Console.ReadLine();
-
-            _subscriptionService.Stop();
-        }
-    }
+    
 }
