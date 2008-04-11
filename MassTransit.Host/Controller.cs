5b8408756324cdb8e44f2df08240612d6915647c
Dru Sellers <dru@drusellers.com>
2008-04-11 02:30:18 +0000

    more ignores and legal
    Former-commit-id: 58d9e786d2a98940ccdc63345bffc2b4f4a37d03

--- MassTransit.Host/Controller.cs
+++ MassTransit.Host/Controller.cs
@@ -1,3 +1,15 @@
+/// Copyright 2007-2008 The Apache Software Foundation.
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+/// this file except in compliance with the License. You may obtain a copy of the 
+/// License at 
+/// 
+///   http://www.apache.org/licenses/LICENSE-2.0 
+/// 
+/// Unless required by applicable law or agreed to in writing, software distributed 
+/// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+/// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+/// specific language governing permissions and limitations under the License.
 namespace MassTransit.Host
 {
     using System;
@@ -96,30 +108,7 @@ namespace MassTransit.Host
                 }
                 else
                 {
-                    Console.WriteLine("Starting up as a console application");
-
-                    LoadConfiguration(remaining);
-
-                    if (_configurator != null)
-                    {
-                        foreach (IMessageService service in _configurator.Services)
-                        {
-                            service.Start();
-                        }
-
-                        Console.WriteLine("The service is running, press Control+C to exit.");
-
-                        Console.ReadKey();
-
-                        Console.WriteLine("Exiting.");
-
-                        foreach (IMessageService service in _configurator.Services)
-                        {
-                            service.Stop();
-                        }
-
-                        _configurator.Dispose();
-                    }
+                    RunAsConsoleApp(remaining);
                 }
             }
             catch (Exception ex)
@@ -127,6 +116,34 @@ namespace MassTransit.Host
                 _log.Error("Controller caught exception", ex);
 
                 Console.WriteLine("An exception occurred: {0}", ex.Message);
+            }
+        }
+
+        private void RunAsConsoleApp(IEnumerable<IArgument> remaining)
+        {
+            Console.WriteLine("Starting up as a console application");
+
+            LoadConfiguration(remaining);
+
+            if (_configurator != null)
+            {
+                foreach (IMessageService service in _configurator.Services)
+                {
+                    service.Start();
+                }
+
+                Console.WriteLine("The service is running, press Control+C to exit.");
+
+                Console.ReadKey();
+
+                Console.WriteLine("Exiting.");
+
+                foreach (IMessageService service in _configurator.Services)
+                {
+                    service.Stop();
+                }
+
+                _configurator.Dispose();
             }
         }
 
