998eff8ff1b9dc35622d3e1e25c3153f68d8bdc4
Dru Sellers <dru@drusellers.com>
2008-04-14 18:44:43 +0000

    host update
    Former-commit-id: 78ec4b36682fe5a6490681b5ecfad1e4eb599087

--- MassTransit.SubscriptionStorage/Subscriptions_Security.sql
+++ MassTransit.SubscriptionStorage/Subscriptions_Security.sql
@@ -1,4 +1,8 @@
 --ctrl-shift-m  in sql to replace the sql template params
+USE [master]
+GO
+CREATE LOGIN [App-SubscriptionManager] WITH PASSWORD=N'Replace Me', DEFAULT_DATABASE=[<database_name, sysname, test>], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
 
 USE [<database_name, sysname, test>]
 GO
@@ -12,3 +16,5 @@ USE [<database_name, sysname, test>]
 GO
 EXEC sp_addrolemember N'db_datareader', N'<user_name, sysname, App-SubscriptionManager>'
 GO
+
+
