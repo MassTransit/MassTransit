1775bfcfd4365c568ae06674663f335b6d66aa2e
Chris Patterson <chris@phatboyg.com>
2008-04-23 04:50:38 +0000

    Okay, time to rename the interfaces for clarity -- at least for now anyway
    Former-commit-id: ecba8ea828847cb3083a110953f35cee447d588e

--- MassTransit.Patterns/Fabric/IProduce.cs
+++
@@ -1,9 +0,0 @@
-namespace MassTransit.Patterns.Fabric
-{
-	using ServiceBus;
-
-	public interface IProduce<TMessage> where TMessage : IMessage
-	{
-		void Attach(IConsume<TMessage> consumer);
-	}
-}
