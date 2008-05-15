b29827e8ac7e03c1f9ba0b41491de4a707264347
Chris Patterson <chris@phatboyg.com>
2008-05-15 14:57:24 +0000

    More tests for correlated dispatcher
    Former-commit-id: 8d080566b58217679b63a687453222972256b316

---
+++ MassTransit.ServiceBus.Tests/CorrelatedMessageDispatcher_Specs.cs
@@ -0,0 +1,185 @@
+namespace MassTransit.ServiceBus.Tests
+{
+	using System;
+	using Internal;
+	using NUnit.Framework;
+	using NUnit.Framework.SyntaxHelpers;
+
+	[TestFixture]
+	public class When_a_correlated_message_is_dispatched_through_the_message_dispatcher
+	{
+		#region Setup/Teardown
+
+		[SetUp]
+		public void Before_each()
+		{
+			_dispatcher = new CorrelatedMessageDispatcher();
+			_message = new TestMessage(_value);
+		}
+
+		#endregion
+
+		private CorrelatedMessageDispatcher _dispatcher;
+		private TestMessage _message;
+		private readonly int _value = 27;
+
+		internal class InvalidConsumer { }
+
+		internal class TestConsumer : Consumes<TestMessage>.For<Guid>
+		{
+			private readonly Guid _correlationId;
+			private int _value;
+
+			public TestConsumer(Guid correlationId)
+			{
+				_correlationId = correlationId;
+			}
+
+			public int Value
+			{
+				get { return _value; }
+			}
+
+			#region For<Guid> Members
+
+			public Guid CorrelationId
+			{
+				get { return _correlationId; }
+			}
+
+			public void Consume(TestMessage message)
+			{
+				_value = message.Value;
+			}
+
+			#endregion
+		}
+
+		internal class TestMessage : CorrelatedBy<Guid>
+		{
+			private readonly Guid _correlationId;
+			private readonly int _value;
+
+			public TestMessage(int value)
+			{
+				_value = value;
+				_correlationId = Guid.NewGuid();
+			}
+
+			public int Value
+			{
+				get { return _value; }
+			}
+
+			#region CorrelatedBy<Guid> Members
+
+			public Guid CorrelationId
+			{
+				get { return _correlationId; }
+			}
+
+			#endregion
+		}
+
+		[Test]
+		public void It_should_be_dispatched_to_the_consumer()
+		{
+			TestConsumer consumerA = new TestConsumer(_message.CorrelationId);
+			_dispatcher.Subscribe(consumerA);
+
+			_dispatcher.Dispatch(_message);
+
+			Assert.That(consumerA.Value, Is.EqualTo(_message.Value));
+		}
+
+		[Test]
+		public void It_should_not_be_dispatched_if_the_correlation_does_not_match()
+		{
+			TestConsumer consumerA = new TestConsumer(Guid.NewGuid());
+			_dispatcher.Subscribe(consumerA);
+
+			_dispatcher.Dispatch(_message);
+
+			Assert.That(consumerA.Value, Is.EqualTo(default(int)));
+		}
+
+		[Test]
+		public void The_object_should_be_dispatched_to_the_consumer()
+		{
+			TestConsumer consumerA = new TestConsumer(_message.CorrelationId);
+			_dispatcher.Subscribe(consumerA);
+
+			object obj = _message;
+
+			_dispatcher.Dispatch(obj);
+
+			Assert.That(consumerA.Value, Is.EqualTo(_message.Value));
+		}
+
+		[Test]
+		public void It_should_be_dispatched_to_all_consumers()
+		{
+			TestConsumer consumerA = new TestConsumer(_message.CorrelationId);
+			_dispatcher.Subscribe(consumerA);
+
+			TestConsumer consumerB = new TestConsumer(_message.CorrelationId);
+			_dispatcher.Subscribe(consumerB);
+
+			_dispatcher.Dispatch(_message);
+
+			Assert.That(consumerA.Value, Is.EqualTo(_value));
+			Assert.That(consumerB.Value, Is.EqualTo(_value));
+		}
+
+		[Test]
+		public void It_should_not_be_sent_to_uninterested_consumers()
+		{
+			TestConsumer consumerA = new TestConsumer(_message.CorrelationId);
+			_dispatcher.Subscribe(consumerA);
+
+			TestConsumer consumerB = new TestConsumer(_message.CorrelationId);
+			_dispatcher.Subscribe(consumerB);
+
+			_dispatcher.Unsubscribe(consumerA);
+
+			_dispatcher.Dispatch(_message);
+
+			Assert.That(consumerA.Value, Is.EqualTo(default(int)));
+			Assert.That(consumerB.Value, Is.EqualTo(_value));
+		}
+
+		[Test]
+		public void It_should_not_be_sent_to_consumers_that_are_no_longer_interested()
+		{
+			TestConsumer consumerA = new TestConsumer(_message.CorrelationId);
+			_dispatcher.Subscribe(consumerA);
+
+			TestConsumer consumerB = new TestConsumer(Guid.NewGuid());
+			_dispatcher.Subscribe(consumerB);
+
+			_dispatcher.Dispatch(_message);
+
+			Assert.That(consumerA.Value, Is.EqualTo(_value));
+			Assert.That(consumerB.Value, Is.EqualTo(default(int)));
+		}
+
+		[Test]
+		public void It_should_only_be_sent_to_consumers_who_are_interested_in_the_specific_correlation_id()
+		{
+			TestMessage anotherMessage = new TestMessage(42);
+
+			TestConsumer consumerA = new TestConsumer(_message.CorrelationId);
+			_dispatcher.Subscribe(consumerA);
+
+			TestConsumer consumerB = new TestConsumer(anotherMessage.CorrelationId);
+			_dispatcher.Subscribe(consumerB);
+
+			_dispatcher.Dispatch(_message);
+			_dispatcher.Dispatch(anotherMessage);
+
+			Assert.That(consumerA.Value, Is.EqualTo(_value));
+			Assert.That(consumerB.Value, Is.EqualTo(42));
+		}
+
+	}
+}
