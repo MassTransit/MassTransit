0acbc32a68db09ef11d8230fa5cc0493076840f6
Chris Patterson <chris@phatboyg.com>
2008-05-16 22:03:27 +0000

    Added support for dispatching and some tests around it to make sure it works.

---
+++ MassTransit.ServiceBus.Tests/Specification.cs
@@ -0,0 +1,50 @@
+namespace MassTransit.ServiceBus.Tests
+{
+	using NUnit.Framework;
+	using Rhino.Mocks;
+
+	[TestFixture]
+	public abstract class Specification
+	{
+		[SetUp]
+		public void Setup()
+		{
+			_mocks = new MockRepository();
+
+			Before_each();
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			After_each();
+
+			_mocks = null;
+		}
+
+		protected MockRepository _mocks;
+
+		protected virtual void After_each()
+		{
+		}
+
+		protected virtual void Before_each()
+		{
+		}
+
+		protected T Mock<T>()
+		{
+			return _mocks.DynamicMock<T>();
+		}
+
+		protected T Stub<T>()
+		{
+			return _mocks.Stub<T>();
+		}
+
+		protected void ReplayAll()
+		{
+			_mocks.ReplayAll();
+		}
+	}
+}
