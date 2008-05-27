8bd1b3d8f1a6fdb136804df2528bbaa4f26a8a62
Chris Patterson <chris@phatboyg.com>
2008-05-27 20:03:15 +0000

    Integrated the thread based asynchronous dispatcher to the service bus

--- MassTransit.ServiceBus.Tests/When_A_Message_Is_Received_On_The_Service_Bus.cs
+++ MassTransit.ServiceBus.Tests/When_A_Message_Is_Received_On_The_Service_Bus.cs
@@ -1,6 +1,7 @@
 namespace MassTransit.ServiceBus.Tests
 {
     using System;
+    using System.Threading;
     using Internal;
     using NUnit.Framework;
     using NUnit.Framework.SyntaxHelpers;
@@ -21,8 +22,8 @@ namespace MassTransit.ServiceBus.Tests
         public void SetUp()
         {
             mocks = new MockRepository();
-            mockServiceBusEndPoint = mocks.CreateMock<IEndpoint>();
-            mockReceiver = mocks.CreateMock<IMessageReceiver>();
+			mockServiceBusEndPoint = mocks.DynamicMock<IEndpoint>();
+            mockReceiver = mocks.DynamicMock<IMessageReceiver>();
             _serviceBus = new ServiceBus(mockServiceBusEndPoint);
 
             SetupResult.For(mockServiceBusEndPoint.Uri).Return(new Uri("msmq://localhost/test_servicebus")); //stupid log4net
@@ -74,5 +75,54 @@ namespace MassTransit.ServiceBus.Tests
 
             }
         }
+
+		[Test]
+		public void Dispatching_a_message_asynchronously_should_deliver_the_message()
+		{
+			SetupResult.For(mockServiceBusEndPoint.Receiver).Return(mockReceiver);
+			mocks.ReplayAll();
+
+			ManualResetEvent received = new ManualResetEvent(false);
+
+			Action<IMessageContext<PingMessage>> handler = delegate { received.Set(); };
+			_serviceBus.Subscribe(handler);
+			
+			PingMessage message = new PingMessage();
+			_serviceBus.Dispatch(message, DispatchMode.Asynchronous);
+
+			Assert.That(received.WaitOne(TimeSpan.FromSeconds(30), true), Is.True, "Timeout waiting for message handler to be called");
+		}
+
+		[Test]
+		public void Dispatching_a_message_asynchronously_to_an_object_should_deliver_the_message()
+		{
+			SetupResult.For(mockServiceBusEndPoint.Receiver).Return(mockReceiver);
+			mocks.ReplayAll();
+
+			ManualResetEvent received = new ManualResetEvent(false);
+
+			TestConsumer<PingMessage> consumer = new TestConsumer<PingMessage>(delegate { received.Set(); });
+			_serviceBus.Subscribe(consumer);
+
+			PingMessage message = new PingMessage();
+			_serviceBus.Dispatch(message, DispatchMode.Asynchronous);
+
+			Assert.That(received.WaitOne(TimeSpan.FromSeconds(30), true), Is.True, "Timeout waiting for message handler to be called");
+		}
+
+		internal class TestConsumer<T> : Consumes<T>.Any where T : class
+		{
+			private readonly Action<T> callback;
+
+			public TestConsumer(Action<T> callback)
+			{
+				this.callback = callback;
+			}
+
+			public void Consume(T message)
+			{
+				callback(message);
+			}
+		}
     }
 }
