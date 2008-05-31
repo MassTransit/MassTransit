404708e551fdd7c3747e976d4b31407ff85a7302
Chris Patterson <chris@phatboyg.com>
2008-05-31 11:51:12 +0000

    More Test Fixes
    Former-commit-id: a0c1f090b2d37035337eadc3a2404ea63874641a

--- MassTransit.ServiceBus.Tests/When_A_Message_Is_Received_On_The_Service_Bus.cs
+++ MassTransit.ServiceBus.Tests/When_A_Message_Is_Received_On_The_Service_Bus.cs
@@ -9,86 +9,11 @@ namespace MassTransit.ServiceBus.Tests
 	[TestFixture]
 	public class When_A_Message_Is_Received_On_The_Service_Bus
 	{
-		[SetUp]
-		public void SetUp()
-		{
-			mocks = new MockRepository();
-			mockServiceBusEndPoint = mocks.DynamicMock<IEndpoint>();
-			_serviceBus = new ServiceBus(mockServiceBusEndPoint);
-
-			SetupResult.For(mockServiceBusEndPoint.Uri).Return(new Uri("msmq://localhost/test_servicebus")); //stupid log4net
-		}
-
-		[TearDown]
-		public void TearDown()
-		{
-			mocks = null;
-			_serviceBus = null;
-			mockServiceBusEndPoint = null;
-		}
-
-
-		[Test]
-		public void An_Event_Handler_Should_Be_Called()
-		{
-			using (mocks.Playback())
-			{
-				Action<IMessageContext<PingMessage>> handler = delegate { _received = true; };
-				_serviceBus.Subscribe(handler);
-
-				_serviceBus.Dispatch(new PingMessage());
-
-				Assert.That(_received, Is.True);
-			}
-		}
-
-		[Test]
-		public void Dispatching_a_message_asynchronously_should_Dispatch_the_message()
-		{
-			mocks.ReplayAll();
-
-			ManualResetEvent received = new ManualResetEvent(false);
-
-			Action<IMessageContext<PingMessage>> handler = delegate { received.Set(); };
-			_serviceBus.Subscribe(handler);
-
-			PingMessage message = new PingMessage();
-			_serviceBus.Dispatch(message, DispatchMode.Asynchronous);
-
-			Assert.That(received.WaitOne(TimeSpan.FromSeconds(30), true), Is.True, "Timeout waiting for message handler to be called");
-		}
-
-		[Test]
-		public void Dispatching_a_message_asynchronously_to_an_object_should_Dispatch_the_message()
-		{
-			mocks.ReplayAll();
-
-			ManualResetEvent received = new ManualResetEvent(false);
-
-			TestConsumer<PingMessage> consumer = new TestConsumer<PingMessage>(delegate { received.Set(); });
-			_serviceBus.Subscribe(consumer);
-
-			PingMessage message = new PingMessage();
-			_serviceBus.Dispatch(message, DispatchMode.Asynchronous);
-
-			Assert.That(received.WaitOne(TimeSpan.FromSeconds(30), true), Is.True, "Timeout waiting for message handler to be called");
-		}
-
-		[Test]
-		public void If_there_are_no_subscriptions_the_message_should_be_ignored()
-		{
-			using (mocks.Playback())
-			{
-				_serviceBus.Dispatch(new PingMessage());
-
-				//the test is kind of that no errors happened.
-			}
-		}
-
-		private MockRepository mocks;
+		private MockRepository _mocks;
 		private ServiceBus _serviceBus;
-		private IEndpoint mockServiceBusEndPoint;
+		private IEndpoint _mockServiceBusEndPoint;
 		private bool _received = false;
+		private readonly PingMessage _message = new PingMessage();
 
 		internal class TestConsumer<T> : Consumes<T>.Any where T : class
 		{
@@ -103,6 +28,76 @@ namespace MassTransit.ServiceBus.Tests
 			{
 				callback(message);
 			}
+		}
+
+		[SetUp]
+		public void SetUp()
+		{
+			_mocks = new MockRepository();
+			_mockServiceBusEndPoint = _mocks.DynamicMock<IEndpoint>();
+			_serviceBus = new ServiceBus(_mockServiceBusEndPoint);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			_mocks = null;
+			_serviceBus = null;
+			_mockServiceBusEndPoint = null;
+		}
+
+
+		[Test]
+		public void An_Event_Handler_Should_Be_Called()
+		{
+			_mocks.ReplayAll();
+
+			Action<IMessageContext<PingMessage>> handler = delegate { _received = true; };
+			_serviceBus.Subscribe(handler);
+
+			_serviceBus.Dispatch(_message);
+
+			Assert.That(_received, Is.True);
+		}
+
+		[Test]
+		public void Dispatching_a_message_asynchronously_should_Dispatch_the_message()
+		{
+			_mocks.ReplayAll();
+
+			ManualResetEvent received = new ManualResetEvent(false);
+
+			Action<IMessageContext<PingMessage>> handler = delegate { received.Set(); };
+			_serviceBus.Subscribe(handler);
+
+			_serviceBus.Dispatch(_message, DispatchMode.Asynchronous);
+
+			Assert.That(received.WaitOne(TimeSpan.FromSeconds(30), true), Is.True, "Timeout waiting for message handler to be called");
+		}
+
+		[Test]
+		public void Dispatching_a_message_asynchronously_to_an_object_should_Dispatch_the_message()
+		{
+			_mocks.ReplayAll();
+
+			ManualResetEvent received = new ManualResetEvent(false);
+
+			TestConsumer<PingMessage> consumer = new TestConsumer<PingMessage>(delegate { received.Set(); });
+			_serviceBus.Subscribe(consumer);
+
+			_serviceBus.Dispatch(_message, DispatchMode.Asynchronous);
+
+			Assert.That(received.WaitOne(TimeSpan.FromSeconds(30), true), Is.True, "Timeout waiting for message handler to be called");
+		}
+
+		[Test]
+		public void If_there_are_no_subscriptions_the_message_should_be_ignored()
+		{
+			_mocks.ReplayAll();
+
+			_serviceBus.Dispatch(_message);
+
+			//the test is kind of that no errors happened.
 		}
 	}
 }
