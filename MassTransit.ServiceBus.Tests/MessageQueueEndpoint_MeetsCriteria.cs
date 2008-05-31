d28e4758f92a5cb4ff25c1595865b756a73eef53
Chris Patterson <chris@phatboyg.com>
2008-05-31 11:41:36 +0000

    Working the tests
    Former-commit-id: b4be4cc2db6b74e7174070f5ff1cb59cbaa7d8f0

--- MassTransit.ServiceBus.Tests/MessageQueueEndpoint_MeetsCriteria.cs
+++ MassTransit.ServiceBus.Tests/MessageQueueEndpoint_MeetsCriteria.cs
@@ -1,6 +1,5 @@
 namespace MassTransit.ServiceBus.Tests
 {
-	using System;
 	using NUnit.Framework;
 	using NUnit.Framework.SyntaxHelpers;
 	using Rhino.Mocks;
@@ -8,146 +7,101 @@ namespace MassTransit.ServiceBus.Tests
 	[TestFixture]
 	public class MessageQueueEndpoint_MeetsCriteria
 	{
+		private MockRepository _mocks;
+		private ServiceBus _serviceBus;
+		private IEndpoint _mockServiceBusEndPoint;
+		private readonly PingMessage _message = new PingMessage();
+
 		[SetUp]
 		public void SetUp()
 		{
-			mocks = new MockRepository();
-			mockServiceBusEndPoint = mocks.CreateMock<IEndpoint>();
+			_mocks = new MockRepository();
+			_mockServiceBusEndPoint = _mocks.DynamicMock<IEndpoint>();
+			_serviceBus = new ServiceBus(_mockServiceBusEndPoint);
 		}
 
 		[TearDown]
 		public void TearDown()
 		{
-			mocks = null;
-			mockServiceBusEndPoint = null;
+			_mocks = null;
+			_mockServiceBusEndPoint = null;
 			_serviceBus = null;
 		}
 
 		[Test]
-		public void dru_test()
+		public void Subscring_to_an_endpoint_should_accept_and_dispatch_messages()
 		{
+			_mocks.ReplayAll();
+
 			bool workDid = false;
 
-			using (mocks.Record())
-			{
-				Expect.Call(mockServiceBusEndPoint.Uri).Return(new Uri("msmq://localhost/test_servicebus")).Repeat.Any(); //stupid log4net
-			}
-			using (mocks.Playback())
-			{
-				_serviceBus = new ServiceBus(mockServiceBusEndPoint);
+			_serviceBus.Subscribe<PingMessage>(
+				delegate { workDid = true; },
+				delegate { return true; });
 
-				_serviceBus.Subscribe<PingMessage>(
-					delegate { workDid = true; },
-					delegate { return true; });
+			Assert.That(_serviceBus.Accept(_message), Is.True);
 
-				Assert.That(_serviceBus.Accept(new PingMessage()), Is.True);
 
-				_serviceBus.Dispatch(new PingMessage());
-				Assert.That(workDid, Is.True, "Lazy Test!");
-			}
+			_serviceBus.Dispatch(_message);
+
+			Assert.That(workDid, Is.True, "Lazy Test!");
 		}
-
-		//[Test]
-		//public void NAME()
-		//{
-		//    string input = @"FormatName:DIRECT=OS:" + Environment.MachineName + @"\private$\test_servicebus";
-		//    string output = ServiceBusSetupFixture.GetQueueName(input);
-		//    Assert.AreEqual(@".\private$\test_servicebus", output);
-		//}
 
 		[Test]
 		public void The_Service_Bus_Should_Return_False_If_The_Message_Will_Be_Handled_By_none_Of_the_handlers()
 		{
-			using (mocks.Record())
-			{
-				Expect.Call(mockServiceBusEndPoint.Uri).Return(new Uri("msmq://localhost/test_servicebus")).Repeat.Any(); //stupid log4net
-			}
-			using (mocks.Playback())
-			{
-				_serviceBus = new ServiceBus(mockServiceBusEndPoint);
+			_mocks.ReplayAll();
 
-				_serviceBus.Subscribe<PingMessage>(
-					delegate { },
-					delegate { return false; });
+			_serviceBus.Subscribe<PingMessage>(
+				delegate { },
+				delegate { return false; });
 
-				_serviceBus.Subscribe<PingMessage>(
-					delegate { },
-					delegate { return false; });
+			_serviceBus.Subscribe<PingMessage>(
+				delegate { },
+				delegate { return false; });
 
-				object message = new PingMessage();
-
-				Assert.That(_serviceBus.Accept(message), Is.False);
-			}
+			Assert.That(_serviceBus.Accept(_message), Is.False);
 		}
 
 		[Test]
 		public void The_Service_Bus_Should_Return_False_If_The_Message_Will_Not_Be_Handled()
 		{
-			using (mocks.Record())
-			{
-				Expect.Call(mockServiceBusEndPoint.Uri).Return(new Uri("msmq://localhost/test_servicebus")).Repeat.Any(); //stupid log4net
-			}
-			using (mocks.Playback())
-			{
-				_serviceBus = new ServiceBus(mockServiceBusEndPoint);
-				_serviceBus.Subscribe<PingMessage>(
-					delegate { },
-					delegate { return false; });
+			_mocks.ReplayAll();
 
-				object message = new PingMessage();
+			_serviceBus.Subscribe<PingMessage>(
+				delegate { },
+				delegate { return false; });
 
-				Assert.That(_serviceBus.Accept(message), Is.False);
-			}
+			Assert.That(_serviceBus.Accept(_message), Is.False);
 		}
 
 		[Test]
 		public void The_Service_Bus_Should_Return_True_If_The_Message_Will_Be_Handled()
 		{
-			using (mocks.Record())
-			{
-				Expect.Call(mockServiceBusEndPoint.Uri).Return(new Uri("msmq://localhost/test_servicebus")).Repeat.Any(); //stupid log4net
-			}
-			using (mocks.Playback())
-			{
-				_serviceBus = new ServiceBus(mockServiceBusEndPoint);
+			_mocks.ReplayAll();
 
-				_serviceBus.Subscribe<PingMessage>(
-					delegate { },
-					delegate { return true; });
+			_serviceBus.Subscribe<PingMessage>(
+				delegate { },
+				delegate { return true; });
 
-				object message = new PingMessage();
-
-				Assert.That(_serviceBus.Accept(message), Is.True);
-			}
+			Assert.That(_serviceBus.Accept(_message), Is.True);
 		}
 
 		[Test]
 		public void The_Service_Bus_Should_Return_True_If_The_Message_Will_Be_Handled_By_One_Of_multiple_handlers()
 		{
-			using (mocks.Record())
-			{
-				Expect.Call(mockServiceBusEndPoint.Uri).Return(new Uri("msmq://localhost/test_servicebus")).Repeat.Any(); //stupid log4net
-			}
-			using (mocks.Playback())
-			{
-				_serviceBus = new ServiceBus(mockServiceBusEndPoint);
+			_mocks.ReplayAll();
 
-				_serviceBus.Subscribe<PingMessage>(
-					delegate { },
-					delegate { return false; });
+			_serviceBus.Subscribe<PingMessage>(
+				delegate { },
+				delegate { return false; });
 
-				_serviceBus.Subscribe<PingMessage>(
-					delegate { },
-					delegate { return true; });
+			_serviceBus.Subscribe<PingMessage>(
+				delegate { },
+				delegate { return true; });
 
-				object message = new PingMessage();
 
-				Assert.That(_serviceBus.Accept(message), Is.True);
-			}
+			Assert.That(_serviceBus.Accept(_message), Is.True);
 		}
-
-		private MockRepository mocks;
-		private ServiceBus _serviceBus;
-		private IEndpoint mockServiceBusEndPoint;
 	}
 }
