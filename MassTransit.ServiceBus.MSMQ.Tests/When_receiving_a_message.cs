ff40f4cdef3b8dcbc61961f9e2fbed90a008a7c2
Chris Patterson <chris@phatboyg.com>
2008-05-31 18:00:47 +0000

    More tests are working in the transport tests
    Former-commit-id: 1eeb87de2e115f2ce9526e8b70d11da679699e71

--- MassTransit.ServiceBus.MSMQ.Tests/When_receiving_a_message.cs
+++ MassTransit.ServiceBus.MSMQ.Tests/When_receiving_a_message.cs
@@ -9,16 +9,28 @@ namespace MassTransit.ServiceBus.MSMQ.Tests
 	[TestFixture]
 	public class When_receiving_a_message
 	{
+		private MockRepository mocks;
+		private string uri = "msmq://localhost/test_transactions";
+		private MsmqEndpoint ep;
+		private DeleteMessage msg;
+
+		private void Put_a_test_message_on_the_queue()
+		{
+			using (TransactionScope tr = new TransactionScope())
+			{
+				ep.Send(msg);
+				tr.Complete();
+			}
+		}
+
 		[SetUp]
 		public void SetUp()
 		{
 			mocks = new MockRepository();
 			ep = new MsmqEndpoint(uri);
 			QueueTestContext.ValidateAndPurgeQueue(ep.QueuePath, true);
-			mr = new MsmqMessageReceiver(ep);
 
 			msg = new DeleteMessage();
-			env = new Envelope(msg);
 
 			Put_a_test_message_on_the_queue();
 		}
@@ -26,30 +38,18 @@ namespace MassTransit.ServiceBus.MSMQ.Tests
 		[Test]
 		public void From_A_Transactional_Queue_With_a_transaction()
 		{
+			using (TransactionScope tr = new TransactionScope())
+			{
+				ep.Receive();
+				tr.Complete();
+			}
 		}
 
 		[Test]
 		[ExpectedException(typeof (EndpointException))]
 		public void From_A_Transactional_Queue_Without_a_transaction()
 		{
-		}
-
-		private MockRepository mocks;
-		private string uri = "msmq://localhost/test_transactions";
-		private MsmqEndpoint ep;
-		private MsmqMessageReceiver mr;
-		private Envelope env;
-		private DeleteMessage msg;
-
-		private void Put_a_test_message_on_the_queue()
-		{
-			MsmqMessageSender s = new MsmqMessageSender(ep);
-
-			using (TransactionScope tr = new TransactionScope())
-			{
-				s.Send(env);
-				tr.Complete();
-			}
+			ep.Receive();
 		}
 	}
 }
