b4be4cc2db6b74e7174070f5ff1cb59cbaa7d8f0
Chris Patterson <chris@phatboyg.com>
2008-05-31 11:41:36 +0000

    Working the tests

--- MassTransit.ServiceBus.Tests/MessageFilter_Specs.cs
+++ MassTransit.ServiceBus.Tests/MessageFilter_Specs.cs
@@ -9,66 +9,12 @@ namespace MassTransit.ServiceBus.Tests
 	[TestFixture]
 	public class When_a_message_filter_is_subscribed_to_the_service_bus
 	{
-		[SetUp]
-		public void SetUp()
-		{
-			mocks = new MockRepository();
-			mockServiceBusEndPoint = mocks.DynamicMock<IEndpoint>();
-			_serviceBus = new ServiceBus(mockServiceBusEndPoint);
-
-			SetupResult.For(mockServiceBusEndPoint.Uri).Return(new Uri("msmq://localhost/test_servicebus")); //stupid log4net
-		}
-
-		[TearDown]
-		public void TearDown()
-		{
-			mocks = null;
-			_serviceBus = null;
-			mockServiceBusEndPoint = null;
-		}
-
-		[Test]
-		public void A_message_should_only_reach_the_consumer_if_the_filter_passes_it_forward()
-		{
-			mocks.ReplayAll();
-
-			ManualResetEvent passed = new ManualResetEvent(false);
-
-			TestConsumer<RequestMessage> consumer = new TestConsumer<RequestMessage>(delegate { passed.Set(); });
-
-			MessageFilter<RequestMessage> filter = new MessageFilter<RequestMessage>(delegate { return false; }, consumer);
-
-			_serviceBus.Subscribe(filter);
-
-			RequestMessage message = new RequestMessage();
-			_serviceBus.Dispatch(message, DispatchMode.Synchronous);
-
-			Assert.That(passed.WaitOne(TimeSpan.FromSeconds(0), true), Is.False, "Timeout waiting for message handler to be called");
-		}
-
-		[Test]
-		public void A_message_should_only_reach_the_consumer_if_the_filter_passes_it_forward_success()
-		{
-			mocks.ReplayAll();
-
-			ManualResetEvent passed = new ManualResetEvent(false);
-
-			TestConsumer<RequestMessage> consumer = new TestConsumer<RequestMessage>(delegate { passed.Set(); });
-
-			MessageFilter<RequestMessage> filter = new MessageFilter<RequestMessage>(delegate { return true; }, consumer);
-
-			_serviceBus.Subscribe(filter);
-
-			RequestMessage message = new RequestMessage();
-			_serviceBus.Dispatch(message, DispatchMode.Synchronous);
-
-			Assert.That(passed.WaitOne(TimeSpan.FromSeconds(0), true), Is.True, "Timeout waiting for message handler to be called");
-		}
-
-
-		private MockRepository mocks;
+		private MockRepository _mocks;
 		private ServiceBus _serviceBus;
-		private IEndpoint mockServiceBusEndPoint;
+		private IEndpoint _mockServiceBusEndPoint;
+		private readonly RequestMessage _message = new RequestMessage();
+		private readonly ManualResetEvent _passed = new ManualResetEvent(false);
+		private TestConsumer<RequestMessage> _consumer;
 
 		internal class TestConsumer<T> : Consumes<T>.Any where T : class
 		{
@@ -88,6 +34,52 @@ namespace MassTransit.ServiceBus.Tests
 		public bool FilterFunction(RequestMessage message)
 		{
 			return false;
+		}
+
+		[SetUp]
+		public void SetUp()
+		{
+			_mocks = new MockRepository();
+			_mockServiceBusEndPoint = _mocks.DynamicMock<IEndpoint>();
+			_serviceBus = new ServiceBus(_mockServiceBusEndPoint);
+
+			_consumer = new TestConsumer<RequestMessage>(delegate { _passed.Set(); });
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			_mocks = null;
+			_serviceBus = null;
+			_mockServiceBusEndPoint = null;
+		}
+
+		[Test]
+		public void A_message_should_only_reach_the_consumer_if_the_filter_passes_it_forward()
+		{
+			_mocks.ReplayAll();
+
+			MessageFilter<RequestMessage> filter = new MessageFilter<RequestMessage>(delegate { return false; }, _consumer);
+
+			_serviceBus.Subscribe(filter);
+
+			_serviceBus.Dispatch(_message);
+
+			Assert.That(_passed.WaitOne(TimeSpan.FromSeconds(0), true), Is.False, "Timeout waiting for message handler to be called");
+		}
+
+		[Test]
+		public void A_message_should_only_reach_the_consumer_if_the_filter_passes_it_forward_success()
+		{
+			_mocks.ReplayAll();
+
+			MessageFilter<RequestMessage> filter = new MessageFilter<RequestMessage>(delegate { return true; }, _consumer);
+
+			_serviceBus.Subscribe(filter);
+
+			_serviceBus.Dispatch(_message);
+
+			Assert.That(_passed.WaitOne(TimeSpan.FromSeconds(0), true), Is.True, "Timeout waiting for message handler to be called");
 		}
 	}
 }
