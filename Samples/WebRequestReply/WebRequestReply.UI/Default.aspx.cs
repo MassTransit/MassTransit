9c9ce8647a0953be5b8dcfda78ccc6350c21b99d
Dru Sellers <dru@drusellers.com>
2008-06-02 01:14:15 +0000

    more files
    Former-commit-id: 9a33c2d4033e27ad471c5152d27e6f61ac4a1143

---
+++ Samples/WebRequestReply/WebRequestReply.UI/Default.aspx.cs
@@ -0,0 +1,64 @@
+namespace WebRequestReply.UI
+{
+    using System;
+    using System.Web.UI;
+    using Core;
+
+    public partial class _Default :
+        Page, IRequestReplyView
+    {
+        private readonly RequestReplyController _controller;
+
+        public _Default()
+        {
+            _controller = new RequestReplyController(this, Container.Instance.ServiceBus);
+        }
+
+        #region IRequestReplyView Members
+
+        public string RequestText
+        {
+            get { return requestText.Text; }
+        }
+
+        public string ResponseText
+        {
+            set { responseBox.Text = value; }
+        }
+
+        public event EventHandler RequestEntered;// = delegate { }; //is this delegate{}; necessary?
+
+        #endregion
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            RequestEntered(this, new EventArgs());
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            RegisterAsyncTask(new PageAsyncTask(beginTask, endTask, timeoutTask, this));
+        }
+
+
+        private IAsyncResult beginTask(object sender, EventArgs e, AsyncCallback cb, object extraData)
+        {
+            return _controller.BeginRequest(cb, extraData);
+        }
+
+        private void endTask(IAsyncResult ar)
+        {
+            _controller.EndRequest(ar);
+        }
+
+        private void timeoutTask(IAsyncResult ar)
+        {
+            ResponseText = "Async Task Timeout";
+        }
+
+    }
+}
