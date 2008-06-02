0158e75d5f22740037e6ab1b52e122cfcefeaaa7
Dru Sellers <dru@drusellers.com>
2008-06-02 22:45:48 +0000

    Former-commit-id: 109030bc782b228d16cd3ab483030bf8babb1b28

--- Samples/WebRequestReply/WebRequestReply.UI.MonoRail/Controllers/DemoController.cs
+++ Samples/WebRequestReply/WebRequestReply.UI.MonoRail/Controllers/DemoController.cs
@@ -2,17 +2,20 @@ namespace WebRequestReply.UI.MonoRail.Controllers
 {
     using System;
     using Castle.MonoRail.Framework;
+    using Core;
     using MassTransit.ServiceBus;
 
     public class DemoController :
-        SmartDispatcherController
+        SmartDispatcherController,
+        Consumes<ResponseMessage>.For<Guid>
     {
         private IServiceBus _bus;
-
+        private Guid _correlationId;
 
         public DemoController(IServiceBus bus)
         {
             _bus = bus;
+            _correlationId = Guid.NewGuid();
         }
 
         public void Default()
@@ -29,12 +32,35 @@ namespace WebRequestReply.UI.MonoRail.Controllers
         //http://www.ayende.com/Blog/archive/2008/03/25/Async-Actions-in-Monorail.aspx
         public IAsyncResult BeginAsync(string requestText)
         {
-            return null; //return IAsyncResult
+            return AsyncRequest.From(this)
+                .Via(_bus)
+                .Send(new RequestMessage(this.CorrelationId, requestText));
         }
 
         public void EndAsync()
         {
-            this.RenderText("MT: ??");
+            IAsyncResult r = ControllerContext.Async.Result;
+            this.PropertyBag.Add("responseText", "dru");
+            this.RenderView("Default");
         }
+
+        #region Consumes<ResponseMessage>.All Members
+
+        private ResponseMessage msg;
+        public void Consume(ResponseMessage message)
+        {
+            msg = message;
+        }
+
+        #endregion
+
+        #region CorrelatedBy<Guid> Members
+
+        public Guid CorrelationId
+        {
+            get { return _correlationId; }
+        }
+
+        #endregion
     }
 }
