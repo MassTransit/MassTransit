7dd1d619ae19b5ab5b13a2c4e2b1337b5ec9d59a
Dru Sellers <dru@drusellers.com>
2008-06-02 02:00:39 +0000

    Former-commit-id: d1c50c7b1aedc0131725efce22afc45c05d6edc7

--- Samples/WebRequestReply/WebRequestReply.UI.MonoRail/Controllers/DemoController.cs
+++ Samples/WebRequestReply/WebRequestReply.UI.MonoRail/Controllers/DemoController.cs
@@ -1,10 +1,19 @@
 namespace WebRequestReply.UI.MonoRail.Controllers
 {
     using Castle.MonoRail.Framework;
+    using MassTransit.ServiceBus;
 
     public class DemoController :
         Controller
     {
+        private IServiceBus _bus;
+
+
+        public DemoController(IServiceBus bus)
+        {
+            _bus = bus;
+        }
+
         public void Default()
         {
             
