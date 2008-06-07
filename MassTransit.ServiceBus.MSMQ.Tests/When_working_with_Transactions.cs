b9b3ab8a731eb95be6ead67f8e3600a6f64d18dc
Dru Sellers <dru@drusellers.com>
2008-06-07 12:02:37 +0000

    put the transactional tests back in
    Former-commit-id: 65d19dbe050c39545981c87649cf2df98e043ff1

---
+++ MassTransit.ServiceBus.MSMQ.Tests/When_working_with_Transactions.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Transactions;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace MassTransit.ServiceBus.MSMQ.Tests
+{
+    using Exceptions;
+    using Messages;
+
+    [Explicit]
+    [TestFixture(Description = "Integration Test for Transaction Handling")]
+    public class When_working_with_Transactions
+    {
+        private readonly string nonTransactionalQueueName = @"msmq://localhost/test_nonTransaction";
+        private readonly string transactionalQueueName = @"msmq://localhost/test_transaction";
+        private readonly string returnToQueueName = @"msmq://localhost/test_return";
+
+        private DeleteMessage msg = new DeleteMessage();
+
+        [SetUp]
+        public void SetUp()
+        {
+            QueueTestContext.ValidateAndPurgeQueue(new MsmqEndpoint(nonTransactionalQueueName).QueuePath);
+            QueueTestContext.ValidateAndPurgeQueue(new MsmqEndpoint(returnToQueueName).QueuePath);
+            QueueTestContext.ValidateAndPurgeQueue(new MsmqEndpoint(transactionalQueueName).QueuePath, true);
+        }
+
+
+
+
+
+        [Test]
+        public void When_The_Queue_Is_NonTransactional()
+        {
+            MsmqEndpoint ep = nonTransactionalQueueName;
+            ep.Send(msg);
+
+            QueueTestContext.VerifyMessageInQueue(new MsmqEndpoint(nonTransactionalQueueName).QueuePath, msg);
+        }
+
+        [Test]
+        public void When_The_Queue_Is_NonTransactional_In_A_Transaction()
+        {
+            using (TransactionScope tr = new TransactionScope())
+            {
+                MsmqEndpoint ep = nonTransactionalQueueName;
+                ep.Send(msg);
+
+                tr.Complete();
+            }
+
+            using (TransactionScope tr = new TransactionScope())
+            {
+                QueueTestContext.VerifyMessageInQueue(new MsmqEndpoint(nonTransactionalQueueName).QueuePath, msg);
+
+                tr.Complete();
+            }
+        }
+
+
+        [Test]
+        public void When_The_Queue_Is_Transactional()
+        {
+            using (TransactionScope tr = new TransactionScope())
+            {
+                MsmqEndpoint ep = transactionalQueueName;
+                ep.Send(msg);
+
+                tr.Complete();
+            }
+
+
+            using (TransactionScope tr = new TransactionScope())
+            {
+                QueueTestContext.VerifyMessageInQueue(new MsmqEndpoint(transactionalQueueName).QueuePath, msg);
+
+                tr.Complete();
+            }
+        }
+
+
+        [Test]
+        [ExpectedException(typeof(EndpointException))]
+        public void When_The_Queue_Is_Transactional_Not_In_A_Transaction()
+        {
+            MsmqEndpoint ep = transactionalQueueName;
+            ep.Send(msg);
+        }
+    }
+}
