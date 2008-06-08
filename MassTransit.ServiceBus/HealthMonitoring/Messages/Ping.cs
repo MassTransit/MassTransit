e919d275f52c74102b592670426a15fd53af8aca
Dru Sellers <dru@drusellers.com>
2008-06-08 16:38:42 +0000

    closer?

--- MassTransit.ServiceBus/HealthMonitoring/Messages/Ping.cs
+++ MassTransit.ServiceBus/HealthMonitoring/Messages/Ping.cs
@@ -4,7 +4,7 @@ namespace MassTransit.ServiceBus.HealthMonitoring.Messages
 
     [Serializable]
     public class Ping :
-        CorrelatedBy<Guid>
+        CorrelatedBy<Guid>, IEquatable<Ping>
     {
         private readonly Guid _correlationId;
 
@@ -16,6 +16,24 @@ namespace MassTransit.ServiceBus.HealthMonitoring.Messages
         public Guid CorrelationId
         {
             get { return _correlationId; }
+        }
+
+
+        public bool Equals(Ping ping)
+        {
+            if (ping == null) return false;
+            return Equals(_correlationId, ping._correlationId);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+            return Equals(obj as Ping);
+        }
+
+        public override int GetHashCode()
+        {
+            return _correlationId.GetHashCode();
         }
     }
 }
