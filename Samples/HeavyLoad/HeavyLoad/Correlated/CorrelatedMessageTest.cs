1b14367b8a20212e5a5365c69f520398bcaab002
Chris Patterson <chris@phatboyg.com>
2008-06-09 04:41:36 +0000

    LocalSubscriptionCache now supports correlated subscriptions (it previously treated them all the same) so that it can properly add/remove correlations without causing major issues under heavy thread loads

--- Samples/HeavyLoad/HeavyLoad/Correlated/CorrelatedMessageTest.cs
+++ Samples/HeavyLoad/HeavyLoad/Correlated/CorrelatedMessageTest.cs
@@ -8,7 +8,7 @@ namespace HeavyLoad.Correlated
     public class CorrelatedMessageTest : IDisposable
     {
         private readonly ManualResetEvent _finishedEvent = new ManualResetEvent(false);
-        private int _attempts = 10;
+        private int _attempts = 1500;
         private IServiceBus _bus;
         private MsmqEndpoint _localEndpoint;
         private int _successes;
