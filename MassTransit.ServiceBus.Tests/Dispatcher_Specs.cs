43a8f464a59578d204bf1dabfbb165b57bcdb264
Dru Sellers <dru@drusellers.com>
2008-06-09 00:04:26 +0000

    early commit due to power outages: 4/10 tests fixed

--- MassTransit.ServiceBus.Tests/Dispatcher_Specs.cs
+++ MassTransit.ServiceBus.Tests/Dispatcher_Specs.cs
@@ -2,11 +2,24 @@ namespace MassTransit.ServiceBus.Tests
 {
 	using NUnit.Framework;
 	using NUnit.Framework.SyntaxHelpers;
+	using Rhino.Mocks;
 
-	[TestFixture]
+    [TestFixture]
 	public class When_a_message_is_delivered_to_the_service_bus :
 		Specification
 	{
+
+		private ServiceBus _bus;
+		    IObjectBuilder obj;
+
+		protected override void Before_each()
+		{
+			IEndpoint endpoint = DynamicMock<IEndpoint>();
+		    obj = StrictMock<IObjectBuilder>();
+
+		    _bus = new ServiceBus(endpoint, obj);
+		}
+
 		[Test]
 		public void A_consumer_object_should_receive_the_message()
 		{
@@ -26,6 +39,11 @@ namespace MassTransit.ServiceBus.Tests
 		[Test]
 		public void A_consumer_type_should_be_created_to_receive_the_message()
 		{
+            PingHandler ph = new PingHandler();
+
+		    Expect.Call(obj.Build<Consumes<PingMessage>.All>(typeof (PingHandler))).Return(ph);
+            obj.Release<Consumes<PingMessage>.All>(ph);
+
 			ReplayAll();
 
 			_bus.AddComponent<PingHandler>();
@@ -37,15 +55,6 @@ namespace MassTransit.ServiceBus.Tests
 			Assert.That(PingHandler.Pinged, Is.GreaterThan(old));
 		}
 
-		protected override void Before_each()
-		{
-			IEndpoint endpoint = DynamicMock<IEndpoint>();
-		    IObjectBuilder obj = StrictMock<IObjectBuilder>();
-
-			_bus = new ServiceBus(endpoint, obj);
-		}
-
-		private ServiceBus _bus;
 
 		internal class PingHandler : Consumes<PingMessage>.All
 		{
