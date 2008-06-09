91d30352fbd45537035734234f5b81a0374dda28
Dru Sellers <dru@drusellers.com>
2008-06-09 00:35:47 +0000

    dashboard is updated
    Former-commit-id: 1a5d25cf5345b9a764a5217aa9a296cdc140c336

--- MassTransit.Dashboard/WebAppContainer.cs
+++ MassTransit.Dashboard/WebAppContainer.cs
@@ -1,21 +1,19 @@
 namespace MassTransit.Dashboard
 {
+    using Castle.Facilities.FactorySupport;
     using Castle.Facilities.Startable;
-    using Castle.MicroKernel.Registration;
     using Castle.MonoRail.WindsorExtension;
     using Castle.Windsor;
+    using Castle.Windsor.Configuration.Interpreters;
     using Controllers;
-    using MassTransit.ServiceBus.MSMQ;
-    using MassTransit.ServiceBus.Subscriptions;
-    using ServiceBus;
+    using WindsorIntegration;
 
     public class WebAppContainer :
         WindsorContainer
     {
-        public WebAppContainer()
+        public WebAppContainer() : base(new XmlInterpreter())
         {
             RegisterFacilities();
-            LoadMassTransit();
             RegisterComponents();
         }
 
@@ -23,41 +21,13 @@ namespace MassTransit.Dashboard
         {
             AddFacility("rails", new MonoRailFacility());
             AddFacility("startable", new StartableFacility());
+            AddFacility("masstransit", new MassTransitFacility());
+            AddFacility("factory", new FactorySupportFacility());
         }
 
         protected void RegisterComponents()
         {
             AddComponent("home.controller", typeof(SubscriptionsController));
-        }
-
-        protected void LoadMassTransit()
-        {
-            this.Register(
-                Component.For<IEndpoint>()
-                    .ImplementedBy<MsmqEndpoint>()
-                    .Named("masstransit.bus.listen")
-                    .Parameters(Parameter.ForKey("uriString").Eq("msmq://localhost/mt_dashboard")),
-                Component.For<IServiceBus>()
-                    .ImplementedBy<ServiceBus>()
-                    .Named("masstransit.bus")
-                    .Parameters(Parameter.ForKey("endpointToListenOn").Eq("${masstransit.bus.listen}")),
-                Component.For<IEndpoint>()
-                    .ImplementedBy<MsmqEndpoint>()
-                    .Named("masstransit.subscription.endpoint")
-                    .Parameters(Parameter.ForKey("uriString").Eq("msmq://localhost/mt_pubsub")),
-                Component.For<IHostedService>()
-                    .ImplementedBy<SubscriptionClient>()
-                    .Named("masstransit.subscription.client")
-                    .AddAttributeDescriptor("startable", "true")
-                    .AddAttributeDescriptor("startMethod", "Start")
-                    .AddAttributeDescriptor("stopMethod", "Stop")
-                    .Parameters(Parameter.ForKey("subscriptionServiceEndpoint").Eq("${masstransit.subscription.endpoint}")),
-                Component.For<ISubscriptionCache>()
-                    .ImplementedBy<LocalSubscriptionCache>()
-                    .Named("masstransit.cache")
-                );
-
-            IHostedService hs = this.Resolve<IHostedService>();
         }
     }
 }
