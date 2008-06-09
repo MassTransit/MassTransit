6f00c2fd5b86dc146aa18a88f4c0d896943a7a52
Dru Sellers <dru@drusellers.com>
2008-06-09 00:19:32 +0000

    early commit due to power outages: 10/10 tests fixed

--- MassTransit.ServiceBus.Tests/Subscriptions/When_managing_subscriptions_remotely.cs
+++ MassTransit.ServiceBus.Tests/Subscriptions/When_managing_subscriptions_remotely.cs
@@ -1,7 +1,10 @@
 namespace MassTransit.ServiceBus.Tests.Subscriptions
 {
+    using System;
     using MassTransit.ServiceBus.Subscriptions;
+    using MassTransit.ServiceBus.Subscriptions.Messages;
     using NUnit.Framework;
+    using Rhino.Mocks;
 
     [TestFixture]
     public class When_managing_subscriptions_remotely :
@@ -11,18 +14,21 @@ namespace MassTransit.ServiceBus.Tests.Subscriptions
         private IServiceBus _mockBus;
         private ISubscriptionCache _mockCache;
         private IEndpoint _mockEndpoint;
+        private IEndpoint _mockBusEndpoint;
 
         protected override void Before_each()
         {
             _mockBus = DynamicMock<IServiceBus>();
             _mockCache = DynamicMock<ISubscriptionCache>();
             _mockEndpoint = DynamicMock<IEndpoint>();
+            _mockBusEndpoint = DynamicMock<IEndpoint>();
 
             sc = new SubscriptionClient(_mockBus, _mockCache, _mockEndpoint);
         }
 
         protected override void After_each()
         {
+            _mockBusEndpoint = null;
             _mockEndpoint = null;
             _mockCache = null;
             _mockBus = null;
@@ -32,13 +38,25 @@ namespace MassTransit.ServiceBus.Tests.Subscriptions
         [Test]
         public void start()
         {
-            sc.Start();
+            Uri uri = new Uri("msmq://localhost/test");
+            using(Record())
+            {
+                _mockBus.Subscribe(sc);
+                Expect.Call(_mockBus.Endpoint).Return(_mockBusEndpoint);
+                Expect.Call(_mockBusEndpoint.Uri).Return(uri);
+                _mockEndpoint.Send(new CacheUpdateRequest(uri));
+                LastCall.IgnoreArguments();
+            }
+            using (Playback())
+            {
+                sc.Start();
+            }
         }
 
         [Test]
         public void stop()
         {
-            
+            //Assert.Fail("start back up here");
         }
 
         //consumes
