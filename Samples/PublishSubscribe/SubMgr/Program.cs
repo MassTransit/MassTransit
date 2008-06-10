391d1c76040fe27138ae1ec508f9da1b3cb9ec9e
Dru Sellers <dru@drusellers.com>
2008-06-10 03:08:41 +0000

    weirdness with the nh

--- Samples/PublishSubscribe/SubMgr/Program.cs
+++ Samples/PublishSubscribe/SubMgr/Program.cs
@@ -1,36 +1,18 @@
 namespace SubMgr
 {
+    using log4net.Config;
+    using MassTransit.Host2;
+
     internal class Program
     {
-		//private static readonly IMessageQueueEndpoint _endpoint = new MessageQueueEndpoint("msmq://localhost/test_subscriptions");
-		//private static IServiceBus _bus;
-		//private static ISessionFactory _sessionFactory;
-		//private static ISubscriptionStorage _subscriptionCache;
-		//private static ISubscriptionStorage _subscriptionRepository;
-		//private static SubscriptionService _subscriptionService;
+        private static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
 
-		//protected static void Initialize()
-		//{
-		//    string connectionString = "Server=localhost;initial catalog=test;Trusted_Connection=yes";
+            HostedEnvironment env = new SubscriptionManagerEnvironment("castle.xml");
 
-		//    Configuration cfg = new Configuration();
-
-		//    cfg.SetProperty("hibernate.connection.provider", "NHibernate.Connection.DriverConnectionProvider");
-		//    cfg.SetProperty("hibernate.connection.driver_class", "NHibernate.Driver.SqlClientDriver");
-		//    cfg.SetProperty("hibernate.connection.connection_string", connectionString);
-		//    cfg.SetProperty("hibernate.dialect", "NHibernate.Dialect.MsSql2005Dialect");
-
-		//    cfg.AddAssembly("MassTransit.ServiceBus.SubscriptionsManager");
-
-		//    _sessionFactory = cfg.BuildSessionFactory();
-
-		//    _subscriptionRepository = new PersistantSubscriptionStorage(_sessionFactory);
-
-		//    _subscriptionCache = new LocalSubscriptionCache();
-
-		//    _bus = new ServiceBus(_endpoint, _subscriptionCache);
-
-		//    _subscriptionService = new SubscriptionService(_bus, _subscriptionCache, _subscriptionRepository);
+            Runner.Run(env, args);
         }
+    }
     
 }
