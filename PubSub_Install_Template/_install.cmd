2cd741890c61c41bdd3971cdb5573bc876624afc
Dru Sellers <dru@drusellers.com>
2008-06-12 04:16:40 +0000

    pub sub install contributed by Rob Reynolds
    Former-commit-id: 1243b32ccfb5380a57b280764a778b270c1f39aa

---
+++ PubSub_Install_Template/_install.cmd
@@ -0,0 +1,17 @@
+@echo off
+SET DIR=%~d0%~p0%
+SET INSTANCE=(local)
+
+osql -S %INSTANCE% -E -i "%DIR%SQLScripts\InstallMassTransitDatabaseDDL.sql"
+osql -S %INSTANCE% -E -i "%DIR%SQLScripts\CreateMassTransitTableDDL.sql"
+osql -S %INSTANCE% -E -i "%DIR%SQLScripts\CreateUserAccountLoginDDL.sql"
+
+echo .
+
+"%DIR%host.exe" -install
+
+echo .
+
+NET START "MassTransit Subscription Manager"
+
+PAUSE
---
+++ PubSub_Install_Template/_uninstall.cmd
@@ -0,0 +1,15 @@
+@echo off
+SET DIR=%~d0%~p0%
+SET INSTANCE=(local)
+
+NET STOP "MassTransit Subscription Manager"
+
+echo .
+
+osql -S %INSTANCE% -E -i "%DIR%SQLScripts\UninstallMassTransitDatabaseDDL.sql"
+
+echo .
+
+"%DIR%host.exe" -uninstall
+
+PAUSE
