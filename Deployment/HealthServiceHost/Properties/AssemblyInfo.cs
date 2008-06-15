d0fcd4154acb11d02352804c2bbfa57247a6cae9
Dru Sellers <dru@drusellers.com>
2008-06-15 12:11:18 +0000

    getting install projects setup
    Former-commit-id: c50369c80838835b5fbb29a8a9d94a4e0be37862

---
+++ Deployment/HealthServiceHost/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HealthServiceHost")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("HealthServiceHost")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("50e3b557-3b5f-46c8-a916-1b845ff4e646")]
