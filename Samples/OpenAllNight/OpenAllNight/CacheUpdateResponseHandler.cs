bbc22e094083d2d4af4f0a65bd3d9d0395a07283
Dru Sellers <dru@drusellers.com>
2008-06-25 11:33:11 +0000

    updated OpenAllNight so that it works. Its blowing through 20ms intervals. now we need the long haul test
    Former-commit-id: f53de98ada7b7107486723b6664e10244f82f77a

--- Samples/OpenAllNight/OpenAllNight/CacheUpdateResponseHandler.cs
+++ Samples/OpenAllNight/OpenAllNight/CacheUpdateResponseHandler.cs
@@ -9,11 +9,17 @@ namespace OpenAllNight
         Consumes<CacheUpdateResponse>.All
     {
         private static readonly ILog _log = LogManager.GetLogger(typeof (CacheUpdateResponseHandler));
+        private readonly Counter _counter;
 
+        public CacheUpdateResponseHandler(Counter counter)
+        {
+            _counter = counter;
+        }
 
         public void Consume(CacheUpdateResponse message)
         {
-            _log.InfoFormat("Received update message number");
+            _counter.IncrementMessagesReceived();
+            //Console.WriteLine("Received update message number {0}", _counter.MessagesReceived);
         }
     }
 }
