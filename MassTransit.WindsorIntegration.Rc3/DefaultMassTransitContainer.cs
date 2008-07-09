1054b18d2b985c514d893b8a84648e08e90788da
Dru Sellers <dru@drusellers.com>
2008-07-09 02:35:38 +0000

    added castle RC3 container support
    Former-commit-id: b7fdeba8d0e01fe72477a5647453f237e27e8285

---
+++ MassTransit.WindsorIntegration.Rc3/DefaultMassTransitContainer.cs
@@ -0,0 +1,37 @@
+using MassTransit.WindsorIntegration.Rc3;
+
+namespace MassTransit.WindsorIntegration
+{
+	using Castle.Facilities.FactorySupport;
+	using Castle.Facilities.Startable;
+	using Castle.Windsor;
+	using Castle.Windsor.Configuration;
+
+	public class DefaultMassTransitContainer :
+		WindsorContainer
+	{
+		public DefaultMassTransitContainer()
+		{
+			Initialize();
+		}
+
+		public DefaultMassTransitContainer(string xmlFile) 
+			: base(xmlFile)
+		{
+			Initialize();
+		}
+
+		public DefaultMassTransitContainer(IConfigurationInterpreter configurationInterpreter)
+			: base(configurationInterpreter)
+		{
+			Initialize();
+		}
+
+		public void Initialize()
+		{
+			AddFacility("startable", new StartableFacility());
+			AddFacility("factory", new FactorySupportFacility());
+			AddFacility("masstransit", new MassTransitFacility());
+		}
+	}
+}
