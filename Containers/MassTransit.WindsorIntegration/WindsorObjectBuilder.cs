db4f206730b180a22121f9f403e1c1fcfc80b64c
Chris Patterson <chris@phatboyg.com>
2008-07-15 11:07:12 +0000

    Resharper shared file for coding style
    Former-commit-id: b7fbab1e82e36e33a686d7c40d8fa80f17cde428

--- Containers/MassTransit.WindsorIntegration/WindsorObjectBuilder.cs
+++ Containers/MassTransit.WindsorIntegration/WindsorObjectBuilder.cs
@@ -1,3 +1,15 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace MassTransit.WindsorIntegration
 {
     using System;
@@ -29,7 +41,7 @@ namespace MassTransit.WindsorIntegration
 
         public T Build<T>(Type component) where T : class
         {
-        	return _container.Resolve(component) as T;
+            return _container.Resolve(component) as T;
         }
 
         public T Build<T>(IDictionary arguments)
@@ -39,14 +51,14 @@ namespace MassTransit.WindsorIntegration
 
         public void Release<T>(T obj)
         {
-        	_container.ReleaseComponent(obj);
+            _container.ReleaseComponent(obj);
         }
 
-        public void Register<T>() where  T : class
+        public void Register<T>() where T : class
         {
             if (!_container.HasComponent(typeof (T)))
             {
-                _container.AddComponent(typeof(T).Name, typeof(T), LifestyleType.Transient);
+                _container.AddComponent(typeof (T).Name, typeof (T), LifestyleType.Transient);
             }
         }
     }
