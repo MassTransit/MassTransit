56569db1d3615bb7c3802eca6816fd8ac33b425b
Chris Patterson <chris@phatboyg.com>
2008-08-25 21:01:35 +0000

    Added a SubscriptionManagerContext so that the various aspects of the SubscriptionClient and the SubscriptionService could be verified.

---
+++ MassTransit.ServiceBus.Tests/SubscriptionManagerContext.cs
@@ -0,0 +1,110 @@
+﻿// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.ServiceBus.Tests
+{
+	using Castle.Windsor;
+	using MassTransit.ServiceBus.Subscriptions;
+	using NUnit.Framework;
+	using WindsorIntegration;
+
+	[TestFixture]
+	public class SubscriptionManagerContext
+	{
+		#region Setup/Teardown
+
+		[SetUp]
+		public void Setup()
+		{
+			_container = new DefaultMassTransitContainer("subscriptions.castle.xml");
+
+			_localBus = _container.Resolve<IServiceBus>("local");
+			_remoteBus = _container.Resolve<IServiceBus>("remote");
+			_subscriptionBus = _container.Resolve<IServiceBus>("subscriptions");
+
+			_subscriptionService = _container.Resolve<SubscriptionService>();
+			_subscriptionService.Start();
+
+			_subscriptionCache = _container.Resolve<ISubscriptionCache>("SubscriptionServiceCache");
+			Before_each();
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			After_each();
+
+
+			_localBus.Dispose();
+			_container.Release(_localBus);
+
+			_remoteBus.Dispose();
+			_container.Release(_remoteBus);
+
+			_subscriptionService.Stop();
+			_subscriptionService.Dispose();
+			_container.Release(_subscriptionService);
+
+			_subscriptionBus.Dispose();
+			_container.Release(_subscriptionBus);
+
+			_container.Dispose();
+		}
+
+		#endregion
+
+		private IWindsorContainer _container;
+		private IServiceBus _localBus;
+		private IServiceBus _remoteBus;
+		private IServiceBus _subscriptionBus;
+		private SubscriptionService _subscriptionService;
+		private ISubscriptionCache _subscriptionCache;
+
+		public SubscriptionService SubscriptionService
+		{
+			get { return _subscriptionService; }
+		}
+
+		protected virtual void Before_each()
+		{
+		}
+
+		protected virtual void After_each()
+		{
+		}
+
+		public IServiceBus LocalBus
+		{
+			get { return _localBus; }
+		}
+
+		public IServiceBus RemoteBus
+		{
+			get { return _remoteBus; }
+		}
+
+		public IServiceBus SubscriptionBus
+		{
+			get { return _subscriptionBus; }
+		}
+
+		public IWindsorContainer Container
+		{
+			get { return _container; }
+		}
+
+		public ISubscriptionCache SubscriptionCache
+		{
+			get { return _subscriptionCache; }
+		}
+	}
+}
---
+++ MassTransit.ServiceBus.Tests/Subscriptions/MonitorSubscriptionCache.cs
@@ -0,0 +1,57 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.ServiceBus.Tests.Subscriptions
+{
+	using System;
+	using System.Threading;
+	using MassTransit.ServiceBus.Subscriptions;
+	using NUnit.Framework;
+	using NUnit.Framework.SyntaxHelpers;
+
+	public class MonitorSubscriptionCache<TMessage>
+	{
+		private readonly ManualResetEvent _added = new ManualResetEvent(false);
+		private readonly ISubscriptionCache _cache;
+		private readonly ManualResetEvent _removed = new ManualResetEvent(false);
+
+		public MonitorSubscriptionCache(ISubscriptionCache cache)
+		{
+			_cache = cache;
+
+			_cache.OnAddSubscription += OnAddSubscription;
+			_cache.OnRemoveSubscription += OnRemoveSubscription;
+		}
+
+		private void OnAddSubscription(object sender, SubscriptionEventArgs e)
+		{
+			if (e.Subscription.MessageName == typeof (TMessage).FullName)
+				_added.Set();
+		}
+
+		private void OnRemoveSubscription(object sender, SubscriptionEventArgs e)
+		{
+			if (e.Subscription.MessageName == typeof (TMessage).FullName)
+				_removed.Set();
+		}
+
+		public void ShouldHaveBeenAdded(TimeSpan timeout)
+		{
+			Assert.That(_added.WaitOne(timeout, true), Is.True, "The subscription should have been added");
+		}
+
+		public void ShouldHaveBeenRemoved(TimeSpan timeout)
+		{
+			Assert.That(_removed.WaitOne(timeout, true), Is.True, "The subscription should have been removed");
+		}
+	}
+}
