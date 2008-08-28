4df1bfaa8e052a891545eec79252dc9457d10686
Dru Sellers <dru@drusellers.com>
2008-08-28 03:20:21 +0000

    Added a NullEndpoint and a PoisonEndpointDecorator

---
+++ MassTransit.ServiceBus/Internal/NullEndpoint.cs
@@ -0,0 +1,39 @@
+namespace MassTransit.ServiceBus.Internal
+{
+    using System;
+    using Exceptions;
+
+    public class NullEndpoint :
+        IEndpoint
+    {
+        public void Dispose()
+        {
+            //do nothing
+        }
+
+        public Uri Uri
+        {
+            get { return new Uri("null://middleof/nowhere"); }
+        }
+
+        public void Send<T>(T message) where T : class
+        {
+            //do nothing
+        }
+
+        public void Send<T>(T message, TimeSpan timeToLive) where T : class
+        {
+            //do nothing
+        }
+
+        public object Receive(TimeSpan timeout)
+        {
+            return Receive(TimeSpan.Zero, null);
+        }
+
+        public object Receive(TimeSpan timeout, Predicate<object> accept)
+        {
+            throw new EndpointException(this, "NullEndpoints have no messages");
+        }
+    }
+}
