fb6fd6af735a5b85b8453ac7954584606ba47d7a
Chris Patterson <chris@phatboyg.com>
2008-08-29 02:13:59 +0000

    Refactored the TypeInfo cache to make NDepend happy. It's easier to follow now as well. May think about making dispatcher a plug-in feature in time.
    Former-commit-id: d1de20a79441785a5e0c8fa6826b432a1dd4bfea

--- MassTransit.ServiceBus/Internal/TypeInfo.cs
+++ MassTransit.ServiceBus/Internal/TypeInfo.cs
@@ -12,10 +12,21 @@
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.ServiceBus.Internal
 {
+    using System;
+
     public class TypeInfo : ITypeInfo
     {
+        private static readonly Type _batchType = typeof (Batch<,>);
+
         private readonly SubscriptionTypeInfo _subscriptionTypeInfo = new SubscriptionTypeInfo();
         private IPublicationTypeInfo _publicationTypeInfo;
+
+        public TypeInfo(Type componentType)
+        {
+            Type publicationType = typeof (MessageTypePublication<>).MakeGenericType(componentType);
+
+            _publicationTypeInfo = (IPublicationTypeInfo) Activator.CreateInstance(publicationType);
+        }
 
         internal IPublicationTypeInfo PublicationTypeInfo
         {
@@ -40,6 +51,71 @@ namespace MassTransit.ServiceBus.Internal
         public void Dispose()
         {
             _subscriptionTypeInfo.Dispose();
+        }
+
+        internal void AddCorrelatedSubscription(Type componentType, Type[] types)
+        {
+            Type subscriptionType = typeof (CorrelatedSubscription<,,>).MakeGenericType(componentType, types[0], types[1]);
+
+            _subscriptionTypeInfo.AddSubscriber((ISubscriptionTypeInfo) Activator.CreateInstance(subscriptionType));
+        }
+
+        internal void AddMessageSubscription(Type componentType, Type[] types)
+        {
+            Type subscriptionType = typeof (MessageTypeSubscription<,>).MakeGenericType(componentType, types[0]);
+
+            _subscriptionTypeInfo.AddSubscriber((ISubscriptionTypeInfo) Activator.CreateInstance(subscriptionType, SubscriptionMode.All));
+        }
+
+        internal void AddSelectiveSubscription(Type componentType, Type[] types)
+        {
+            Type messageType = types[0];
+
+            if (messageType.IsGenericType && messageType.GetGenericTypeDefinition() == _batchType)
+            {
+                AddBatchMessageSubscription(componentType, messageType);
+            }
+            else
+            {
+                AddMessageSubscription(componentType, messageType);
+            }
+        }
+
+        internal void AddSagaSubscription(Type componentType, Type[] types)
+        {
+            Type subscriptionType = typeof (SagaSubscription<,>).MakeGenericType(componentType, types[0]);
+
+            _subscriptionTypeInfo.AddSubscriber((ISubscriptionTypeInfo) Activator.CreateInstance(subscriptionType));
+        }
+
+        internal void SetPublicationType(Type componentType, Type[] types)
+        {
+            Type publicationType = typeof (CorrelatedPublication<,>).MakeGenericType(componentType, types[0]);
+
+            _publicationTypeInfo = (IPublicationTypeInfo) Activator.CreateInstance(publicationType);
+        }
+
+        internal void AddStartSagaSubscription(Type componentType, Type[] types)
+        {
+            Type subscriptionType = typeof (StartSagaSubscription<,>).MakeGenericType(componentType, types[0]);
+
+            _subscriptionTypeInfo.AddSubscriber((ISubscriptionTypeInfo) Activator.CreateInstance(subscriptionType));
+        }
+
+        private void AddMessageSubscription(Type componentType, Type messageType)
+        {
+            Type subscriptionType = typeof (MessageTypeSubscription<,>).MakeGenericType(componentType, messageType);
+
+            _subscriptionTypeInfo.AddSubscriber((ISubscriptionTypeInfo) Activator.CreateInstance(subscriptionType, SubscriptionMode.Selected));
+        }
+
+        private void AddBatchMessageSubscription(Type componentType, Type messageType)
+        {
+            Type[] batchArguments = messageType.GetGenericArguments();
+
+            Type subscriptionType = typeof (BatchMessageSubscription<,,>).MakeGenericType(componentType, batchArguments[0], batchArguments[1]);
+
+            _subscriptionTypeInfo.AddSubscriber((ISubscriptionTypeInfo) Activator.CreateInstance(subscriptionType));
         }
     }
 }
