68b7800f995cc72517398f657916c676740fcba5
Chris Patterson <chris@phatboyg.com>
2008-08-30 05:14:27 +0000

    I think all hands are on deck now.

--- Samples/WinFormSample/ClientGUI/Program.cs
+++ Samples/WinFormSample/ClientGUI/Program.cs
@@ -15,7 +15,7 @@ namespace ClientGUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ClientForm());
         }
     }
 }
