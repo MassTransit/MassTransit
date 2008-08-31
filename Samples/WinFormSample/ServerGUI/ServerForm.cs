4736e215dac38a3fb507a73db6c6e723134ea602
Chris Patterson <chris@phatboyg.com>
2008-08-31 04:40:25 +0000

    I think the shutdown is working now.

--- Samples/WinFormSample/ServerGUI/ServerForm.cs
+++ Samples/WinFormSample/ServerGUI/ServerForm.cs
@@ -1,4 +1,16 @@
-﻿namespace ServerGUI
+﻿// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace ServerGUI
 {
     using System;
     using System.ComponentModel;
@@ -6,7 +18,6 @@
     using Castle.Windsor;
     using log4net;
     using MassTransit.ServiceBus;
-    using MassTransit.ServiceBus.Subscriptions;
     using MassTransit.WindsorIntegration;
     using Messages;
 
@@ -55,7 +66,7 @@
             {
                 if (_bus != null)
                 {
-                    _container.Release(_bus);
+                    _bus.Dispose();
                     _bus = null;
                 }
             }
