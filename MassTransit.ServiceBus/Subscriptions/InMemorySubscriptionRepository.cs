bde22e0daaacfa09fb094a4cbba779d765707976
Dru Sellers <dru@drusellers.com>
2008-09-02 02:36:45 +0000

    added logging
    Former-commit-id: 6348d5ddaec309f5fe7f5be51a99fbf31b814fc3

--- MassTransit.ServiceBus/Subscriptions/InMemorySubscriptionRepository.cs
+++ MassTransit.ServiceBus/Subscriptions/InMemorySubscriptionRepository.cs
@@ -1,10 +1,12 @@
 namespace MassTransit.ServiceBus.Subscriptions
 {
     using System.Collections.Generic;
+    using log4net;
 
     public class InMemorySubscriptionRepository :
         ISubscriptionRepository
     {
+        private ILog _log = LogManager.GetLogger(typeof (InMemorySubscriptionRepository));
         readonly IList<Subscription> _subscriptions = new List<Subscription>();
 
         public void Dispose()
@@ -14,11 +16,13 @@ namespace MassTransit.ServiceBus.Subscriptions
 
         public void Save(Subscription subscription)
         {
+            _log.Info("Subscription Saved");
             _subscriptions.Add(subscription);
         }
 
         public void Remove(Subscription subscription)
         {
+            _log.Info("Subscription REmoved");
             _subscriptions.Remove(subscription);
         }
 
