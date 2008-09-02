c2f25a447853fd11a9efbdb3e44e8be7d3d8b731
Dru Sellers <dru@drusellers.com>
2008-09-02 02:35:08 +0000

    host clean up work: still in progress
    Former-commit-id: cdb8e723c614ff6e85b9c56b5c75f10fbedcc9b4

---
+++ MassTransit.Host.Tests/CommandLine_specs.cs
@@ -0,0 +1,91 @@
+namespace MassTransit.Host.Tests
+{
+    using Configurations;
+    using NUnit.Framework;
+    using Rhino.Mocks;
+
+    [TestFixture]
+    public class CommandLine_specs
+    {
+        MockRepository mocks;
+        readonly string[] installArgs = new[] { "/install" };
+        readonly string[] uninstallArgs = new[] { "/uninstall" };
+        readonly string[] consoleArgs = new[] { "/console" };
+        readonly string[] serviceArgs = new[] { "/service" };
+        IInstallationConfiguration configuration;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mocks = new MockRepository();
+            configuration = mocks.CreateMock<IInstallationConfiguration>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            mocks = null;
+            configuration = null;
+        }
+
+        [Test]
+        public void When_it_gets_install_should_install()
+        {
+
+            using (mocks.Record())
+            {
+                Expect.Call(configuration.LifeCycle).Return(null);
+            }
+            using (mocks.Playback())
+            {
+                Runner.Run(configuration, installArgs);
+            }
+        }
+
+        [Test]
+        public void When_it_gets_uninstall_should_uninstall()
+        {
+            using (mocks.Record())
+            {
+                Expect.Call(configuration.LifeCycle).Return(null);
+            }
+            using (mocks.Playback())
+            {
+                Runner.Run(configuration, uninstallArgs);
+            }
+        }
+
+        [Test]
+        public void Should_run_as_command_line()
+        {
+
+        }
+
+        [Test]
+        public void Should_run_as_command_line_by_default()
+        {
+
+        }
+
+        [Test]
+        public void Should_run_as_a_service()
+        {
+
+        }
+
+        [Test]
+        public void Test_parser()
+        {
+            Assert.AreEqual("install", Parser.ParseArgs(installArgs).GetActionKey());
+            Assert.AreEqual("uninstall", Parser.ParseArgs(uninstallArgs).GetActionKey());
+            Assert.AreEqual("console", Parser.ParseArgs(consoleArgs).GetActionKey());
+            Assert.AreEqual("service", Parser.ParseArgs(serviceArgs).GetActionKey());
+
+            Assert.AreEqual("console", Parser.ParseArgs(null).GetActionKey());
+            Assert.AreEqual("console", Parser.ParseArgs(new string[] { "" }).GetActionKey());
+            Assert.AreEqual("console", Parser.ParseArgs(new string[0]).GetActionKey());
+        }
+
+
+    }
+}
