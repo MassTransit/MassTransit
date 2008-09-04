cf7e5e22470298594a8d8087cdfb910594f7b602
Dru Sellers <dru@drusellers.com>
2008-09-04 02:15:21 +0000

    adding structuremap support
    Former-commit-id: a54eb08a2dc02b51e70d3a10bc93e7876021b251

---
+++ Containers/MassTransit.StructureMapIntegration/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MassTransit.StructureMapIntegration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("MassTransit.StructureMapIntegration")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("15fcfedc-ed2b-4af7-83a8-2a4779420a1a")]
