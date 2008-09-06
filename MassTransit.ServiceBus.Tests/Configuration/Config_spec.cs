fa0d315c19b0a33dfc23373764213a291fba51f0
Dru Sellers <dru@drusellers.com>
2008-09-06 16:06:57 +0000

    Adding configuration exploration

---
+++ MassTransit.ServiceBus.Tests/Configuration/Config_spec.cs
@@ -0,0 +1,106 @@
+namespace MassTransit.ServiceBus.Tests.Configuration
+{
+    using NUnit.Framework;
+    using Transports;
+
+    [TestFixture]
+    public class Config_spec :
+        Specification
+    {
+        [Test]
+        public void Minimal_Setup()
+        {
+            BusBuilder.WithName("funk_bus")
+                .ListensOn("msmq://localhost/bill")
+                .ReceivesCommandsOn("activemq://localhost/bob")
+                .WithSharedSubscriptions(Via.SubscriptionService("msmq://localhost/mt_pubsub"))
+                .WithSharedSubscriptions(Via.DistributedCache("192.168.0.1"))
+                .CommunicatesOn<LoopbackEndpoint>()
+                .AsACompetingConsumer()
+                .Using(Serializers.Binary);
+        }
+    }
+
+    public static class BusBuilder
+    {
+        public static ConfigureTheBus WithName(string name)
+        {
+            return new ConfigureTheBus();
+        }
+    }
+    
+    public class ConfigureTheBus
+    {
+        public ConfigureTheBus ListensOn(string uri)
+        {
+            return this;
+        }
+
+        public ConfigureTheBus ReceivesCommandsOn(string uri)
+        {
+            return this;
+        }
+
+        public ConfigureTheBus WithSharedSubscriptions(object subscriptionStore)
+        {
+            return this;
+        }
+
+        public ConfigureTheBus CommunicatesOn<T>()
+        {
+            return this;
+        }
+
+        public ConfigureTheBus AsACompetingConsumer()
+        {
+            return this;
+        }
+
+        public ConfigureTheBus Using(object serializer)
+        {
+            return this;
+        }
+    }
+    public static class Serializers
+    {
+        public static SerializationOptions Binary
+        {
+            get { return new SerializationOptions(); }
+        }
+
+        public static SerializationOptions Xml
+        {
+            get { return new SerializationOptions(); }
+        }
+
+        public static SerializationOptions Custom<T>()
+        {
+            return new SerializationOptions();
+        }
+    }
+    public class SerializationOptions
+    {
+    }
+    public static class Via
+    {
+        public static SubscriptionOptions SubscriptionService(string uri)
+        {
+            return new SubscriptionOptions();
+        }
+
+        public static SubscriptionOptions DistributedCache(string uri)
+        {
+            return new SubscriptionOptions();
+        }
+
+        public static SubscriptionOptions Custom<T>(string uri)
+        {
+            return new SubscriptionOptions();
+        }
+    }
+    public class SubscriptionOptions
+    {
+        public string Address;
+        public object TypeOfSubscriptionStore;
+    }
+}
