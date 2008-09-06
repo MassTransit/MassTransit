c54a05827b303efa92b6eeaef3116a81b09c72c1
Dru Sellers <dru@drusellers.com>
2008-09-06 16:28:05 +0000

    some more tweaks

--- MassTransit.ServiceBus.Tests/Configuration/Config_spec.cs
+++ MassTransit.ServiceBus.Tests/Configuration/Config_spec.cs
@@ -1,6 +1,7 @@
 namespace MassTransit.ServiceBus.Tests.Configuration
 {
     using System;
+    using MassTransit.ServiceBus.Internal;
     using NUnit.Framework;
     using Transports;
 
@@ -11,7 +12,7 @@ namespace MassTransit.ServiceBus.Tests.Configuration
         [Test]
         public void Minimal_Setup()
         {
-            BusBuilder.WithName("funk_bus")
+            var busOptions = BusBuilder.WithName("funk_bus")
                 .ListensOn("msmq://localhost/bill")
                 .ReceivesCommandsOn("activemq://localhost/bob")
                 .WithSharedSubscriptions(Via.SubscriptionService("msmq://localhost/mt_pubsub"))
@@ -22,44 +23,12 @@ namespace MassTransit.ServiceBus.Tests.Configuration
         }
     }
 
+    //facade classes
     public static class BusBuilder
     {
         public static ConfigureTheBus WithName(string name)
         {
-            return new ConfigureTheBus();
-        }
-    }
-    
-    public class ConfigureTheBus
-    {
-        public ConfigureTheBus ListensOn(string uri)
-        {
-            return this;
-        }
-
-        public ConfigureTheBus ReceivesCommandsOn(string uri)
-        {
-            return this;
-        }
-
-        public ConfigureTheBus WithSharedSubscriptions(object subscriptionStore)
-        {
-            return this;
-        }
-
-        public ConfigureTheBus CommunicatesOn<T>()
-        {
-            return this;
-        }
-
-        public ConfigureTheBus AsACompetingConsumer()
-        {
-            return this;
-        }
-
-        public ConfigureTheBus Using(object serializer)
-        {
-            return this;
+            return new ConfigureTheBus(new BusOptions{Name = name});
         }
     }
     public static class Serializers
@@ -98,23 +67,84 @@ namespace MassTransit.ServiceBus.Tests.Configuration
     }
 
     //state pattern?
+    public class ConfigureTheBus
+    {
+        private readonly BusOptions _options;
+
+        public ConfigureTheBus(BusOptions options)
+        {
+            _options = options;
+        }
+
+        public ConfigureTheBus ListensOn(string uri)
+        {
+            return ListensOn(new Uri(uri));
+        }
+        public ConfigureTheBus ListensOn(Uri uri)
+        {
+            _options.ListensOn = uri;
+            return this;
+        }
+
+        public ConfigureTheBus ReceivesCommandsOn(string uri)
+        {
+            return ReceivesCommandsOn(new Uri(uri));
+        }
+        public ConfigureTheBus ReceivesCommandsOn(Uri uri)
+        {
+            _options.CommandedOn = uri;
+            return this;
+        }
+
+        public ConfigureTheBus WithSharedSubscriptions(SubscriptionOptions subscriptionOptions)
+        {
+            _options.Subcriptions = subscriptionOptions;
+            return this;
+        }
+        public ConfigureTheBus Using(SerializationOptions serializer)
+        {
+            _options.Serialization = serializer;
+            return this;
+        }
+        public ConfigureTheBus AsACompetingConsumer()
+        {
+            _options.MarkAsCompetingConsumer();
+            return this;
+        }
+
+        public ConfigureTheBus CommunicatesOn<T>()
+        {
+            _options.RegisterTransport<T>();
+            return this;
+        }
+
+
+    }
     public class BusOptions
     {
         public Uri ListensOn { get; set; }
         public Uri CommandedOn { get; set; }
-        public bool IsACompetingConsumer { get; set; }
+        public bool IsACompetingConsumer { get; private set; }
+        public void MarkAsCompetingConsumer()
+        {
+            IsACompetingConsumer = true;
+        }
         public SerializationOptions Serialization { get; set; }
         public SubscriptionOptions Subcriptions { get; set; }
-        //transportation
+        public void RegisterTransport<T>()
+        {
+            EndpointResolver.AddTransport(typeof (T));
+        }
         public string Name { get; set; }
         public object ResolvesDependenciesFrom { get; set; }
     }
     public class SerializationOptions
     {
+        public Type Serializer { get; set; }
     }
     public class SubscriptionOptions
     {
-        public string Address;
-        public object TypeOfSubscriptionStore;
+        public string Address { get; set;}
+        public Type SubscriptionStore { get; set; }
     }
 }
