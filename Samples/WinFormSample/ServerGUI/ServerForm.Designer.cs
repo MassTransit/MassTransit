e9c0b091e03d40671c707643cef9c4233948ec3a
Chris Patterson <chris@phatboyg.com>
2008-09-08 18:38:11 +0000

    Message counts update on server GUI now
    Former-commit-id: 9e25c4cdfe5a46fa63af2ce27868f239e3315a3a

--- Samples/WinFormSample/ServerGUI/ServerForm.Designer.cs
+++ Samples/WinFormSample/ServerGUI/ServerForm.Designer.cs
@@ -28,129 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.questionsReceived = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.answersSent = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.serverTime = new System.Windows.Forms.NumericUpDown();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label32 = new System.Windows.Forms.Label();
-            this.answerQuestions = new System.Windows.Forms.CheckBox();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.serverTime)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // questionsReceived
-            // 
-            this.questionsReceived.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.questionsReceived.Location = new System.Drawing.Point(6, 72);
-            this.questionsReceived.Name = "questionsReceived";
-            this.questionsReceived.Size = new System.Drawing.Size(114, 15);
-            this.questionsReceived.TabIndex = 5;
-            this.questionsReceived.Text = "0";
-            this.questionsReceived.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Window;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Controls.Add(this.questionsReceived);
-            this.panel1.Controls.Add(this.answersSent);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.serverTime);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label32);
-            this.panel1.Controls.Add(this.answerQuestions);
-            this.panel1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
-            this.panel1.TabIndex = 1;
-            // 
-            // answersSent
-            // 
-            this.answersSent.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.answersSent.Location = new System.Drawing.Point(6, 55);
-            this.answersSent.Name = "answersSent";
-            this.answersSent.Size = new System.Drawing.Size(114, 15);
-            this.answersSent.TabIndex = 3;
-            this.answersSent.Text = "0";
-            this.answersSent.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Server Time (ms):";
-            // 
-            // serverTime
-            // 
-            this.serverTime.Increment = new decimal(new int[] {
+			this.components = new System.ComponentModel.Container();
+			this.questionsReceived = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.answersSent = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.serverTime = new System.Windows.Forms.NumericUpDown();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label32 = new System.Windows.Forms.Label();
+			this.answerQuestions = new System.Windows.Forms.CheckBox();
+			this.messageTimer = new System.Windows.Forms.Timer(this.components);
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.serverTime)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// questionsReceived
+			// 
+			this.questionsReceived.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.questionsReceived.Location = new System.Drawing.Point(6, 72);
+			this.questionsReceived.Name = "questionsReceived";
+			this.questionsReceived.Size = new System.Drawing.Size(114, 15);
+			this.questionsReceived.TabIndex = 5;
+			this.questionsReceived.Text = "0";
+			this.questionsReceived.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.Window;
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel1.Controls.Add(this.questionsReceived);
+			this.panel1.Controls.Add(this.answersSent);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.serverTime);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label32);
+			this.panel1.Controls.Add(this.answerQuestions);
+			this.panel1.ForeColor = System.Drawing.SystemColors.WindowText;
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(200, 100);
+			this.panel1.TabIndex = 1;
+			// 
+			// answersSent
+			// 
+			this.answersSent.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.answersSent.Location = new System.Drawing.Point(6, 55);
+			this.answersSent.Name = "answersSent";
+			this.answersSent.Size = new System.Drawing.Size(114, 15);
+			this.answersSent.TabIndex = 3;
+			this.answersSent.Text = "0";
+			this.answersSent.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(3, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(105, 15);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Server Time (ms):";
+			// 
+			// serverTime
+			// 
+			this.serverTime.Increment = new decimal(new int[] {
             5,
             0,
             0,
             0});
-            this.serverTime.Location = new System.Drawing.Point(109, 23);
-            this.serverTime.Name = "serverTime";
-            this.serverTime.Size = new System.Drawing.Size(49, 20);
-            this.serverTime.TabIndex = 2;
-            this.serverTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.serverTime.ValueChanged += new System.EventHandler(this.serverTime_ValueChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(126, 72);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 15);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Received";
-            // 
-            // label32
-            // 
-            this.label32.AutoSize = true;
-            this.label32.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label32.Location = new System.Drawing.Point(126, 55);
-            this.label32.Name = "label32";
-            this.label32.Size = new System.Drawing.Size(32, 15);
-            this.label32.TabIndex = 4;
-            this.label32.Text = "Sent";
-            // 
-            // answerQuestions
-            // 
-            this.answerQuestions.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.answerQuestions.Checked = true;
-            this.answerQuestions.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.answerQuestions.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.answerQuestions.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.answerQuestions.Location = new System.Drawing.Point(-2, -2);
-            this.answerQuestions.Margin = new System.Windows.Forms.Padding(0);
-            this.answerQuestions.Name = "answerQuestions";
-            this.answerQuestions.Padding = new System.Windows.Forms.Padding(6, 0, 0, 0);
-            this.answerQuestions.Size = new System.Drawing.Size(200, 22);
-            this.answerQuestions.TabIndex = 0;
-            this.answerQuestions.Text = "Answer Questions";
-            this.answerQuestions.UseVisualStyleBackColor = false;
-            this.answerQuestions.CheckedChanged += new System.EventHandler(this.answerQuestions_CheckedChanged);
-            // 
-            // ServerForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
-            this.Controls.Add(this.panel1);
-            this.Name = "ServerForm";
-            this.Text = "ServerForm";
-            this.Load += new System.EventHandler(this.ServerForm_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.serverTime)).EndInit();
-            this.ResumeLayout(false);
+			this.serverTime.Location = new System.Drawing.Point(109, 23);
+			this.serverTime.Name = "serverTime";
+			this.serverTime.Size = new System.Drawing.Size(49, 20);
+			this.serverTime.TabIndex = 2;
+			this.serverTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.serverTime.ValueChanged += new System.EventHandler(this.serverTime_ValueChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(126, 72);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(58, 15);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Received";
+			// 
+			// label32
+			// 
+			this.label32.AutoSize = true;
+			this.label32.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label32.Location = new System.Drawing.Point(126, 55);
+			this.label32.Name = "label32";
+			this.label32.Size = new System.Drawing.Size(32, 15);
+			this.label32.TabIndex = 4;
+			this.label32.Text = "Sent";
+			// 
+			// answerQuestions
+			// 
+			this.answerQuestions.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.answerQuestions.Checked = true;
+			this.answerQuestions.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.answerQuestions.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.answerQuestions.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.answerQuestions.Location = new System.Drawing.Point(-2, -2);
+			this.answerQuestions.Margin = new System.Windows.Forms.Padding(0);
+			this.answerQuestions.Name = "answerQuestions";
+			this.answerQuestions.Padding = new System.Windows.Forms.Padding(6, 0, 0, 0);
+			this.answerQuestions.Size = new System.Drawing.Size(200, 22);
+			this.answerQuestions.TabIndex = 0;
+			this.answerQuestions.Text = "Answer Questions";
+			this.answerQuestions.UseVisualStyleBackColor = false;
+			this.answerQuestions.CheckedChanged += new System.EventHandler(this.answerQuestions_CheckedChanged);
+			// 
+			// messageTimer
+			// 
+			this.messageTimer.Tick += new System.EventHandler(this.messageTimer_Tick);
+			// 
+			// ServerForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(292, 273);
+			this.Controls.Add(this.panel1);
+			this.Name = "ServerForm";
+			this.Text = "ServerForm";
+			this.Load += new System.EventHandler(this.ServerForm_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.serverTime)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
@@ -164,6 +170,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label32;
         private System.Windows.Forms.CheckBox answerQuestions;
+		private System.Windows.Forms.Timer messageTimer;
     }
 }
 
--- Samples/WinFormSample/ServerGUI/ServerForm.cs
+++ Samples/WinFormSample/ServerGUI/ServerForm.cs
@@ -12,136 +12,112 @@
 // specific language governing permissions and limitations under the License.
 namespace ServerGUI
 {
-    using System;
-    using System.ComponentModel;
-    using System.Threading;
-    using System.Windows.Forms;
-    using Castle.Windsor;
-    using log4net;
-    using MassTransit.ServiceBus;
-    using MassTransit.WindsorIntegration;
-    using Messages;
+	using System;
+	using System.ComponentModel;
+	using System.Windows.Forms;
+	using Castle.Windsor;
+	using log4net;
+	using MassTransit.ServiceBus;
+	using MassTransit.WindsorIntegration;
 
-    public partial class ServerForm : Form
-    {
-        private static readonly ILog _log = LogManager.GetLogger(typeof (ServerForm));
+	public partial class ServerForm : Form
+	{
+		private static readonly ILog _log = LogManager.GetLogger(typeof (ServerForm));
 
-        private IServiceBus _bus;
+		private IServiceBus _bus;
 
-        private IWindsorContainer _container;
+		private IWindsorContainer _container;
 
-        public ServerForm()
-        {
-            InitializeComponent();
-        }
+		public ServerForm()
+		{
+			InitializeComponent();
+		}
 
-        private void ServerForm_Load(object sender, EventArgs e)
-        {
-            StartService();
-        }
+		private void ServerForm_Load(object sender, EventArgs e)
+		{
+			StartService();
+		}
 
-        private void StartService()
-        {
-            StopService();
+		private void StartService()
+		{
+			StopService();
 
-            try
-            {
-                _container = new DefaultMassTransitContainer("Server.Castle.xml");
+			try
+			{
+				_container = new DefaultMassTransitContainer("Server.Castle.xml");
 
-                _bus = _container.Resolve<IServiceBus>("server");
+				_bus = _container.Resolve<IServiceBus>("server");
 
-                _bus.AddComponent<UserAgentSession>();
-                _bus.AddComponent<TheAnswerMan>();
-            }
-            catch (Exception ex)
-            {
-                _log.Error(ex);
-                MessageBox.Show("The server failed to start:\n\n" + ex.Message);
-            }
-        }
+				_bus.AddComponent<UserAgentSession>();
+				_bus.AddComponent<TheAnswerMan>();
 
-
-        private void StopService()
-        {
-            try
-            {
-                if (_bus != null)
-                {
-                    _bus.Dispose();
-                    _bus = null;
-                }
-            }
-            catch (Exception ex)
-            {
-                _log.Error(ex);
-            }
-
-            try
-            {
-                if (_container != null)
-                {
-                    _container.Dispose();
-                    _container = null;
-                }
-            }
-            catch (Exception ex)
-            {
-                _log.Error(ex);
-            }
-        }
-
-        protected override void OnClosing(CancelEventArgs e)
-        {
-            base.OnClosing(e);
-
-            StopService();
-
-            e.Cancel = false;
-        }
-
-        private void answerQuestions_CheckedChanged(object sender, EventArgs e)
-        {
-            TheAnswerMan.Enabled = answerQuestions.Checked;
-        }
-
-        private void serverTime_ValueChanged(object sender, EventArgs e)
-        {
-            TheAnswerMan.ServerTime = Convert.ToInt32(serverTime.Value);
-        }
-    }
-
-    public class TheAnswerMan :
-        Consumes<SubmitQuestion>.Selected
-    {
-        private static bool _enabled = true;
-        public static bool Enabled
-        {
-            get { return _enabled; }
-            set { _enabled = value; }
-        }
+				messageTimer.Start();
+			}
+			catch (Exception ex)
+			{
+				_log.Error(ex);
+				MessageBox.Show("The server failed to start:\n\n" + ex.Message);
+			}
+		}
 
 
-        public IServiceBus Bus { get; set; }
+		private void StopService()
+		{
+			messageTimer.Stop();
 
-        private static volatile int _serverTime;
-        public static int ServerTime
-        {
-            get { return _serverTime; }
-            set { _serverTime = value; }
-        }
+			try
+			{
+				if (_bus != null)
+				{
+					_bus.Dispose();
+					_bus = null;
+				}
+			}
+			catch (Exception ex)
+			{
+				_log.Error(ex);
+			}
 
-        public void Consume(SubmitQuestion message)
-        {
-            Thread.Sleep(_serverTime);
+			try
+			{
+				if (_container != null)
+				{
+					_container.Dispose();
+					_container = null;
+				}
+			}
+			catch (Exception ex)
+			{
+				_log.Error(ex);
+			}
+		}
 
-            QuestionAnswered answer = new QuestionAnswered(message.CorrelationId);
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			base.OnClosing(e);
 
-            Bus.Publish(answer);
-        }
+			StopService();
 
-        public bool Accept(SubmitQuestion message)
-        {
-            return _enabled;
-        }
-    }
+			e.Cancel = false;
+		}
+
+		private void answerQuestions_CheckedChanged(object sender, EventArgs e)
+		{
+			TheAnswerMan.Enabled = answerQuestions.Checked;
+		}
+
+		private void serverTime_ValueChanged(object sender, EventArgs e)
+		{
+			TheAnswerMan.ServerTime = Convert.ToInt32(serverTime.Value);
+		}
+
+		private void messageTimer_Tick(object sender, EventArgs e)
+		{
+			string text = string.Format("{0}", TheAnswerMan.MessageCount);
+			questionsReceived.Text = text;
+
+			text = string.Format("{0}", TheAnswerMan.MessagesSent);
+			answersSent.Text = text;
+		}
+	}
 }
