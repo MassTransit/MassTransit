70c4b131bc67e50eb87c95aa195040b7157d5134
Dru Sellers <dru@drusellers.com>
2008-09-09 03:13:08 +0000

    Updating tests
    Former-commit-id: 0042cb26325223f249c166d4a2cc8387899e3b6c

--- MassTransit.ServiceBus.Tests/Configuration/Config_spec.cs
+++ MassTransit.ServiceBus.Tests/Configuration/Config_spec.cs
@@ -2,6 +2,9 @@ namespace MassTransit.ServiceBus.Tests.Configuration
 {
     using System;
     using MassTransit.ServiceBus.Configuration;
+    using MassTransit.ServiceBus.Formatters;
+    using MassTransit.ServiceBus.Internal;
+    using MassTransit.ServiceBus.Subscriptions;
     using NUnit.Framework;
     using Transports;
 
@@ -13,27 +16,68 @@ namespace MassTransit.ServiceBus.Tests.Configuration
         public void Minimal_Setup()
         {
             Uri listenUri = new Uri("msmq://localhost/bill");
+
+            var busOptions = BusBuilder.WithName("funk_bus")
+                .ListensOn("msmq://localhost/bill")
+                .CommunicatesOn<LoopbackEndpoint>() //TODO: what should the default be?
+                .Validate();
+
+            Assert.AreEqual(listenUri, busOptions.ListensOn);
+            Assert.AreEqual(EndpointResolver.Null.Uri, busOptions.CommandedOn);
+            Assert.AreEqual(typeof(BinaryBodyFormatter), busOptions.Serialization.Serializer); //how to handle this?
+            Assert.AreEqual(false, busOptions.IsACompetingConsumer);
+            Assert.IsNull(busOptions.Subcriptions);
+            //transports
+        }
+
+        [Test]
+        public void As_a_competing_consumer()
+        {
+            var busOptions = BusBuilder.WithName("funk_bus")
+                .AsACompetingConsumer()
+                .Validate();
+
+            Assert.AreEqual(true, busOptions.IsACompetingConsumer);
+        }
+
+        [Test]
+        public void As_a_subscriber()
+        {
+            Uri listenUri = new Uri("msmq://localhost/bill");
             Uri commandUri = new Uri("activemq://localhost/bob");
             Uri subUri = new Uri("msmq://localhost/mt_pubsub");
 
             var busOptions = BusBuilder.WithName("funk_bus")
                 .ListensOn("msmq://localhost/bill")
-                .ReceivesCommandsOn("activemq://localhost/bob")
-                .WithSharedSubscriptions(Via.SubscriptionService("msmq://localhost/mt_pubsub"))
+                .WithSharedSubscriptions<LocalSubscriptionCache>("msmq://localhost/mt_pubsub")
                 //.WithSharedSubscriptions(Via.DistributedCache("tcp://192.168.0.1"))
                 .CommunicatesOn<LoopbackEndpoint>()
-                .AsACompetingConsumer()
-                .Using(Serializers.Binary)
+                .UsingForSerialization<JsonBodyFormatter>()
                 .Validate();
             //.ActivateHeartBeat();
 
             Assert.AreEqual(listenUri, busOptions.ListensOn);
-            Assert.AreEqual(commandUri, busOptions.CommandedOn);
             Assert.AreEqual(subUri, busOptions.Subcriptions.Address);
-            //Assert.AreEqual(null, busOptions.Subcriptions.SubscriptionStore);
-//            Assert.AreEqual(null, busOptions.Serialization.Serializer);
-//            Assert.AreEqual(null, busOptions.IsACompetingConsumer);
+            Assert.AreEqual(typeof(LocalSubscriptionCache), busOptions.Subcriptions.SubscriptionStore); //how to best handle this?
+            Assert.AreEqual(typeof(JsonBodyFormatter), busOptions.Serialization.Serializer); //how to handle this?
+            Assert.AreEqual(false, busOptions.IsACompetingConsumer);
             //transports
+        }
+
+        [Test]
+        public void As_a_commandee()
+        {
+            Uri listenUri = new Uri("msmq://localhost/bill");
+            Uri commandUri = new Uri("activemq://localhost/bob");
+
+            var busOptions = BusBuilder.WithName("funk_bus")
+                .ListensOn("msmq://localhost/bill")
+                .ReceivesCommandsOn(commandUri)
+                .CommunicatesOn<LoopbackEndpoint>() //TODO: what should the default be?
+                .Validate();
+
+            Assert.AreEqual(listenUri, busOptions.ListensOn);
+            Assert.AreEqual(commandUri, busOptions.CommandedOn);
         }
     }
 }
