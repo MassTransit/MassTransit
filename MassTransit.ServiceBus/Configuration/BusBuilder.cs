526a8e7c53a15d58db174b188053d869057cd168
Dru Sellers <dru@drusellers.com>
2008-09-10 01:10:55 +0000

    nice solid step towards bus configuration

--- MassTransit.ServiceBus/Configuration/BusBuilder.cs
+++ MassTransit.ServiceBus/Configuration/BusBuilder.cs
@@ -2,9 +2,16 @@ namespace MassTransit.ServiceBus.Configuration
 {
     public static class BusBuilder
     {
+        private static IObjectBuilder _objectBuilder;
+
+        public static void SetObjectBuilder(IObjectBuilder builder)
+        {
+            _objectBuilder = builder;
+        }
+
         public static ConfigureTheBus WithName(string name)
         {
-            return new ConfigureTheBus(new BusOptions{Name = name});
+            return new ConfigureTheBus(new BusOptions(_objectBuilder){Name = name});
         }
     }
 }
