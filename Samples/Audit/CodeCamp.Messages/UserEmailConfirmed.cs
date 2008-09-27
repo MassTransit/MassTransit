0f4d7f1bed57b2fe978bafd9276c1d3a710017d6
Dru Sellers <dru@drusellers.com>
2008-09-27 16:15:37 +0000

    added the message to publish when user confirms email

--- Samples/Audit/CodeCamp.Messages/UserEmailConfirmed.cs
+++ Samples/Audit/CodeCamp.Messages/UserEmailConfirmed.cs
@@ -5,6 +5,16 @@ namespace CodeCamp.Messages
     [Serializable]
     public class UserEmailConfirmed
     {
-        
+        private readonly Guid _registarionId;
+
+        public UserEmailConfirmed(Guid registarionId)
+        {
+            _registarionId = registarionId;
+        }
+
+        public Guid RegistarionId
+        {
+            get { return _registarionId; }
+        }
     }
 }
