2c2768c84f2d785088e7495d7319491f09322399
Dru Sellers <dru@drusellers.com>
2008-09-27 14:41:04 +0000

    web page updates
    Former-commit-id: 7693512b86a070551fac5326fd4e18d2851fd689

--- Samples/Audit/CodeCamp.Web/Registration.aspx.designer.cs
+++ Samples/Audit/CodeCamp.Web/Registration.aspx.designer.cs
@@ -12,5 +12,32 @@ namespace CodeCamp.Web {
     
     
     public partial class Registration {
+        
+        /// <summary>
+        /// inputName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputName;
+        
+        /// <summary>
+        /// inputUsername control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputUsername;
+        
+        /// <summary>
+        /// inputPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputPassword;
     }
 }
