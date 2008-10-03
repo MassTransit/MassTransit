966cc356c6a99bd90da45c666ce3eaba323b2c38
Dru Sellers <dru@drusellers.com>
2008-10-03 21:15:28 +0000

    fix build
    Former-commit-id: afb50329ba139b2e5f5e0efbb6a3f85c5714908d

--- Samples/Audit/CodeCamp.Domain/RegisterUserOrchestration.cs
+++ Samples/Audit/CodeCamp.Domain/RegisterUserOrchestration.cs
@@ -1,17 +1,14 @@
-namespace CodeCamp.Service
-{
-    using System;
-    using System.Net.Mail;
-    using Domain;
-    using Magnum.Common.Repository;
-    using MassTransit.Saga;
-    using MassTransit.ServiceBus;
-    using MassTransit.ServiceBus.Timeout.Messages;
-    using Magnum.Common.DateTimeExtensions;
-    using MassTransit.ServiceBus.Util;
-    using Messages;
-    using PostalService.Messages;
+using System;
+using CodeCamp.Messages;
+using Magnum.Common.DateTimeExtensions;
+using MassTransit.Saga;
+using MassTransit.ServiceBus;
+using MassTransit.ServiceBus.Timeout.Messages;
+using MassTransit.ServiceBus.Util;
+using PostalService.Messages;
 
+namespace CodeCamp.Domain
+{
     public class RegisterUserOrchestration :
         InitiatedBy<RegisterUser>,
         Orchestrates<UserVerificationEmailSent>,
@@ -19,42 +16,34 @@ namespace CodeCamp.Service
         Orchestrates<EmailSent>,
         ISaga
     {
+        private readonly Guid _correlationId = CombGuid.NewCombGuid();
         private IServiceBus _bus;
-        private Guid _correlationId = CombGuid.NewCombGuid();
-        private IObjectBuilder _builder;
-        private User _user;
         private DateTime _lastEmailSent;
+        private User _user;
 
-        public RegisterUserOrchestration()
+        public User User
         {
+            get { return _user; }
         }
 
         //Starts things off
+
+        #region InitiatedBy<RegisterUser> Members
+
         public void Consume(RegisterUser message)
         {
             _user = new User(message.Name, message.Username, message.Password);
 
-            var body = string.Format("Please verify email http://localhost/ConfirmEmail.aspx?registrationId={0}", this._correlationId);
-            _bus.Publish(new SendEmail(_correlationId, "bob","dru","verify email", body));
+            string body = string.Format("Please verify email http://localhost/ConfirmEmail.aspx?registrationId={0}",
+                                        _correlationId);
+            _bus.Publish(new SendEmail(_correlationId, "bob", "dru", "verify email", body));
 
-            _bus.Publish(new UserVerificationEmailSent(this._correlationId));
+            _bus.Publish(new UserVerificationEmailSent(_correlationId));
         }
 
-        public void Consume(UserVerificationEmailSent message)
-        {
-            _user.SetPending();
+        #endregion
 
-            _bus.Publish(new ScheduleTimeout(message.CorrelationId, 24.Hours().FromNow()));
-        }
-
-        public void Consume(UserVerifiedEmail message)
-        {
-            _user.EmailHasBeenConfirmed();
-            var body = string.Format("Thank you. You are now registered");
-
-            // use a new guid because we don't want any more messages to this saga about e-mails
-            _bus.Publish(new SendEmail(Guid.Empty, "bob", "dru", "Register Successful", body));
-        }
+        #region ISaga Members
 
         public Guid CorrelationId
         {
@@ -67,21 +56,41 @@ namespace CodeCamp.Service
             set { _bus = value; }
         }
 
-        public IObjectBuilder Builder
-        {
-            get { return _builder; }
-            set { _builder = value; }
-        }
+        public IObjectBuilder Builder { get; set; }
 
-        public User User
-        {
-            get { return _user; }
-        }
+        #endregion
+
+        #region Orchestrates<EmailSent> Members
 
         public void Consume(EmailSent message)
         {
             _lastEmailSent = message.SentAt;
-            
         }
+
+        #endregion
+
+        #region Orchestrates<UserVerificationEmailSent> Members
+
+        public void Consume(UserVerificationEmailSent message)
+        {
+            _user.SetPending();
+
+            _bus.Publish(new ScheduleTimeout(message.CorrelationId, 24.Hours().FromNow()));
+        }
+
+        #endregion
+
+        #region Orchestrates<UserVerifiedEmail> Members
+
+        public void Consume(UserVerifiedEmail message)
+        {
+            _user.EmailHasBeenConfirmed();
+            string body = string.Format("Thank you. You are now registered");
+
+            // use a new guid because we don't want any more messages to this saga about e-mails
+            _bus.Publish(new SendEmail(Guid.Empty, "bob", "dru", "Register Successful", body));
+        }
+
+        #endregion
     }
 }
