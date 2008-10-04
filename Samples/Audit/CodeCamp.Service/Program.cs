09eb9dbbc839368ce5d85ae5ad4c9d213513aa32
Dru Sellers <dru@drusellers.com>
2008-10-04 23:30:57 +0000

    i think the host configuration is done.

--- Samples/Audit/CodeCamp.Service/Program.cs
+++ Samples/Audit/CodeCamp.Service/Program.cs
@@ -1,48 +1,13 @@
 namespace CodeCamp.Service
 {
-	using System;
-	using Castle.MicroKernel;
-	using MassTransit.ServiceBus;
-	using MassTransit.ServiceBus.MSMQ;
-	using MassTransit.ServiceBus.Subscriptions;
-	using MassTransit.WindsorIntegration;
-	using Messages;
+    using MassTransit.Host;
 
-	internal class Program :
-		Consumes<UserPasswordSuccess>.All,
-		Consumes<UserPasswordFailure>.All
-	{
-		public void Consume(UserPasswordFailure message)
-		{
-			Console.WriteLine("Audit: Failed user password check for user {0} at {1}", message.Username, message.TimeStamp);
-		}
-
-		public void Consume(UserPasswordSuccess message)
-		{
-			Console.WriteLine("Audit: User password succeeded password check for user {0} at {1}", message.Username, message.TimeStamp);
-		}
-
-		private static void Main()
-		{
-			IEndpoint endpoint = new MsmqEndpoint("msmq://localhost/mt_server");
-			ISubscriptionCache cache = new DistributedSubscriptionCache();
-
-		    IObjectBuilder obj = new WindsorObjectBuilder(new DefaultKernel());
-
-			using (IServiceBus serviceBus = new ServiceBus(endpoint, null, cache))
-			{
-				serviceBus.AddComponent<Program>();
-
-				Console.WriteLine("Service running...");
-
-				Console.ReadKey();
-
-				Console.WriteLine("Service exiting...");
-
-				serviceBus.RemoveComponent<Program>();
-			}
-
-			Console.WriteLine("End of line");
-		}
-	}
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var cfg = new AuditServiceConfiguration("audit-castle.config");
+            Runner.Run(cfg, args);
+        }
+    }
 }
