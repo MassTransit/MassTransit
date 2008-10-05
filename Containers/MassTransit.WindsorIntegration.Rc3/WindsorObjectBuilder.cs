a3142ac6fdc354247dfc5f6897084e481a1bf910
Dru Sellers <dru@drusellers.com>
2008-10-05 16:58:59 +0000

    added Msft.IServiceLocator
    Former-commit-id: ff6b8a8e8aa54fc5c138fecd046a63002472b81c

--- Containers/MassTransit.WindsorIntegration.Rc3/WindsorObjectBuilder.cs
+++ Containers/MassTransit.WindsorIntegration.Rc3/WindsorObjectBuilder.cs
@@ -2,6 +2,7 @@ namespace MassTransit.WindsorIntegration
 {
     using System;
     using System.Collections;
+    using System.Collections.Generic;
     using Castle.Core;
     using Castle.MicroKernel;
     using ServiceBus;
@@ -48,6 +49,52 @@ namespace MassTransit.WindsorIntegration
             {
                 _container.AddComponent(typeof(T).Name, typeof(T), LifestyleType.Transient);
             }
+        }
+        public object GetService(Type serviceType)
+        {
+            return _container.Resolve(serviceType);
+        }
+
+        public object GetInstance(Type serviceType)
+        {
+            return _container.Resolve(serviceType);
+        }
+
+        public object GetInstance(Type serviceType, string key)
+        {
+            return _container.Resolve(key, serviceType);
+        }
+
+        public IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            var x = _container.GetHandlers(serviceType);
+            ArrayList result = new ArrayList();
+            foreach (var o in x)
+            {
+                result.Add(o.Resolve(CreationContext.Empty));
+            }
+            return result.ToArray();
+        }
+
+        public TService GetInstance<TService>()
+        {
+            return _container.Resolve<TService>();
+        }
+
+        public TService GetInstance<TService>(string key)
+        {
+            return (TService)_container.Resolve(key, typeof(TService));
+        }
+
+        public IEnumerable<TService> GetAllInstances<TService>()
+        {
+            var x = _container.GetHandlers(typeof(TService));
+            var result = new List<TService>();
+            foreach (var o in x)
+            {
+                result.Add((TService)o.Resolve(CreationContext.Empty));
+            }
+            return result.ToArray();
         }
     }
 }
--- Containers/MassTransit.WindsorIntegration/WindsorObjectBuilder.cs
+++ Containers/MassTransit.WindsorIntegration/WindsorObjectBuilder.cs
@@ -14,6 +14,7 @@ namespace MassTransit.WindsorIntegration
 {
     using System;
     using System.Collections;
+    using System.Collections.Generic;
     using Castle.Core;
     using Castle.MicroKernel;
     using ServiceBus;
@@ -60,6 +61,47 @@ namespace MassTransit.WindsorIntegration
             {
                 _container.AddComponent(typeof (T).Name, typeof (T), LifestyleType.Transient);
             }
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _container.Resolve(serviceType);
+        }
+
+        public object GetInstance(Type serviceType)
+        {
+            return _container.Resolve(serviceType);
+        }
+
+        public object GetInstance(Type serviceType, string key)
+        {
+            return _container.Resolve(key, serviceType);
+        }
+
+        public IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            var x = _container.ResolveAll(serviceType, new {});
+            ArrayList result = new ArrayList();
+            foreach (var o in x)
+            {
+                result.Add(o);
+            }
+            return result.ToArray();
+        }
+
+        public TService GetInstance<TService>()
+        {
+            return _container.Resolve<TService>();
+        }
+
+        public TService GetInstance<TService>(string key)
+        {
+            return _container.Resolve<TService>(key);
+        }
+
+        public IEnumerable<TService> GetAllInstances<TService>()
+        {
+            return _container.ResolveAll<TService>();
         }
     }
 }
