a6404785f931815b9f723034cdbc4ca97231ac9d
Dru Sellers <dru@drusellers.com>
2008-10-05 12:42:48 +0000

    moved domain context and removed Castle dependency.
    Former-commit-id: 8b1faadb84e6bb4c6e2c139b68ced431a9c0e62f

--- Samples/Audit/CodeCamp.Domain/DomainContext.cs
+++ Samples/Audit/CodeCamp.Domain/DomainContext.cs
@@ -1,45 +1,40 @@
-using CodeCamp.Domain;
-
-namespace CodeCamp.Core
+namespace CodeCamp.Domain
 {
-    using Castle.MicroKernel;
+    using Core;
     using MassTransit.ServiceBus;
     using MassTransit.ServiceBus.Configuration;
     using MassTransit.ServiceBus.MSMQ;
-	using MassTransit.ServiceBus.Subscriptions;
-    using MassTransit.WindsorIntegration;
+    using MassTransit.ServiceBus.Subscriptions;
 
     public static class DomainContext
-	{
-		private static readonly IEndpoint _endpoint;
-		private static readonly IServiceBus _serviceBus;
-		private static readonly IRepository<User> _userRepository;
+    {
+        private static readonly IServiceBus _serviceBus;
+        private static readonly IRepository<User> _userRepository;
 
-		static DomainContext()
-		{
-			//_userRepository = new Repository<User>(new [] {new User("joe", "password"), new User("david", "password"),});
+        static DomainContext()
+        {
+            //_userRepository = new Repository<User>(new [] {new User("joe", "password"), new User("david", "password"),});
 
-		    BusBuilder.SetObjectBuilder(new WindsorObjectBuilder(new DefaultKernel()));
             _serviceBus = BusBuilder.WithName("bill")
-		        .ListensOn("msmq://localhost/mt_client")
+                .ListensOn("msmq://localhost/mt_client")
                 .SharesSubscriptionsVia<DistributedSubscriptionCache>("tcpip://127.0.0.1:11211")
-		        .CommunicatesOver<MsmqEndpoint>()
-		        .Validate()
-		        .Build();
-		}
+                .CommunicatesOver<MsmqEndpoint>()
+                .Validate()
+                .Build();
+        }
 
-		public static void Publish<T>(T message) where T : class
-		{
-			_serviceBus.Publish(message);
-		}
+        public static void Publish<T>(T message) where T : class
+        {
+            _serviceBus.Publish(message);
+        }
 
-		public static IRepository<User> UserRepository
-		{
-			get { return _userRepository; }
-		}
+        public static IRepository<User> UserRepository
+        {
+            get { return _userRepository; }
+        }
 
-		public static void Initialize()
-		{
-		}
-	}
+        public static void Initialize()
+        {
+        }
+    }
 }
