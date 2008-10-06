fe076847cb2c268f2d66e9c0d472d9951043a7c6
Dru Sellers <dru@drusellers.com>
2008-10-06 18:16:42 +0000

    added lifecycle events. in memory storage right now
    Former-commit-id: c74b807fdcf53a225c6784ce95d0e8fef5e6f08d

--- Deployment/TimeoutServiceHost/Program.cs
+++ Deployment/TimeoutServiceHost/Program.cs
@@ -1,9 +1,23 @@
 ﻿namespace TimeoutServiceHost
 {
+    using System.IO;
+    using log4net;
+    using log4net.Config;
+    using MassTransit.Host;
+
     class Program
     {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Program));
+
         static void Main(string[] args)
         {
+            XmlConfigurator.ConfigureAndWatch(new FileInfo("log4net.xml"));
+            _log.Info("Timeout Service Loading");
+
+            var env = new TimeoutServiceConfiguration("timeout.castle.xml");
+
+
+            Runner.Run(env, args);
         }
     }
 }
