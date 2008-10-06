ee84ecac6f594269d60a0434c69acd73c277837c
Dru Sellers <dru@drusellers.com>
2008-10-06 18:48:10 +0000

    deferred message service classes added

--- Deployment/DeferredMessageServiceHost/Program.cs
+++ Deployment/DeferredMessageServiceHost/Program.cs
@@ -1,9 +1,22 @@
 ﻿namespace DeferredMessageServiceHost
 {
+    using System.IO;
+    using log4net;
+    using log4net.Config;
+    using MassTransit.Host;
+
     class Program
     {
+        private static readonly ILog _log = LogManager.GetLogger(typeof (Program));
         static void Main(string[] args)
         {
+            XmlConfigurator.ConfigureAndWatch(new FileInfo("log4net.xml"));
+            _log.Info("Deferred Message Service Loading");
+
+            var env = new DeferredMessageConfiguration("deferred.castle.xml");
+
+
+            Runner.Run(env, args);
         }
     }
 }
