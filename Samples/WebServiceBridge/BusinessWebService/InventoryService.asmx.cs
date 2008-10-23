5b55675926c30abc87d77da584e09e12f0bc0f01
Chris Patterson <chris@phatboyg.com>
2008-10-23 03:20:48 +0000

    Sample now works, working on script and story for screencast

--- Samples/WebServiceBridge/BusinessWebService/InventoryService.asmx.cs
+++ Samples/WebServiceBridge/BusinessWebService/InventoryService.asmx.cs
@@ -1,9 +1,15 @@
 ﻿namespace BusinessWebService
 {
+    using System;
     using System.ComponentModel;
+    using System.Net;
     using System.Web.Services;
     using Inventory.Messages;
     using Magnum.Common;
+    using Magnum.Common.DateTimeExtensions;
+    using MassTransit.ServiceBus;
+    using MassTransit.ServiceBus.Services.Timeout.Messages;
+    using MassTransit.ServiceBus.Util;
 
     /// <summary>
     /// Summary description for InventoryService
@@ -11,9 +17,18 @@
     [WebService(Namespace = "http://masstransit.googlecode.com/Samples/BusinessWebService")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [ToolboxItem(false)]
-    public class InventoryService : WebService
+    public class InventoryService : 
+        WebService,
+        Consumes<PartInventoryLevelStatus>.For<string>,
+        Consumes<TimeoutExpired>.For<Guid>
     {
         private static readonly Mapper<PartInventoryLevelStatus, InventoryLevel> _mapper;
+
+        private string _partNumber;
+        private PartInventoryLevelStatus _status;
+        private ServiceBusRequest<InventoryService> _request;
+        private IServiceBus _bus;
+        private Guid _requestId;
 
         static InventoryService()
         {
@@ -23,13 +38,59 @@
             _mapper.From(x => x.OnOrder).To(y => y.QuantityOnOrder);
         }
 
-        [WebMethod]
-        public InventoryLevel CheckInventory(string partNumber)
+        public InventoryService()
         {
-            PartInventoryLevelStatus status = new PartInventoryLevelStatus(partNumber, -1, -1);
+            _bus = IoC.Container.Resolve<IServiceBus>("client");
+        }
 
+        [WebMethod]
+        public IAsyncResult BeginCheckInventory(string partNumber, AsyncCallback callback, object state)
+        {
+            _partNumber = partNumber;
+            _requestId = CombGuid.NewCombGuid();
 
-            return _mapper.Transform(status);
+            _request = _bus.Request().From(this).WithCallback(callback, state);
+
+            QueryInventoryLevel queryInventoryLevel = new QueryInventoryLevel(_requestId, partNumber);
+            _request.Send(queryInventoryLevel);
+
+            ScheduleTimeout scheduleTimeout = new ScheduleTimeout(_requestId, 30.Seconds().FromNow());
+            _bus.Publish(scheduleTimeout);
+
+            return _request;
+        }
+
+        [WebMethod]
+        public InventoryLevel EndCheckInventory(IAsyncResult asyncResult)
+        {
+            if (_status == null)
+                throw new ApplicationException("The service did not respond quickly enough.");
+
+            return _mapper.Transform(_status);
+        }
+
+        public void Consume(PartInventoryLevelStatus message)
+        {
+            _status = message;
+
+            _bus.Publish(new CancelTimeout(_requestId));
+
+            _request.Complete();
+        }
+
+        string CorrelatedBy<string>.CorrelationId
+        {
+            get { return _partNumber; }
+        }
+
+        public void Consume(TimeoutExpired message)
+        {
+            _request.Complete();
+        }
+
+        Guid CorrelatedBy<Guid>.CorrelationId
+        {
+            get { return _requestId; }
         }
     }
 
--- Samples/WebServiceBridge/BusinessWebService/Properties/AssemblyInfo.cs
+++ Samples/WebServiceBridge/BusinessWebService/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using log4net.Config;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -14,14 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -33,3 +24,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
