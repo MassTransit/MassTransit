7f13fd5bcdc50effa21fc3da10fd447144f507a5
Dru Sellers <dru@drusellers.com>
2008-10-24 00:57:16 +0000

    structure map support improvements
    Former-commit-id: 50922a46936ce26587e20fdf26cd134470602f96

---
+++ Containers/MassTransit.StructureMapIntegration/MassTransitRegistry.cs
@@ -0,0 +1,48 @@
+﻿namespace MassTransit.StructureMapIntegration
+{
+    using MassTransit.ServiceBus.Internal;
+    using MassTransit.ServiceBus.Subscriptions;
+    using ServiceBus;
+    using ServiceBus.Services.HealthMonitoring;
+    using StructureMap.Configuration.DSL;
+
+    public class MassTransitRegistry : Registry
+    {
+        protected override void configure()
+        {
+            ForRequestedType<IObjectBuilder>().AddConcreteType<StructureMapObjectBuilder>().AsSingletons();
+            ForRequestedType<ISubscriptionCache>().AddConcreteType<LocalSubscriptionCache>().AsSingletons();
+            ForRequestedType<IEndpointResolver>().AddConcreteType<EndpointResolver>().AsSingletons();
+
+            CreateProfile("distributed")
+                .For<ISubscriptionCache>().UseConcreteType<DistributedSubscriptionCache>();
+
+            CreateProfile("local")
+                .For<ISubscriptionCache>().UseConcreteType<LocalSubscriptionCache>();
+
+
+            ForRequestedType<IServiceBus>().AddInstances(o =>
+                                                             {
+                                                                 o.OfConcreteType<ServiceBus>().WithName("data-bus")
+                                                                     .WithCtorArg("endpointListenOn").EqualTo("msmq://localhost/address")
+                                                                     .SetProperty(x=>x.MinThreadCount = 1)
+                                                                     .SetProperty(x=>x.MaxThreadCount = 10);
+
+                                                                 o.OfConcreteType<ServiceBus>().WithName("control-bus")
+                                                                     .WithCtorArg("endpointToListenOn").EqualTo("msmq://localhost/address_control")
+                                                                     .SetProperty(x=>x.MinThreadCount = 1)
+                                                                     .SetProperty(x=>x.MaxThreadCount = 10);
+                                                             });
+
+            ForConcreteType<HealthClient>().Configure.WithName("health_client")
+                .CtorDependency<IServiceBus>().IsTheDefault().WithName("data-bus")
+                .WithCtorArg("heartbeatInterval").EqualTo(3)
+                .OnCreation(o=>o.Start());
+
+            ForConcreteType<SubscriptionClient>().Configure
+                .CtorDependency<IServiceBus>().IsTheDefault().WithName("data-bus")
+                .WithCtorArg("subscriptionServiceEndpoint").EqualTo("msmq://localhost/mt_pubsub")
+                .OnCreation(o => o.Start());
+        }
+    }
+}
