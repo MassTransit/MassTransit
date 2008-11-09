46b566c91dec24ed1a7e84f4be1f6540410359ab
Dru Sellers <dru@drusellers.com>
2008-11-09 16:32:38 +0000

    Adding Unity support. Its missing an important feature, but I am working to resolve that. -d
    Former-commit-id: 33ccd7a7f7fca1e7e480e2176fd0abfa038ff9ad

---
+++ Containers/MassTransit.UnityIntegration/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MassTransit.UnityIntegration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("MassTransit.UnityIntegration")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d3ee6402-d155-46d1-a6b4-5011b0cdcd59")]
