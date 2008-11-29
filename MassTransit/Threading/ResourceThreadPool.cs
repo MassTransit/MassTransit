db0f7632a329b83beb437a5f66f0a45045e59e9a
Chris Patterson <chris@phatboyg.com>
2008-11-29 22:46:55 +0000

    Cleanup on thread pool

--- MassTransit/Threading/ResourceThreadPool.cs
+++ MassTransit/Threading/ResourceThreadPool.cs
@@ -12,105 +12,102 @@
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.Threading
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Threading;
-    using System.Transactions;
-    using log4net;
-    using Util;
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using System.Threading;
+	using System.Transactions;
+	using log4net;
+	using Util;
 
-    public delegate T Function<R, T>(R resource);
+	public class ResourceThreadPool<TResource, TElement> :
+		IDisposable
+	{
+		private static readonly ILog _log = LogManager.GetLogger(typeof (ResourceThreadPool<TResource, TElement>));
 
-    public class ResourceThreadPool<TResource, TElement> :
-        IDisposable
-    {
-        private static readonly ILog _log = LogManager.GetLogger(typeof (ResourceThreadPool<TResource, TElement>));
+		private readonly Action<TElement> _action;
+		private readonly Func<TResource, TElement> _function;
+		private readonly TResource _resource;
+		private readonly ManualResetEvent _shutdown = new ManualResetEvent(false);
+		private readonly List<Thread> _threads = new List<Thread>();
+		private bool _disposed;
+		private int _maxThreads;
+		private int _minThreads;
+		private Semaphore _resourceGovernor;
+		private int _resourceLimit;
 
-        private readonly Action<TElement> _action;
-        private readonly Function<TResource, TElement> _function;
-        private readonly TResource _resource;
-        private readonly Semaphore _resourceGovernor;
-        private readonly ManualResetEvent _shutdown = new ManualResetEvent(false);
-        private readonly List<Thread> _threads = new List<Thread>();
-        private int _maxThreads;
-        private int _minThreads;
-    	private bool _disposed;
-    	private int _resourceLimit;
+		public ResourceThreadPool(TResource resource,
+		                          Func<TResource, TElement> function,
+		                          Action<TElement> action,
+		                          int resourceLimit,
+		                          int minThreads,
+		                          int maxThreads)
+		{
+			Check.Ensure(resourceLimit > 0, "The resource limit must be greater than zero");
+			Check.Ensure(minThreads > 0, "The minimum thread count must be greater than zero");
+			Check.Ensure(maxThreads >= minThreads, string.Format("The maximum thread count (currently={0}) must be at least equal to the minimum thread count (currently={1})", maxThreads, minThreads));
+			Check.Parameter(action).WithMessage("The handler must not be null").IsNotNull();
 
-    	public ResourceThreadPool(TResource resource, Function<TResource, TElement> function, Action<TElement> action)
-            : this(resource, function, action, 1, 1, 10)
-        {
-        }
+			_resource = resource;
+			_function = function;
+			_action = action;
+			_minThreads = minThreads;
+			_maxThreads = maxThreads;
+			_resourceLimit = resourceLimit;
+		}
 
-        public ResourceThreadPool(TResource resource, Function<TResource, TElement> function, Action<TElement> action, int resourceLimit)
-            : this(resource, function, action, resourceLimit, 1, 10)
-        {
-        }
+		public int MaxThreads
+		{
+			[DebuggerStepThrough]
+			get { return _maxThreads; }
+			set
+			{
+				Check.Ensure(value >= _minThreads, "The maximum thread count must be at least equal to the minimum thread count");
 
-        public ResourceThreadPool(TResource resource, Function<TResource, TElement> function, Action<TElement> action, int resourceLimit, int minThreads, int maxThreads)
-        {
-            Check.Ensure(resourceLimit > 0, "The resource limit must be greater than zero");
-            Check.Ensure(minThreads > 0, "The minimum thread count must be greater than zero");
-            Check.Ensure(maxThreads >= minThreads, string.Format("The maximum thread count (currently={0}) must be at least equal to the minimum thread count (currently={1})", maxThreads, minThreads));
-            Check.Parameter(action).WithMessage("The handler must not be null").IsNotNull();
+				_maxThreads = value;
+			}
+		}
 
-            _resource = resource;
-            _function = function;
-            _action = action;
-            _resourceGovernor = new Semaphore(resourceLimit, resourceLimit);
-            _minThreads = minThreads;
-            _maxThreads = maxThreads;
-        	_resourceLimit = resourceLimit;
-        }
+		public int MinThreads
+		{
+			[DebuggerStepThrough]
+			get { return _minThreads; }
+			set
+			{
+				Check.Ensure(value > 0, "The minimum thread count must be greater than zero");
+				Check.Ensure(_maxThreads >= value, "The maximum thread count must be at least equal to the minimum thread count");
 
-        public int MaxThreads
-        {
-            [DebuggerStepThrough]
-            get { return _maxThreads; }
-            set
-            {
-                Check.Ensure(value >= _minThreads, "The maximum thread count must be at least equal to the minimum thread count");
+				_minThreads = value;
+			}
+		}
 
-                _maxThreads = value;
-            }
-        }
+		public int CurrentThreadCount
+		{
+			[DebuggerStepThrough]
+			get { lock (_threads) return _threads.Count; }
+		}
 
-        public int MinThreads
-        {
-            [DebuggerStepThrough]
-            get { return _minThreads; }
-            set
-            {
-                Check.Ensure(value > 0, "The minimum thread count must be greater than zero");
-                Check.Ensure(_maxThreads >= value, "The maximum thread count must be at least equal to the minimum thread count");
-
-                _minThreads = value;
-            }
-        }
-
-        public int CurrentThreadCount
-        {
-            [DebuggerStepThrough]
-            get { lock (_threads) return _threads.Count; }
-        }
-
-    	public int ResourceLimit
-    	{
+		public int ResourceLimit
+		{
 			[DebuggerStepThrough]
 			get { return _resourceLimit; }
-    		set
-    		{
-    			Check.Ensure(value > 0, "The resource limit must be greather than zero");
+			set
+			{
+				Check.Ensure(value > 0, "The resource limit must be greather than zero");
 				Check.Ensure(_maxThreads >= value, "The resource limit must be less than or equal to the number of threads");
-    			_resourceLimit = value;
-    		}
-    	}
+				_resourceLimit = value;
+			}
+		}
 
-    	public void Dispose()
+		public void Dispose()
 		{
 			Dispose(true);
 			GC.SuppressFinalize(this);
+		}
+
+		~ResourceThreadPool()
+		{
+			Dispose(false);
 		}
 
 		protected virtual void Dispose(bool disposing)
@@ -135,111 +132,116 @@ namespace MassTransit.Threading
 			_disposed = true;
 		}
 
-        private void AdjustQueueCount()
-        {
-            if (CurrentThreadCount < MaxThreads)
-            {
-                bool available = _resourceGovernor.WaitOne(0, false);
-                try
-                {
-                    if (available)
-                        AddThread();
-                }
-                finally
-                {
-                    if (available)
-                        _resourceGovernor.Release(1);
-                }
-            }
+		private void AdjustQueueCount()
+		{
+			if (CurrentThreadCount < MaxThreads)
+			{
+				bool available = _resourceGovernor.WaitOne(0, false);
+				try
+				{
+					if (available)
+						AddThread();
+				}
+				finally
+				{
+					if (available)
+						_resourceGovernor.Release(1);
+				}
+			}
 
-            while (CurrentThreadCount < MinThreads)
-                AddThread();
-        }
+			while (CurrentThreadCount < MinThreads)
+				AddThread();
+		}
 
-        private void AddThread()
-        {
-            Thread thread = new Thread(RunThread);
-            thread.SetApartmentState(ApartmentState.MTA);
+		private void AddThread()
+		{
+			Thread thread = new Thread(RunThread);
+			thread.SetApartmentState(ApartmentState.MTA);
 
-            lock (_threads)
-                _threads.Add(thread);
+			lock (_threads)
+				_threads.Add(thread);
 
-            thread.Start();
+			thread.Start();
 
-            if (_log.IsDebugEnabled)
-                _log.DebugFormat("Created Thread {0}, Total Threads = {1}", thread.ManagedThreadId, CurrentThreadCount);
-        }
+			if (_log.IsDebugEnabled)
+				_log.DebugFormat("Created Thread {0}, Total Threads = {1}", thread.ManagedThreadId, CurrentThreadCount);
+		}
 
-        private void RunThread()
-        {
-            int threadId = Thread.CurrentThread.ManagedThreadId;
+		private void RunThread()
+		{
+			int threadId = Thread.CurrentThread.ManagedThreadId;
 
-            if (_log.IsDebugEnabled)
-                _log.DebugFormat("Starting Thread {0}", threadId);
+			if (_log.IsDebugEnabled)
+				_log.DebugFormat("Starting Thread {0}", threadId);
 
-            WaitHandle[] handles = new WaitHandle[] {_shutdown, _resourceGovernor};
+			WaitHandle[] handles = new WaitHandle[] {_shutdown, _resourceGovernor};
 
-            int result;
-            while ((result = WaitHandle.WaitAny(handles, TimeSpan.FromSeconds(5), true)) != 0)
-            {
-            	if (result == WaitHandle.WaitTimeout)
-            	{
-            		lock (_threads)
-            		{
-            			if (CurrentThreadCount > MinThreads)
-            			{
-            				_threads.Remove(Thread.CurrentThread);
-            				break;
-            			}
-            		}
+			int result;
+			while ((result = WaitHandle.WaitAny(handles, TimeSpan.FromSeconds(5), true)) != 0)
+			{
+				if (result == WaitHandle.WaitTimeout)
+				{
+					lock (_threads)
+					{
+						if (CurrentThreadCount > MinThreads)
+						{
+							_threads.Remove(Thread.CurrentThread);
+							break;
+						}
+					}
 
-            		continue;
-            	}
+					continue;
+				}
 
-            	bool released = false;
+				bool released = false;
 
-            	try
-            	{
-            		using (TransactionScope scope = new TransactionScope())
-            		{
-            			TElement element;
-            			try
-            			{
-            				AdjustQueueCount();
+				try
+				{
+					using (TransactionScope scope = new TransactionScope())
+					{
+						TElement element;
+						try
+						{
+							AdjustQueueCount();
 
-            				element = _function(_resource);
-            			}
-            			finally
-            			{
-            				_resourceGovernor.Release(1);
-            				released = true;
-            			}
+							element = _function(_resource);
+						}
+						finally
+						{
+							_resourceGovernor.Release(1);
+							released = true;
+						}
 
-            			_action(element);
+						_action(element);
 
-            			scope.Complete();
-            		}
-            	}
-            	catch (Exception ex)
-            	{
-            		_log.Error("An exception occurred processing an item of type: " + typeof (TResource).FullName, ex);
-            	}
-            	finally
-            	{
-            		if (!released)
-            			_resourceGovernor.Release(1);
-            	}
-            }
+						scope.Complete();
+					}
+				}
+				catch (Exception ex)
+				{
+					_log.Error("An exception occurred processing an item of type: " + typeof (TResource).FullName, ex);
+				}
+				finally
+				{
+					if (!released)
+						_resourceGovernor.Release(1);
+				}
+			}
 
-            AdjustQueueCount();
+			AdjustQueueCount();
 
-            if (_log.IsDebugEnabled)
-                _log.DebugFormat("ExitingThread {0}", threadId);
-        }
+			if (_log.IsDebugEnabled)
+				_log.DebugFormat("ExitingThread {0}", threadId);
+		}
 
-    	public void WakeUp()
-    	{
+		public void WakeUp()
+		{
+			if (_resourceGovernor == null)
+			{
+				_resourceGovernor = new Semaphore(_resourceLimit, _resourceLimit);
+			}
+
 			AdjustQueueCount();
 		}
-    }
+	}
 }
