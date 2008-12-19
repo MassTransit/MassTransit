be2ff04d7dba67414b72fc65286cc0a66c44c902
Dru Sellers <dru@drusellers.com>
2008-12-19 00:03:20 +0000

    Moving the batch stuff around
    Former-commit-id: 7587d36f4ebda321ac3a4777295b6f0669bd3872

--- MassTransit/Services/Metadata/WireTap.cs
+++ MassTransit/Services/Metadata/WireTap.cs
@@ -1,43 +1,23 @@
-namespace MassTransit.Metadata
+namespace MassTransit.Services.Metadata
 {
     using System;
-    using System.Collections.Generic;
     using Pipeline;
     using Pipeline.Sinks;
 
     public class WireTap<TMessage> :
-        MessageSinkBase<TMessage, TMessage> where TMessage : class
-	{
-		private readonly Func<TMessage, bool> _allow;
+        MessageFilter<TMessage> where TMessage : class
+    {
         private readonly Action<TMessage> _action;
 
-		public WireTap(string description, Func<IMessageSink<TMessage>, IMessageSink<TMessage>> insertAfter, Func<TMessage, bool> allow, Action<TMessage> action) :
-			base(null)
-		{
-			Description = description ?? string.Empty;
+        public WireTap(string description, 
+            Func<IMessageSink<TMessage>, IMessageSink<TMessage>> insertAfter,
+            Func<TMessage, bool> allow,
+            Action<TMessage> action) :
+            base(description, insertAfter, allow)
+        {
+            _action = action;
 
-			_allow = allow;
-		    _action = action;
-
-			ReplaceOutputSink(insertAfter(this));
-		}
-
-		public string Description { get; private set; }
-
-		public override IEnumerable<Consumes<TMessage>.All> Enumerate(TMessage message)
-		{
-			if (!_allow(message))
-				yield break;
-
-			foreach (Consumes<TMessage>.All consumer in _outputSink.ReadLock(x => x.Enumerate(message)))
-			{
-				yield return consumer;
-			}
-		}
-
-		public override bool Inspect(IPipelineInspector inspector)
-		{
-			return inspector.Inspect(this) && _outputSink.ReadLock(x => x.Inspect(inspector));
-		}
-	}
+            ReplaceOutputSink(insertAfter(this));
+        }
+    }
 }
