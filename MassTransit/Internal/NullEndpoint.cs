348baf36a3eea8b104a9e7843c2018b9b541ea46
Chris Patterson <chris@phatboyg.com>
2008-12-21 04:27:06 +0000

    Dead code removal (MessageContext)

--- MassTransit/Internal/NullEndpoint.cs
+++ MassTransit/Internal/NullEndpoint.cs
@@ -1,44 +1,46 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace MassTransit.Internal
 {
-    using System;
-    using Exceptions;
+	using System;
+	using Exceptions;
 
-    public class NullEndpoint :
-        IEndpoint
-    {
-        public void Dispose()
-        {
-            //do nothing
-        }
-
-        public Uri Uri
-        {
-            get { return new Uri("null://middleof/nowhere"); }
-        }
-
-        public void Send<T>(T message) where T : class
-        {
-            //do nothing
-        }
-
-        public void Send<T>(T message, TimeSpan timeToLive) where T : class
-        {
-            //do nothing
-        }
-
-        public object Receive(TimeSpan timeout)
-        {
-			throw new EndpointException(this, "NullEndpoints have no messages");
+	public class NullEndpoint :
+		IEndpoint
+	{
+		public void Dispose()
+		{
+			//do nothing
 		}
 
-        public object Receive(TimeSpan timeout, Predicate<object> accept)
-        {
-            throw new EndpointException(this, "NullEndpoints have no messages");
-        }
+		public Uri Uri
+		{
+			get { return new Uri("null://middleof/nowhere"); }
+		}
 
-    	public void Receive(TimeSpan timeout, Func<object, Func<object, bool>, bool> receiver)
-    	{
+		public void Send<T>(T message) where T : class
+		{
+			//do nothing
+		}
+
+		public void Send<T>(T message, TimeSpan timeToLive) where T : class
+		{
+			//do nothing
+		}
+
+		public void Receive(TimeSpan timeout, Func<object, Func<object, bool>, bool> receiver)
+		{
 			throw new EndpointException(this, "NullEndpoints have no messages");
 		}
-    }
+	}
 }
