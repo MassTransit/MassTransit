19aecaa7d47093a25d60c16253ff76e04f78ae40
Dru Sellers <dru@drusellers.com>
2008-12-22 01:34:47 +0000

    cleaning up the msmq tests
    Former-commit-id: 2a879358f8e1fa22f85de446e2ee1893de3c78f8

--- Transports/MassTransit.Transports.Msmq.Tests/As_a_ServiceBus_working_with_an_endpoint.cs
+++ Transports/MassTransit.Transports.Msmq.Tests/As_a_ServiceBus_working_with_an_endpoint.cs
@@ -15,6 +15,7 @@ namespace MassTransit.Transports.Msmq.Tests
     using System;
     using System.Collections;
     using System.Threading;
+    using Magnum.Common.DateTimeExtensions;
     using MassTransit.Tests;
     using MassTransit.Tests.Messages;
     using MassTransit.Tests.TestConsumers;
@@ -239,6 +240,99 @@ namespace MassTransit.Transports.Msmq.Tests
 
                 Assert.That(_repliedEvent.WaitOne(TimeSpan.FromSeconds(3), true), Is.True, "NO response message received");
             }
+        }
+    }
+
+    [TestFixture]
+    public class When_an_accept_method_throws_an_exception
+    {
+        [Test]
+        public void The_exception_should_not_disrupt_the_flow_of_messages()
+        {
+            MsmqEndpoint endpoint = new MsmqEndpoint("msmq://localhost/test_servicebus");
+            IObjectBuilder obj = null;
+            ServiceBus bus = new ServiceBus(endpoint, obj);
+
+            CrashingService service = new CrashingService();
+
+            bus.Subscribe(service);
+
+            endpoint.Send(new BogusMessage());
+
+            CrashingService.Received.WaitOne(5.Seconds(), true).ShouldBeTrue("No message received");
+
+            CrashingService.Received.Reset();
+
+            endpoint.Send(new LegitMessage());
+
+            CrashingService.LegitReceived.WaitOne(5.Seconds(), true).ShouldBeTrue("No message received");
+        }
+
+        internal class CrashingService :
+            Consumes<BogusMessage>.All,
+            Consumes<LegitMessage>.All
+        {
+            public static ManualResetEvent Received
+            {
+                get { return _received; }
+            }
+
+            private static readonly ManualResetEvent _received = new ManualResetEvent(false);
+
+            public static ManualResetEvent LegitReceived
+            {
+                get { return _legitReceived; }
+            }
+
+            private static readonly ManualResetEvent _legitReceived = new ManualResetEvent(false);
+
+            public void Consume(BogusMessage message)
+            {
+                _received.Set();
+
+                throw new NotImplementedException();
+            }
+
+            public void Consume(LegitMessage message)
+            {
+                _legitReceived.Set();
+            }
+        }
+
+        [Serializable]
+        internal class BogusMessage
+        {
+        }
+
+        [Serializable]
+        internal class LegitMessage
+        {
+        }
+    }
+
+    [TestFixture]
+    public class When_receiving_messages_slowly :
+        LocalAndRemoteTestContext
+    {
+        protected override string GetCastleConfigurationFile()
+        {
+            return "msmq.castle.xml";
+        }
+
+        private readonly TimeSpan _timeout = TimeSpan.FromSeconds(10);
+
+        [Test]
+        public void It_should_be_received_by_one_subscribed_consumer()
+        {
+            var consumer = new TestMessageConsumer<PingMessage>();
+            RemoteBus.Subscribe(consumer);
+
+            Thread.Sleep(5.Seconds());
+
+            var message = new PingMessage();
+            LocalBus.Publish(message);
+
+            consumer.ShouldHaveReceivedMessage(message, _timeout);
         }
     }
 }
