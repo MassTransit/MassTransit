3d63160d53b401f76266007a3ca7ca8a7833a612
Dru Sellers <dru@drusellers.com>
2008-12-24 03:26:22 +0000

    De-nesting the tests

--- Transports/MassTransit.Transports.Msmq.Tests/As_a_nontransactional_endpoint.cs
+++ Transports/MassTransit.Transports.Msmq.Tests/As_a_nontransactional_endpoint.cs
@@ -14,98 +14,95 @@ namespace MassTransit.Transports.Msmq.Tests
 {
     using System;
     using System.Transactions;
-    using Exceptions;
     using MassTransit.Tests;
     using Messages;
     using NUnit.Framework;
-    using DeleteMessage=MassTransit.Tests.DeleteMessage;
+    using DeleteMessage = MassTransit.Tests.DeleteMessage;
 
-    public class As_a_nontransactional_endpoint
+
+    //as a non-transactional endpoint
+    [TestFixture]
+    public class When_in_a_transaction_non
     {
+        private MsmqEndpoint _ep;
 
-        [TestFixture]
-        public class When_in_a_transaction
+        [SetUp]
+        public void SetUp()
         {
-            private MsmqEndpoint _ep;
-
-            [SetUp]
-            public void SetUp()
-            {
-                MsmqUtilities.ValidateAndPurgeQueue(".\\private$\\mt_client", true);
-                _ep = new MsmqEndpoint("msmq://localhost/mt_client");
-            }
-
-            [TearDown]
-            public void TearDown()
-            {
-            	_ep.Dispose();
-                _ep = null;
-            }
-
-
-            [Test]
-            public void While_writing_it_should_perisist_on_complete()
-            {
-                using (TransactionScope trx = new TransactionScope())
-                {
-                    _ep.Send(new DeleteMessage());
-                    trx.Complete();
-                }
-
-                _ep.VerifyMessageInQueue<DeleteMessage>();
-            }
-
-            [Test]
-            public void While_writing_it_should_perisist_even_on_rollback()
-            {
-                using (TransactionScope trx = new TransactionScope())
-                {
-                    _ep.Send(new DeleteMessage());
-                    //no complete
-                }
-
-                _ep.VerifyMessageInQueue<DeleteMessage>();
-            }
+            MsmqUtilities.ValidateAndPurgeQueue(".\\private$\\mt_client", true);
+            _ep = new MsmqEndpoint("msmq://localhost/mt_client");
         }
 
-        [TestFixture]
-        public class When_outside_a_transaction
+        [TearDown]
+        public void TearDown()
         {
-            private MsmqEndpoint _ep;
-
-            [SetUp]
-            public void SetUp()
-            {
-                MsmqUtilities.ValidateAndPurgeQueue(".\\private$\\mt_client", true);
-                _ep = new MsmqEndpoint("msmq://localhost/mt_client");
-            }
-
-            [TearDown]
-            public void TearDown()
-            {
-				_ep.Dispose();
-                _ep = null;
-            }
+            _ep.Dispose();
+            _ep = null;
+        }
 
 
-            [Test]
-            public void While_writing_it_should_persist()
+        [Test]
+        public void While_writing_it_should_perisist_on_complete()
+        {
+            using (TransactionScope trx = new TransactionScope())
             {
                 _ep.Send(new DeleteMessage());
-
-                _ep.VerifyMessageInQueue<DeleteMessage>();
+                trx.Complete();
             }
 
-            [Test]
-            public void While_reading_it_should_pull_object_from_queue()
+            _ep.VerifyMessageInQueue<DeleteMessage>();
+        }
+
+        [Test]
+        public void While_writing_it_should_perisist_even_on_rollback()
+        {
+            using (TransactionScope trx = new TransactionScope())
             {
-                _ep.Purge();
-                _ep.Send(new VariableMessage("dru"));
-                object obj = _ep.Receive(TimeSpan.FromSeconds(30));
-
-                obj.ShouldNotBeNull();
-                obj.ShouldBeSameType<VariableMessage>();
+                _ep.Send(new DeleteMessage());
+                //no complete
             }
+
+            _ep.VerifyMessageInQueue<DeleteMessage>();
+        }
+    }
+
+    [TestFixture]
+    public class When_outside_a_transaction_non
+    {
+        private MsmqEndpoint _ep;
+
+        [SetUp]
+        public void SetUp()
+        {
+            MsmqUtilities.ValidateAndPurgeQueue(".\\private$\\mt_client", true);
+            _ep = new MsmqEndpoint("msmq://localhost/mt_client");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _ep.Dispose();
+            _ep = null;
+        }
+
+
+        [Test]
+        public void While_writing_it_should_persist()
+        {
+            _ep.Send(new DeleteMessage());
+
+            _ep.VerifyMessageInQueue<DeleteMessage>();
+        }
+
+        [Test]
+        public void While_reading_it_should_pull_object_from_queue()
+        {
+            _ep.Purge();
+            _ep.Send(new VariableMessage("dru"));
+            object obj = _ep.Receive(TimeSpan.FromSeconds(30));
+
+            obj.ShouldNotBeNull();
+            obj.ShouldBeSameType<VariableMessage>();
         }
     }
 }
--- Transports/MassTransit.Transports.Msmq.Tests/As_a_transactional_endpoint.cs
+++ Transports/MassTransit.Transports.Msmq.Tests/As_a_transactional_endpoint.cs
@@ -18,83 +18,82 @@ namespace MassTransit.Transports.Msmq.Tests
     using MassTransit.Tests;
     using NUnit.Framework;
 
-    public class As_a_transactional_endpoint
+
+    //as a transactional endpoint
+    [TestFixture]
+    public class When_in_a_transaction
     {
-        [TestFixture]
-        public class When_in_a_transaction
+        private MsmqEndpoint _ep;
+
+        [SetUp]
+        public void SetUp()
         {
-            private MsmqEndpoint _ep;
-
-            [SetUp]
-            public void SetUp()
-            {
-                MsmqUtilities.ValidateAndPurgeQueue(".\\private$\\mt_client_tx", true);
-                _ep = new MsmqEndpoint("msmq://localhost/mt_client_tx");
-            }
-
-            [TearDown]
-            public void TearDown()
-            {
-				_ep.Dispose();
-                _ep = null;
-            }
-
-
-            [Test]
-            public void While_writing_it_should_perisist_on_complete()
-            {
-                using (TransactionScope trx = new TransactionScope())
-                {
-                    _ep.Send(new DeleteMessage());
-                    trx.Complete();
-                }
-
-
-                _ep.VerifyMessageInTransactionalQueue<DeleteMessage>();
-            }
-
-            [Test]
-            public void While_writing_it_should_not_perisist_on_rollback()
-            {
-                using (TransactionScope trx = new TransactionScope())
-                {
-                    _ep.Send(new DeleteMessage());
-                    //no complete
-                }
-
-                _ep.VerifyMessageNotInTransactionalQueue();
-            }
-
-            
+            MsmqUtilities.ValidateAndPurgeQueue(".\\private$\\mt_client_tx", true);
+            _ep = new MsmqEndpoint("msmq://localhost/mt_client_tx");
         }
 
-        [TestFixture]
-        public class When_outside_a_transaction
+        [TearDown]
+        public void TearDown()
         {
-            private MsmqEndpoint _ep;
-
-            [SetUp]
-            public void SetUp()
-            {
-                MsmqUtilities.ValidateAndPurgeQueue(".\\private$\\mt_client_tx", true);
-                _ep = new MsmqEndpoint("msmq://localhost/mt_client_tx");
-            }
-
-            [TearDown]
-            public void TearDown()
-            {
-				_ep.Dispose();
-				_ep = null;
-            }
+            _ep.Dispose();
+            _ep = null;
+        }
 
 
-            [Test]
-            public void It_should_auto_enlist_a_transaction_and_persist()
+        [Test]
+        public void While_writing_it_should_perisist_on_complete()
+        {
+            using (TransactionScope trx = new TransactionScope())
             {
                 _ep.Send(new DeleteMessage());
-                _ep.VerifyMessageNotInTransactionalQueue();
+                trx.Complete();
             }
 
+
+            _ep.VerifyMessageInTransactionalQueue<DeleteMessage>();
         }
+
+        [Test]
+        public void While_writing_it_should_not_perisist_on_rollback()
+        {
+            using (TransactionScope trx = new TransactionScope())
+            {
+                _ep.Send(new DeleteMessage());
+                //no complete
+            }
+
+            _ep.VerifyMessageNotInTransactionalQueue();
+        }
+
+
+    }
+
+    [TestFixture]
+    public class When_outside_a_transaction
+    {
+        private MsmqEndpoint _ep;
+
+        [SetUp]
+        public void SetUp()
+        {
+            MsmqUtilities.ValidateAndPurgeQueue(".\\private$\\mt_client_tx", true);
+            _ep = new MsmqEndpoint("msmq://localhost/mt_client_tx");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _ep.Dispose();
+            _ep = null;
+        }
+
+
+        [Test]
+        public void It_should_auto_enlist_a_transaction_and_persist()
+        {
+            _ep.Send(new DeleteMessage());
+            _ep.VerifyMessageNotInTransactionalQueue();
+        }
+
     }
 }
