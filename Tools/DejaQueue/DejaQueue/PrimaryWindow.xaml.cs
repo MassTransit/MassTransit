63fb496a600005d5dbf659a67d2bea6ba84fd240
Chris Patterson <chris@phatboyg.com>
2008-12-29 22:22:29 +0000

    Working on a tool to visual the queues in the system, the contents of those queues, and the message contents

---
+++ Tools/DejaQueue/DejaQueue/PrimaryWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace DejaQueue
+{
+	using System.Windows;
+
+	/// <summary>
+	/// Interaction logic for PrimaryWindow.xaml
+	/// </summary>
+	public partial class PrimaryWindow :
+		Window
+	{
+		public PrimaryWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
