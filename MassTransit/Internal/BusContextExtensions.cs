f96762565b503eed20123ef1293f1076dcf8345f
Chris Patterson <chris@phatboyg.com>
2008-12-30 23:13:55 +0000

    Starting to work some message context stuff back into the bus as an aside, along with context for things like NHibernate sessions and so forth.
    Former-commit-id: ba65a5f4c71ab8160185c0b77beb9986f3540c9a

---
+++ MassTransit/Internal/BusContextExtensions.cs
@@ -0,0 +1,32 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.Internal
+{
+	using System;
+
+	public static class BusContextExtensions
+	{
+		private static readonly Guid _inboundMessageContextKey = new Guid("E4DD4A53-C0B5-4B56-86A0-A7A88AE09B7A");
+		private static readonly Guid _outboundMessageContextKey = new Guid("366BD633-7D83-4137-B764-422AE8B71E24");
+
+		public static OutboundMessageContext OutboundMessage(this IBusContext busContext)
+		{
+			return busContext.Retrieve(_outboundMessageContextKey, () => new OutboundMessageContext());
+		}
+
+		public static InboundMessageContext InboundMessage(this IBusContext busContext)
+		{
+			return busContext.Retrieve(_inboundMessageContextKey, () => new InboundMessageContext());
+		}
+	}
+}
