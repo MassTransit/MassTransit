c6ff6868b0479e287012ea9f5349e43f50fd56eb
Dru Sellers <dru@drusellers.com>
2009-01-13 03:58:35 +0000

    more logging

--- Samples/Starbucks/Starbucks.Cashier/Program.cs
+++ Samples/Starbucks/Starbucks.Cashier/Program.cs
@@ -1,33 +1,36 @@
-﻿using System;
-using Castle.Windsor;
-using MassTransit.Host;
-using MassTransit.WindsorIntegration;
-using Microsoft.Practices.ServiceLocation;
-
-namespace Starbucks.Cashier
+﻿namespace Starbucks.Cashier
 {
-    static class Program
+    using System;
+    using System.IO;
+    using Castle.Windsor;
+    using log4net.Config;
+    using MassTransit.Host;
+    using MassTransit.WindsorIntegration;
+    using Microsoft.Practices.ServiceLocation;
+
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            XmlConfigurator.Configure(new FileInfo("cashier.log4net.xml"));
             IWindsorContainer container = new DefaultMassTransitContainer("Starbucks.Cashier.Castle.xml");
             var builder = new WindsorObjectBuilder(container.Kernel);
             ServiceLocator.SetLocatorProvider(() => builder);
 
             container.AddComponent<FriendlyCashier>();
 
-            var credentials = Credentials.LocalSystem;
-            var settings = WinServiceSettings.Custom(
+            Credentials credentials = Credentials.LocalSystem;
+            WinServiceSettings settings = WinServiceSettings.Custom(
                 "StarbucksCashier",
                 "Starbucks Cashier",
                 "a Mass Transit sample service for handling orders of coffee.",
                 KnownServiceNames.Msmq);
             var lifecycle = new CashierLifecycle(ServiceLocator.Current);
-            
+
             Runner.Run(credentials, settings, lifecycle, args);
         }
     }
