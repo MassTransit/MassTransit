d985f865f3e2317303bc1779e6e94c416679b931
Dru Sellers <dru@drusellers.com>
2009-02-22 20:22:03 +0000

    web service bridge converted
    Former-commit-id: f3ae1c7d36fdcfa277c1fc45f3802c1f0a3ac173

--- Samples/WebServiceBridge/InternalInventoryService/InternalInventoryServiceLifeCycle.cs
+++ Samples/WebServiceBridge/InternalInventoryService/InternalInventoryServiceLifeCycle.cs
@@ -1,35 +1,22 @@
 namespace InternalInventoryService
 {
-	using System;
 	using MassTransit;
-    using MassTransit.Host.Actions;
-    using MassTransit.Host.LifeCycles;
     using Microsoft.Practices.ServiceLocation;
 
-    public class InternalInventoryServiceLifeCycle :
-        HostedLifecycle
+    public class InternalInventoryServiceLifeCycle
     {
         private IServiceBus _bus;
     	private UnsubscribeAction _unsubscribeToken;
 
-    	public InternalInventoryServiceLifeCycle(IServiceLocator serviceLocator)
-            : base(serviceLocator)
-        {
-        }
 
-        public override NamedAction DefaultAction
+        public void Start()
         {
-            get { return NamedAction.Console; }
-        }
-
-        public override void Start()
-        {
-            _bus = this.ServiceLocator.GetInstance<IServiceBus>("server");
+            _bus = ServiceLocator.Current.GetInstance<IServiceBus>("server");
 
         	_unsubscribeToken = _bus.Subscribe<InventoryLevelService>();
         }
 
-        public override void Stop()
+        public void Stop()
         {
         	_unsubscribeToken();
         }
