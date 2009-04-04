902b1a6731358e92a3b6e13851ed2189c8df022e
Dru Sellers <dru@drusellers.com>
2009-04-04 11:16:31 +0000

    adding timout tester to openallnight
    Former-commit-id: 672d5eb6c58df884978d0db13a6a31efe81fe2ba

---
+++ Samples/OpenAllNight/OpenAllNight/Testers/TimeoutTester.cs
@@ -0,0 +1,37 @@
+namespace OpenAllNight.Testers
+{
+    using System;
+    using Magnum.DateTimeExtensions;
+    using MassTransit;
+    using MassTransit.Services.Timeout.Messages;
+
+    public class TimeoutTester :
+        Consumes<TimeoutExpired>.All
+
+    {
+        private IServiceBus _bus;
+        private Guid _ticket;
+
+        public TimeoutTester(IServiceBus bus)
+        {
+            _bus = bus;
+            _bus.Subscribe(this);
+            _ticket = Guid.NewGuid();
+        }
+
+        public void Test()
+        {
+            _bus.Publish(new ScheduleTimeout(_ticket, 1.Seconds()));
+        }
+
+        public Guid CorrelationId
+        {
+            get { return _ticket; }
+        }
+
+        public void Consume(TimeoutExpired message)
+        {
+            Console.WriteLine("Timeout Expired: Whoot!");
+        }
+    }
+}
