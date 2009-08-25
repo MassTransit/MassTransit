777ddd9588ae5d103926dd7e81b97b55c614eb6d
Dru Sellers <dru@drusellers.com>
2009-08-25 02:18:23 +0000

    adding the visualizer

---
+++ MassTransit.Visualizers/ServiceBusForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MassTransit.Visualizers
+{
+    public partial class ServiceBusForm : Form
+    {
+        public ServiceBusForm()
+        {
+            InitializeComponent();
+        }
+
+        public void SetContent(string input)
+        {
+            this.InputContent.Text = input;
+        }
+    }
+}
