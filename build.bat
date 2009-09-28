d96731ac94c484450a9ac4fd76fa62f3b352853f
Dru Sellers <dru@drusellers.com>
2009-09-28 23:38:10 +0000

    more reorg and uppercut

--- build.bat
+++ build.bat
@@ -1,1 +1,31 @@
-.\libs\nant-0.85\nant.exe /f:.\BuildScripts\masstransit.build
+@echo off
+
+::Project UppercuT - http://uppercut.googlecode.com
+::No edits to this file are required - http://uppercut.pbwiki.com
+
+if '%2' NEQ '' goto usage
+if '%3' NEQ '' goto usage
+if '%1' == '/?' goto usage
+if '%1' == '-?' goto usage
+if '%1' == '?' goto usage
+if '%1' == '/help' goto usage
+
+SET DIR=%~d0%~p0%
+
+SET build.config.settings="%DIR%settings\UppercuT.config"
+"%DIR%lib\Nant\nant.exe" %1 /f:.\build\default.build -D:build.config.settings=%build.config.settings%
+
+if %ERRORLEVEL% NEQ 0 goto errors
+
+goto finish
+
+:usage
+echo.
+echo Usage: build.bat
+echo.
+goto finish
+
+:errors
+EXIT /B %ERRORLEVEL%
+
+:finish
---
+++ settings/SettingsFileReadMe.txt
@@ -0,0 +1,2 @@
+﻿Warning!
+You cannot edit these settings files with Visual Studio. It believes they are something they are not.
