2910a1759400ee7ed59b9ea90247a58b40117a17
Chris Patterson <chris@phatboyg.com>
2009-11-12 16:56:42 +0000

    If there is only one node in the grid, we automatically agree who will do the work. This might bite us later in a network split, so need to read up again on what to do here.
    Former-commit-id: 947cab38e39bca5558fb23f21446f625af83d954

--- src/Samples/Distributed Grid/DistributedGrid.Activator/CollectCompletedWork.cs
+++ src/Samples/Distributed Grid/DistributedGrid.Activator/CollectCompletedWork.cs
@@ -1,49 +1,61 @@
-using System.Threading;
-
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace DistributedGrid.Activator
 {
-    using System;
-    using Shared;
-    using Shared.Messages;
-    using MassTransit;
+	using System;
+	using System.Threading;
+	using MassTransit;
+	using Shared;
+	using Shared.Messages;
 
-    public class CollectCompletedWork :
-        Consumes<CompletedSimpleWorkItem>.All,
-        IServiceInterface
-    {
-        private readonly IServiceBus _bus;
-        private UnsubscribeAction _unsubscribeAction;
+	public class CollectCompletedWork :
+		Consumes<CompletedSimpleWorkItem>.All,
+		IServiceInterface
+	{
+		private readonly IServiceBus _bus;
+		private UnsubscribeAction _unsubscribeAction;
 
-        public CollectCompletedWork(IServiceBus bus)
-        {
-            _bus = bus;
+		public CollectCompletedWork(IServiceBus bus)
+		{
+			_bus = bus;
 
-            Thread.Sleep(1000);
+			Thread.Sleep(1000);
 
-            for (int i = 0; i < 100; i++)
-            {
-                var g = Guid.NewGuid();
-                Console.WriteLine("Publishing: " + g);
-                _bus.Publish(new DoSimpleWorkItem(g));
-            }
-        }
-        public void Consume(CompletedSimpleWorkItem message)
-        {
-            Console.WriteLine("Got Item");
-        }
+			for (int i = 0; i < 100; i++)
+			{
+				var g = Guid.NewGuid();
+				Console.WriteLine("Publishing: " + g);
+				_bus.Publish(new DoSimpleWorkItem(g));
+			}
+		}
 
-        public void Start()
-        {
-            _unsubscribeAction = _bus.Subscribe(this);
-        }
+		public void Consume(CompletedSimpleWorkItem message)
+		{
+			Console.WriteLine("Got Item");
+		}
 
-        public void Stop()
-        {
-            var action = _unsubscribeAction;
-            if (action != null)
-            {
-                action();
-            }
-        }
-    }
+		public void Start()
+		{
+			_unsubscribeAction = _bus.Subscribe(this);
+		}
+
+		public void Stop()
+		{
+			var action = _unsubscribeAction;
+			if (action != null)
+			{
+				action();
+			}
+		}
+	}
 }
--- src/Samples/Distributed Grid/DistributedGrid.Worker/DoWork.cs
+++ src/Samples/Distributed Grid/DistributedGrid.Worker/DoWork.cs
@@ -1,121 +1,83 @@
-using System;
-using DistributedGrid.Shared;
-using DistributedGrid.Shared.Messages;
-using MassTransit;
-using MassTransit.Configuration;
-using MassTransit.Grid;
-using MassTransit.Grid.Configuration;
-using MassTransit.Grid.Paxos;
-using MassTransit.Grid.Sagas;
-using MassTransit.Saga;
-using MassTransit.Services.Subscriptions.Configuration;
-
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace DistributedGrid.Worker
 {
-    public class DoWork :
-        IServiceInterface,
-        IDisposable,
-        Consumes<DoSimpleWorkItem>.All
-    {
-        public DoWork(IObjectBuilder objectBuilder)
-        {
-            ObjectBuilder = objectBuilder;
+	using System;
+	using MassTransit;
+	using MassTransit.Configuration;
+	using MassTransit.Grid.Configuration;
+	using MassTransit.Services.Subscriptions.Configuration;
+	using Shared;
+	using Shared.Messages;
 
-            SetupGridNodeRepository();
-            SetupGridAcceptorRepository();
-            SetupGridListenerRepository();
-            SetupGridMessageNodeRepository();
-            SetupGridServiceNodeRepository();
-            SetupGridServiceRepository();
+	public class DoWork :
+		IServiceInterface,
+		IDisposable,
+		Consumes<DoSimpleWorkItem>.All
+	{
+		public DoWork(IObjectBuilder objectBuilder)
+		{
+			ObjectBuilder = objectBuilder;
+		}
 
-            ControlBus = ControlBusConfigurator.New(x =>
-            {
-                x.SetObjectBuilder(ObjectBuilder);
-                
-                x.ReceiveFrom(ObjectBuilder.GetInstance<String>("SourceQueue") + "_control");
+		public void Start()
+		{
+			ControlBus = ControlBusConfigurator.New(x =>
+				{
+					x.SetObjectBuilder(ObjectBuilder);
 
-                x.PurgeBeforeStarting();
-            });
+					x.ReceiveFrom(ObjectBuilder.GetInstance<String>("SourceQueue") + "_control");
 
-            DataBus = ServiceBusConfigurator.New(x =>
-            {
-                x.SetObjectBuilder(ObjectBuilder);
-                x.ConfigureService<SubscriptionClientConfigurator>(y =>
-                {
-                    // setup endpoint
-                    y.SetSubscriptionServiceEndpoint(ObjectBuilder.GetInstance<String>("SubscriptionQueue"));
-                });
-                x.ReceiveFrom(ObjectBuilder.GetInstance<String>("SourceQueue"));
-                x.UseControlBus(ControlBus);
-                x.SetConcurrentConsumerLimit(4);
+					x.PurgeBeforeStarting();
+				});
 
-                x.ConfigureService<GridConfigurator>(grid => 
-                {
-                    grid.SetProposer();
-                    grid.For<DoSimpleWorkItem>().Use<DoWork>();
-                });
-            });
-        }
+			DataBus = ServiceBusConfigurator.New(x =>
+				{
+					x.SetObjectBuilder(ObjectBuilder);
+					x.ConfigureService<SubscriptionClientConfigurator>(y =>
+						{
+							// setup endpoint
+							y.SetSubscriptionServiceEndpoint(ObjectBuilder.GetInstance<String>("SubscriptionQueue"));
+						});
+					x.ReceiveFrom(ObjectBuilder.GetInstance<String>("SourceQueue"));
+					x.UseControlBus(ControlBus);
+					x.SetConcurrentConsumerLimit(4);
+
+					x.ConfigureService<GridConfigurator>(grid =>
+						{
+							grid.SetProposer();
+							grid.For<DoSimpleWorkItem>().Use<DoWork>();
+						});
+				});
+		}
+
+		public void Stop()
+		{
+		}
+
+		public IServiceBus DataBus { get; private set; }
+		public IControlBus ControlBus { get; private set; }
+		public IObjectBuilder ObjectBuilder { get; private set; }
 
 
-        public void Start()
-        {
+		public void Dispose()
+		{
+		}
 
-        }
-
-        public void Stop()
-        {
-
-        }
-
-        public IServiceBus DataBus { get; private set; }
-        public IControlBus ControlBus { get; private set; }
-        public ISagaRepository<Node> GridNodeRepository { get; private set; }
-        public ISagaRepository<ServiceType> GridServiceRepository { get; private set; }
-        public ISagaRepository<ServiceMessage> GridMessageNodeRepository { get; private set; }
-        public ISagaRepository<ServiceNode> GridServiceNodeRepository { get; private set; }
-        public ISagaRepository<Learner<AvailableGridServiceNode>> GridListenerRepository { get; private set; }
-        public ISagaRepository<Acceptor<AvailableGridServiceNode>> GridAcceptorRepository { get; private set; }
-        public IObjectBuilder ObjectBuilder { get; private set; }
-
-        private void SetupGridNodeRepository()
-        {
-            GridNodeRepository = ObjectBuilder.GetInstance(typeof(ISagaRepository<Node>)) as ISagaRepository<Node>;
-        }
-
-        private void SetupGridServiceRepository()
-        {
-            GridServiceRepository = ObjectBuilder.GetInstance(typeof(ISagaRepository<ServiceType>)) as ISagaRepository<ServiceType>;
-        }
-
-        private void SetupGridMessageNodeRepository()
-        {
-            GridMessageNodeRepository = ObjectBuilder.GetInstance(typeof(ISagaRepository<ServiceMessage>)) as ISagaRepository<ServiceMessage>;
-        }
-
-        private void SetupGridListenerRepository()
-        {
-            GridListenerRepository = ObjectBuilder.GetInstance(typeof(ISagaRepository<Learner<AvailableGridServiceNode>>)) as ISagaRepository<Learner<AvailableGridServiceNode>>;
-        }
-
-        private void SetupGridAcceptorRepository()
-        {
-            GridAcceptorRepository = ObjectBuilder.GetInstance(typeof(ISagaRepository<Acceptor<AvailableGridServiceNode>>)) as ISagaRepository<Acceptor<AvailableGridServiceNode>>;
-        }
-
-        private void SetupGridServiceNodeRepository()
-        {
-            GridServiceNodeRepository = ObjectBuilder.GetInstance(typeof(ISagaRepository<ServiceNode>)) as ISagaRepository<ServiceNode>;
-        }
-
-        public void Dispose()
-        {
-        }
-
-        public void Consume(DoSimpleWorkItem message)
-        {
-            Console.WriteLine(message.CorrelationId);
-            CurrentMessage.Respond(new CompletedSimpleWorkItem(message.CorrelationId, message.CreatedAt));
-        }
-    }
+		public void Consume(DoSimpleWorkItem message)
+		{
+			Console.WriteLine(message.CorrelationId);
+			CurrentMessage.Respond(new CompletedSimpleWorkItem(message.CorrelationId, message.CreatedAt));
+		}
+	}
 }
