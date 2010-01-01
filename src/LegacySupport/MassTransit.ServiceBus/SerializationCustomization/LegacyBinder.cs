e89810a00bc6cb58763073dd6bb1d467131d9c3d
Dru Sellers <dru@drusellers.com>
2010-01-01 03:34:49 +0000

    adding some more legacy goodness
    Former-commit-id: c51cac418b1b38a860388fd87514bea322e6eb4c

--- src/LegacySupport/MassTransit.ServiceBus/SerializationCustomization/LegacyBinder.cs
+++ src/LegacySupport/MassTransit.ServiceBus/SerializationCustomization/LegacyBinder.cs
@@ -1,7 +1,25 @@
 namespace MassTransit.LegacySupport.SerializationCustomization
 {
-    public class LegacyBinder
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    public class LegacyBinder :
+        SerializationBinder
     {
-        
+        Dictionary<string, Type> _map = new Dictionary<string, Type>();
+
+        public void AddMap(string oldTypeName, Type newType)
+        {
+            _map.Add(oldTypeName, newType);
+        }
+
+        public override Type BindToType(string assemblyName, string typeName)
+        {
+            if(_map.ContainsKey(typeName))
+                return _map[typeName];
+
+            return Type.GetType("{0}, {1}".FormatWith(assemblyName, typeName));
+        }
     }
 }
--- src/LegacySupport/MassTransit.ServiceBus/SerializationCustomization/LegacySurrogateSelector.cs
+++ src/LegacySupport/MassTransit.ServiceBus/SerializationCustomization/LegacySurrogateSelector.cs
@@ -1,24 +1,41 @@
 namespace MassTransit.LegacySupport.SerializationCustomization
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Runtime.Serialization;
 
     public class LegacySurrogateSelector :
         ISurrogateSelector
     {
+        readonly SurrogateSelector _inner = new SurrogateSelector();
+        readonly List<LegacySurrogate> _surrogates = new List<LegacySurrogate>();
+
         public void ChainSelector(ISurrogateSelector selector)
         {
-            throw new NotImplementedException();
+            _inner.ChainSelector(selector);
         }
 
         public ISerializationSurrogate GetSurrogate(Type type, StreamingContext context, out ISurrogateSelector selector)
         {
-            throw new NotImplementedException();
+            if (_surrogates.Any(x => x.SurrogateType == type))
+            {
+                selector = this;
+                return _surrogates.First(x => x.SurrogateType == type);
+            }
+
+
+            return _inner.GetSurrogate(type, context, out selector);
         }
 
         public ISurrogateSelector GetNextSelector()
         {
-            throw new NotImplementedException();
+            return _inner.GetNextSelector();
+        }
+
+        public void AddSurrogate<T>(LegacySurrogate<T> surrogate)
+        {
+            _surrogates.Add(surrogate);
         }
     }
 }
