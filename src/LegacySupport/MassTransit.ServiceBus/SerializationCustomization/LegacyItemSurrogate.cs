6c597fe44bafb173d82525348aadb4bc3ae260e8
Dru Sellers <dru@drusellers.com>
2010-01-12 11:55:09 +0000

    starting to add list covarience support

--- src/LegacySupport/MassTransit.ServiceBus/SerializationCustomization/LegacyItemSurrogate.cs
+++ src/LegacySupport/MassTransit.ServiceBus/SerializationCustomization/LegacyItemSurrogate.cs
@@ -1,3 +1,15 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace MassTransit.LegacySupport.SerializationCustomization
 {
     using System;
@@ -7,11 +19,16 @@ namespace MassTransit.LegacySupport.SerializationCustomization
     using System.Runtime.Serialization;
     using Magnum.Reflection;
 
-    public class LegacySurrogate :
+    public interface LegacySurrogate :
         ISerializationSurrogate
     {
+        Type SurrogateType { get; }
+    }
 
-        public LegacySurrogate(string otherAssemblyName, string otherTypeName, Type newType)
+    public class LegacyItemSurrogate :
+        LegacySurrogate
+    {
+        public LegacyItemSurrogate(string otherAssemblyName, string otherTypeName, Type newType)
         {
             OtherAssemblyName = otherAssemblyName;
             OtherTypeName = otherTypeName;
@@ -23,14 +40,12 @@ namespace MassTransit.LegacySupport.SerializationCustomization
         public string OtherTypeName { get; set; }
         public List<MemberInfo> Members { get; set; }
 
-        public Type SurrogateType
-        {
-            get; private set;
-        }
+        #region LegacySurrogate Members
+
+        public Type SurrogateType { get; private set; }
 
         public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
         {
-
             info.AssemblyName = OtherAssemblyName;
             info.FullTypeName = OtherTypeName;
 
@@ -46,6 +61,8 @@ namespace MassTransit.LegacySupport.SerializationCustomization
         {
             throw new NotImplementedException("I cannot implement this.");
         }
+
+        #endregion
     }
 
     public static class FastPropertyExtensions
--- src/LegacySupport/MassTransit.ServiceBus/SerializationCustomization/LegacySurrogateSelector.cs
+++ src/LegacySupport/MassTransit.ServiceBus/SerializationCustomization/LegacySurrogateSelector.cs
@@ -1,3 +1,15 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace MassTransit.LegacySupport.SerializationCustomization
 {
     using System;
@@ -10,6 +22,8 @@ namespace MassTransit.LegacySupport.SerializationCustomization
     {
         readonly SurrogateSelector _inner = new SurrogateSelector();
         readonly List<LegacySurrogate> _surrogates = new List<LegacySurrogate>();
+
+        #region ISurrogateSelector Members
 
         public void ChainSelector(ISurrogateSelector selector)
         {
@@ -32,6 +46,8 @@ namespace MassTransit.LegacySupport.SerializationCustomization
         {
             return _inner.GetNextSelector();
         }
+
+        #endregion
 
         public void AddSurrogate(LegacySurrogate surrogate)
         {
