d1b17e7e50e5ee451f24367955f51373f59abd19
Dru Sellers <dru@drusellers.com>
2010-01-17 13:11:44 +0000

    building out the legacy service host

---
+++ src/LegacyRuntime/IConfiguration.cs
@@ -0,0 +1,21 @@
+namespace LegacyRuntime
+{
+    using StructureMap.Configuration.DSL;
+
+    public interface IConfiguration
+    {
+        
+    }
+
+    public class Configuration :
+        IConfiguration
+    {
+        
+    }
+
+    public class LegacySupportRegistry :
+        Registry
+    {
+        
+    }
+}
