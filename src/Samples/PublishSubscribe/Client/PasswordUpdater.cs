478701694fee0e4558b0569e0e89c5d2e1d5c64f
Chris Patterson <Chris@Chris-Pattersons-MacBook-Pro-6.local>
2010-02-27 13:15:17 -0600

    Revert "removing old sample code"
    This reverts commit 1f0c7b56c63a654dd5aa8c24913a2e5c4947b066 [formerly 7901c88d2de4cd22744b7f5b5443d1429dcc221b].
    
    
    Former-commit-id: 05bf61c84f23b0c111f78e1e60e8c7230be9fc6b

---
+++ src/Samples/PublishSubscribe/Client/PasswordUpdater.cs
@@ -0,0 +1,17 @@
+namespace Client
+{
+    using log4net;
+    using MassTransit;
+    using SecurityMessages;
+
+    public class PasswordUpdater :
+        Consumes<PasswordUpdateComplete>.All
+    {
+		private static readonly ILog _log = LogManager.GetLogger(typeof(PasswordUpdater));
+
+		public void Consume(PasswordUpdateComplete message)
+        {
+			_log.InfoFormat("Global password update complete: {0} ({1})", message.ErrorCode, message.CorrelationId);
+        }
+    }
+}
