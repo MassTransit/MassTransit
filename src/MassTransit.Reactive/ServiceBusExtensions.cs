fd243ac20414358b07e077444762d93dc774c118
dahlbyk <dahlbyk@gmail.com>
2010-02-28 00:58:36 -0600

    Add IServiceBus.AsObservable<T>() extension
    Former-commit-id: b3d961c366ca92255ca72cc8a1bc5d8fd950f52f

---
+++ src/MassTransit.Reactive/ServiceBusExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MassTransit;
+
+namespace MassTransit.Reactive
+{
+    public static class ServiceBusExtensions
+    {
+        public static IObservable<T> AsObservable<T>(this IServiceBus @this) where T : class
+        {
+            return new ServiceBusObserver<T>(@this);
+        }
+
+        public static IObservable<T> AsObservable<T>(this IServiceBus @this, Predicate<T> condition) where T : class
+        {
+            return new ServiceBusObserver<T>(@this, condition);
+        }
+    }
+}
