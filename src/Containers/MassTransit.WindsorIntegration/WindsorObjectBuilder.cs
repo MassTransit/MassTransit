96c4c2305b63235f39fbf50a2a088e149ef0f060
Dru Sellers <dru@drusellers.com>
2010-10-29 13:19:16 -0500

    tweaking the log file, and removing unused methods on the WOB

--- src/Containers/MassTransit.WindsorIntegration/WindsorObjectBuilder.cs
+++ src/Containers/MassTransit.WindsorIntegration/WindsorObjectBuilder.cs
@@ -1,5 +1,5 @@
-// Copyright 2007-2008 The Apache Software Foundation.
-//  
+// Copyright 2007-2010 The Apache Software Foundation.
+// 
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 // this file except in compliance with the License. You may obtain a copy of the 
 // License at 
@@ -14,15 +14,12 @@ namespace MassTransit.WindsorIntegration
 {
     using System;
     using System.Collections;
-    using System.Collections.Generic;
     using Castle.MicroKernel;
-    
 
     public class WindsorObjectBuilder :
         IObjectBuilder
     {
-        private readonly IKernel _container;
-
+        readonly IKernel _container;
 
         public WindsorObjectBuilder(IKernel container)
         {
@@ -39,45 +36,14 @@ namespace MassTransit.WindsorIntegration
             _container.ReleaseComponent(obj);
         }
 
-        public object GetService(Type serviceType)
-        {
-            return _container.Resolve(serviceType);
-        }
-
         public object GetInstance(Type serviceType)
         {
             return _container.Resolve(serviceType);
         }
 
-        public object GetInstance(Type serviceType, string key)
-        {
-            return _container.Resolve(key, serviceType);
-        }
-
-        public IEnumerable<object> GetAllInstances(Type serviceType)
-        {
-            var x = _container.ResolveAll(serviceType, new {});
-            ArrayList result = new ArrayList();
-            foreach (var o in x)
-            {
-                result.Add(o);
-            }
-            return result.ToArray();
-        }
-
         public TService GetInstance<TService>()
         {
             return _container.Resolve<TService>();
-        }
-
-        public TService GetInstance<TService>(string key)
-        {
-            return _container.Resolve<TService>(key);
-        }
-
-        public IEnumerable<TService> GetAllInstances<TService>()
-        {
-            return _container.ResolveAll<TService>();
         }
     }
 }
