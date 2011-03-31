2d8a4de6a18d92516e25782757bfe74c45ddfc22
Moukarram Kabbash <mouk9000@hotmail.com>
2011-03-31 15:53:20 +0800

    Fixed a bug in MessageRetryTracker.cs causing it to always return false

---
+++ src/MassTransit.Tests/MessageRetryTracker_Specs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MassTransit.Transports;
+using NUnit.Framework;
+
+namespace MassTransit.Tests
+{
+    [TestFixture]
+    public class MessageRetryTracker_Specs
+    {
+        [Test]
+        public void IsLimitExceeded_return_false_untill_specified_limit_reached()
+        {
+            const int retryLimit = 5;
+
+            var tracker = new MessageRetryTracker(retryLimit);
+            const string id = "qelofjsw";
+
+            for (int i = 0; i < retryLimit; i++)
+            {
+                Assert.IsFalse(tracker.IsRetryLimitExceeded(id));
+                tracker.IncrementRetryCount(id);
+            }
+            Assert.IsTrue(tracker.IsRetryLimitExceeded(id));
+        }
+    }
+}
