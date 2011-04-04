5503e727a6c8cf844d2b3a95835bb4fc6b627c8c
Chris Patterson <chris@phatboyg.com>
2011-04-04 17:04:41 -0500

    Updated dependencies again
    Former-commit-id: 1426b592161470fcee7f508f006e8217d80d60e4

--- build.bat
+++ build.bat
@@ -3,11 +3,11 @@
 echo Building for .NET 3.5
 call rake BUILD_CONFIG_KEY=NET35
 
-echo Building for .NET 4.0
-call rake unclean
+rem echo Building for .NET 4.0
+rem call rake unclean
 
 echo Creating NU package
-lib\nuget pack Stact.nuspec /OutputDirectory build_artifacts 
+lib\nuget pack MassTransit.nuspec /OutputDirectory build_artifacts 
 
 echo Create ZIP package
 call rake package
