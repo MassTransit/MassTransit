5a47bbdf933dcf25c7d513fa6fc4ab7ee458196e
Dru Sellers <dru@drusellers.com>
2011-04-23 21:14:10 -0500

    added autofac support, and cleaned up ninject and structuremap
    Former-commit-id: 31e0181965e98b819ec1fcaca0d7256eebccfee7

--- src/Containers/MassTransit.AutoFacIntegration/MassTransitAutofacExtensions.cs
+++ src/Containers/MassTransit.AutoFacIntegration/MassTransitAutofacExtensions.cs
@@ -1,4 +1,4 @@
-// Copyright 2007-2011 The Apache Software Foundation.
+﻿// Copyright 2007-2011 The Apache Software Foundation.
 // 
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 // this file except in compliance with the License. You may obtain a copy of the 
@@ -10,20 +10,23 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
-namespace MassTransit.StructureMapIntegration
+namespace MassTransit.AutoFacIntegration
 {
     using System.Linq;
+    using Autofac;
     using Configuration;
-    using StructureMap;
+    using Magnum.Extensions;
 
-    public static class MassTransitStructureMapExtensions
+    public static class MassTransitAutofacExtensions
     {
-        public static UnsubscribeAction LoadConsumersFromContainer(this BusConfiguration cfg, IContainer container)
+        public static UnsubscribeAction LoadConsumersFromContainer(this BusConfiguration config, IContainer container)
         {
-            var concreteTypes = from i in container.Model.InstancesOf<IConsumer>()
-                                select i.ConcreteType;
+            var concretes = from r in container.ComponentRegistry.Registrations
+                            where r.Services.Any(s => s.Implements<IConsumer>())
+                            select r.Target;
 
             //cfg.AddSubscribersByType(concretes);
+
             return () => true;
         }
     }
