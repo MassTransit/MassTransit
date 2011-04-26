6e2b9e65b386562451c7ee605d0a83133c0e7b6b
Dru Sellers <dru@drusellers.com>
2011-04-26 07:44:06 -0500

    updating with chris' work
    Former-commit-id: 57e12ae332959aa059315586e94e25fcedf6db40

--- src/Containers/MassTransit.AutoFacIntegration/MassTransitAutofacExtensions.cs
+++ src/Containers/MassTransit.AutoFacIntegration/MassTransitAutofacExtensions.cs
@@ -10,22 +10,22 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
-namespace MassTransit.AutoFacIntegration
+namespace MassTransit.AutofacIntegration
 {
     using System.Linq;
     using Autofac;
-    using Configuration;
+    using BusConfigurators;
     using Magnum.Extensions;
 
     public static class MassTransitAutofacExtensions
     {
-        public static UnsubscribeAction LoadConsumersFromContainer(this BusConfiguration config, IContainer container)
+        public static UnsubscribeAction LoadConsumersFromContainer(this ServiceBusConfigurator config, IContainer container)
         {
             var concretes = from r in container.ComponentRegistry.Registrations
                             where r.Services.Any(s => s.Implements<IConsumer>())
                             select r.Target;
 
-            //cfg.AddSubscribersByType(concretes);
+            //concretes.Each(config.RegisterSubscription);
 
             return () => true;
         }
--- src/Containers/MassTransit.WindsorIntegration/MassTransitWindsorContainerExtensions.cs
+++ src/Containers/MassTransit.WindsorIntegration/MassTransitWindsorContainerExtensions.cs
@@ -2,16 +2,16 @@ namespace MassTransit.WindsorIntegration
 {
     using System.Linq;
     using Castle.Windsor;
-    using Configuration;
+    using BusConfigurators;
 
     public static class MassTransitWindsorContainerExtensions
     {
-        public static UnsubscribeAction LoadConsumersFromContainer(this BusConfiguration cfg, IWindsorContainer container)
+        public static UnsubscribeAction LoadConsumersFromContainer(this ServiceBusConfigurator cfg, IWindsorContainer container)
         {
             var concretes = from h in container.Kernel.GetHandlers(typeof (IConsumer))
                            select h.ComponentModel.Implementation;
 
-            //cfg.AddSubscribersByType(concretes);
+            concretes.Each(cfg.RegisterSubscription);
 
             return () => true;
         }
