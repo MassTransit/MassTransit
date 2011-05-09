0e0a5c05f6e214e83600c1acf291b7d62f1904cb
Chris Patterson <chris@phatboyg.com>
2011-05-09 08:25:38 -0500

    Exchange assumptions
    Former-commit-id: 9331b96f265310cfc50ccd33b3305333fc41a054

--- src/Transports/MassTransit.Transports.RabbitMq/InboundRabbitMqTransport.cs
+++ src/Transports/MassTransit.Transports.RabbitMq/InboundRabbitMqTransport.cs
@@ -1,59 +1,72 @@
-﻿namespace MassTransit.Transports.RabbitMq
+﻿// Copyright 2007-2011 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.Transports.RabbitMq
 {
-    using System;
-    using log4net;
-    using RabbitMQ.Client;
-    using Util;
+	using System;
+	using log4net;
+	using RabbitMQ.Client;
+	using Util;
 
-    public class InboundRabbitMqTransport : IInboundTransport
-    {
-        static readonly ILog _log = LogManager.GetLogger(typeof (InboundRabbitMqTransport));
+	public class InboundRabbitMqTransport : 
+		IInboundTransport
+	{
+		static readonly ILog _log = LogManager.GetLogger(typeof (InboundRabbitMqTransport));
 
-        readonly RabbitMqAddress _address;
-        readonly IConnection _connection;
+		readonly RabbitMqAddress _address;
+		readonly IConnection _connection;
 
-        public InboundRabbitMqTransport(RabbitMqAddress address, IConnection connection)
-        {
-            _address = address;
-            _connection = connection;
-        }
+		public InboundRabbitMqTransport(RabbitMqAddress address, IConnection connection)
+		{
+			_address = address;
+			_connection = connection;
+		}
 
-        public IEndpointAddress Address { get; private set; }
-        
-        public void Receive(Func<IReceiveContext, Action<IReceiveContext>> callback, TimeSpan timeout)
-        {
-            //GuardAgainstDisposed();
+		public IEndpointAddress Address { get; private set; }
 
-            using (IModel channel = _connection.CreateModel())
-            {
-                channel.QueueDeclare(_address.Path, true, true, true, null);
+		public void Receive(Func<IReceiveContext, Action<IReceiveContext>> callback, TimeSpan timeout)
+		{
+			//GuardAgainstDisposed();
+
+			using (IModel channel = _connection.CreateModel())
+			{
+				channel.QueueDeclare(_address.Path, true, true, true, null);
                 BasicGetResult result = channel.BasicGet(_address.Path, false); //this is odd but false turns ack on.
 
-                using (var context = new RabbitMqReceiveContext(result))
-                {
-                    Action<IReceiveContext> receive = callback(context);
-                    if (receive == null)
-                    {
-                        if (_log.IsDebugEnabled)
-                            _log.DebugFormat("SKIP:{0}:{1}", Address, result.BasicProperties.MessageId);
+				using (var context = new RabbitMqReceiveContext(result))
+				{
+					Action<IReceiveContext> receive = callback(context);
+					if (receive == null)
+					{
+						if (_log.IsDebugEnabled)
+							_log.DebugFormat("SKIP:{0}:{1}", Address, result.BasicProperties.MessageId);
 
-                        if (SpecialLoggers.Messages.IsInfoEnabled)
-                            SpecialLoggers.Messages.InfoFormat("SKIP:{0}:{1}", Address, result.BasicProperties.MessageId);
-                    }
-                    else
-                    {
-                        receive(context);
-                    }
-                }
+						if (SpecialLoggers.Messages.IsInfoEnabled)
+							SpecialLoggers.Messages.InfoFormat("SKIP:{0}:{1}", Address, result.BasicProperties.MessageId);
+					}
+					else
+					{
+						receive(context);
+					}
+				}
 
-                channel.BasicAck(result.DeliveryTag, false);
-                channel.Close(200, "ok");
-            }
-        }
+				channel.BasicAck(result.DeliveryTag, false);
+				channel.Close(200, "ok");
+			}
+		}
 
-        public void Dispose()
-        {
-            _connection.Dispose();
-        }
-    }
+		public void Dispose()
+		{
+			_connection.Dispose();
+		}
+	}
 }
--- src/Transports/MassTransit.Transports.RabbitMq/RabbitMqAddress.cs
+++ src/Transports/MassTransit.Transports.RabbitMq/RabbitMqAddress.cs
@@ -1,41 +1,117 @@
+// Copyright 2007-2011 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace MassTransit.Transports.RabbitMq
 {
-    using System;
-    using System.Text.RegularExpressions;
-    using System.Threading;
-    using Magnum.Extensions;
-    using Util;
+	using System;
+	using System.Text.RegularExpressions;
+	using System.Threading;
+	using Magnum.Extensions;
+	using Util;
 
-    public class RabbitMqAddress :
-        IEndpointAddress
-    {
-        const int DEFAULT_PORT = 5432;
+	public class RabbitMqAddress :
+		IEndpointAddress
+	{
+		const int DefaultPort = 5432;
 
-		protected static readonly string LocalMachineName = Environment.MachineName.ToLowerInvariant();
-        readonly Uri _rawUri;
-        readonly Uri _rebuiltUri;
-        readonly bool _isTransactional;
-        Func<bool> _isLocal;
+		const string FormatErrorMsg =
+			"The path can be empty, or a sequence of these characters: letters, digits, hyphen, underscore, period, or colon.";
+
+		static readonly string LocalMachineName = Environment.MachineName.ToLowerInvariant();
+
+		static readonly Regex _regex = new Regex(@"^[A-Za-z0-9\-_\.:]+$");
+		readonly bool _isTransactional;
+		readonly Uri _rawUri;
+		readonly Uri _rebuiltUri;
+		Func<bool> _isLocal;
 
 
-        public RabbitMqAddress(Uri uri, Uri rebuiltUri, string username, string password, string vhost, string path, string args)
-        {
-            _rawUri = uri;
-            _rebuiltUri = rebuiltUri;
+		public RabbitMqAddress(Uri uri, Uri rebuiltUri, string username, string password, string vhost, string path,
+		                       string args)
+		{
+			_rawUri = uri;
+			_rebuiltUri = rebuiltUri;
 
-            Username = username;
-            Password = password;
-            Host = rebuiltUri.Host;
-            Path = path;
-            VHost = vhost;
-            Port = rebuiltUri.Port;
-            _isTransactional = args.GetValueFromQueryString("tx", false);
-            _isLocal = ()=>DetermineIfEndpointIsLocal(_rawUri);
-        }
+			Username = username;
+			Password = password;
+			Host = rebuiltUri.Host;
+			Path = path;
+			VHost = vhost;
+			Port = rebuiltUri.Port;
+			_isTransactional = args.GetValueFromQueryString("tx", false);
+			_isLocal = () => DetermineIfEndpointIsLocal(_rawUri);
+		}
 
-        bool DetermineIfEndpointIsLocal(Uri uri)
-        {
-            string hostName = uri.Host;
+		public int Port { get; private set; }
+		public string VHost { get; private set; }
+		public string Host { get; private set; }
+		public string Username { get; private set; }
+		public string Password { get; private set; }
+
+		public Uri RawUri
+		{
+			get { return _rawUri; }
+		}
+
+		public Uri RebuiltUri
+		{
+			get { return _rebuiltUri; }
+		}
+
+		public Uri Uri
+		{
+			get { return _rebuiltUri; }
+		}
+
+		public bool IsLocal
+		{
+			get { return _isLocal(); }
+		}
+
+		public string Path { get; private set; }
+
+		public bool IsTransactional
+		{
+			get { return _isTransactional; }
+		}
+
+		public Uri GetConnectionUri()
+		{
+			return new UriBuilder("", _rawUri.Host, _rawUri.Port, "").Uri;
+		}
+
+		public bool Equals(RabbitMqAddress other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return Equals(other._rebuiltUri, _rebuiltUri);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != typeof (RabbitMqAddress)) return false;
+			return Equals((RabbitMqAddress) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return (_rebuiltUri != null ? _rebuiltUri.GetHashCode() : 0);
+		}
+
+		bool DetermineIfEndpointIsLocal(Uri uri)
+		{
+			string hostName = uri.Host;
 			bool local = string.Compare(hostName, ".") == 0 ||
 			             string.Compare(hostName, "localhost", true) == 0 ||
 			             string.Compare(uri.Host, LocalMachineName, true) == 0;
@@ -43,132 +119,72 @@ namespace MassTransit.Transports.RabbitMq
 			Interlocked.Exchange(ref _isLocal, () => local);
 
 			return local;
-        }
+		}
 
-        public Uri GetConnectionUri()
-        {
-            return new UriBuilder("", _rawUri.Host, _rawUri.Port, "").Uri;
-        }
+		int ParsePort()
+		{
+			return _rawUri.Port == -1 ? DefaultPort : _rawUri.Port;
+		}
 
-        public int Port { get; private set; }
-        public string VHost { get; private set; }
-        public string Host { get; private set; }
-        public string Username { get; private set; }
-        public string Password { get; private set; }
-        
-        public Uri RawUri
-        {
-            get { return _rawUri; }
-        }
-        public Uri RebuiltUri
-        {
-            get { return _rebuiltUri; }
-        }
+		string ParseVHost(Uri uri)
+		{
+			string[] bits = uri.LocalPath.Split('/');
 
-        int ParsePort()
-        {
-            return _rawUri.Port == -1 ? DEFAULT_PORT : _rawUri.Port;
-        }
+			if (bits.Length == 3)
+				return @"/";
+			else
+				return bits[1];
+		}
 
-        string ParseVHost(Uri uri)
-        {
-            string[] bits = uri.LocalPath.Split('/');
+		string ParseQueue(Uri uri)
+		{
+			string[] bits = uri.LocalPath.Split('/');
+			if (bits.Length == 3)
+				return bits[2];
+			else
+				return bits[3];
+		}
 
-            if (bits.Length == 3)
-                return @"/";
-            else
-                return bits[1];
-        }
-        string ParseQueue(Uri uri)
-        {
-            string[] bits = uri.LocalPath.Split('/');
-            if (bits.Length == 3)
-                return bits[2];
-            else
-                return bits[3];
-        }
+		public static RabbitMqAddress Parse(string address)
+		{
+			return Parse(new Uri(address));
+		}
 
-        #region object overrides
-        public bool Equals(RabbitMqAddress other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return Equals(other._rebuiltUri, _rebuiltUri);
-        }
+		public static RabbitMqAddress Parse(Uri address)
+		{
+			int port = address.Port == -1 ? DefaultPort : address.Port;
+			string user = "guest";
+			string password = "guest";
+			if (!address.UserInfo.IsEmpty())
+			{
+				string[] parts = address.UserInfo.Split(':');
+				user = parts[0];
+				password = parts[1];
+			}
+			string[] pathbits = address.LocalPath.Split('/');
+			string vhost = "/";
+			string path = address.LocalPath.Substring(1); //removes first '/'
+			if (pathbits.Length == 3)
+			{
+				vhost = pathbits[1];
+				path = pathbits[2];
+			}
 
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != typeof (RabbitMqAddress)) return false;
-            return Equals((RabbitMqAddress) obj);
-        }
+			VerifyRabbitPath(path);
 
-        public override int GetHashCode()
-        {
-            return (_rebuiltUri != null ? _rebuiltUri.GetHashCode() : 0);
-        }
-        #endregion
+			var ub = new UriBuilder(address.Scheme, address.Host, port, address.LocalPath);
+			ub.UserName = user;
+			ub.Password = password;
 
-        public Uri Uri
-        {
-            get { return _rebuiltUri; }
-        }
+			return new RabbitMqAddress(address, ub.Uri, user, password, vhost, path, address.Query);
+		}
 
-        public bool IsLocal
-        {
-            get { return _isLocal(); }
-        }
+		static void VerifyRabbitPath(string path)
+		{
+			Match m = _regex.Match(path);
 
-        public string Path { get; private set; }
-
-        public bool IsTransactional
-        {
-            get { return _isTransactional; }
-        }
-
-        public static RabbitMqAddress Parse(string address)
-        {
-            return Parse(new Uri(address));
-        }
-
-        public static RabbitMqAddress Parse(Uri address)
-        {
-            var port = address.Port == -1 ? DEFAULT_PORT : address.Port;
-            var user = "guest";
-            var password = "guest";
-            if(!address.UserInfo.IsEmpty())
-            {
-                var parts = address.UserInfo.Split(':');
-                user = parts[0];
-                password = parts[1];
-            }
-            var pathbits = address.LocalPath.Split('/');
-            var vhost = "/";
-            var path = address.LocalPath.Substring(1); //removes first '/'
-            if(pathbits.Length == 3)
-            {
-                vhost = pathbits[1];
-                path = pathbits[2];
-            }
-
-            VerifyRabbitPath(path);
-
-            var ub = new UriBuilder(address.Scheme, address.Host, port, address.LocalPath);
-            ub.UserName = user;
-            ub.Password = password;
-
-            return new RabbitMqAddress(address, ub.Uri, user, password, vhost, path, address.Query);
-            
-        }
-        private const string FORMAT_ERROR_MSG = "The path can be empty, or a sequence of these characters: letters, digits, hyphen, underscore, period, or colon.";
-        static Regex _regex = new Regex(@"^[A-Za-z0-9\-_\.:]+$");
-        static void VerifyRabbitPath(string path)
-        {
-            var m = _regex.Match(path);
-
-            if(!m.Success)
-                throw new RabbitMqAddressException(FORMAT_ERROR_MSG);
-        }
-    }
+			if (!m.Success)
+				throw new RabbitMqAddressException(FormatErrorMsg);
+		}
+	}
 }
