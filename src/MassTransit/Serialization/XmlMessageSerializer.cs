468be52dacf82ca33e6da19bf7c5f22fa2a8060d
Dru Sellers <dru@drusellers.com>
2011-05-14 16:37:35 -0500

    legal and formatting

--- src/MassTransit/Serialization/XmlMessageSerializer.cs
+++ src/MassTransit/Serialization/XmlMessageSerializer.cs
@@ -1,4 +1,4 @@
-// Copyright 2007-2008 The Apache Software Foundation.
+// Copyright 2007-2011 Chris Patterson, Dru Sellers, Travis Smith, et. al.
 //  
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 // this file except in compliance with the License. You may obtain a copy of the 
@@ -12,73 +12,73 @@
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.Serialization
 {
-	using System;
-	using System.IO;
-	using System.Runtime.Serialization;
-	using Context;
-	using Custom;
+    using System;
+    using System.IO;
+    using System.Runtime.Serialization;
+    using Context;
+    using Custom;
 
-	public class XmlMessageSerializer :
-		IMessageSerializer
-	{
-		private static readonly IXmlSerializer _serializer = new CustomXmlSerializer();
+    public class XmlMessageSerializer :
+        IMessageSerializer
+    {
+        static readonly IXmlSerializer _serializer = new CustomXmlSerializer();
 
-		public void Serialize<T>(Stream stream, ISendContext<T> context) 
-			where T : class
-		{
-			try
-			{
-				var envelope = XmlMessageEnvelope.Create(context);
+        public void Serialize<T>(Stream stream, ISendContext<T> context)
+            where T : class
+        {
+            try
+            {
+                var envelope = XmlMessageEnvelope.Create(context);
 
-				_serializer.Serialize(stream, envelope, (declaringType, propertyType, value) =>
-					{
-						if (declaringType == typeof(XmlMessageEnvelope) && propertyType == typeof(object))
-							return typeof (T);
+                _serializer.Serialize(stream, envelope, (declaringType, propertyType, value) =>
+                    {
+                        if (declaringType == typeof (XmlMessageEnvelope) && propertyType == typeof (object))
+                            return typeof (T);
 
-						if (propertyType == typeof(object))
-							return value.GetType();
+                        if (propertyType == typeof (object))
+                            return value.GetType();
 
-						return propertyType;
-					});
-			}
-			catch (SerializationException)
-			{
-				throw;
-			}
-			catch (Exception ex)
-			{
-				throw new SerializationException("Failed to serialize message", ex);
-			}
-		}
+                        return propertyType;
+                    });
+            }
+            catch (SerializationException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw new SerializationException("Failed to serialize message", ex);
+            }
+        }
 
-		public object Deserialize(IReceiveContext context)
-		{
-			try
-			{
-				object message = _serializer.Deserialize(context.BodyStream);
+        public object Deserialize(IReceiveContext context)
+        {
+            try
+            {
+                object message = _serializer.Deserialize(context.BodyStream);
 
-				if (message == null)
-					throw new SerializationException("Could not deserialize message.");
+                if (message == null)
+                    throw new SerializationException("Could not deserialize message.");
 
-				if (message is XmlMessageEnvelope)
-				{
-					var envelope = message as XmlMessageEnvelope;
+                if (message is XmlMessageEnvelope)
+                {
+                    var envelope = message as XmlMessageEnvelope;
 
-					context.SetUsingMessageEnvelope(envelope);
+                    context.SetUsingMessageEnvelope(envelope);
 
-					return envelope.Message;
-				}
+                    return envelope.Message;
+                }
 
-				return message;
-			}
-			catch (SerializationException)
-			{
-				throw;
-			}
-			catch (Exception ex)
-			{
-				throw new SerializationException("Failed to serialize message", ex);
-			}
-		}
-	}
+                return message;
+            }
+            catch (SerializationException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw new SerializationException("Failed to serialize message", ex);
+            }
+        }
+    }
 }
