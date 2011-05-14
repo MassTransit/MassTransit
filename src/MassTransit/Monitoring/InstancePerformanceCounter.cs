61f64eb8a81bc6b00e4add0c6f44f361aa52064f
Dru Sellers <dru@drusellers.com>
2011-05-14 14:11:22 -0500

    Legal / Formatting
    Former-commit-id: 55edeb70663e8881d2de59a3dfed76b7da6ba683

--- src/MassTransit/Monitoring/InstancePerformanceCounter.cs
+++ src/MassTransit/Monitoring/InstancePerformanceCounter.cs
@@ -1,4 +1,4 @@
-// Copyright 2007-2008 The Apache Software Foundation.
+// Copyright 2007-2011 Chris Patterson, Dru Sellers, Travis Smith, et. al.
 //  
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 // this file except in compliance with the License. You may obtain a copy of the 
@@ -12,83 +12,83 @@
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.Monitoring
 {
-	using System;
-	using System.Diagnostics;
+    using System;
+    using System.Diagnostics;
 
-	public class InstancePerformanceCounter :
-		IPerformanceCounter
-	{
-		private bool _disposed;
-		private PerformanceCounter _pcs;
+    public class InstancePerformanceCounter :
+        IPerformanceCounter
+    {
+        bool _disposed;
+        PerformanceCounter _performanceCounter;
 
-		public InstancePerformanceCounter(string name, string categoryName, string instanceName)
-		{
-			_pcs = new PerformanceCounter(categoryName, name, instanceName, false)
-				{
-					RawValue = 0
-				};
-		}
+        public InstancePerformanceCounter(string name, string categoryName, string instanceName)
+        {
+            _performanceCounter = new PerformanceCounter(categoryName, name, instanceName, false)
+                {
+                    RawValue = 0
+                };
+        }
 
-		public string Name
-		{
-			get { return _pcs.CounterName; }
-		}
+        public string Name
+        {
+            get { return _performanceCounter.CounterName; }
+        }
 
-		public string InstanceName
-		{
-			get { return _pcs.InstanceName; }
-		}
+        public string InstanceName
+        {
+            get { return _performanceCounter.InstanceName; }
+        }
 
-		public string CategoryName
-		{
-			get { return _pcs.CategoryName; }
-		}
+        public string CategoryName
+        {
+            get { return _performanceCounter.CategoryName; }
+        }
 
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
-		public void Close()
-		{
-			if (_pcs != null)
-			{
-				_pcs.RemoveInstance();
-				_pcs.Close();
-				_pcs = null;
-			}
-		}
+        public void Close()
+        {
+            if (_performanceCounter != null)
+            {
+                _performanceCounter.RemoveInstance();
+                _performanceCounter.Close();
+                _performanceCounter = null;
+            }
+        }
 
-		public virtual void Increment()
-		{
-			_pcs.Increment();
-		}
+        public virtual void Increment()
+        {
+            _performanceCounter.Increment();
+        }
 
-		public virtual void IncrementBy(long val)
-		{
-			_pcs.IncrementBy(val);
-		}
+        public virtual void IncrementBy(long val)
+        {
+            _performanceCounter.IncrementBy(val);
+        }
 
-		public virtual void Set(long val)
-		{
-			_pcs.RawValue = val;
-		}
+        public virtual void Set(long val)
+        {
+            _performanceCounter.RawValue = val;
+        }
 
-		private void Dispose(bool disposing)
-		{
-			if (_disposed) return;
-			if (disposing)
-			{
-				Close();
-			}
+        void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+            if (disposing)
+            {
+                Close();
+            }
 
-			_disposed = true;
-		}
+            _disposed = true;
+        }
 
-		~InstancePerformanceCounter()
-		{
-			Dispose(false);
-		}
-	}
+        ~InstancePerformanceCounter()
+        {
+            Dispose(false);
+        }
+    }
 }
