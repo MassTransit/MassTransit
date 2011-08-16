c1895c588df06fc1fc7c5e1185467333447beea9
Chris Patterson <chris@phatboyg.com>
2011-08-16 09:27:35 -0500

    Adding testing for container support

---
+++ src/Containers/MassTransit.Containers.Tests/StructureMap_Specs.cs
@@ -0,0 +1,45 @@
+// Copyright 2007-2011 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.Containers.Tests
+{
+    using Magnum.TestFramework;
+    using Scenarios;
+    using StructureMap;
+    using SubscriptionConfigurators;
+
+    [Scenario]
+    public class Using_a_structuremap_simple_consumer :
+        When_resolving_a_simple_consumer
+    {
+        readonly IContainer _container;
+
+        public Using_a_structuremap_simple_consumer()
+        {
+            _container = new Container(x =>
+                {
+                    x.ForConcreteType<SimpleConsumer>();
+                });
+        }
+
+        [Finally]
+        public void Close_container()
+        {
+            _container.Dispose();
+        }
+
+        protected override void SubscribeLocalBus(SubscriptionBusServiceConfigurator subscriptionBusServiceConfigurator)
+        {
+            subscriptionBusServiceConfigurator.LoadFrom(_container);
+        }
+    }
+}
