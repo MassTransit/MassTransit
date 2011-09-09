de6ea2c72feaede749fb4f6c17a2eac7c05cbc2b
Chris Patterson <chris@phatboyg.com>
2011-09-09 08:59:29 -0500

    Amazing, only one static modifier is allowed per member.
    Former-commit-id: 95622ab32c5b7e49bdae5f6eae55f93a2f3f13fa

--- src/Transports/MassTransit.Transports.Msmq/MsmqEndpointManagement.cs
+++ src/Transports/MassTransit.Transports.Msmq/MsmqEndpointManagement.cs
@@ -1,4 +1,4 @@
-// Copyright 2007-2011 The Apache Software Foundation.
+// Copyright 2007-2011 Chris Patterson, Dru Sellers, Travis Smith, et. al.
 //  
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 // this file except in compliance with the License. You may obtain a copy of the 
@@ -12,201 +12,206 @@
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.Transports.Msmq
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Messaging;
-	using System.Security.Principal;
-	using Exceptions;
-	using log4net;
+    using System;
+    using System.Collections.Generic;
+    using System.Messaging;
+    using System.Security.Principal;
+    using Exceptions;
+    using log4net;
 
-	public class MsmqEndpointManagement :
-		IEndpointManagement
-	{
-		private static readonly string AdministratorsGroupName;
-		private static readonly string EveryoneAccountName;
-        static static readonly string AnonymousLoginAccountName;
-		private static readonly ILog _log = LogManager.GetLogger(typeof (MsmqEndpointManagement));
-		private readonly IMsmqEndpointAddress _address;
+    public class MsmqEndpointManagement :
+        IEndpointManagement
+    {
+        static readonly string AdministratorsGroupName;
+        static readonly string AnonymousLoginAccountName;
+        static readonly string EveryoneAccountName;
+        static readonly ILog _log = LogManager.GetLogger(typeof (MsmqEndpointManagement));
+        readonly IMsmqEndpointAddress _address;
 
-	    static MsmqEndpointManagement()
-		{
-			// WellKnowSidType.WorldSid == "Everyone"; 
-			// whoda thunk (http://social.msdn.microsoft.com/forums/en-US/netfxbcl/thread/0737f978-a998-453d-9a6a-c348285d7ea3/)
+        static MsmqEndpointManagement()
+        {
+            // WellKnowSidType.WorldSid == "Everyone"; 
+            // whoda thunk (http://social.msdn.microsoft.com/forums/en-US/netfxbcl/thread/0737f978-a998-453d-9a6a-c348285d7ea3/)
 
-			AdministratorsGroupName = (new SecurityIdentifier(WellKnownSidType.BuiltinAdministratorsSid, null))
-				.Translate(typeof (NTAccount)).ToString();
-			EveryoneAccountName = (new SecurityIdentifier(WellKnownSidType.WorldSid, null))
-				.Translate(typeof (NTAccount)).ToString();
-		    AnonymousLoginAccountName = (new SecurityIdentifier(WellKnownSidType.AnonymousSid, null))
-		        .Translate(typeof (NTAccount)).ToString();
-		}
+            AdministratorsGroupName = (new SecurityIdentifier(WellKnownSidType.BuiltinAdministratorsSid, null))
+                .Translate(typeof (NTAccount)).ToString();
+            EveryoneAccountName = (new SecurityIdentifier(WellKnownSidType.WorldSid, null))
+                .Translate(typeof (NTAccount)).ToString();
+            AnonymousLoginAccountName = (new SecurityIdentifier(WellKnownSidType.AnonymousSid, null))
+                .Translate(typeof (NTAccount)).ToString();
+        }
 
-		private MsmqEndpointManagement(IEndpointAddress address)
-		{
-			_address = address as MsmqEndpointAddress ?? new MsmqEndpointAddress(address.Uri);
-		}
+        MsmqEndpointManagement(IEndpointAddress address)
+        {
+            _address = address as MsmqEndpointAddress ?? new MsmqEndpointAddress(address.Uri);
+        }
 
-		public void Create(bool transactional)
-		{
-			if (!_address.IsLocal)
-				throw new InvalidOperationException("A remote queue cannot be created: " + _address);
+        public void Create(bool transactional)
+        {
+            if (!_address.IsLocal)
+                throw new InvalidOperationException("A remote queue cannot be created: " + _address);
 
-			if (CheckForExistingQueue(transactional))
-				return;
+            if (CheckForExistingQueue(transactional))
+                return;
 
-			using (MessageQueue queue = MessageQueue.Create(_address.LocalName, transactional))
-			{
-				_log.Debug("A queue was created: " + _address + (transactional ? " (transactional)" : ""));
+            using (MessageQueue queue = MessageQueue.Create(_address.LocalName, transactional))
+            {
+                _log.Debug("A queue was created: " + _address + (transactional ? " (transactional)" : ""));
 
-				queue.SetPermissions(AdministratorsGroupName, MessageQueueAccessRights.FullControl, AccessControlEntryType.Allow);
-				queue.SetPermissions(EveryoneAccountName, MessageQueueAccessRights.GenericWrite, AccessControlEntryType.Allow);
-                queue.SetPermissions(AnonymousLoginAccountName, MessageQueueAccessRights.GenericWrite, AccessControlEntryType.Allow);
-			}
+                queue.SetPermissions(AdministratorsGroupName, MessageQueueAccessRights.FullControl,
+                    AccessControlEntryType.Allow);
+                queue.SetPermissions(EveryoneAccountName, MessageQueueAccessRights.GenericWrite,
+                    AccessControlEntryType.Allow);
+                queue.SetPermissions(AnonymousLoginAccountName, MessageQueueAccessRights.GenericWrite,
+                    AccessControlEntryType.Allow);
+            }
 
-			VerifyQueueSendAndReceive();
-		}
+            VerifyQueueSendAndReceive();
+        }
 
-		public long Count()
-		{
-			return Count(long.MaxValue);
-		}
+        public long Count()
+        {
+            return Count(long.MaxValue);
+        }
 
-		public long Count(long limit)
-		{
-			long count = 0;
+        public long Count(long limit)
+        {
+            long count = 0;
 
-			using (var queue = new MessageQueue(_address.InboundFormatName, QueueAccessMode.Receive))
-			{
-				using (MessageEnumerator enumerator = queue.GetMessageEnumerator2())
-				{
-					while (enumerator.MoveNext(TimeSpan.Zero))
-					{
-						if (++count >= limit)
-							break;
-					}
-				}
-			}
+            using (var queue = new MessageQueue(_address.InboundFormatName, QueueAccessMode.Receive))
+            {
+                using (MessageEnumerator enumerator = queue.GetMessageEnumerator2())
+                {
+                    while (enumerator.MoveNext(TimeSpan.Zero))
+                    {
+                        if (++count >= limit)
+                            break;
+                    }
+                }
+            }
 
-			return count;
-		}
+            return count;
+        }
 
-		public void Purge()
-		{
-			using (var queue = new MessageQueue(_address.LocalName, QueueAccessMode.ReceiveAndAdmin))
-			{
-				queue.Purge();
-			}
-		}
+        public void Purge()
+        {
+            using (var queue = new MessageQueue(_address.LocalName, QueueAccessMode.ReceiveAndAdmin))
+            {
+                queue.Purge();
+            }
+        }
 
-		public bool Exists
-		{
-			get { return MessageQueue.Exists(_address.LocalName); }
-		}
+        public bool Exists
+        {
+            get { return MessageQueue.Exists(_address.LocalName); }
+        }
 
-		public bool IsTransactional
-		{
-			get
-			{
-				if (!_address.IsLocal)
-					return true;
+        public bool IsTransactional
+        {
+            get
+            {
+                if (!_address.IsLocal)
+                    return true;
 
-				using (var queue = new MessageQueue(_address.InboundFormatName, QueueAccessMode.ReceiveAndAdmin))
-				{
-					return queue.Transactional;
-				}
-			}
-		}
+                using (var queue = new MessageQueue(_address.InboundFormatName, QueueAccessMode.ReceiveAndAdmin))
+                {
+                    return queue.Transactional;
+                }
+            }
+        }
 
-		public Dictionary<string, int> MessageTypes()
-		{
-			var dict = new Dictionary<string, int>();
+        public Dictionary<string, int> MessageTypes()
+        {
+            var dict = new Dictionary<string, int>();
 
-			using (var queue = new MessageQueue(_address.InboundFormatName, QueueAccessMode.Receive))
-			{
-				using (MessageEnumerator enumerator = queue.GetMessageEnumerator2())
-				{
-					while (enumerator.MoveNext(TimeSpan.Zero))
-					{
-						Message q = enumerator.Current;
-						if (!dict.ContainsKey(q.Label))
-							dict.Add(q.Label, 0);
+            using (var queue = new MessageQueue(_address.InboundFormatName, QueueAccessMode.Receive))
+            {
+                using (MessageEnumerator enumerator = queue.GetMessageEnumerator2())
+                {
+                    while (enumerator.MoveNext(TimeSpan.Zero))
+                    {
+                        Message q = enumerator.Current;
+                        if (!dict.ContainsKey(q.Label))
+                            dict.Add(q.Label, 0);
 
-						dict[q.Label]++;
-					}
-				}
-			}
+                        dict[q.Label]++;
+                    }
+                }
+            }
 
-			return dict;
-		}
+            return dict;
+        }
 
-		private void VerifyQueueSendAndReceive()
-		{
-			using (var queue = new MessageQueue(_address.InboundFormatName, QueueAccessMode.SendAndReceive))
-			{
-				if (!queue.CanRead)
-					throw new InvalidOperationException("A queue was created but cannot be read: " + _address);
+        void VerifyQueueSendAndReceive()
+        {
+            using (var queue = new MessageQueue(_address.InboundFormatName, QueueAccessMode.SendAndReceive))
+            {
+                if (!queue.CanRead)
+                    throw new InvalidOperationException("A queue was created but cannot be read: " + _address);
 
-				if (!queue.CanWrite)
-					throw new InvalidOperationException("A queue was created but cannot be written: " + _address);
-			}
-		}
+                if (!queue.CanWrite)
+                    throw new InvalidOperationException("A queue was created but cannot be written: " + _address);
+            }
+        }
 
-		private bool CheckForExistingQueue(bool transactional)
-		{
-			if (!MessageQueue.Exists(_address.LocalName))
-				return false;
+        bool CheckForExistingQueue(bool transactional)
+        {
+            if (!MessageQueue.Exists(_address.LocalName))
+                return false;
 
-			using (var queue = new MessageQueue(_address.InboundFormatName, QueueAccessMode.ReceiveAndAdmin))
-			{
-				if (transactional && !queue.Transactional)
-				{
-					throw new InvalidOperationException("A non-transactional queue already exists with the same name: " + _address);
-				}
+            using (var queue = new MessageQueue(_address.InboundFormatName, QueueAccessMode.ReceiveAndAdmin))
+            {
+                if (transactional && !queue.Transactional)
+                {
+                    throw new InvalidOperationException(
+                        "A non-transactional queue already exists with the same name: " + _address);
+                }
 
-				if (!transactional && queue.Transactional)
-				{
-					throw new InvalidOperationException("A transactional queue already exists with the same name: " + _address);
-				}
-			}
+                if (!transactional && queue.Transactional)
+                {
+                    throw new InvalidOperationException("A transactional queue already exists with the same name: " +
+                                                        _address);
+                }
+            }
 
-			_log.Debug("The queue already exists: " + _address);
-			return true;
-		}
+            _log.Debug("The queue already exists: " + _address);
+            return true;
+        }
 
-		public static void Manage(IEndpointAddress address, Action<IEndpointManagement> action)
-		{
-			try
-			{
-				var management = new MsmqEndpointManagement(address);
-				action(management);
-			}
-			catch (EndpointException)
-			{
-				throw;
-			}
-			catch (Exception ex)
-			{
-				throw new TransportException(address.Uri, "There was a problem managing the transport", ex);
-			}
-		}
+        public static void Manage(IEndpointAddress address, Action<IEndpointManagement> action)
+        {
+            try
+            {
+                var management = new MsmqEndpointManagement(address);
+                action(management);
+            }
+            catch (EndpointException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw new TransportException(address.Uri, "There was a problem managing the transport", ex);
+            }
+        }
 
-		public static IEndpointManagement New(Uri uri)
-		{
-			try
-			{
-				var address = new MsmqEndpointAddress(uri);
+        public static IEndpointManagement New(Uri uri)
+        {
+            try
+            {
+                var address = new MsmqEndpointAddress(uri);
 
-				return New(address);
-			}
-			catch (UriFormatException ex)
-			{
-				throw new EndpointException(uri, "The MSMQ queue address is invalid.", ex);
-			}
-		}
+                return New(address);
+            }
+            catch (UriFormatException ex)
+            {
+                throw new EndpointException(uri, "The MSMQ queue address is invalid.", ex);
+            }
+        }
 
-		public static IEndpointManagement New(IEndpointAddress address)
-		{
-			return new MsmqEndpointManagement(address);
-		}
-	}
+        public static IEndpointManagement New(IEndpointAddress address)
+        {
+            return new MsmqEndpointManagement(address);
+        }
+    }
 }
