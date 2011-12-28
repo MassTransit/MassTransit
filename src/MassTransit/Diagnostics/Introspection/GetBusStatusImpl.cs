f609d47aa4a9ca7fbd614aac952712c27988933a
Dru Sellers <dru@drusellers.com>
2011-12-28 10:30:20 -0600

    first pass at adding bus driver 'status' action
    Former-commit-id: d2cf1c9713190dde2ce2a674b3f4c42a2d52ed0e

--- src/MassTransit/Diagnostics/Introspection/GetBusStatusImpl.cs
+++ src/MassTransit/Diagnostics/Introspection/GetBusStatusImpl.cs
@@ -12,7 +12,8 @@
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.Diagnostics.Introspection
 {
-    public interface GetBusStatus
+    public class GetBusStatusImpl :
+        GetBusStatus
     {
     }
 }
