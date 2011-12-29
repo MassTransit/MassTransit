c38bb2e50053d7a25eb0e9bee998c9aeeb0c8ff8
Dru Sellers <dru@drusellers.com>
2011-12-29 09:58:25 -0600

    cleaning up a small mess

--- src/MassTransit/Diagnostics/Introspection/IntrospectionBusBuilder.cs
+++ src/MassTransit/Diagnostics/Introspection/IntrospectionBusBuilder.cs
@@ -12,7 +12,6 @@
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.Diagnostics.Introspection
 {
-    using System;
     using System.Collections.Generic;
     using Builders;
     using BusConfigurators;
@@ -21,32 +20,14 @@ namespace MassTransit.Diagnostics.Introspection
     public class IntrospectionBusBuilder :
         BusBuilderConfigurator
     {
-        readonly Action<DiagnosticsProbe> _writeAction;
-
-        public IntrospectionBusBuilder(Action<DiagnosticsProbe> action)
-        {
-            _writeAction = action;
-        }
-
         public IEnumerable<ValidationResult> Validate()
         {
-            if (_writeAction == null)
-                yield return this.Failure("introspection", "You must provide a write method");
-            else
-                yield return this.Success("Introspection has been turned on.");
+            yield return this.Success("Remote Introspection has been turned on.");
         }
 
         public BusBuilder Configure(BusBuilder builder)
         {
-            var probe = new InMemoryDiagnosticsProbe();
             builder.AddBusServiceConfigurator(new IntrospectionServiceConfigurator());
-            builder.AddPostCreateAction(bus =>
-                {
-                    bus.Inspect(probe);
-
-
-                    _writeAction(probe);
-                });
 
             return builder;
         }
