0abbda8f7b367bff61fd24335f526a6236e7c43b
Chris Patterson <chris@phatboyg.com>
2012-01-10 07:56:04 -0600

    Static tests
    Former-commit-id: dcd4200174d0ae50f77effed9f58821822698424

---
+++ src/MassTransit.Tests/Subscriptions/DynamicAndStaticSubscriptions_Specs.cs
@@ -0,0 +1,82 @@
+﻿namespace MassTransit.Tests.Subscriptions
+{
+    using System.Linq;
+    using Magnum.Extensions;
+    using Magnum.TestFramework;
+    using NUnit.Framework;
+    using TextFixtures;
+    using MassTransit.Testing;
+
+    [TestFixture]
+    public class When_a_static_and_dynamic_subscription_are_on_the_same_bus :
+        LoopbackLocalAndRemoteTestFixture
+    {
+        FutureMessage<A> _receivedA;
+
+        public When_a_static_and_dynamic_subscription_are_on_the_same_bus()
+        {
+            _receivedA = new FutureMessage<A>();
+        }
+
+        protected override void ConfigureRemoteBus(BusConfigurators.ServiceBusConfigurator configurator)
+        {
+            base.ConfigureRemoteBus(configurator);
+
+            configurator.Subscribe(x =>
+                {
+                    x.Handler<A>((context, message) => _receivedA.Set(message));
+                });
+        }
+
+        protected override void EstablishContext()
+        {
+            base.EstablishContext();
+
+            LocalBus.HasSubscription<A>(8.Seconds()).Any().ShouldBeTrue("No subscription found on local bus");
+        }
+
+        [Test]
+        public void Dynamic_should_receive_the_message()
+        {
+            var receivedDynamic = new FutureMessage<A>();
+
+            UnsubscribeAction subscription = RemoteBus.SubscribeHandler<A>(receivedDynamic.Set);
+            using (subscription.Disposable())
+            {
+                LocalBus.Publish(new A());
+
+                receivedDynamic.IsAvailable(8.Seconds()).ShouldBeTrue("Dynamic not received");
+                _receivedA.IsAvailable(8.Seconds()).ShouldBeTrue("Static not received");
+            }
+        }
+
+        [Test]
+        public void Static_should_remain_after_dynamic_removed()
+        {
+            var receivedDynamic = new FutureMessage<A>();
+
+            UnsubscribeAction subscription = RemoteBus.SubscribeHandler<A>(receivedDynamic.Set);
+            using (subscription.Disposable())
+            {
+            }
+
+            LocalBus.Publish(new A());
+
+            receivedDynamic.IsAvailable(8.Seconds()).ShouldBeFalse("Dynamic should not have received");
+            _receivedA.IsAvailable(8.Seconds()).ShouldBeTrue("Static not received");
+        }
+
+        [Test]
+        public void Static_should_receive_the_message()
+        {
+            LocalBus.Publish(new A());
+
+            _receivedA.IsAvailable(8.Seconds()).ShouldBeTrue();
+        }
+
+        class A
+        {
+            public string Value { get; set; }
+        }
+    }
+}
