a977a2d20577d7b2813353c696922693f6dd1725
Chris Patterson <chris@phatboyg.com>
2012-01-16 13:13:23 -0600

    More unit tests on sagas, and subscribers

--- src/Containers/MassTransit.Containers.Tests/CastleWindsor_Specs.cs
+++ src/Containers/MassTransit.Containers.Tests/CastleWindsor_Specs.cs
@@ -12,12 +12,16 @@
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.Containers.Tests
 {
+    using System;
+    using System.Linq;
     using Castle.MicroKernel.Registration;
     using Castle.Windsor;
     using Magnum.TestFramework;
     using Saga;
     using Scenarios;
     using SubscriptionConfigurators;
+    using Testing;
+    using Util;
 
     [Scenario]
     public class Castle_Consumer :
@@ -69,6 +73,72 @@ namespace MassTransit.Containers.Tests
         protected override void SubscribeLocalBus(SubscriptionBusServiceConfigurator subscriptionBusServiceConfigurator)
         {
             subscriptionBusServiceConfigurator.LoadFrom(_container);
+        }
+    }
+
+    [Scenario]
+    public class Test_Bus_Subscriptions_For_Consumers_In_Dummy_Saga_Using_Castle_As_IoC :
+        Given_a_service_bus_instance
+    {
+        readonly IWindsorContainer _container;
+
+        public Test_Bus_Subscriptions_For_Consumers_In_Dummy_Saga_Using_Castle_As_IoC()
+        {
+            _container = new WindsorContainer();
+            _container.Register(
+                Component.For<DummySaga>(),
+                Component.For(typeof(ISagaRepository<>))
+                    .ImplementedBy(typeof(InMemorySagaRepository<>))
+                    .LifeStyle.Singleton);
+        }
+
+        [Finally]
+        public void Close_container()
+        {
+            _container.Dispose();
+        }
+
+        protected override void SubscribeLocalBus(SubscriptionBusServiceConfigurator subscriptionBusServiceConfigurator)
+        {
+            subscriptionBusServiceConfigurator.LoadFrom(_container);
+        }
+
+        [When]
+        public void Registering_a_dummy_saga()
+        {
+        }
+
+        [Then]
+        public void Should_have_a_subscription_for_the_first_saga_message()
+        {
+            LocalBus.HasSubscription<FirstSagaMessage>().Count()
+                .ShouldEqual(1, "No subscription for the FirstSagaMessage was found.");
+        }
+
+        [Then]
+        public void Should_have_a_subscription_for_the_second_saga_message()
+        {
+            LocalBus.HasSubscription<SecondSagaMessage>().Count()
+                .ShouldEqual(1, "No subscription for the SecondSagaMessage was found.");
+        }
+
+        [UsedImplicitly]
+        class DummySaga : ISaga, InitiatedBy<FirstSagaMessage>, Orchestrates<SecondSagaMessage>
+        {
+            public Guid CorrelationId
+            {
+                get { return Guid.Empty; }
+            }
+
+            public IServiceBus Bus { get; set; }
+
+            public void Consume(FirstSagaMessage message)
+            {
+            }
+
+            public void Consume(SecondSagaMessage message)
+            {
+            }
         }
     }
 }
--- src/MassTransit/Util/IndexedSagaProperty.cs
+++ src/MassTransit/Util/IndexedSagaProperty.cs
@@ -12,100 +12,152 @@
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.Util
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Reflection;
-	using Magnum.Reflection;
-	using Saga;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using Saga;
 
-	public interface IndexedSagaProperty<TSaga>
-		where TSaga : class, ISaga
-	{
-		void Add(TSaga newItem);
-		void Remove(TSaga item);
-		
-		TSaga this[object key] { get; }
+    /// <summary>
+    /// For the in-memory saga repository, this maintains an index of saga properties
+    /// for fast searching
+    /// </summary>
+    /// <typeparam name="TSaga">The saga type</typeparam>
+    public interface IndexedSagaProperty<TSaga>
+        where TSaga : class, ISaga
+    {
+        /// <summary>
+        /// Returns the saga with the specified key
+        /// </summary>
+        /// <param name="key"></param>
+        TSaga this[object key] { get; }
 
-		IEnumerable<TSaga> Where(Func<TSaga, bool> filter);
-		IEnumerable<TSaga> Where(object key, Func<TSaga, bool> filter);
+        /// <summary>
+        /// Adds a new saga to the index
+        /// </summary>
+        /// <param name="newItem"></param>
+        void Add(TSaga newItem);
 
-		IEnumerable<TResult> Select<TResult>(Func<TSaga, TResult> transformer);
-	}
+        /// <summary>
+        /// Removes a saga from the index
+        /// </summary>
+        /// <param name="item"></param>
+        void Remove(TSaga item);
 
-	public class IndexedSagaProperty<TSaga, TProperty> :
-		IndexedSagaProperty<TSaga>
-		where TSaga : class, ISaga
-	{
-		readonly FastProperty<TSaga, TProperty> _property;
-		readonly IDictionary<TProperty, HashSet<TSaga>> _values;
+        /// <summary>
+        /// Returns sagas matching the filter function
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        IEnumerable<TSaga> Where(Func<TSaga, bool> filter);
 
-		public IndexedSagaProperty(PropertyInfo propertyInfo)
-		{
-			_values = new Dictionary<TProperty, HashSet<TSaga>>();
-			_property = new FastProperty<TSaga, TProperty>(propertyInfo, BindingFlags.NonPublic);
-		}
+        /// <summary>
+        /// Returns sagas matching the filter function where the key also matches
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        IEnumerable<TSaga> Where(object key, Func<TSaga, bool> filter);
 
-		public TSaga this[object key]
-		{
-			get
-			{
-				var keyValue = (TProperty) key;
+        /// <summary>
+        /// Selects sagas from the index, running the transformation function and returning the output type
+        /// </summary>
+        /// <param name="transformer"></param>
+        /// <typeparam name="TResult"></typeparam>
+        /// <returns></returns>
+        IEnumerable<TResult> Select<TResult>(Func<TSaga, TResult> transformer);
+    }
 
-				HashSet<TSaga> result;
-				if (_values.TryGetValue(keyValue, out result))
-					return result.Single();
+    /// <summary>
+    /// A dictionary index of the sagas
+    /// </summary>
+    /// <typeparam name="TSaga"></typeparam>
+    /// <typeparam name="TProperty"></typeparam>
+    public class IndexedSagaProperty<TSaga, TProperty> :
+        IndexedSagaProperty<TSaga>
+        where TSaga : class, ISaga
+    {
+        readonly Func<TSaga, TProperty> _getProperty;
+        readonly IDictionary<TProperty, HashSet<TSaga>> _values;
 
-				return null;
-			}
-		}
+        /// <summary>
+        /// Creates an index for the specified property of a saga
+        /// </summary>
+        /// <param name="propertyInfo"></param>
+        public IndexedSagaProperty(PropertyInfo propertyInfo)
+        {
+            _values = new Dictionary<TProperty, HashSet<TSaga>>();
+            _getProperty = GetGetMethod(propertyInfo);
+        }
 
-		public void Add(TSaga newItem)
-		{
-			TProperty key = _property.Get(newItem);
+        public TSaga this[object key]
+        {
+            get
+            {
+                var keyValue = (TProperty) key;
 
-			HashSet<TSaga> hashSet;
-			if (!_values.TryGetValue(key, out hashSet))
-			{
-				hashSet = new HashSet<TSaga>();
-				_values.Add(key, hashSet);
-			}
+                HashSet<TSaga> result;
+                if (_values.TryGetValue(keyValue, out result))
+                    return result.Single();
 
-			hashSet.Add(newItem);
-		}
+                return null;
+            }
+        }
 
-		public void Remove(TSaga item)
-		{
-			TProperty key = _property.Get(item);
+        public void Add(TSaga newItem)
+        {
+            TProperty key = _getProperty(newItem);
 
-			HashSet<TSaga> hashSet;
-			if (!_values.TryGetValue(key, out hashSet))
-				return;
+            HashSet<TSaga> hashSet;
+            if (!_values.TryGetValue(key, out hashSet))
+            {
+                hashSet = new HashSet<TSaga>();
+                _values.Add(key, hashSet);
+            }
 
-			hashSet.Remove(item);
-		}
+            hashSet.Add(newItem);
+        }
 
-		public IEnumerable<TSaga> Where(Func<TSaga, bool> filter)
-		{
-			return _values.Values.SelectMany(x => x).Where(filter);
-		}
+        public void Remove(TSaga item)
+        {
+            TProperty key = _getProperty(item);
 
-		public IEnumerable<TSaga> Where(object key, Func<TSaga, bool> filter)
-		{
-			var keyValue = (TProperty) key;
+            HashSet<TSaga> hashSet;
+            if (!_values.TryGetValue(key, out hashSet))
+                return;
 
-			HashSet<TSaga> resultSet;
-			if (_values.TryGetValue(keyValue, out resultSet))
-			{
-				return resultSet.Where(filter);
-			}
+            hashSet.Remove(item);
+        }
 
-			return Enumerable.Empty<TSaga>();
-		}
+        public IEnumerable<TSaga> Where(Func<TSaga, bool> filter)
+        {
+            return _values.Values.SelectMany(x => x).Where(filter);
+        }
 
-		public IEnumerable<TResult> Select<TResult>(Func<TSaga, TResult> transformer)
-		{
-			return _values.Values.SelectMany(x => x).Select(transformer);
-		}
-	}
+        public IEnumerable<TSaga> Where(object key, Func<TSaga, bool> filter)
+        {
+            var keyValue = (TProperty) key;
+
+            HashSet<TSaga> resultSet;
+            if (_values.TryGetValue(keyValue, out resultSet))
+            {
+                return resultSet.Where(filter);
+            }
+
+            return Enumerable.Empty<TSaga>();
+        }
+
+        public IEnumerable<TResult> Select<TResult>(Func<TSaga, TResult> transformer)
+        {
+            return _values.Values.SelectMany(x => x).Select(transformer);
+        }
+
+        static Func<TSaga, TProperty> GetGetMethod(PropertyInfo property)
+        {
+            ParameterExpression parameterExpression = Expression.Parameter(typeof (TSaga), "instance");
+            return Expression.Lambda<Func<TSaga, TProperty>>(
+                Expression.Call(parameterExpression, property.GetGetMethod()), new[] {parameterExpression}).Compile();
+        }
+    }
 }
