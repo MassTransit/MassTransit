47bd774ce315f5c090f53d9f40c76a6d5d767491
Henrik <henrik@haf.se>
2012-01-24 11:03:56 +0100

    removed old nuspecs (they are build in rakefile) and fixed versioning - so it seems to be working. Also added :versioning requirement for nuget tasks and for build task. Fixed a tiny bit of formatting. Merged with new log4net integration nuget.

--- build_support/versioning.rb
+++ build_support/versioning.rb
@@ -12,10 +12,17 @@ def commit_data
   [commit, commit_date]
 end
 
+def version(str)
+  ver = /v?(\d+)\.(\d+)\.(\d+)\.?(\d+)?/i.match(str).to_a()
+  ver[1,4].map{|s|s.to_i} unless ver == nil or ver.empty?
+end
+
 task :versioning do
     fv = version SemVer.find.to_s
     #revision = (!fv[3] || fv[3] == 0) ? (ENV['BUILD_NUMBER'] || Time.now.strftime('%j%H')) : fv[3] #  (day of year 0-265)(hour 00-24)
-    revision = ENV['BUILD_NUMBER'].to_i || fv[3]
+    revision = (ENV['BUILD_NUMBER'] || fv[2]).to_i
     ENV['BUILD_VERSION'] = BUILD_VERSION = "#{ SemVer.new(fv[0], fv[1], revision).format "%M.%m.%p" }-#{commit_data()[0]}"
     ENV['FORMAL_VERSION'] = FORMAL_VERSION = "#{ SemVer.new(fv[0], fv[1], revision).format "%M.%m.%p"}.0"
+
+    puts "##teamcity[buildNumber '#{BUILD_VERSION}']" # tell teamcity our decision
 end
