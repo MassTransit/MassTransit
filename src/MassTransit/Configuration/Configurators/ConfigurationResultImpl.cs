4789bff436b13c771a2ba9b6d468e488a9951a39
Henrik <henrik@haf.se>
2012-01-24 09:35:27 +0100

    fixes for .Net 3.5, missing methods of 4.0
    Former-commit-id: d4345b09798779f7ab48e2c8a8e2e7677159107f

--- src/MassTransit/Configuration/Configurators/ConfigurationResultImpl.cs
+++ src/MassTransit/Configuration/Configurators/ConfigurationResultImpl.cs
@@ -43,8 +43,17 @@ namespace MassTransit.Configurators
 		[UsedImplicitly]
 		protected string DebuggerString()
 		{
+#if NET40
 			var debuggerString = string.Join(", ", _results);
+#else
+			var debuggerString = string.Join(", ", _results.Select(x => x.ToString()).ToArray());
+#endif
+
+#if NET40
 			return string.IsNullOrWhiteSpace(debuggerString)
+#else
+			return string.IsNullOrEmpty(debuggerString)
+#endif
 				? "No Obvious Problems says ConfigurationResult"
 				: debuggerString;
 		}
