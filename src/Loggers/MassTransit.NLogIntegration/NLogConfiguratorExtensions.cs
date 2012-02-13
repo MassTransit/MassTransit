1dd5a56108ab33765a9ea1fbc975105e74ae7fae
Henrik <henrik@haf.se>
2012-02-13 17:45:08 +0100

    deleted assemblyinfo that caused build failure, added documentation in log4net and nlog projects on public APIs and made log4netintegration dll have its partner XML file alongside

--- src/Loggers/MassTransit.NLogIntegration/NLogConfiguratorExtensions.cs
+++ src/Loggers/MassTransit.NLogIntegration/NLogConfiguratorExtensions.cs
@@ -1,11 +1,19 @@
 ﻿using MassTransit.BusConfigurators;
 using MassTransit.Logging;
+using MassTransit.Util;
 
 namespace MassTransit.NLogIntegration
 {
+	/// <summary>
+	/// Extensions for configuring NLog with MassTransit
+	/// </summary>
 	public static class NLogConfiguratorExtensions
 	{
-		public static void UseNLog(this ServiceBusConfigurator configrator)
+		/// <summary>
+		/// Specify that you want to use the NLog logging framework with MassTransit.
+		/// </summary>
+		/// <param name="configrator">Optional service bus configurator</param>
+		public static void UseNLog([CanBeNull] this ServiceBusConfigurator configrator)
 		{
 			Logger.UseLogger(new NLogLogger());
 		}
