3c1e42ec4c17e2eb8e834b4ca770e2cecfed344d
Chris Patterson <chris@phatboyg.com>
2012-02-13 13:57:17 -0600

    Remaining bits of NLog integration
    Former-commit-id: 63a68ebb3690e0fc5b8948a1623c6b425c109cd2

--- src/Loggers/MassTransit.Log4NetIntegration/Log4NetConfigurationExtensions.cs
+++ src/Loggers/MassTransit.Log4NetIntegration/Log4NetConfigurationExtensions.cs
@@ -10,15 +10,24 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
+
+using MassTransit.Util;
+
 namespace MassTransit.Log4NetIntegration
 {
     using BusConfigurators;
     using Logging;
 
+	/// <summary>
+	/// Extensions for configuring MassTransit for log4net
+	/// </summary>
     public static class Log4NetConfigurationExtensions
     {
-
-        public static void UseLog4Net(this ServiceBusConfigurator configurator)
+		/// <summary>
+		/// Specify that you want to use the Log4net logging engine for logging with MassTransit.
+		/// </summary>
+		/// <param name="configurator"></param>
+        public static void UseLog4Net([CanBeNull] this ServiceBusConfigurator configurator)
         {
             MassTransit.Logging.Logger.UseLogger(new Log4NetLogger());
         }
