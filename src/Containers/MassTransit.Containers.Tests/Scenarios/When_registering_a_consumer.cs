cb4a53b33b005eb30ba426d54fee08e35416b46f
Chris Patterson <chris@phatboyg.com>
2012-03-29 10:39:07 -0500

    INcrease timeout
    Former-commit-id: 1cd9b6d93b48274179fa8b1859a0040b6878390b

--- src/Containers/MassTransit.Containers.Tests/Scenarios/When_registering_a_consumer.cs
+++ src/Containers/MassTransit.Containers.Tests/Scenarios/When_registering_a_consumer.cs
@@ -1,18 +1,21 @@
-// Copyright 2007-2011 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+// Copyright 2007-2012 Chris Patterson, Dru Sellers, Travis Smith, et. al.
 //  
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file except in compliance with the License. You may obtain a copy of the 
 // License at 
 // 
 //     http://www.apache.org/licenses/LICENSE-2.0 
 // 
-// Unless required by applicable law or agreed to in writing, software distributed 
+// Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
+
 namespace MassTransit.Containers.Tests.Scenarios
 {
     using System.Linq;
+    using System.Threading;
+    using Magnum.Extensions;
     using Magnum.TestFramework;
     using Testing;
 
@@ -44,7 +47,12 @@ namespace MassTransit.Containers.Tests.Scenarios
         {
             const string name = "Joe";
 
+            var complete = new ManualResetEvent(false);
+
+            LocalBus.SubscribeHandler<SimpleMessageClass>(x => complete.Set());
             LocalBus.Publish(new SimpleMessageClass(name));
+
+            complete.WaitOne(8.Seconds());
 
             GetSimpleConsumer()
                 .Last.Name.ShouldEqual(name);
