d134f1240fc1c2b25d45877d2d2ef183d88a47c0
Chris Patterson <chris@phatboyg.com>
2012-04-09 08:36:08 -0500

    Added SolutionVersion so people could build out of the box with Visual Studio without using Rake

---
+++ src/SolutionVersion.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+[assembly: AssemblyDescription("MassTransit is a distributed application framework for .NET http://masstransit-project.com")]
+[assembly: AssemblyProduct("MassTransit")]
+[assembly: AssemblyCopyright("Copyright 2007-2011 Chris Patterson, Dru Sellers, Travis Smith, et. al. - All rights reserved.")]
+[assembly: AssemblyVersion("2.1.1")]
+[assembly: AssemblyFileVersion("2.1.1")]
+
+[assembly: AssemblyInformationalVersion("2.1.1.5dd8fe")]
+[assembly: ComVisibleAttribute(false)]
+[assembly: CLSCompliantAttribute(false)]
+
