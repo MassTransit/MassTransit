5df3ee0ca56c00b8b7fb6bec386fc646ae33a7b0
NikGovorov <nikita.govorov@gmail.com>
2012-04-14 13:29:01 +1000

    Workaround to get working multi cast subscription client on machines with .net 4.5; Details: https://connect.microsoft.com/VisualStudio/feedback/details/736877/msmq-multicast-in-system-messaging

--- src/Transports/MassTransit.Transports.Msmq/MessageQueueConnection.cs
+++ src/Transports/MassTransit.Transports.Msmq/MessageQueueConnection.cs
@@ -98,6 +98,14 @@ namespace MassTransit.Transports.Msmq
 			{
 				_queue.MulticastAddress = _multicastAddress;
 			}
+			
+            //https://connect.microsoft.com/VisualStudio/feedback/details/736877/msmq-multicast-in-system-messaging
+            // ReSharper disable EmptyGeneralCatchClause
+            // ReSharper disable UnusedVariable
+            try { var touch = _queue.FormatName; } catch { }
+            // ReSharper restore UnusedVariable
+            // ReSharper restore EmptyGeneralCatchClause
+
 
 			var filter = new MessagePropertyFilter();
 			filter.SetAll();
