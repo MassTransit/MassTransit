6e6cf8fde8af8205f6d5dd4f5e792e5ad1b9d2d9
Chris Patterson <chris@phatboyg.com>
2012-04-24 17:01:53 -0500

    Updated distributor sample to use new API
    Former-commit-id: 8c06ca8eca92f1b30362f88f68c8d798277128b3

--- src/Samples/Distributor/Grid.Distributor.Activator/CollectCompletedWork.cs
+++ src/Samples/Distributor/Grid.Distributor.Activator/CollectCompletedWork.cs
@@ -1,58 +1,46 @@
-// Copyright 2007-2008 The Apache Software Foundation.
-//  
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
-// this file except in compliance with the License. You may obtain a copy of the 
-// License at 
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software distributed 
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
-// specific language governing permissions and limitations under the License.
 namespace Grid.Distributor.Activator
 {
     using System;
     using System.Collections.Generic;
+    using System.Configuration;
     using System.Linq;
     using System.Threading;
+    using MassTransit;
     using Shared;
     using Shared.Messages;
     using log4net;
-    using MassTransit;
-    using MassTransit.Distributor;
-    using System.Configuration;
 
-	public class CollectCompletedWork :
+    public class CollectCompletedWork :
         Consumes<CompletedSimpleWorkItem>.All,
         IServiceInterface
     {
-        private UnsubscribeAction _unsubscribeAction;
-        private readonly ILog _log = LogManager.GetLogger(typeof(CollectCompletedWork));
-        private int _received;
-        private int _sent;
-        private readonly List<int> _values = new List<int>();
-
-		public IServiceBus ControlBus { get; set; }
-        public IServiceBus DataBus { get; set; }
+        readonly ILog _log = LogManager.GetLogger(typeof(CollectCompletedWork));
+        readonly List<int> _values = new List<int>();
+        int _received;
+        int _sent;
+        UnsubscribeAction _unsubscribeAction;
 
         public CollectCompletedWork()
         {
-        	DataBus = ServiceBusFactory.New(x =>
-        		{
-					x.ReceiveFrom(ConfigurationManager.AppSettings["SourceQueue"]);
-        			x.SetPurgeOnStartup(true);
+            DataBus = ServiceBusFactory.New(x =>
+                {
+                    x.ReceiveFrom(ConfigurationManager.AppSettings["SourceQueue"]);
+                    x.SetPurgeOnStartup(true);
 
-					x.UseMsmq();
-        			x.UseMulticastSubscriptionClient();
+                    x.UseMsmq();
+                    x.UseMulticastSubscriptionClient();
+                    x.UseControlBus();
 
-					x.SetConcurrentConsumerLimit(4);
-	                x.UseDistributorFor<DoSimpleWorkItem>();
-        			x.UseControlBus();
-        		});
+                    x.SetConcurrentConsumerLimit(4);
+                    
+                    x.Distributor(d => d.Handler<DoSimpleWorkItem>());
+                });
 
-        	ControlBus = DataBus.ControlBus;
+            ControlBus = DataBus.ControlBus;
         }
+
+        public IServiceBus ControlBus { get; set; }
+        public IServiceBus DataBus { get; set; }
 
         public void Consume(CompletedSimpleWorkItem message)
         {
@@ -60,19 +48,19 @@ namespace Grid.Distributor.Activator
 
             int messageMs = DateTime.UtcNow.Subtract(message.RequestCreatedAt).Milliseconds;
 
-        	int max;
-        	int min;
-        	double average;
-        	lock (_values)
+            int max;
+            int min;
+            double average;
+            lock (_values)
             {
                 _values.Add(messageMs);
-            	min = _values.Min();
-            	max = _values.Max();
-            	average = _values.Average();
+                min = _values.Min();
+                max = _values.Max();
+                average = _values.Average();
             }
 
             _log.InfoFormat("Received: {0} - {1} [{2}ms]", _received, message.CorrelationId, messageMs);
-        	_log.InfoFormat("Stats\n\tMin: {0:0000.0}ms\n\tMax: {1:0000.0}ms\n\tAvg: {2:0000.0}ms", min, max, average);
+            _log.InfoFormat("Stats\n\tMin: {0:0000.0}ms\n\tMax: {1:0000.0}ms\n\tAvg: {2:0000.0}ms", min, max, average);
         }
 
         public void Start()
@@ -81,15 +69,15 @@ namespace Grid.Distributor.Activator
 
             Thread.Sleep(5000);
 
-        	DataBus.OutboundPipeline.View(Console.WriteLine);
+            DataBus.OutboundPipeline.View(Console.WriteLine);
 
-			Thread.Sleep(5000);
-			Thread.Sleep(5000);
-			Thread.Sleep(5000);
+            Thread.Sleep(5000);
+            Thread.Sleep(5000);
+            Thread.Sleep(5000);
 
             for (int i = 0; i < 100; i++)
             {
-                var g = Guid.NewGuid();
+                Guid g = Guid.NewGuid();
                 _log.InfoFormat("Publishing: {0}", g);
                 DataBus.Publish(new DoSimpleWorkItem(g));
 
@@ -99,7 +87,7 @@ namespace Grid.Distributor.Activator
 
         public void Stop()
         {
-            var action = _unsubscribeAction;
+            UnsubscribeAction action = _unsubscribeAction;
             if (action != null)
             {
                 action();
--- src/Samples/Distributor/Grid.Distributor.Worker/DoWork.cs
+++ src/Samples/Distributor/Grid.Distributor.Worker/DoWork.cs
@@ -1,61 +1,47 @@
 namespace Grid.Distributor.Worker
 {
-	using System;
-	using System.Configuration;
-	using System.Threading;
-	using log4net;
-	using MassTransit;
-	using MassTransit.Distributor;
-	using Shared;
-	using Shared.Messages;
+    using System;
+    using System.Configuration;
+    using MassTransit;
+    using Shared;
 
-	public class DoWork :
-		IServiceInterface,
-		IDisposable
-	{
-		ILog _log = LogManager.GetLogger(typeof (DoWork));
+    public class DoWork :
+        IServiceInterface,
+        IDisposable
+    {
+        public DoWork()
+        {
+            DataBus = ServiceBusFactory.New(x =>
+                {
+                    x.ReceiveFrom(ConfigurationManager.AppSettings["SourceQueue"]);
+                    x.SetPurgeOnStartup(true);
 
-		public DoWork()
-		{
-			DataBus = ServiceBusFactory.New(x =>
-				{
-					x.ReceiveFrom(ConfigurationManager.AppSettings["SourceQueue"]);
-					x.SetPurgeOnStartup(true);
+                    x.UseMsmq();
+                    x.UseMulticastSubscriptionClient();
+                    x.UseControlBus();
 
-					x.UseMsmq();
-					x.UseMulticastSubscriptionClient();
+                    x.SetConcurrentConsumerLimit(4);
 
-					x.UseControlBus();
-					x.SetConcurrentConsumerLimit(4);
-					x.ImplementDistributorWorker<DoSimpleWorkItem>(ConsumeMessage, 2, 8);
-				});
+                    x.Worker(w => w.Consumer<SimpleWorkItemConsumer>());
+                });
 
-			ControlBus = DataBus.ControlBus;
-		}
+            ControlBus = DataBus.ControlBus;
+        }
 
-		public IServiceBus ControlBus { get; set; }
-		public IServiceBus DataBus { get; set; }
+        public IServiceBus ControlBus { get; set; }
+        public IServiceBus DataBus { get; set; }
 
-		public void Dispose()
-		{
-		}
+        public void Dispose()
+        {
+        }
 
-		public void Start()
-		{
-			DataBus.InboundPipeline.View(Console.WriteLine);
-		}
+        public void Start()
+        {
+            DataBus.InboundPipeline.View(Console.WriteLine);
+        }
 
-		public void Stop()
-		{
-		}
-
-		Action<DoSimpleWorkItem> ConsumeMessage(DoSimpleWorkItem message)
-		{
-			return m =>
-				{
-					_log.InfoFormat("Responding to {0}", m.CorrelationId);
-					DataBus.Context().Respond(new CompletedSimpleWorkItem(m.CorrelationId, m.CreatedAt));
-				};
-		}
-	}
+        public void Stop()
+        {
+        }
+    }
 }
