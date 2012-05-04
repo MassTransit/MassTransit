fef9677a805b88ad057eeba96701f7003001ac00
Wazzq <wuhqureshi@gmail.com>
2012-05-04 12:49:46 +0100

    When a connection needs to be re-established, retry the operation/callback that initiated the re-connection.

---
+++ src/MassTransit/Transports/RetryPolicy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MassTransit.Transports
+{
+	class RetryPolicy : ConnectionPolicy
+	{
+		readonly ConnectionPolicyChain _policyChain;
+
+		public RetryPolicy(ConnectionPolicyChain policyChain)
+		{
+			_policyChain = policyChain;
+		}
+
+		public void Execute(Action callback)
+		{
+			callback();
+			_policyChain.Pop(this);
+			_policyChain.Next(callback);
+		}
+	}
+}
