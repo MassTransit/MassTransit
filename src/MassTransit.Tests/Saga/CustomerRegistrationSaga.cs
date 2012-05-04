133e44fcea59cf37791ca4c48fc230faddcef25e
Chris Patterson <chris@phatboyg.com>
2012-05-04 10:05:40 -0500

    Added saga test that shows some bad behavior of the Combine() method
    Former-commit-id: 19c39dacae32190b578c87962ba46aecb413057b

---
+++ src/MassTransit.Tests/Saga/CustomerRegistrationSaga.cs
@@ -0,0 +1,126 @@
+﻿// Copyright 2007-2012 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.Tests.Saga
+{
+    using System;
+    using Magnum.StateMachine;
+    using MassTransit.Saga;
+
+    public class CustomerRegistrationSaga :
+        SagaStateMachine<CustomerRegistrationSaga>,
+        ISaga
+    {
+        static CustomerRegistrationSaga()
+        {
+            Define(() =>
+                {
+                    Correlate(Initialised).By((saga, message) => saga.SystemId == message.SystemId);
+                    Correlate(SettingsCreated).By((saga, message) => saga.SystemId == message.SystemId);
+                    Correlate(UserCreated).By((saga, message) => saga.SystemId == message.SystemId);
+                    Correlate(SystemActivated).By((saga, message) => saga.SystemId == message.SystemId);
+
+                    Initially(
+                        When(Initialised)
+                            .Then((saga, message) => saga.SystemId = message.SystemId)
+                            .Then((saga, message) => saga.Bus.Publish(new CreateSettingsCommand()))
+                            .Then((saga, message) => saga.Bus.Publish(new CreateUserCommand()))
+                            .TransitionTo(WaitingForCustomerAndSettings));
+
+                    Combine(SettingsCreated, UserCreated)
+                        .Into(SettingsAndUserCreated, saga => saga.ReadyFlags);
+
+                    During(
+                        WaitingForCustomerAndSettings,
+                        When(SettingsAndUserCreated)
+                            .Then((saga, message) => saga.Bus.Publish(new ActivateSystemCommand()))
+                            .TransitionTo(WaitingForSystemActivation));
+
+                    During(
+                        WaitingForSystemActivation,
+                        When(SystemActivated)
+                            .Complete());
+                });
+        }
+
+        public CustomerRegistrationSaga(Guid correlationId)
+        {
+            CorrelationId = correlationId;
+        }
+
+        protected CustomerRegistrationSaga()
+        {
+        }
+
+        public Guid SystemId { get; set; }
+
+        public int ReadyFlags { get; private set; }
+
+        public static State Initial { get; set; }
+        public static State WaitingForCustomerAndSettings { get; set; }
+        public static State WaitingForSystemActivation { get; set; }
+        public static State Completed { get; set; }
+
+        public static Event<ClientSystemCreatedEvent> Initialised { get; set; }
+        public static Event<SettingsCreatedEvent> SettingsCreated { get; set; }
+        public static Event<UserCreatedEvent> UserCreated { get; set; }
+        public static Event<SystemActivatedEvent> SystemActivated { get; set; }
+        public static Event SettingsAndUserCreated { get; set; }
+
+        #region ISaga Members
+
+        public Guid CorrelationId { get; set; }
+        public IServiceBus Bus { get; set; }
+
+        #endregion
+    }
+
+    public class ClientSystemCreatedEvent
+        //: CorrelatedBy<Guid>
+    {
+        public Guid SystemId { get; set; }
+        //public Guid CorrelationId { get; set; }
+    }
+
+    public class UserCreatedEvent
+        //: CorrelatedBy<Guid>
+    {
+        public Guid SystemId { get; set; }
+        //public Guid CorrelationId { get; set; }
+    }
+
+    public class SettingsCreatedEvent
+        //: CorrelatedBy<Guid>
+    {
+        public Guid SystemId { get; set; }
+        //public Guid CorrelationId { get; set; }
+    }
+
+    public class SystemActivatedEvent
+        //: CorrelatedBy<Guid>
+    {
+        public Guid SystemId { get; set; }
+        //public Guid CorrelationId { get; set; }
+    }
+
+    public class ActivateSystemCommand
+    {
+    }
+
+    public class CreateUserCommand
+    {
+    }
+
+    public class CreateSettingsCommand
+    {
+    }
+}
---
+++ src/MassTransit.Tests/Saga/CustomerRegistrationSagaTest.cs
@@ -0,0 +1,90 @@
+﻿// Copyright 2007-2012 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.Tests.Saga
+{
+    using System;
+    using Magnum.TestFramework;
+    using MassTransit.Testing;
+    using MassTransit.Testing.ScenarioBuilders;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_completing_CustomerRegistrationSaga
+    {
+        [Test]
+        public void Should_complete_saga()
+        {
+            test.Saga.AnyInState(CustomerRegistrationSaga.Completed).ShouldBeTrue();
+        }
+
+        [Test]
+        public void Should_create_a_new_saga_for_the_message()
+        {
+            bool createdSaga = test.Saga.Created.Any(x => x.SystemId == systemId);
+            createdSaga.ShouldBeTrue();
+        }
+
+        Guid systemId;
+        SagaTest<BusTestScenario, CustomerRegistrationSaga> test;
+
+        [TestFixtureSetUp]
+        public void A_state_machine_saga_is_being_tested()
+        {
+            systemId = Guid.NewGuid();
+
+            test = TestFactory.ForSaga<CustomerRegistrationSaga>()
+                .InSingleBusScenario()
+                .New(x =>
+                    {
+                        x.UseScenarioBuilder(() => new CustomBusScenarioBuilder());
+                        x.Send(new ClientSystemCreatedEvent
+                            {
+                                SystemId = systemId
+                            });
+                        x.Send(new UserCreatedEvent
+                            {
+                                SystemId = systemId
+                            });
+                        x.Send(new SettingsCreatedEvent
+                            {
+                                SystemId = systemId
+                            });
+                        x.Send(new SystemActivatedEvent
+                            {
+                                SystemId = systemId
+                            });
+                    });
+
+            test.Execute();
+        }
+
+        [TestFixtureTearDown]
+        public void Teardown()
+        {
+            test.Dispose();
+            test = null;
+        }
+
+        public class CustomBusScenarioBuilder :
+            BusScenarioBuilderImpl
+        {
+            const string DefaultUri = "loopback://localhost/mt_client";
+
+            public CustomBusScenarioBuilder()
+                : base(new Uri(DefaultUri))
+            {
+                ConfigureBus(x => x.SetConcurrentConsumerLimit(1));
+            }
+        }
+    }
+}
