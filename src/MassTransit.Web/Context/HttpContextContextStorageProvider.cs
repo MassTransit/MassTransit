3011fb9c9a5facbbcf8707187a0427a4bcb763c8
Henrik <henrik@haf.se>
2012-05-06 19:31:03 +0200

    Start supporting client libraries, see GH-128

---
+++ src/MassTransit.Web/Context/HttpContextContextStorageProvider.cs
@@ -0,0 +1,57 @@
+﻿// Copyright 2007-2012 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.Context
+{
+	using System.Web;
+
+	public class HttpContextContextStorageProvider :
+		ContextStorageProvider
+	{
+		const string ReceiveContextKey = "MassTransitReceiveContext";
+
+		readonly ContextStorageProvider _fallback;
+
+		public HttpContextContextStorageProvider()
+		{
+			_fallback = new ThreadStaticContextStorageProvider();
+		}
+
+		public IConsumeContext ConsumeContext
+		{
+			get
+			{
+				HttpContext httpContext = HttpContext.Current;
+				if (httpContext != null)
+				{
+					var value = httpContext.Items[ReceiveContextKey] as IConsumeContext;
+					return value;
+				}
+
+				return _fallback.ConsumeContext;
+			}
+
+			set
+			{
+				HttpContext httpContext = HttpContext.Current;
+				if (httpContext != null)
+				{
+					httpContext.Items[ReceiveContextKey] = value;
+				}
+				else
+				{
+					_fallback.ConsumeContext = value;
+				}
+			}
+		}
+	}
+}
---
+++ src/MassTransit.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: Guid("b4d959f6-75a4-409e-97e0-114bcc9bc2a9")]
