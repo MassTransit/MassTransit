cd0d46f3ce2d09e0de6fe243b07e73de51df27e6
Chris Patterson <chris@phatboyg.com>
2012-05-29 11:38:01 -0500

    Added test for two rabbit buses talking to each other

--- src/Transports/MassTransit.Transports.RabbitMq.Tests/When_a_message_is_published.cs
+++ src/Transports/MassTransit.Transports.RabbitMq.Tests/When_a_message_is_published.cs
@@ -74,4 +74,61 @@ namespace MassTransit.Transports.RabbitMq.Tests
 			public string StringB { get; set; }
 		}
 	}
+
+	[Scenario]
+	public class When_a_message_is_published_between_buses :
+		Given_two_rabbitmq_buses_walk_into_a_bar
+	{
+		Future<A> _received;
+		Future<B> _receivedB;
+
+		protected override void ConfigureLocalBus(ServiceBusConfigurator configurator)
+		{
+		    base.ConfigureLocalBus(configurator);
+
+			_received = new Future<A>();
+			_receivedB = new Future<B>();
+
+			configurator.Subscribe(s =>
+				{
+					s.Handler<A>(message => _received.Complete(message));
+					s.Handler<B>(message => _receivedB.Complete(message));
+				});
+		}
+
+		[When]
+		public void A_message_is_published()
+		{
+			RemoteBus.Publish(new A
+				{
+					StringA = "ValueA",
+					StringB = "ValueB",
+				});
+		}
+
+		[Then]
+		public void Should_be_received_by_the_queue()
+		{
+			_received.WaitUntilCompleted(8.Seconds()).ShouldBeTrue();
+			_received.Value.StringA.ShouldEqual("ValueA");
+		}
+
+		[Then]
+		public void Should_receive_the_inherited_version()
+		{
+			_receivedB.WaitUntilCompleted(8.Seconds()).ShouldBeTrue();
+			_receivedB.Value.StringB.ShouldEqual("ValueB");
+		}
+
+		class A :
+			B
+		{
+			public string StringA { get; set; }
+		}
+
+		class B
+		{
+			public string StringB { get; set; }
+		}
+	}
 }
