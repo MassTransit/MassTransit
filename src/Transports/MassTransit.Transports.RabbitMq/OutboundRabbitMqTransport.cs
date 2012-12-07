ce4fe8f3acd5c488f34a22c821392a3160fb8823
Adam Mills <adam@chambills.com>
2012-12-07 17:01:19 -0400

    Copying MT Headers into Rabbit Headers

--- src/Transports/MassTransit.Transports.RabbitMq/OutboundRabbitMqTransport.cs
+++ src/Transports/MassTransit.Transports.RabbitMq/OutboundRabbitMqTransport.cs
@@ -18,6 +18,7 @@ namespace MassTransit.Transports.RabbitMq
     using Magnum;
     using RabbitMQ.Client;
     using RabbitMQ.Client.Exceptions;
+    using System.Linq;
 
     public class OutboundRabbitMqTransport :
         IOutboundTransport
@@ -65,7 +66,8 @@ namespace MassTransit.Transports.RabbitMq
                         using (var body = new MemoryStream())
                         {
                             context.SerializeTo(body);
-                            properties.Headers = new Hashtable {{"Content-Type", context.ContentType}};
+                            properties.Headers = context.Headers.ToDictionary(entry => entry.Key, entry => entry.Value);
+                            properties.Headers["Content-Type"]=context.ContentType;
 
                             _producer.Publish(_address.Name, properties, body.ToArray());
 
