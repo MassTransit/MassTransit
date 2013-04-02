1c28fd1078b1fd3d00940e5d25b907e2c2588daf
Chris Patterson <chris.patterson@relayhealth.com>
2013-04-01 17:43:59 -0700

    Added a authentication test failure connecting to RMQ and proper handling of barely-connected state

---
+++ src/Transports/MassTransit.Transports.RabbitMq.Tests/ConnectingFailure_Specs.cs
@@ -0,0 +1,32 @@
+﻿namespace MassTransit.Transports.RabbitMq.Tests
+{
+    using System;
+    using System.Threading;
+    using Magnum.Extensions;
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class When_unable_to_connect_to_RabbitMQ
+    {
+        [Test, Explicit]
+        public void Should_not_destroy_the_box()
+        {
+            using (IServiceBus bus = ServiceBusFactory.New(c =>
+            {
+                c.ReceiveFrom(new Uri("rabbitmq://localhost/restricted/no_legs"));
+                c.UseRabbitMq();
+            }))
+            {
+                bus.Publish(new A());
+
+                Thread.Sleep(30.Seconds());
+            }
+        }
+
+
+        class A
+        {
+        }
+    }
+}
