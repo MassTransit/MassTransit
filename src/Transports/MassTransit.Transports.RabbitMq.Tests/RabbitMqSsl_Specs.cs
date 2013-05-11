5c93296526c4c8d1ed2adea17bd031bb86ea96f7
Chris Patterson <chris.patterson@relayhealth.com>
2013-05-11 08:01:38 -0700

    Allow an SSL connection to RabbitMQ without needing a client certificate.
    This change is to allow a client to connection to a RabbitMQ server
    without need to present a client certificate. This means we can now
    connect to RabbitMQ over SSL just like we would connection to any SSL
    enabled web-site which does not require client certificates.

--- src/Transports/MassTransit.Transports.RabbitMq.Tests/RabbitMqSsl_Specs.cs
+++ src/Transports/MassTransit.Transports.RabbitMq.Tests/RabbitMqSsl_Specs.cs
@@ -1,60 +1,78 @@
-﻿// Copyright 2007-2011 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+﻿// Copyright 2007-2012 Chris Patterson, Dru Sellers, Travis Smith, et. al.
 //  
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file except in compliance with the License. You may obtain a copy of the 
 // License at 
 // 
 //     http://www.apache.org/licenses/LICENSE-2.0 
 // 
-// Unless required by applicable law or agreed to in writing, software distributed 
+// Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.Transports.RabbitMq.Tests
 {
-	using System;
-	using Magnum.TestFramework;
-	using NUnit.Framework;
+    using System;
+    using System.Net;
+    using Magnum.TestFramework;
+    using NUnit.Framework;
 
-	[Scenario, Explicit]
-	public class When_connecting_to_a_rabbit_mq_server_using_ssl
-	{
-		IServiceBus _bus;
 
-		[When]
-		public void Connecting_to_a_rabbit_mq_server_using_ssl()
-		{
-			Uri inputAddress = new Uri("rabbitmq://localhost:5671/test_queue");
+    [Scenario, Explicit]
+    public class When_connecting_to_a_rabbit_mq_server_using_ssl
+    {
+        IServiceBus _bus;
 
-			_bus = ServiceBusFactory.New(c =>
-				{
-					c.ReceiveFrom(inputAddress);
-					c.UseRabbitMq(r =>
-						{
-							r.ConfigureHost(inputAddress, h =>
-								{
-									h.UseSsl(s =>
-										{
-											s.SetServerName(System.Net.Dns.GetHostName());
-											s.SetCertificatePath("client.p12");
-											s.SetCertificatePassphrase("Passw0rd");
-										});
-								});
-						});
-				});
-		}
+        [When]
+        public void Connecting_to_a_rabbit_mq_server_using_ssl_and_with_client_certificate()
+        {
+            var inputAddress = new Uri("rabbitmq://localhost:5671/test_queue");
 
-		[Finally]
-		public void Finally()
-		{
-			_bus.Dispose();
-		}
+            _bus = ServiceBusFactory.New(c =>
+                {
+                    c.ReceiveFrom(inputAddress);
+                    c.UseRabbitMq(r =>
+                        {
+                            r.ConfigureHost(inputAddress, h =>
+                                {
+                                    h.UseSsl(s =>
+                                        {
+                                            s.SetServerName(Dns.GetHostName());
+                                            s.SetCertificatePath("client.p12");
+                                            s.SetCertificatePassphrase("Passw0rd");
+                                        });
+                                });
+                        });
+                });
+        }
 
-		[Then, Explicit]
-		public void Should_connect_to_the_queue()
-		{
-			
-		}
+        [When]
+        public void Connecting_to_a_rabbit_mq_server_using_ssl_without_client_certificate()
+        {
+            var inputAddress = new Uri("rabbitmq://localhost:5671/test_queue");
 
-	}
+            _bus = ServiceBusFactory.New(c =>
+                {
+                    c.ReceiveFrom(inputAddress);
+                    c.UseRabbitMq(
+                        r =>
+                            {
+                                r.ConfigureHost(inputAddress,
+                                    h => { h.UseSsl(s => { s.SetClientCertificateRequired(false); }); });
+                            });
+                });
+        }
+
+
+        [Finally]
+        public void Finally()
+        {
+            _bus.Dispose();
+        }
+
+        [Then, Explicit]
+        public void Should_connect_to_the_queue()
+        {
+        }
+    }
 }
