280b2d55d5fd89bc60f8cdbd1fea3843d297c2c3
Chris Patterson <chris@phatboyg.com>
2013-05-27 18:59:19 -0700

    If on .NET 4.0 or later, publisher confirms are built-in by default.

---
+++ src/Transports/MassTransit.Transports.RabbitMq.Tests/PublisherConfirm_Specs.cs
@@ -0,0 +1,53 @@
+﻿namespace MassTransit.Transports.RabbitMq.Tests
+{
+    using BusConfigurators;
+    using Configuration.Configurators;
+    using Magnum.Extensions;
+    using Magnum.TestFramework;
+    using NUnit.Framework;
+    using TestFramework;
+
+
+    [TestFixture]
+    public class PublisherConfirm_Specs:
+        Given_two_rabbitmq_buses_walk_into_a_bar
+    {
+        [Test]
+        public void Should_call_the_ack_method_upon_delivery()
+        {
+            RemoteBus.Publish(new A
+                {
+                    StringA = "ValueA",
+                });
+
+            _received.WaitUntilCompleted(8.Seconds()).ShouldBeTrue();
+        }
+
+        Future<A> _received;
+
+        protected override void ConfigureLocalBus(ServiceBusConfigurator configurator)
+        {
+            base.ConfigureLocalBus(configurator);
+
+
+            _received = new Future<A>();
+
+            configurator.Subscribe(s => s.Handler<A>(message => _received.Complete(message)));
+        }
+
+        protected override void ConfigureRabbitMq(RabbitMqTransportFactoryConfigurator configurator)
+        {
+            base.ConfigureRabbitMq(configurator);
+        }
+
+        class A
+        {
+            public string StringA { get; set; }
+        }
+
+        class B
+        {
+            public string StringB { get; set; }
+        }
+    }
+}
--- src/Transports/MassTransit.Transports.RabbitMq/RabbitMqProducer.cs
+++ src/Transports/MassTransit.Transports.RabbitMq/RabbitMqProducer.cs
@@ -15,18 +15,28 @@ namespace MassTransit.Transports.RabbitMq
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Threading.Tasks;
+    using Magnum.Caching;
     using RabbitMQ.Client;
+    using RabbitMQ.Client.Events;
+#if !NET35
+#endif
 
 
     public class RabbitMqProducer :
         ConnectionBinding<RabbitMqConnection>
     {
+#if !NET35
+        readonly Cache<ulong, TaskCompletionSource<bool>> _confirms;
+#endif
         readonly IRabbitMqEndpointAddress _address;
         readonly bool _bindToQueue;
         readonly object _channelLock = new object();
         readonly HashSet<ExchangeBinding> _exchangeBindings;
         readonly HashSet<string> _exchanges;
         IModel _channel;
+        bool _immediate;
+        bool _mandatory;
 
         public RabbitMqProducer(IRabbitMqEndpointAddress address, bool bindToQueue)
         {
@@ -34,6 +44,9 @@ namespace MassTransit.Transports.RabbitMq
             _bindToQueue = bindToQueue;
             _exchangeBindings = new HashSet<ExchangeBinding>();
             _exchanges = new HashSet<string>();
+#if !NET35
+            _confirms = new ConcurrentCache<ulong, TaskCompletionSource<bool>>();
+#endif
         }
 
         public void Bind(RabbitMqConnection connection)
@@ -48,6 +61,8 @@ namespace MassTransit.Transports.RabbitMq
                     DeclareAndBindQueue(channel);
 
                     RebindExchanges(channel);
+
+                    BindEvents(channel);
 
                     _channel = channel;
                 }
@@ -70,18 +85,61 @@ namespace MassTransit.Transports.RabbitMq
             }
         }
 
+        void BindEvents(IModel channel)
+        {
+            channel.BasicAcks += HandleAck;
+            channel.BasicNacks += HandleNack;
+            channel.BasicReturn += HandleReturn;
+            channel.FlowControl += HandleFlowControl;
+            channel.ModelShutdown += HandleModelShutdown;
+            channel.ConfirmSelect();
+        }
+
         public void Unbind(RabbitMqConnection connection)
         {
             lock (_channelLock)
             {
-                if (_channel != null)
+                try
                 {
-                    if (_channel.IsOpen)
-                        _channel.Close(200, "producer unbind");
-                    _channel.Dispose();
-                    _channel = null;
+                    if (_channel != null)
+                    {
+                        UnbindEvents(_channel);
+
+                        if (_channel.IsOpen)
+                            _channel.Close(200, "producer unbind");
+                        _channel.Dispose();
+                        _channel = null;
+                    }
+                }
+                finally
+                {
+                    FailPendingConfirms();
                 }
             }
+        }
+
+        void UnbindEvents(IModel channel)
+        {
+            channel.BasicAcks -= HandleAck;
+            channel.BasicNacks -= HandleNack;
+            channel.BasicReturn -= HandleReturn;
+            channel.FlowControl -= HandleFlowControl;
+            channel.ModelShutdown -= HandleModelShutdown;
+        }
+
+        void FailPendingConfirms()
+        {
+            try
+            {
+                var exception = new InvalidOperationException("Publish not confirmed before channel closed");
+
+                _confirms.Each((id, task) => task.TrySetException(exception));
+            }
+            catch (Exception)
+            {
+            }
+
+            _confirms.Clear();
         }
 
         public void ExchangeDeclare(string name)
@@ -120,14 +178,10 @@ namespace MassTransit.Transports.RabbitMq
                                                                  .Distinct();
 
                 foreach (string exchange in exchanges)
-                {
                     channel.ExchangeDeclare(exchange, ExchangeType.Fanout, true, false, null);
-                }
 
                 foreach (ExchangeBinding exchange in _exchangeBindings)
-                {
                     channel.ExchangeBind(exchange.Destination, exchange.Source, "");
-                }
             }
         }
 
@@ -150,6 +204,72 @@ namespace MassTransit.Transports.RabbitMq
                     throw new InvalidConnectionException(_address.Uri, "No connection to RabbitMQ Host");
 
                 _channel.BasicPublish(exchangeName, "", properties, body);
+            }
+        }
+
+        public Task PublishAsync(string exchangeName, IBasicProperties properties, byte[] body)
+        {
+            lock (_channelLock)
+            {
+                if (_channel == null)
+                    throw new InvalidConnectionException(_address.Uri, "No connection to RabbitMQ Host");
+
+                ulong deliveryTag = _channel.NextPublishSeqNo;
+
+                var task = new TaskCompletionSource<bool>();
+                _confirms.Add(deliveryTag, task);
+
+                try
+                {
+                    _channel.BasicPublish(exchangeName, "", _mandatory, _immediate, properties, body);
+                }
+                catch
+                {
+                    _confirms.Remove(deliveryTag);                    
+                    throw;
+                }
+
+                return task.Task;
+            }
+        }
+
+        void HandleModelShutdown(IModel model, ShutdownEventArgs reason)
+        {
+        }
+
+        void HandleFlowControl(IModel sender, FlowControlEventArgs args)
+        {
+        }
+
+        void HandleReturn(IModel model, BasicReturnEventArgs args)
+        {
+        }
+
+        void HandleNack(IModel model, BasicNackEventArgs args)
+        {
+            IEnumerable<ulong> ids = Enumerable.Repeat(args.DeliveryTag, 1);
+            if (args.Multiple)
+                ids = _confirms.GetAllKeys().Where(x => x <= args.DeliveryTag);
+
+            var exception = new InvalidOperationException("Publish was nacked by the broker");
+
+            foreach (ulong id in ids)
+            {
+                _confirms[id].TrySetException(exception);
+                _confirms.Remove(id);
+            }
+        }
+
+        void HandleAck(IModel model, BasicAckEventArgs args)
+        {
+            IEnumerable<ulong> ids = Enumerable.Repeat(args.DeliveryTag, 1);
+            if (args.Multiple)
+                ids = _confirms.GetAllKeys().Where(x => x <= args.DeliveryTag);
+
+            foreach (ulong id in ids)
+            {
+                _confirms[id].TrySetResult(true);
+                _confirms.Remove(id);
             }
         }
     }
