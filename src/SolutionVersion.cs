fdec5fb90a8be2fe938e93abc4b8c1c350f144f5
Chris Patterson <chris@phatboyg.com>
2013-07-29 11:13:22 -0700

    Normalized changes to serializer in preparation for release
    Former-commit-id: de318246516a495e7b5c86d3a8222869a996963b

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -5,10 +5,10 @@ using System.Security;
 [assembly: AssemblyDescription("MassTransit is a distributed application framework for .NET http://masstransit-project.com")]
 [assembly: AssemblyProduct("MassTransit")]
 [assembly: AssemblyCopyright("Copyright 2007-2013 Chris Patterson, Dru Sellers, Travis Smith, et. al. - All rights reserved.")]
-[assembly: AssemblyVersion("2.7.0")]
-[assembly: AssemblyFileVersion("2.7.4")]
+[assembly: AssemblyVersion("2.8.0")]
+[assembly: AssemblyFileVersion("2.8.0")]
 
-[assembly: AssemblyInformationalVersion("2.7.4.f4b138")]
+[assembly: AssemblyInformationalVersion("2.8.0.75a0e8")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(false)]
 
