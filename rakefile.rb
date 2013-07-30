f9ea1d86e7789d076d9ccf13e023541cfa000bec
Chris Patterson <chris@phatboyg.com>
2013-07-30 14:49:25 -0700

    Updated RabbitMQ to 3.1.1
    Former-commit-id: 560c3eb7ea36d56b1ec8a72c447e251a4f886639

--- rakefile.rb
+++ rakefile.rb
@@ -442,7 +442,7 @@ task :all_nuspecs => [:mt_nuspec, :mtl4n_nuspec, :mtnlog_nuspec, :mtsm_nuspec, :
     nuspec.licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0"
     nuspec.requireLicenseAcceptance = "false"
     nuspec.dependency "MassTransit", NUGET_VERSION
-    nuspec.dependency "RabbitMQ.Client", "3.0.4"
+    nuspec.dependency "RabbitMQ.Client", "3.1.1"
     nuspec.output_file = 'nuspecs/MassTransit.RabbitMQ.nuspec'
 
 	add_files props[:stage], "#{File.join('Transports', 'RabbitMQ', 'MassTransit.Transports.RabbitMq.{dll,pdb,xml}')}", nuspec
