267b751d63f833de2c323a623a960da2b4419880
Chris Patterson <chris@phatboyg.com>
2013-07-30 08:45:33 -0700

    Poor timing in the unit test, ugh.
    Former-commit-id: 4f8ea2ce54376b96540d31812b63368921f499eb

--- src/MassTransit.Tests/Subscriptions/Removing_a_subscription_client.cs
+++ src/MassTransit.Tests/Subscriptions/Removing_a_subscription_client.cs
@@ -1,68 +1,73 @@
-// Copyright 2007-2011 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+// Copyright 2007-2012 Chris Patterson, Dru Sellers, Travis Smith, et. al.
 //  
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file except in compliance with the License. You may obtain a copy of the 
 // License at 
 // 
 //     http://www.apache.org/licenses/LICENSE-2.0 
 // 
-// Unless required by applicable law or agreed to in writing, software distributed 
+// Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.Tests.Subscriptions
 {
-	using System.Threading;
-	using Magnum;
-	using Magnum.Extensions;
-	using MassTransit.Transports;
-	using MassTransit.Transports.Loopback;
-	using NUnit.Framework;
-	using TestFramework;
-	using TextFixtures;
+    using Magnum;
+    using Magnum.Extensions;
+    using MassTransit.Transports.Loopback;
+    using NUnit.Framework;
+    using TestFramework;
+    using TextFixtures;
 
-	[TestFixture]
-	public class Removing_a_subscription_client :
-		SubscriptionServiceTestFixture<LoopbackTransportFactory>
-	{
-		class A
-		{
-		}
 
-		[Test]
-		public void Should_not_remove_any_existing_subscriptions()
-		{
-			RemoteBus.SubscribeHandler<A>(x => { });
-			RemoteBus.Dispose();
+    [TestFixture]
+    public class Removing_a_subscription_client :
+        SubscriptionServiceTestFixture<LoopbackTransportFactory>
+    {
+        [Test]
+        public void Should_not_remove_any_existing_subscriptions()
+        {
+            RemoteBus.SubscribeHandler<A>(x => { });
 
-			ThreadUtil.Sleep(2.Seconds());
+            RemoteBus.ShouldHaveSubscriptionFor<A>();
+            LocalBus.ShouldHaveRemoteSubscriptionFor<A>();
 
-			LocalBus.ShouldHaveRemoteSubscriptionFor<A>();
-		}
-	}
+            RemoteBus.Dispose();
 
-	[TestFixture]
-	public class Removing_a_subscription_client_and_readding_it :
-		SubscriptionServiceTestFixture<LoopbackTransportFactory>
-	{
-		class A
-		{
-		}
+            ThreadUtil.Sleep(2.Seconds());
+            LocalBus.ShouldHaveRemoteSubscriptionFor<A>();
+        }
 
-		[Test]
-		public void Should_remove_any_previous_subscriptions()
-		{
-			RemoteBus.SubscribeHandler<A>(x => { });
 
-			LocalBus.ShouldHaveRemoteSubscriptionFor<A>();
+        class A
+        {
+        }
+    }
 
-			RemoteBus.Dispose();
 
-			ThreadUtil.Sleep(2.Seconds());
+    [TestFixture]
+    public class Removing_a_subscription_client_and_readding_it :
+        SubscriptionServiceTestFixture<LoopbackTransportFactory>
+    {
+        [Test]
+        public void Should_remove_any_previous_subscriptions()
+        {
+            RemoteBus.SubscribeHandler<A>(x => { });
+            RemoteBus.ShouldHaveSubscriptionFor<A>();
+            LocalBus.ShouldHaveRemoteSubscriptionFor<A>();
 
-			SetupRemoteBus();
+            RemoteBus.Dispose();
 
-			LocalBus.ShouldNotHaveSubscriptionFor<A>();
-		}
-	}
+            ThreadUtil.Sleep(2.Seconds());
+
+            SetupRemoteBus();
+
+            LocalBus.ShouldNotHaveSubscriptionFor<A>();
+        }
+
+
+        class A
+        {
+        }
+    }
 }
