1947fafbe605f7b244dae4d421bf45db45d5f183
Chris Patterson <chris@phatboyg.com>
2013-11-19 15:59:34 -0800

    UPdated build for samples
    Former-commit-id: 52fa2aab875fb56296540045adcddd6306009579

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -5,10 +5,10 @@ using System.Security;
 [assembly: AssemblyDescription("MassTransit is a distributed application framework for .NET http://masstransit-project.com")]
 [assembly: AssemblyProduct("MassTransit")]
 [assembly: AssemblyCopyright("Copyright 2007-2013 Chris Patterson, Dru Sellers, Travis Smith, et. al. - All rights reserved.")]
-[assembly: AssemblyVersion("2.8.0")]
-[assembly: AssemblyFileVersion("2.8.2")]
+[assembly: AssemblyVersion("2.9.0")]
+[assembly: AssemblyFileVersion("2.9.0")]
 
-[assembly: AssemblyInformationalVersion("2.8.2.6c8048")]
+[assembly: AssemblyInformationalVersion("2.9.0.9de6ff")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(false)]
 
