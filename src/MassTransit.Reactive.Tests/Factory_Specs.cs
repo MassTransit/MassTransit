2ff84450f03f0ab9fce3fdc34447ba2d558b9f1c
Chris Patterson <chris@phatboyg.com>
2014-01-20 15:19:39 -0800

    Added support for adding observers during the Subscribe configurator for Reactive extensions
    Former-commit-id: f16afc300773767fbe018e757da8b2e7e1eed4cd

---
+++ src/MassTransit.Reactive.Tests/Factory_Specs.cs
@@ -0,0 +1,84 @@
+﻿// Copyright 2007-2012 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.Reactive.Tests
+{
+    using System;
+    using System.Reactive;
+    using System.Threading.Tasks;
+    using Magnum.Extensions;
+    using Magnum.TestFramework;
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class Subscribing_from_the_service_bus_factory
+    {
+        class A
+        {
+            public string Name { get; set; }
+        }
+
+
+        class MyObserver<T> :
+            IObserver<T>
+        {
+            readonly TaskCompletionSource<bool> _completed = new TaskCompletionSource<bool>();
+            readonly TaskCompletionSource<Exception> _exception = new TaskCompletionSource<Exception>();
+            readonly TaskCompletionSource<T> _value = new TaskCompletionSource<T>();
+
+            public Task<T> Value
+            {
+                get { return _value.Task; }
+            }
+
+
+            public void OnNext(T value)
+            {
+                _value.TrySetResult(value);
+            }
+
+            public void OnError(Exception error)
+            {
+                _exception.TrySetException(error);
+            }
+
+            public void OnCompleted()
+            {
+                _completed.TrySetResult(true);
+            }
+        }
+
+
+        [Test]
+        public void Should_allow_rx_subscribers()
+        {
+            var observer = new MyObserver<A>();
+
+            using (var bus = ServiceBusFactory.New(x =>
+                {
+                    x.ReceiveFrom("loopback://localhost/queue");
+
+                    x.Subscribe(s =>
+                        {
+                            s.Observe(observer);
+                            s.Observe(Observer.Create<A>(m => Console.WriteLine(m.Name)));
+                        });
+                }))
+            {
+                bus.Publish(new A {Name = "Joe"});
+
+                observer.Value.Wait(8.Seconds()).ShouldBeTrue();
+            }
+        }
+    }
+}
--- src/MassTransit.Reactive/ServiceBusExtensions.cs
+++ src/MassTransit.Reactive/ServiceBusExtensions.cs
@@ -1,12 +1,12 @@
-﻿// Copyright 2007-2008 The Apache Software Foundation.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+﻿// Copyright 2007-2012 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file except in compliance with the License. You may obtain a copy of the 
 // License at 
 // 
 //     http://www.apache.org/licenses/LICENSE-2.0 
 // 
-// Unless required by applicable law or agreed to in writing, software distributed 
+// Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
@@ -15,43 +15,51 @@ namespace MassTransit.Reactive
     using System;
     using System.Reactive;
     using System.Reactive.Linq;
+    using SubscriptionConfigurators;
+
 
     public static class ServiceBusExtensions
     {
         /// <summary>
-        /// <para>Gets an observer that publishes messages to all subscribed consumers for 
-        /// the message type as specified by the generic parameter.</para>
-        /// 
-        /// <para>
-        /// Read up on publishing: http://readthedocs.org/docs/masstransit/en/latest/overview/publishing.html
-        /// </para>
+        ///     <para>
+        ///         Gets an observer that publishes messages to all subscribed consumers for
+        ///         the message type as specified by the generic parameter.
+        ///     </para>
+        ///     <para>
+        ///         Read up on publishing: http://readthedocs.org/docs/masstransit/en/latest/overview/publishing.html
+        ///     </para>
         /// </summary>
-        /// <typeparam name = "T">The type of the message</typeparam>
-        /// <param name = "bus">The message bus</param>
-        public static IObserver<T> AsObserver<T> (this IServiceBus bus) where T : class
+        /// <typeparam name="T">The type of the message</typeparam>
+        /// <param name="bus">The message bus</param>
+        public static IObserver<T> AsObserver<T>(this IServiceBus bus) where T : class
         {
-            return bus.AsObserver<T> (
-                contextCallback => { });
+            return bus.AsObserver<T>(
+                contextCallback =>
+                    {
+                    });
         }
 
         /// <summary>
-        /// <para>Gets an observer that publishes messages to all subscribed consumers for 
-        /// the message type as specified by the generic parameter. The second parameter 
-        /// allows the caller to customize the outgoing publish context and set things 
-        /// like headers on the message.</para>
-        /// 
-        /// <para>
-        /// Read up on publishing: http://readthedocs.org/docs/masstransit/en/latest/overview/publishing.html
-        /// </para>
+        ///     <para>
+        ///         Gets an observer that publishes messages to all subscribed consumers for
+        ///         the message type as specified by the generic parameter. The second parameter
+        ///         allows the caller to customize the outgoing publish context and set things
+        ///         like headers on the message.
+        ///     </para>
+        ///     <para>
+        ///         Read up on publishing: http://readthedocs.org/docs/masstransit/en/latest/overview/publishing.html
+        ///     </para>
         /// </summary>
-        /// <typeparam name = "T">The type of the message</typeparam>
-        /// <param name = "bus">The message bus</param>
-        /// <param name = "contextCallback">A callback that gives the caller
-        /// access to the publish context.</param>
-        public static IObserver<T> AsObserver<T> (this IServiceBus bus, Action<IPublishContext<T>> contextCallback) where T : class
+        /// <typeparam name="T">The type of the message</typeparam>
+        /// <param name="bus">The message bus</param>
+        /// <param name="contextCallback">
+        ///     A callback that gives the caller
+        ///     access to the publish context.
+        /// </param>
+        public static IObserver<T> AsObserver<T>(this IServiceBus bus, Action<IPublishContext<T>> contextCallback) where T : class
         {
-            return Observer.Create<T> (
-                value => bus.Publish<T> (value, contextCallback));
+            return Observer.Create<T>(
+                value => bus.Publish(value, contextCallback));
         }
 
         public static IObservable<T> AsObservable<T>(this IServiceBus bus) where T : class
@@ -64,6 +72,22 @@ namespace MassTransit.Reactive
         {
             return Observable.Create<T>(
                 observer => new ServiceBusSubscription<T>(bus, observer, condition));
+        }
+
+        /// <summary>
+        /// Subscribe an observer to a message on the service bus
+        /// </summary>
+        /// <typeparam name="T">The message type</typeparam>
+        /// <param name="configurator"></param>
+        /// <param name="observer">The observer to subscribe</param>
+        /// <returns>The subscription configurator</returns>
+        public static InstanceSubscriptionConfigurator Observe<T>(this SubscriptionBusServiceConfigurator configurator,
+            IObserver<T> observer)
+            where T : class
+        {
+            var consumer = new ObserverInstanceConsumer<T>(observer);
+
+            return configurator.Instance(consumer);
         }
     }
 }
