013f63af15c80df9af456c62e756885796a56a8a
Chris Patterson <chris@phatboyg.com>
2014-01-20 15:19:39 -0800

    Added support for adding observers during the Subscribe configurator for Reactive extensions
    Former-commit-id: 25ef31fc842968876631efb6ef8360e272f5f9b7

---
+++ src/MassTransit.Reactive/ObserverInstanceConsumer.cs
@@ -0,0 +1,42 @@
+﻿// Copyright 2007-2012 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.Reactive
+{
+    using System;
+    using System.Reactive;
+
+
+    public class ObserverInstanceConsumer<T> :
+        Consumes<T>.Context
+        where T : class
+    {
+        readonly IObserver<T> _observer;
+
+        public ObserverInstanceConsumer(IObserver<T> observer)
+        {
+            _observer = Observer.Synchronize(observer);
+        }
+
+        public void Consume(IConsumeContext<T> context)
+        {
+            try
+            {
+                _observer.OnNext(context.Message);
+            }
+            catch (Exception ex)
+            {
+                _observer.OnError(ex);
+            }
+        }
+    }
+}
