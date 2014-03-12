5e95697ce0380d53d7f4d570d88c6fc9c9392e57
Travis Smith <travis@legomaster.net>
2014-03-11 22:07:59 -0400

    Fixing #219
    * Whitespace was all crazy
    * Header was off
    * Not passing parameters to `GetType()`
    * Unit tests still pass locally

--- src/MassTransit/MessageUrn.cs
+++ src/MassTransit/MessageUrn.cs
@@ -1,13 +1,12 @@
-    
-// Copyright 2010 Chris Patterson
-//  
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// Copyright 2007-2014 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file except in compliance with the License. You may obtain a copy of the 
 // License at 
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software distributed 
+//
+//    http://www.apache.org/licenses/LICENSE-2.0 
+//
+// Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
@@ -18,129 +17,128 @@ namespace MassTransit
     using System.Runtime.Serialization;
     using System.Text;
 
-
     [Serializable]
-	public class MessageUrn :
-		Uri
-	{
-		[ThreadStatic]
-		static IDictionary<Type, string> _cache;
+    public class MessageUrn :
+        Uri
+    {
+        [ThreadStatic]
+        static IDictionary<Type, string> _cache;
 
-		public MessageUrn(Type type)
-			: base(GetUrnForType(type))
-		{
-		}
+        public MessageUrn(Type type)
+            : base(GetUrnForType(type))
+        {
+        }
 
-		public MessageUrn(string uriString)
-			: base(uriString)
-		{
-		}
+        public MessageUrn(string uriString)
+            : base(uriString)
+        {
+        }
 
-		protected MessageUrn(SerializationInfo serializationInfo, StreamingContext streamingContext)
-			: base(serializationInfo, streamingContext)
-		{
-		}
+        protected MessageUrn(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
+        {
+        }
 
-		public Type GetType(bool throwOnError = true, bool ignoreCase = true)
-		{
-			if (Segments.Length == 0)
-				return null;
+        public Type GetType(bool throwOnError = true, bool ignoreCase = true)
+        {
+            if (Segments.Length == 0)
+                return null;
 
-			string[] names = Segments[0].Split(':');
-			if (names[0] != "message")
-				return null;
+            string[] names = Segments[0].Split(':');
+            if (names[0] != "message")
+                return null;
 
-			string typeName;
+            string typeName;
 
-			if (names.Length == 2)
-				typeName = names[1];
-			else if (names.Length == 3)
-				typeName = names[1] + "." + names[2] + ", " + names[1];
-			else if (names.Length >= 4)
-				typeName = names[1] + "." + names[2] + ", " + names[3];
-			else
-				return null;
+            if (names.Length == 2)
+                typeName = names[1];
+            else if (names.Length == 3)
+                typeName = names[1] + "." + names[2] + ", " + names[1];
+            else if (names.Length >= 4)
+                typeName = names[1] + "." + names[2] + ", " + names[3];
+            else
+                return null;
 
-			Type messageType = Type.GetType(typeName, true, true);
+            Type messageType = Type.GetType(typeName, throwOnError, ignoreCase);
 
-			return messageType;
-		}
+            return messageType;
+        }
 
-		static string IsInCache(Type type, Func<Type, string> provider)
-		{
-			if (_cache == null)
-				_cache = new Dictionary<Type, string>();
+        static string IsInCache(Type type, Func<Type, string> provider)
+        {
+            if (_cache == null)
+                _cache = new Dictionary<Type, string>();
 
-			string urn;
-			if (_cache.TryGetValue(type, out urn))
-				return urn;
+            string urn;
+            if (_cache.TryGetValue(type, out urn))
+                return urn;
 
-			urn = provider(type);
+            urn = provider(type);
 
-			_cache[type] = urn;
+            _cache[type] = urn;
 
-			return urn;
-		}
+            return urn;
+        }
 
-		static string GetUrnForType(Type type)
-		{
-			return IsInCache(type, x =>
-				{
-					var sb = new StringBuilder("urn:message:");
+        static string GetUrnForType(Type type)
+        {
+            return IsInCache(type, x =>
+                {
+                    var sb = new StringBuilder("urn:message:");
 
-					return GetMessageName(sb, type, true);
-				});
-		}
+                    return GetMessageName(sb, type, true);
+                });
+        }
 
-		static string GetMessageName(StringBuilder sb, Type type, bool includeScope)
-		{
+        static string GetMessageName(StringBuilder sb, Type type, bool includeScope)
+        {
             if (type.IsGenericParameter)
                 return "";
 
-			if (includeScope && type.Namespace != null)
-			{
-				string ns = type.Namespace;
-				sb.Append(ns);
+            if (includeScope && type.Namespace != null)
+            {
+                string ns = type.Namespace;
+                sb.Append(ns);
 
-				sb.Append(':');
-			}
+                sb.Append(':');
+            }
 
-			if (type.IsNested)
-			{
-				GetMessageName(sb, type.DeclaringType, false);
-				sb.Append('+');
-			}
+            if (type.IsNested)
+            {
+                GetMessageName(sb, type.DeclaringType, false);
+                sb.Append('+');
+            }
 
-			if (type.IsGenericType)
-			{
-			    var name = type.GetGenericTypeDefinition().Name;
+            if (type.IsGenericType)
+            {
+                var name = type.GetGenericTypeDefinition().Name;
 
                 //remove `1
-			    int index = name.IndexOf('`');
-                if(index > 0)
-			        name = name.Remove(index);
+                int index = name.IndexOf('`');
+                if (index > 0)
+                    name = name.Remove(index);
                 //
 
-			    sb.Append(name);
-				sb.Append('[');
+                sb.Append(name);
+                sb.Append('[');
 
-				Type[] arguments = type.GetGenericArguments();
-				for (int i = 0; i < arguments.Length; i++)
-				{
-					if (i > 0)
-						sb.Append(',');
+                Type[] arguments = type.GetGenericArguments();
+                for (int i = 0; i < arguments.Length; i++)
+                {
+                    if (i > 0)
+                        sb.Append(',');
 
-					sb.Append('[');
-					GetMessageName(sb, arguments[i], true);
-					sb.Append(']');
-				}
+                    sb.Append('[');
+                    GetMessageName(sb, arguments[i], true);
+                    sb.Append(']');
+                }
 
-				sb.Append(']');
-			}
-			else
-				sb.Append(type.Name);
+                sb.Append(']');
+            }
+            else
+                sb.Append(type.Name);
 
-			return sb.ToString();
-		}
-	}
+            return sb.ToString();
+        }
+    }
 }
