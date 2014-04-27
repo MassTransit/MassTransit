6e67af63b563c20a813f648d52ae325b3c482482
Dawid Ciecierski <dawid.ciecierski@gmail.com>
2014-04-27 22:13:32 +0200

    Added MessageUrn loopback tests
    A few simple tests to make sure that MessageUrn is able to reacreate the
    type it was passed in... which is not always the case.

--- src/MassTransit.Tests/MessageUrnSpecs.cs
+++ src/MassTransit.Tests/MessageUrnSpecs.cs
@@ -16,10 +16,22 @@ namespace MassTransit.Tests
         }
 
         [Test]
+        public void SimpleMessageLoopback()
+        {
+            LoopbackTestMessage(typeof (Ping));
+        }
+
+        [Test]
         public void NestedMessage()
         {
             var urn = new MessageUrn(typeof (X));
             Assert.AreEqual(urn.AbsolutePath, "message:MassTransit.Tests:MessageUrnSpecs+X");
+        }
+
+        [Test]
+        public void NestedMessageLoopback()
+        {
+            LoopbackTestMessage(typeof (X));
         }
 
         [Test]
@@ -31,11 +43,30 @@ namespace MassTransit.Tests
         }
 
         [Test]
+        public void OpenGenericMessageLoopback()
+        {
+            LoopbackTestMessage(typeof (G<>));
+        }
+
+        [Test]
         public void ClosedGenericMessage()
         {
             var urn = new MessageUrn(typeof (G<Ping>));
             var expected = new Uri("urn:message:MassTransit.Tests:G[[MassTransit.TestFramework.Examples.Messages:Ping]]");
             Assert.AreEqual(expected.AbsolutePath,urn.AbsolutePath) ;
+        }
+
+        [Test]
+        public void ClosedGenericMessageLoopback()
+        {
+            LoopbackTestMessage(typeof (G<Ping>));
+        }
+
+        private static void LoopbackTestMessage(Type messageType)
+        {
+            var urn = new MessageUrn(messageType);
+            var recreatedMessageType = urn.GetType(throwOnError: true, ignoreCase: true);
+            Assert.That(recreatedMessageType, Is.EqualTo(messageType));
         }
 
         class X{}
