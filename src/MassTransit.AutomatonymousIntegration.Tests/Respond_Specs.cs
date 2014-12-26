dcbfc05a39ac9761e9566b08e334f2be37b67d64
Chris Patterson <chris@phatboyg.com>
2014-12-25 16:47:39 -0800

    Final test passing for Automatonymous
    Former-commit-id: 68dd497b198bc8c4e1abd0b0b9895ab00c63343c

--- src/MassTransit.AutomatonymousIntegration.Tests/Respond_Specs.cs
+++ src/MassTransit.AutomatonymousIntegration.Tests/Respond_Specs.cs
@@ -1,105 +1,109 @@
-﻿// Copyright 2011 Chris Patterson, Dru Sellers
+﻿// Copyright 2007-2014 Chris Patterson, Dru Sellers, Travis Smith, et. al.
 //  
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file except in compliance with the License. You may obtain a copy of the 
 // License at 
 // 
 //     http://www.apache.org/licenses/LICENSE-2.0 
 // 
-// Unless required by applicable law or agreed to in writing, software distributed 
+// Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.AutomatonymousTests
 {
     using System;
+    using System.Threading.Tasks;
     using Automatonymous;
     using NUnit.Framework;
     using Saga;
+    using TestFramework;
 
 
-//    [TestFixture]
-//    public class Responding_from_within_a_saga :
-//        MassTransitTestFixture
-//    {
-//        [Test]
-//        public void Should_receive_the_response_message()
-//        {
-//            var responseReceived = new FutureMessage<StartupComplete>();
-//
-//            Bus.PublishRequest(new Start(), x =>
-//                {
-//                    x.Handle<StartupComplete>(responseReceived.Set);
-//                    x.HandleTimeout(8.Seconds(), () => { });
-//                });
-//
-//            Assert.IsTrue(responseReceived.IsAvailable(0.Seconds()));
-//        }
-//
-//        protected override void ConfigureSubscriptions(SubscriptionBusServiceConfigurator configurator)
-//        {
-//            _machine = new TestStateMachine();
-//            _repository = new InMemorySagaRepository<Instance>();
-//
-//            configurator.StateMachineSaga(_machine, _repository);
-//        }
-//
-//        TestStateMachine _machine;
-//        InMemorySagaRepository<Instance> _repository;
-//
-//        class Instance :
-//            SagaStateMachineInstance
-//        {
-//            public Instance(Guid correlationId)
-//            {
-//                CorrelationId = correlationId;
-//            }
-//
-//            protected Instance()
-//            {
-//            }
-//
-//            public State CurrentState { get; set; }
-//            public Guid CorrelationId { get; set; }
-//            public IServiceBus Bus { get; set; }
-//        }
-//
-//
-//        class TestStateMachine :
-//            AutomatonymousStateMachine<Instance>
-//        {
-//            public TestStateMachine()
-//            {
-//                InstanceState(x => x.CurrentState);
-//
-//                State(() => Running);
-//                Event(() => Started);
-//
-//                Initially(
-//                    When(Started)
-//                        .Respond(new StartupComplete())
-//                        .TransitionTo(Running));
-//            }
-//
-//            public State Running { get; private set; }
-//            public Event<Start> Started { get; private set; }
-//        }
-//
-//
-//        class Start :
-//            CorrelatedBy<Guid>
-//        {
-//            public Start()
-//            {
-//                CorrelationId = CombGuid.Generate();
-//            }
-//
-//            public Guid CorrelationId { get; set; }
-//        }
-//
-//
-//        class StartupComplete
-//        {
-//        }
-//    }
+    [TestFixture]
+    public class Responding_from_within_a_saga :
+        InMemoryTestFixture
+    {
+        protected override void ConfigureInputQueueEndpoint(IReceiveEndpointConfigurator configurator)
+        {
+            _machine = new TestStateMachine();
+            _repository = new InMemorySagaRepository<Instance>();
+
+            configurator.StateMachineSaga(_machine, _repository);
+        }
+
+        TestStateMachine _machine;
+        InMemorySagaRepository<Instance> _repository;
+
+
+        class Instance :
+            SagaStateMachineInstance
+        {
+            public Instance(Guid correlationId)
+            {
+                CorrelationId = correlationId;
+            }
+
+            protected Instance()
+            {
+            }
+
+            public State CurrentState { get; set; }
+            public IServiceBus Bus { get; set; }
+            public Guid CorrelationId { get; set; }
+        }
+
+
+        class TestStateMachine :
+            AutomatonymousStateMachine<Instance>
+        {
+            public TestStateMachine()
+            {
+                InstanceState(x => x.CurrentState);
+
+                State(() => Running);
+                Event(() => Started);
+
+                Initially(
+                    When(Started)
+                        .Respond(new StartupComplete())
+                        .TransitionTo(Running));
+            }
+
+            public State Running { get; private set; }
+            public Event<Start> Started { get; private set; }
+        }
+
+
+        class Start :
+            CorrelatedBy<Guid>
+        {
+            public Start()
+            {
+                CorrelationId = NewId.NextGuid();
+            }
+
+            public Guid CorrelationId { get; set; }
+        }
+
+
+        class StartupComplete
+        {
+        }
+
+
+        [Test]
+        public async void Should_receive_the_response_message()
+        {
+            Task<StartupComplete> statusTask = null;
+            Request<Start> request = await Bus.Request(InputQueueAddress, new Start(), x =>
+            {
+                statusTask = x.Handle<StartupComplete>();
+                x.Timeout = TestTimeout;
+            }, TestCancellationToken);
+
+
+            StartupComplete status = await statusTask;
+        }
+    }
 }
