c95af8df135d82b8d1a48d32cb36374c7d44f882
Chris Patterson <chris@phatboyg.com>
2015-01-23 09:55:17 -0800

    Fixed NewId generator to avoid duplicate ids under heavy thread load
    Former-commit-id: 16c41af5a16377ed2b862752d2655a1b66675180

--- src/MassTransit.Tests/NewId_/NewId_Specs.cs
+++ src/MassTransit.Tests/NewId_/NewId_Specs.cs
@@ -13,9 +13,14 @@
 namespace MassTransit.Tests.NewId_
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
     using NUnit.Framework;
     using NewIdProviders;
+    using Util;
 
 
     [TestFixture]
@@ -83,5 +88,68 @@ namespace MassTransit.Tests.NewId_
             Console.WriteLine("Generated {0} ids in {1}ms ({2}/ms)", limit, timer.ElapsedMilliseconds,
                 limit / timer.ElapsedMilliseconds);
         }
+
+#if NET40
+        [Test, Explicit]
+        public void Should_be_completely_thread_safe_to_avoid_duplicates()
+        {
+            NewId.Next();
+
+            Stopwatch timer = Stopwatch.StartNew();
+
+            int threadCount = 20;
+
+            int workerThreads, complete;
+            ThreadPool.GetMinThreads(out workerThreads, out complete);
+            ThreadPool.SetMinThreads(workerThreads + threadCount, complete);
+
+
+            var loopCount = 1024 * 1024;
+
+            int limit = loopCount * threadCount;
+
+            var ids = new NewId[limit];
+
+            var tasks = new List<Task>();
+
+            var begin = new TaskCompletionSource<bool>();
+
+            for (int threadId = 0; threadId < threadCount; threadId++)
+            {
+                var start = threadId * loopCount;
+                var end = start + loopCount;
+
+                var task = Task.Factory.StartNew(() =>
+                {
+                    //begin.Task.Wait();
+
+                    for (int i = start; i < end; i++)
+                        ids[i] = NewId.Next();
+                });
+
+                tasks.Add(task);
+            }
+
+            //begin.SetResult(true);
+
+            Task.WaitAll(tasks.ToArray());
+
+            timer.Stop();
+
+            Console.WriteLine("Generated {0} ids in {1}ms ({2}/ms)", limit, timer.ElapsedMilliseconds,
+                limit / timer.ElapsedMilliseconds);
+
+            Console.WriteLine("Distinct: {0}", ids.Distinct().Count());
+
+            var duplicates = ids.GroupBy(x => x).Where(x => x.Count() > 1).ToArray();
+
+            Console.WriteLine("Duplicates: {0}", duplicates.Count());
+
+            foreach (var newId in duplicates)
+            {
+                Console.WriteLine("{0} {1}", newId.Key, newId.Count());
+            }
+        }
+#endif
     }
 }
--- src/MassTransit/NewId/NewIdGenerator.cs
+++ src/MassTransit/NewId/NewIdGenerator.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2007-2012 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+﻿// Copyright 2007-2014 Chris Patterson, Dru Sellers, Travis Smith, et. al.
 //  
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file except in compliance with the License. You may obtain a copy of the 
@@ -19,8 +19,6 @@ namespace MassTransit
 
         readonly object _sync = new object();
         readonly ITickProvider _tickProvider;
-        readonly byte[] _workerId;
-        readonly int _workerIndex;
         int _a;
         int _b;
         long _lastTick;
@@ -30,18 +28,16 @@ namespace MassTransit
 
         public NewIdGenerator(ITickProvider tickProvider, IWorkerIdProvider workerIdProvider, int workerIndex = 0)
         {
-            _workerIndex = workerIndex;
-            _workerId = workerIdProvider.GetWorkerId(_workerIndex);
             _tickProvider = tickProvider;
 
-            _c = _workerId[0] << 24 | _workerId[1] << 16 | _workerId[2] << 8 | _workerId[3];
-            _d = _workerId[4] << 24 | _workerId[5] << 16;
+            byte[] workerId = workerIdProvider.GetWorkerId(workerIndex);
+
+            _c = workerId[0] << 24 | workerId[1] << 16 | workerId[2] << 8 | workerId[3];
+            _d = workerId[4] << 24 | workerId[5] << 16;
         }
 
         public NewId Next()
         {
-            ushort sequence;
-
             long ticks = _tickProvider.Ticks;
             lock (_sync)
             {
@@ -51,10 +47,10 @@ namespace MassTransit
                 if (_sequence == 65535) // we are about to rollover, so we need to increment ticks
                     UpdateTimestamp(_lastTick + 1);
 
-                sequence = _sequence++;
-            }
+                ushort sequence = _sequence++;
 
-            return new NewId(_a, _b, _c, _d | sequence);
+                return new NewId(_a, _b, _c, _d | sequence);
+            }
         }
 
         void UpdateTimestamp(long tick)
