0265e63a4040c2d4fcbe48758b2be5f20f4e99a0
Chris Patterson <chris@phatboyg.com>
2015-01-24 22:10:08 -0800

    Tweaked build, documentation file
    Former-commit-id: dece2c0975706a8a0d347a46533ad6da135bac40

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -5,9 +5,9 @@ using System.Security;
 [assembly: AssemblyDescription("MassTransit is a distributed application framework for .NET http://masstransit-project.com")]
 [assembly: AssemblyProduct("MassTransit")]
 [assembly: AssemblyCopyright("Copyright 2007-2015 Chris Patterson, Dru Sellers, Travis Smith, et. al. - All rights reserved.")]
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.1")]
+[assembly: AssemblyFileVersion("3.0.1")]
 
-[assembly: AssemblyInformationalVersion("3.0.0-alpha.594766")]
+[assembly: AssemblyInformationalVersion("3.0.1-alpha.605ab9")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(true)]
