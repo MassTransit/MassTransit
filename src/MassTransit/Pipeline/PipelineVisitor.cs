ade7be9b64d419eb469174ed5b85dc5d7d4666a3
Chris Patterson <chris@phatboyg.com>
2015-03-31 12:04:50 -0700

    Cleanup extensions for send, visitor

--- src/MassTransit/Pipeline/PipelineVisitor.cs
+++ src/MassTransit/Pipeline/PipelineVisitor.cs
@@ -12,6 +12,7 @@
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.Pipeline
 {
+    using System;
     using System.Linq;
     using Filters;
     using Internals.Extensions;
@@ -21,6 +22,26 @@ namespace MassTransit.Pipeline
         IConsumerFilterVisitor,
         IConsumeMessageFilterVisitor
     {
+        bool IConsumeMessageFilterVisitor.Visit<T>(IFilter<ConsumeContext<T>> filter, FilterVisitorCallback callback)
+        {
+            var consumeFilter = filter as TeeConsumeFilter<T>;
+            if (consumeFilter != null)
+                return VisitTeeConsumeFilter(consumeFilter, callback);
+
+            var messageFilter = filter as HandlerMessageFilter<T>;
+            if (messageFilter != null)
+                return VisitHandlerMessageFilter(messageFilter, callback);
+
+            var retryFilter = filter as RetryFilter<T>;
+            if (retryFilter != null)
+                return VisitRetryConsumeFilter(retryFilter, callback);
+
+            if (filter.GetType().IsGenericType && filter.GetType().GetGenericTypeDefinition() == typeof(ConsumerMessageFilter<,>))
+                return VisitConsumerMessageFilter(filter, callback);
+
+            return VisitUnknownFilter(filter, callback);
+        }
+
         bool IConsumerFilterVisitor.Visit<TConsumer>(IFilter<ConsumerConsumeContext<TConsumer>> filter, FilterVisitorCallback callback)
         {
             return VisitConsumerConsumeFilter(filter, callback);
@@ -59,35 +80,26 @@ namespace MassTransit.Pipeline
             if (consumeFilter != null)
                 return VisitMessageTypeConsumeFilter(consumeFilter, callback);
 
-            if (filter is RetryFilter)
-                return VisitRetryConsumeFilter((RetryFilter)filter, callback);
+            var retryFilter = filter as RetryFilter;
+            if (retryFilter != null)
+                return VisitRetryConsumeFilter(retryFilter, callback);
 
 
             if (typeof(T).HasInterface(typeof(ConsumerConsumeContext<,>)))
             {
-                var types = typeof(T).GetClosingArguments(typeof(ConsumerConsumeContext<,>)).ToArray();
+                Type[] types = typeof(T).GetClosingArguments(typeof(ConsumerConsumeContext<,>)).ToArray();
 
                 return ConsumerFilterVisitorAdapterCache.GetAdapter(types).Visit(this, filter, callback);
             }
 
             if (typeof(T).HasInterface(typeof(ConsumeContext<>)))
             {
-                var types = typeof(T).GetClosingArguments(typeof(ConsumeContext<>)).ToArray();
+                Type[] types = typeof(T).GetClosingArguments(typeof(ConsumeContext<>)).ToArray();
 
                 return ConsumeMessageFilterVisitorAdapterCache.GetAdapter(types).Visit(this, filter, callback);
             }
 
             return callback(this);
-        }
-
-        bool IConsumeMessageFilterVisitor.Visit<T>(IFilter<ConsumeContext<T>> filter, FilterVisitorCallback callback)
-        {
-            if (filter is TeeConsumeFilter<T>)
-                return VisitTeeConsumeFilter((TeeConsumeFilter<T>)filter, callback);
-            if (filter is HandlerMessageFilter<T>)
-                return VisitHandlerMessageFilter((HandlerMessageFilter<T>)filter, callback);
-          
-            return VisitUnknownFilter(filter, callback);
         }
 
         protected virtual bool VisitPipe<T>(IPipe<T> pipe, PipeVisitorCallback callback)
@@ -124,6 +136,18 @@ namespace MassTransit.Pipeline
             return callback(this);
         }
 
+        protected virtual bool VisitRetryConsumeFilter<T>(RetryFilter<T> filter, FilterVisitorCallback callback)
+            where T : class
+        {
+            return callback(this);
+        }
+
+        protected virtual bool VisitConsumerMessageFilter<T>(IFilter<ConsumeContext<T>> filter, FilterVisitorCallback callback)
+            where T : class
+        {
+            return callback(this);
+        }
+
         protected virtual bool VisitMethodConsumerMessageFilter<TConsumer, T>(MethodConsumerMessageFilter<TConsumer, T> filter,
             FilterVisitorCallback callback)
             where TConsumer : class, IConsumer<T>
@@ -152,10 +176,14 @@ namespace MassTransit.Pipeline
             where TConsumer : class, IConsumer<T>
             where T : class
         {
-            if (filter is ConsumerSplitFilter<TConsumer, T>)
-                return VisitConsumerSplitFilter((ConsumerSplitFilter<TConsumer, T>)filter, callback);
-            if (filter is MethodConsumerMessageFilter<TConsumer, T>)
-                return VisitMethodConsumerMessageFilter((MethodConsumerMessageFilter<TConsumer, T>)filter, callback);
+            var splitFilter = filter as ConsumerSplitFilter<TConsumer, T>;
+            if (splitFilter != null)
+                return VisitConsumerSplitFilter(splitFilter, callback);
+
+            var messageFilter = filter as MethodConsumerMessageFilter<TConsumer, T>;
+            if (messageFilter != null)
+                return VisitMethodConsumerMessageFilter(messageFilter, callback);
+
             return callback(this);
         }
     }
---
+++ src/MassTransit/SendEndpointExtensions.cs
@@ -0,0 +1,181 @@
+// Copyright 2007-2015 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit
+{
+    using System;
+    using System.Threading.Tasks;
+    using Pipeline;
+
+
+    public static class SendEndpointExtensions
+    {
+        /// <summary>
+        /// Send a message
+        /// </summary>
+        /// <typeparam name="T">The message type</typeparam>
+        /// <param name="context"></param>
+        /// <param name="endpointAddress"></param>
+        /// <param name="message">The message</param>
+        /// <returns>The task which is completed once the Send is acknowledged by the broker</returns>
+        public static async Task Send<T>(this ConsumeContext context, Uri endpointAddress, T message)
+            where T : class
+        {
+            ISendEndpoint endpoint = await context.GetSendEndpoint(endpointAddress);
+
+            await endpoint.Send(message, context.CancellationToken);
+        }
+
+        /// <summary>
+        /// Send a message
+        /// </summary>
+        /// <typeparam name="T">The message type</typeparam>
+        /// <param name="context"></param>
+        /// <param name="endpointAddress"></param>
+        /// <param name="message">The message</param>
+        /// <param name="pipe"></param>
+        /// <returns>The task which is completed once the Send is acknowledged by the broker</returns>
+        public static async Task Send<T>(this ConsumeContext context, Uri endpointAddress, T message, IPipe<SendContext<T>> pipe)
+            where T : class
+        {
+            ISendEndpoint endpoint = await context.GetSendEndpoint(endpointAddress);
+
+            await endpoint.Send(message, pipe, context.CancellationToken);
+        }
+
+        /// <summary>
+        /// Send a message
+        /// </summary>
+        /// <typeparam name="T">The message type</typeparam>
+        /// <param name="context"></param>
+        /// <param name="endpointAddress"></param>
+        /// <param name="message">The message</param>
+        /// <param name="pipe"></param>
+        /// <returns>The task which is completed once the Send is acknowledged by the broker</returns>
+        public static async Task Send<T>(this ConsumeContext context, Uri endpointAddress, T message, IPipe<SendContext> pipe)
+            where T : class
+        {
+            ISendEndpoint endpoint = await context.GetSendEndpoint(endpointAddress);
+
+            await endpoint.Send(message, pipe, context.CancellationToken);
+        }
+
+        /// <summary>
+        /// Send a message
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="endpointAddress"></param>
+        /// <param name="message">The message</param>
+        /// <returns>The task which is completed once the Send is acknowledged by the broker</returns>
+        public static async Task Send(this ConsumeContext context, Uri endpointAddress, object message)
+        {
+            ISendEndpoint endpoint = await context.GetSendEndpoint(endpointAddress);
+
+            await endpoint.Send(message, context.CancellationToken);
+        }
+
+        /// <summary>
+        /// Send a message
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="endpointAddress"></param>
+        /// <param name="message">The message</param>
+        /// <param name="messageType"></param>
+        /// <returns>The task which is completed once the Send is acknowledged by the broker</returns>
+        public static async Task Send(this ConsumeContext context, Uri endpointAddress, object message, Type messageType)
+        {
+            ISendEndpoint endpoint = await context.GetSendEndpoint(endpointAddress);
+
+            await endpoint.Send(message, messageType, context.CancellationToken);
+        }
+
+        /// <summary>
+        /// Send a message
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="endpointAddress"></param>
+        /// <param name="message">The message</param>
+        /// <param name="pipe"></param>
+        /// <returns>The task which is completed once the Send is acknowledged by the broker</returns>
+        public static async Task Send(this ConsumeContext context, Uri endpointAddress, object message, Type messageType, IPipe<SendContext> pipe)
+        {
+            ISendEndpoint endpoint = await context.GetSendEndpoint(endpointAddress);
+
+            await endpoint.Send(message, messageType, pipe, context.CancellationToken);
+        }
+
+        /// <summary>
+        /// Send a message
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="endpointAddress"></param>
+        /// <param name="message">The message</param>
+        /// <param name="pipe"></param>
+        /// <returns>The task which is completed once the Send is acknowledged by the broker</returns>
+        public static async Task Send(this ConsumeContext context, Uri endpointAddress, object message, IPipe<SendContext> pipe)
+        {
+            ISendEndpoint endpoint = await context.GetSendEndpoint(endpointAddress);
+
+            await endpoint.Send(message, pipe, context.CancellationToken);
+        }
+
+        /// <summary>
+        /// Send a message
+        /// </summary>
+        /// <typeparam name="T">The message type</typeparam>
+        /// <param name="context"></param>
+        /// <param name="endpointAddress"></param>
+        /// <param name="values"></param>
+        /// <returns>The task which is completed once the Send is acknowledged by the broker</returns>
+        public static async Task Send<T>(this ConsumeContext context, Uri endpointAddress, object values)
+            where T : class
+        {
+            ISendEndpoint endpoint = await context.GetSendEndpoint(endpointAddress);
+
+            await endpoint.Send<T>(values, context.CancellationToken);
+        }
+
+        /// <summary>
+        /// Send a message
+        /// </summary>
+        /// <typeparam name="T">The message type</typeparam>
+        /// <param name="context"></param>
+        /// <param name="endpointAddress"></param>
+        /// <param name="values"></param>
+        /// <param name="pipe"></param>
+        /// <returns>The task which is completed once the Send is acknowledged by the broker</returns>
+        public static async Task Send<T>(this ConsumeContext context, Uri endpointAddress, object values, IPipe<SendContext<T>> pipe)
+            where T : class
+        {
+            ISendEndpoint endpoint = await context.GetSendEndpoint(endpointAddress);
+
+            await endpoint.Send(values, pipe, context.CancellationToken);
+        }
+
+        /// <summary>
+        /// Send a message
+        /// </summary>
+        /// <typeparam name="T">The message type</typeparam>
+        /// <param name="context"></param>
+        /// <param name="endpointAddress"></param>
+        /// <param name="values"></param>
+        /// <param name="pipe"></param>
+        /// <returns>The task which is completed once the Send is acknowledged by the broker</returns>
+        public static async Task Send<T>(this ConsumeContext context, Uri endpointAddress, object values, IPipe<SendContext> pipe)
+            where T : class
+        {
+            ISendEndpoint endpoint = await context.GetSendEndpoint(endpointAddress);
+
+            await endpoint.Send<T>(values, pipe, context.CancellationToken);
+        }
+    }
+}
