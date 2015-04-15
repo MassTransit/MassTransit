fc9aced8cfa6daf7dbe26061621deb6251e8c73e
Chris Patterson <chris@phatboyg.com>
2015-04-15 16:23:43 -0700

    MOre array testing

--- src/MassTransit.Tests/Serialization/Array_Specs.cs
+++ src/MassTransit.Tests/Serialization/Array_Specs.cs
@@ -15,6 +15,7 @@ namespace MassTransit.Tests.Serialization
     namespace Array_Specs
     {
         using System;
+        using System.Collections.Generic;
         using System.Text;
         using MassTransit.Serialization;
         using NUnit.Framework;
@@ -26,20 +27,9 @@ namespace MassTransit.Tests.Serialization
             SerializationTest
         {
             [Test]
-            public void Should_return_a_null_array()
-            {
-                var someArray = new SomeArray();
-
-                SomeArray result = SerializeAndReturn(someArray);
-
-                someArray.Elements.ShouldBe(null);
-                result.Elements.ShouldBe(null);
-            }
-
-            [Test]
             public void Should_come_from_json_as_null()
             {
-                 string source = @"{
+                string source = @"{
   ""messageId"": ""e655000040d800fff4f808d245dca3c8"",
   ""requestId"": """ + _requestId.ToString("N") + @""",
   ""sourceAddress"": ""loopback://localhost/source"",
@@ -58,6 +48,75 @@ namespace MassTransit.Tests.Serialization
                 result.Elements.ShouldBe(null);
             }
 
+            [Test]
+            public void Should_get_one_element()
+            {
+                string source = @"{
+  ""messageId"": ""e655000040d800fff4f808d245dca3c8"",
+  ""requestId"": """ + _requestId.ToString("N") + @""",
+  ""sourceAddress"": ""loopback://localhost/source"",
+  ""destinationAddress"": ""loopback://localhost/destination"",
+  ""responseAddress"": ""loopback://localhost/response"",
+  ""faultAddress"": ""loopback://localhost/fault"",
+  ""messageType"": [
+    ""urn:message:MassTransit.Tests.Serialization.Array_Specs:SomeArray""
+  ],
+  ""message"": {
+    ""elements"": 
+      {
+        ""value"": 27
+      }
+  },
+  ""headers"": {}
+}";
+
+                var result = Return<SomeArray>(Encoding.UTF8.GetBytes(source));
+
+
+                result.Elements.ShouldNotBe(null);
+                result.Elements.Length.ShouldBe(1);
+            }
+
+            [Test]
+            public void Should_return_a_null_array()
+            {
+                var someArray = new SomeArray();
+
+                SomeArray result = SerializeAndReturn(someArray);
+
+                someArray.Elements.ShouldBe(null);
+                result.Elements.ShouldBe(null);
+            }
+
+            [Test]
+            public void Should_serialize_a_single_element()
+            {
+                var someArray = new SomeArray();
+                someArray.Elements = new ArrayElement[1];
+                someArray.Elements[0] = new ArrayElement();
+                someArray.Elements[0].Value = 27;
+
+                SomeArray result = SerializeAndReturn(someArray);
+
+                result.Elements.ShouldNotBe(null);
+                result.Elements.Length.ShouldBe(1);
+            }
+
+            [Test]
+            public void Should_serialize_a_single_element_collection()
+            {
+                var someArray = new SomeCollection();
+                someArray.Elements = new ArrayElement[1]
+                {
+                    new ArrayElement {Value = 27}
+                };
+
+                SomeCollection result = SerializeAndReturn(someArray);
+
+                result.Elements.ShouldNotBe(null);
+                result.Elements.Count.ShouldBe(1);
+            }
+
             public A_null_array(Type serializerType)
                 : base(serializerType)
             {
@@ -74,6 +133,12 @@ namespace MassTransit.Tests.Serialization
         class SomeArray
         {
             public ArrayElement[] Elements { get; set; }
+        }
+
+
+        class SomeCollection
+        {
+            public ICollection<ArrayElement> Elements { get; set; }
         }
     }
 }
