b634ec38610d90e0f2059c0079cb1247339c3122
Chris Patterson <chris@phatboyg.com>
2015-04-26 18:03:14 -0700

    Routing slips can now be terminated with a published event upon termination
    Former-commit-id: 0d18a0afefa0bb2ea26f9f2170faac75361a44a1

---
+++ src/MassTransit/Courier/Results/TerminateExecutionResult.cs
@@ -0,0 +1,66 @@
+// Copyright 2007-2015 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.Courier.Results
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Contracts;
+
+
+    class TerminateExecutionResult<TArguments> :
+        CompletedExecutionResult<TArguments>
+        where TArguments : class
+    {
+        public TerminateExecutionResult(ExecuteContext<TArguments> context, IRoutingSlipEventPublisher publisher, Activity activity,
+            RoutingSlip routingSlip)
+            : base(context, publisher, activity, routingSlip)
+        {
+        }
+
+        protected override RoutingSlipBuilder CreateRoutingSlipBuilder(RoutingSlip routingSlip)
+        {
+            return new RoutingSlipBuilder(routingSlip, Enumerable.Empty<Activity>(), routingSlip.Itinerary.Skip(1));
+        }
+
+        protected override async Task PublishActivityEvents(RoutingSlip routingSlip, RoutingSlipBuilder builder)
+        {
+            await base.PublishActivityEvents(routingSlip, builder);
+
+            await Publisher.PublishRoutingSlipTerminated(Context.ExecutionId, Context.Timestamp, Context.Elapsed, routingSlip.Variables,
+                builder.SourceItinerary);
+        }
+    }
+
+
+    class TerminateWithVariablesExecutionResult<TArguments> :
+        TerminateExecutionResult<TArguments>
+        where TArguments : class
+    {
+        readonly IDictionary<string, object> _variables;
+
+        public TerminateWithVariablesExecutionResult(ExecuteContext<TArguments> context, IRoutingSlipEventPublisher publisher, Activity activity,
+            RoutingSlip routingSlip, IDictionary<string, object> variables)
+            : base(context, publisher, activity, routingSlip)
+        {
+            _variables = variables;
+        }
+
+        protected override void Build(RoutingSlipBuilder builder)
+        {
+            base.Build(builder);
+
+            builder.SetVariables(_variables);
+        }
+    }
+}
