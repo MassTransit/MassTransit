a8065f8c4488ba2dc5390b5b5e5bd20a8f6751b1
Chris Patterson <chris@phatboyg.com>
2015-05-05 07:56:46 -0700

    Fixed Issue #284, including a new unit test to verify when ISerializable is used instead
    Former-commit-id: 34bde9a5db8ce83b380b6892d34f726c28a0e2b5

---
+++ src/MassTransit.Tests/Serialization/Issue284_Array.cs
@@ -0,0 +1,82 @@
+﻿// Copyright 2007-2014 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.Tests.Serialization
+{
+    using System.Runtime.Serialization;
+    using System.Threading;
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class Issue284_Array
+    {
+        [Test]
+        public void VerifyIssue284()
+        {
+            IServiceBus loopbackBus = ServiceBusFactory.New(sbc =>
+            {
+                sbc.ReceiveFrom("loopback://localhost/Issue284");
+                sbc.UseJsonSerializer();
+            });
+
+            var expected = new ContainerWithObjectArray()
+            {
+                ObjectArray = null,
+            };
+
+            var signal = new AutoResetEvent(false);
+            ContainerWithObjectArray actual = null;
+
+            loopbackBus.SubscribeHandler<ContainerWithObjectArray>(container =>
+            {
+                actual = container;
+                signal.Set();
+            });
+
+            loopbackBus.Publish(expected);
+
+            signal.WaitOne();
+
+            Assert.IsNotNull(actual);
+            Assert.IsNull(actual.ObjectArray); // This is where it fails.
+        }
+
+        // this fixes it without touching a thing
+        //[JsonObject]
+        public sealed class ContainerWithObjectArray : ISerializable
+        {
+            public ContainerWithObjectArray()
+            {
+            }
+
+            public ContainerWithObjectArray(SerializationInfo info,
+                StreamingContext context)
+            {
+//                ObjectArray = ((IList<object>)info.GetValue("ObjectArray",
+//                    typeof(IList<object>))) // this fails with null, every time.ToArray();
+
+                // with the ListJsonConverter fix for null, this now works as well.
+
+                // this is the proper way to deserialize an array, and no need to check for Null before calling ToArray()
+                ObjectArray = (object[])info.GetValue("ObjectArray", typeof(object[]));
+            }
+
+            public object[] ObjectArray { get; set; }
+
+            public void GetObjectData(SerializationInfo info, StreamingContext context)
+            {
+                info.AddValue("ObjectArray", ObjectArray);
+            }
+        }
+    }
+}
--- src/MassTransit/Serialization/ListJsonConverter.cs
+++ src/MassTransit/Serialization/ListJsonConverter.cs
@@ -1,12 +1,12 @@
-﻿// Copyright 2007-2011 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+﻿// Copyright 2007-2014 Chris Patterson, Dru Sellers, Travis Smith, et. al.
 //  
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file except in compliance with the License. You may obtain a copy of the 
 // License at 
 // 
 //     http://www.apache.org/licenses/LICENSE-2.0 
 // 
-// Unless required by applicable law or agreed to in writing, software distributed 
+// Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
@@ -29,7 +29,7 @@ namespace MassTransit.Serialization
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
-                                        JsonSerializer serializer)
+            JsonSerializer serializer)
         {
             if (objectType.IsArray)
             {
@@ -48,8 +48,8 @@ namespace MassTransit.Serialization
         public override bool CanConvert(Type objectType)
         {
             return (objectType.IsGenericType
-                    && (objectType.GetGenericTypeDefinition() == typeof (IList<>)
-                        || objectType.GetGenericTypeDefinition() == typeof (List<>)))
+                    && (objectType.GetGenericTypeDefinition() == typeof(IList<>)
+                        || objectType.GetGenericTypeDefinition() == typeof(List<>)))
                    || (objectType.IsArray
                        && objectType.Implements<IEnumerable>());
         }
@@ -58,20 +58,21 @@ namespace MassTransit.Serialization
         {
             var list = new List<T>();
 
-            if (reader.TokenType == JsonToken.StartArray)
+            if (reader.TokenType == JsonToken.Null)
             {
-                serializer.Populate(reader, list);
+                return null;
             }
+
+            if (reader.TokenType == JsonToken.StartArray)
+                serializer.Populate(reader, list);
             else
             {
-                var item = (T) serializer.Deserialize(reader, typeof (T));
+                var item = (T)serializer.Deserialize(reader, typeof(T));
                 list.Add(item);
             }
 
             if (isArray)
-            {
                 return list.ToArray();
-            }
 
             return list;
         }
