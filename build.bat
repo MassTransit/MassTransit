0ad5f8676897a29fffc7e3264973faa3b21372ca
Chris Patterson <chris@phatboyg.com>
2015-05-19 15:26:41 -0700

    Updated build and rake file to use bundler

--- build.bat
+++ build.bat
@@ -1,18 +1,18 @@
 @echo off
 
 echo Building for .NET 3.5
-call rake all BUILD_CONFIG_KEY=NET35 %1
+call bundle exec rake all BUILD_CONFIG_KEY=NET35 %1
 IF NOT %ERRORLEVEL% == 0 goto FAILED
 
 echo Building for .NET 4.0
-call rake unclean %1
+call bundle exec rake unclean %1
 IF NOT %ERRORLEVEL% == 0 goto FAILED
 
 echo Creating NuGet package
-call rake nuget %1
+call bundle exec rake nuget %1
 IF NOT %ERRORLEVEL% == 0 goto FAILED
 
 echo Create ZIP package
-call rake package
+call bundle exec rake package
 
 :FAILED
