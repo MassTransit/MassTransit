b725d9cdff9804daa8a4c3364ade5552a3bdcfb6
Chris Patterson <chris@phatboyg.com>
2015-06-16 20:47:22 -0700

    Adjusted namespaces for easy visibility of extension methods
    Former-commit-id: bbbe8a47122249a4bc72079bc67317bbe471ff15

--- src/MassTransit.RabbitMqTransport/DelayedExchangeRetryExtensions.cs
+++ src/MassTransit.RabbitMqTransport/DelayedExchangeRetryExtensions.cs
@@ -10,12 +10,12 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
-namespace MassTransit.RabbitMqTransport
+namespace MassTransit
 {
     using System;
-    using Configuration;
     using PipeConfigurators;
     using Policies;
+    using RabbitMqTransport.Configuration;
 
 
     public static class DelayedExchangeRetryExtensions
