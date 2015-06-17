bbbe8a47122249a4bc72079bc67317bbe471ff15
Chris Patterson <chris@phatboyg.com>
2015-06-16 20:47:22 -0700

    Adjusted namespaces for easy visibility of extension methods

--- src/MassTransit.RabbitMqTransport/DeferExtensions.cs
+++ src/MassTransit.RabbitMqTransport/DeferExtensions.cs
@@ -10,11 +10,11 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
-namespace MassTransit.RabbitMqTransport
+namespace MassTransit
 {
     using System;
     using System.Threading.Tasks;
-    using Contexts;
+    using RabbitMqTransport.Contexts;
 
 
     public static class DeferExtensions
