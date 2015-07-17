1302c79019a8e38576bb950531a60eb2813e97a4
Travis Smith <travis@legomaster.net>
2015-07-17 11:49:20 -0400

    Distributor is not reporting on right bus
    We want the workers to use the control bus for metadata, so metadata
    channel doesn't back up main content of vice-versa.

--- src/MassTransit/Distributor/WorkerLoad.cs
+++ src/MassTransit/Distributor/WorkerLoad.cs
@@ -85,7 +85,7 @@ namespace MassTransit.Distributor
 
                 _updatePending = false;
 
-                _worker.Bus.Publish(message, x => x.ExpiresAt(DateTime.UtcNow + timeToLive));
+                _worker.Bus.ControlBus.Publish(message, x => x.ExpiresAt(DateTime.UtcNow + timeToLive));
 
                 if (_log.IsDebugEnabled)
                     _log.DebugFormat("Worker {0}: {1} in progress, {2} pending", _worker.DataUri,
