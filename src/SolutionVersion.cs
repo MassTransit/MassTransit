0aaaf65c1a474453eed33fa9b3140de087495680
Thomas Tomanek <thomas.tomanek@starlizard.co.uk>
2015-07-18 14:07:52 +0100

    Tasks that timeout shouldn't be cancelled
    Conflicts:
    	src/SolutionVersion.cs

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -8,6 +8,10 @@ using System.Security;
 [assembly: AssemblyVersion("2.10.0")]
 [assembly: AssemblyFileVersion("2.10.1")]
 
+<<<<<<< HEAD
 [assembly: AssemblyInformationalVersion("2.10.1.a86f75")]
+=======
+[assembly: AssemblyInformationalVersion("2.10.1.64a3ac")]
+>>>>>>> 59cb310... Tasks that timeout shouldn't be cancelled
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(true)]
