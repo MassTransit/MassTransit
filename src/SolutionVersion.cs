53623d67e791e871af960d3c3f2f39f8a190ede5
Chris Patterson <chris@phatboyg.com>
2015-07-21 20:18:28 -0700

    Making Quartz service work properly

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("MassTransit")]
 [assembly: AssemblyDescriptionAttribute("MassTransit is a distributed application framework for .NET http://masstransit-project.com")]
 [assembly: AssemblyProductAttribute("MassTransit")]
-[assembly: AssemblyVersionAttribute("3.0.11")]
+[assembly: AssemblyVersionAttribute("3.0.0.0")]
 [assembly: AssemblyFileVersionAttribute("3.0.11.0")]
-[assembly: AssemblyInformationalVersionAttribute("3.0.11.0 (mt3/9ef12043)")]
+[assembly: AssemblyInformationalVersionAttribute("3.0.11.0 (mt3/ca3896a5)")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "3.0.11";
+        internal const string Version = "3.0.0.0";
     }
 }
