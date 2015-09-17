d44483745853a0a2d8bb6f328e24400f9d4512b9
Chris Patterson <chris@phatboyg.com>
2015-09-17 07:07:47 -0700

    Trying to get the host to automatically add files to the IDE, failing duplication check though.

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -5,8 +5,8 @@ using System.Reflection;
 [assembly: AssemblyDescriptionAttribute("MassTransit is a distributed application framework for .NET http://masstransit-project.com")]
 [assembly: AssemblyProductAttribute("MassTransit")]
 [assembly: AssemblyVersionAttribute("3.0.0.0")]
-[assembly: AssemblyFileVersionAttribute("3.0.12.0")]
-[assembly: AssemblyInformationalVersionAttribute("3.0.12.0 (mt3/927a0fd6)")]
+[assembly: AssemblyFileVersionAttribute("3.0.13.0")]
+[assembly: AssemblyInformationalVersionAttribute("3.0.13.0 (mt3/ca92901f)")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "3.0.0.0";
