5a7f36430c490e311ba5500b6b4fbfecce00b37e
Chris Patterson <chris@phatboyg.com>
2015-10-21 10:53:04 -0700

    Added header object serialization test
    Former-commit-id: c94b73d6f692e07c2af0d996d4bd8d0472994249

---
+++ src/MassTransit.Tests/HeaderObject_Specs.cs
@@ -0,0 +1,80 @@
+﻿// Copyright 2007-2015 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.Tests
+{
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using TestFramework;
+    using TestFramework.Messages;
+    using Util;
+
+
+    [TestFixture]
+    public class Serializing_an_object_in_a_header :
+        InMemoryTestFixture
+    {
+        Task<ConsumeContext<PingMessage>> _handled;
+        TaskCompletionSource<ClaimsIdentity> _header;
+
+        [Test]
+        public async Task Should_properly_serialize_and_deserialize()
+        {
+            await InputQueueSendEndpoint.Send(new PingMessage(), context =>
+            {
+                context.Headers.Set("Claims-Identity", new ClaimsIdentityProxy
+                {
+                    IdentityType = "AAD:Claims",
+                    IdentityId = 27,
+                    Claims = new[] {"One", "two", "Three"},
+                });
+            });
+
+            var consumeContext = await _handled;
+
+            var identity = await _header.Task;
+
+            Assert.AreEqual(27, identity.IdentityId);
+            Assert.AreEqual("AAD:Claims", identity.IdentityType);
+        }
+
+        protected override void ConfigureInputQueueEndpoint(IReceiveEndpointConfigurator configurator)
+        {
+            _header = GetTask<ClaimsIdentity>();
+
+            _handled = Handler<PingMessage>(configurator, context =>
+            {
+                _header.TrySetResult(context.Headers.Get<ClaimsIdentity>("Claims-Identity"));
+
+                return TaskUtil.Completed;
+            });
+        }
+
+
+        public interface ClaimsIdentity 
+        {
+            string IdentityType { get; }
+            int IdentityId { get; }
+            string[] Claims { get; }
+        }
+
+
+        class ClaimsIdentityProxy :
+            ClaimsIdentity
+        {
+            public string IdentityType { get; set; }
+            public int IdentityId { get; set; }
+            public string[] Claims { get; set; }
+        }
+
+    }
+}
