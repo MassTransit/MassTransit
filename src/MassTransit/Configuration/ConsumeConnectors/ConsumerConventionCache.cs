114b816484ecee0c0ab19c71aaa263beb70ca35f
Chris Patterson <chris@phatboyg.com>
2015-11-05 20:22:34 -0800

    Moved MessageInterfaceType to allow for pluggable conventions and connectors

---
+++ src/MassTransit/Configuration/ConsumeConnectors/ConsumerConventionCache.cs
@@ -0,0 +1,49 @@
+// Copyright 2007-2015 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.ConsumeConnectors
+{
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+
+
+    public static class ConsumerConventionCache
+    {
+        public static void Add(string conventionName, IConsumerConvention convention)
+        {
+            Cached.Instance.AddOrUpdate(conventionName, add => convention, (_, update) => convention);
+        }
+
+        /// <summary>
+        /// Returns the conventions registered for identifying message consumer types
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<IConsumerMessageConvention> GetConventions<T>()
+            where T : class
+        {
+            yield return new AsyncConsumerMessageConvention<T>();
+            yield return new LegacyConsumerMessageConvention<T>();
+
+            foreach (var convention in Cached.Instance.Values)
+            {
+                yield return convention.GetConsumerMessageConvention<T>();
+            }
+        }
+
+
+        static class Cached
+        {
+            internal static readonly ConcurrentDictionary<string, IConsumerConvention> Instance = new ConcurrentDictionary<string, IConsumerConvention>();
+        }
+    }
+}
--- src/MassTransit/Configuration/IMessageInterfaceType.cs
+++ src/MassTransit/Configuration/IMessageInterfaceType.cs
@@ -10,15 +10,16 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
-namespace MassTransit.ConsumeConnectors
+namespace MassTransit
 {
     using System;
+    using ConsumeConnectors;
 
 
     public interface IMessageInterfaceType
     {
+        Type MessageType { get; }
         IConsumerMessageConnector GetConsumerConnector();
         IInstanceMessageConnector GetInstanceConnector();
-        Type MessageType { get; }
     }
 }
