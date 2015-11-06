26c6d182c470401a36079405e68482c796921e5e
Chris Patterson <chris@phatboyg.com>
2015-11-06 11:29:06 -0800

    Added SimpleInjector to the main project

--- src/Containers/MassTransit.SimpleInjectorIntegration/SimpleInjectorConsumerFactory.cs
+++ src/Containers/MassTransit.SimpleInjectorIntegration/SimpleInjectorConsumerFactory.cs
@@ -1,31 +1,40 @@
-﻿namespace MassTransit.SimpleInjectorIntegration
+﻿// Copyright 2007-2015 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.SimpleInjectorIntegration
 {
     using System.Threading.Tasks;
-
+    using Pipeline;
     using SimpleInjector;
-
-    using MassTransit.Pipeline;
-    using MassTransit.Util;
-
     using SimpleInjector.Extensions.ExecutionContextScoping;
+    using Util;
 
 
     public class SimpleInjectorConsumerFactory<TConsumer> :
-            IConsumerFactory<TConsumer>
-            where TConsumer : class
+        IConsumerFactory<TConsumer>
+        where TConsumer : class
     {
         readonly Container _container;
 
         public SimpleInjectorConsumerFactory(Container container)
         {
-            this._container = container;
+            _container = container;
         }
 
         async Task IConsumerFactory<TConsumer>.Send<T>(ConsumeContext<T> context, IPipe<ConsumerConsumeContext<TConsumer, T>> next)
         {
-            using (var scope = this._container.BeginExecutionContextScope())
+            using (var scope = _container.BeginExecutionContextScope())
             {
-                var consumer = this._container.GetInstance<TConsumer>();
+                var consumer = _container.GetInstance<TConsumer>();
                 if (consumer == null)
                 {
                     throw new ConsumerException(
