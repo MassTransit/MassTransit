2a221914b3e45ea2a1d07bee3623907c90691359
Chris Patterson <chris@phatboyg.com>
2015-11-28 17:29:28 -0800

    Reverting bad choices in favor of new design
    Former-commit-id: a0b32ef1aa4fd97c0dd66ae0ec9e8c2ab6b3e204

--- src/MassTransit/Configuration/RateLimitExtensions.cs
+++ src/MassTransit/Configuration/RateLimitExtensions.cs
@@ -14,7 +14,6 @@ namespace MassTransit
 {
     using System;
     using PipeConfigurators;
-    using Saga;
 
 
     public static class RateLimitExtensions
@@ -23,64 +22,35 @@ namespace MassTransit
         /// Specify a rate limit for message processing, so that only the specified number of messages are allowed
         /// per interval.
         /// </summary>
-        /// <param name="configurator"></param>
-        /// <param name="rateLimit">The number of messages allowed per interval</param>
-        /// <param name="interval">The reset interval for each set of messages</param>
-        public static void UseRateLimit(this IPipeConfigurator<ConsumeContext> configurator, int rateLimit, TimeSpan? interval = default(TimeSpan?))
-        {
-            ConfigureRateLimit(configurator, rateLimit, interval);
-        }
-
-        /// <summary>
-        /// Specify a rate limit for message processing, so that only the specified number of messages are allowed
-        /// per interval.
-        /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="configurator"></param>
         /// <param name="rateLimit">The number of messages allowed per interval</param>
-        /// <param name="interval">The reset interval for each set of messages</param>
-        public static void UseRateLimit<T>(this IPipeConfigurator<ConsumeContext<T>> configurator, int rateLimit, TimeSpan? interval = default(TimeSpan?))
-            where T : class
-        {
-            ConfigureRateLimit(configurator, rateLimit, interval);
-        }
-
-        /// <summary>
-        /// Specify a rate limit for message processing, so that only the specified number of messages are allowed
-        /// per interval.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="configurator"></param>
-        /// <param name="rateLimit">The number of messages allowed per interval</param>
-        /// <param name="interval">The reset interval for each set of messages</param>
-        public static void UseRateLimit<T>(this IPipeConfigurator<SagaConsumeContext<T>> configurator, int rateLimit, TimeSpan? interval = default(TimeSpan?))
-            where T : class, ISaga
-        {
-            ConfigureRateLimit(configurator, rateLimit, interval);
-        }
-
-        /// <summary>
-        /// Specify a rate limit for message processing, so that only the specified number of messages are allowed
-        /// per interval.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="configurator"></param>
-        /// <param name="rateLimit">The number of messages allowed per interval</param>
-        /// <param name="interval">The reset interval for each set of messages</param>
-        public static void UseRateLimit<T>(this IPipeConfigurator<ConsumerConsumeContext<T>> configurator, int rateLimit,
-            TimeSpan? interval = default(TimeSpan?))
-            where T : class
-        {
-            ConfigureRateLimit(configurator, rateLimit, interval);
-        }
-
-        static void ConfigureRateLimit<T>(IPipeConfigurator<T> configurator, int rateLimit, TimeSpan? interval)
+        public static void UseRateLimit<T>(this IPipeConfigurator<T> configurator, int rateLimit)
             where T : class, PipeContext
         {
             if (configurator == null)
                 throw new ArgumentNullException(nameof(configurator));
 
-            var specification = new RateLimitPipeSpecification<T>(rateLimit, interval ?? TimeSpan.FromSeconds(1));
+            var specification = new RateLimitPipeSpecification<T>(rateLimit, TimeSpan.FromSeconds(1));
+
+            configurator.AddPipeSpecification(specification);
+        }
+
+        /// <summary>
+        /// Specify a rate limit for message processing, so that only the specified number of messages are allowed
+        /// per interval.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="configurator"></param>
+        /// <param name="rateLimit">The number of messages allowed per interval</param>
+        /// <param name="interval">The reset interval for each set of messages</param>
+        public static void UseRateLimit<T>(this IPipeConfigurator<T> configurator, int rateLimit, TimeSpan interval)
+            where T : class, PipeContext
+        {
+            if (configurator == null)
+                throw new ArgumentNullException(nameof(configurator));
+
+            var specification = new RateLimitPipeSpecification<T>(rateLimit, interval);
 
             configurator.AddPipeSpecification(specification);
         }
