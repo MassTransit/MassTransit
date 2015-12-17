7e026a14f3f0a3a99dd1f9103b25a4f127f2ca33
Chris Patterson <chris@phatboyg.com>
2015-12-17 08:35:48 -0800

    Update dependencies
    Former-commit-id: 0d5ab759457221eae4b66383180cecd46bfd14d6

--- build.fsx
+++ build.fsx
@@ -11,8 +11,8 @@ let nugetWorkingPath = FullName "./build_temp"
 let packagesPath = FullName "./src/packages"
 let keyFile = FullName "./MassTransit.snk"
 
-let assemblyVersion = "3.0.0.0"
-let baseVersion = "3.0.18"
+let assemblyVersion = "3.1.0.0"
+let baseVersion = "3.1.0"
 
 let semVersion : SemVerInfo = parse baseVersion
 
