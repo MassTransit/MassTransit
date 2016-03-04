6287e03a82e7219787f14f78998e187d3422a3cf
Chris Patterson <chris@phatboyg.com>
2016-03-04 10:04:00 -0800

    Fixed #482, changed namespace of extension method for greater visiblity

--- src/MassTransit.RabbitMqTransport/RabbitMqSendContextExtensions.cs
+++ src/MassTransit.RabbitMqTransport/RabbitMqSendContextExtensions.cs
@@ -10,10 +10,11 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
-namespace MassTransit.RabbitMqTransport
+namespace MassTransit
 {
     using System;
     using System.Collections.Generic;
+    using RabbitMqTransport;
 
 
     public static class RabbitMqSendContextExtensions
