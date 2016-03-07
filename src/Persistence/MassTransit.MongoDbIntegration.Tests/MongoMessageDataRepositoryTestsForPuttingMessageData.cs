91052debab5601a66fc61803ac2ed07743ee0a82
Chris Patterson <chris@phatboyg.com>
2016-03-06 20:57:14 -0800

    Added MongoDb Message Data from Liberis Labs
    Former-commit-id: 882d6a3dcf1e681a2c22b8b65359a2e7af2a337f

---
+++ src/Persistence/MassTransit.MongoDbIntegration.Tests/MongoMessageDataRepositoryTestsForPuttingMessageData.cs
@@ -0,0 +1,94 @@
+﻿// Copyright 2007-2016 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.MongoDbIntegration.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using MassTransit.MessageData;
+    using MessageData;
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+    using MongoDB.Driver.GridFS;
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class MongoMessageDataRepositoryTestsForPuttingMessageData
+    {
+        [Test]
+        public async Task ThenExpirationIsNotSet()
+        {
+            IAsyncCursor<GridFSFileInfo> cursor = await _bucket.FindAsync(Builders<GridFSFileInfo>.Filter.Eq(x => x.Filename, _filename));
+            List<GridFSFileInfo> list = await cursor.ToListAsync();
+            var doc = list.Single();
+
+            Assert.That(doc.Metadata.Contains("expiration"), Is.False);
+        }
+
+        [Test]
+        public async Task ThenMessageStoredAsExpected()
+        {
+            byte[] result = await _bucket.DownloadAsBytesByNameAsync(_filename);
+
+            Assert.That(result, Is.EqualTo(_expectedData));
+        }
+
+        [TestFixtureSetUp]
+        public void GivenAMongoMessageDataRepository_WhenPuttingMessageData()
+        {
+            var db = new MongoClient().GetDatabase("messagedatastoretests");
+            _bucket = new GridFSBucket(db);
+
+            _expectedData = Encoding.UTF8.GetBytes("This is a test message data block");
+
+            _resolver = new MessageDataResolver();
+            _nameCreator = new StaticFileNameGenerator();
+            _filename = _nameCreator.GenerateFileName();
+
+            IMessageDataRepository repository = new MongoDbMessageDataRepository(_resolver, _bucket, _nameCreator);
+
+            using (var stream = new MemoryStream(_expectedData))
+            {
+                _actualUri = repository.Put(stream).GetAwaiter().GetResult();
+
+            }
+        }
+
+        [TestFixtureTearDown]
+        public void Kill()
+        {
+            _bucket.DropAsync().GetAwaiter().GetResult();
+        }
+
+        GridFSBucket _bucket;
+        byte[] _expectedData;
+        IFileNameGenerator _nameCreator;
+        IMessageDataResolver _resolver;
+        Uri _actualUri;
+        string _filename;
+
+
+        class StaticFileNameGenerator :
+            IFileNameGenerator
+        {
+            public string GenerateFileName()
+            {
+                return "etdpgnbhbtdgjs8h1pzcbxyyyy";
+            }
+        }
+    }
+}
---
+++ src/Persistence/MassTransit.MongoDbIntegration.Tests/MongoMessageDataRepositoryTestsForPuttingMessageDataWithExpiration.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using LiberisLabs.MassTransit.MessageData.MongoDb.Helpers;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using Moq;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace LiberisLabs.MassTransit.MessageData.MongoDb.Tests.MongoMessageDataRepositoryTests
+{
+    [TestFixture]
+    public class MongoMessageDataRepositoryTestsForPuttingMessageDataWithExpiration
+    {
+        private GridFSBucket _bucket;
+        private TimeSpan _expectedTtl;
+        private DateTime _now;
+        private ObjectId _id;
+
+        [OneTimeSetUp]
+        public void GivenAMongoMessageDataRepository_WhenPuttingMessageDataWithExpiration()
+        {
+            var db = new MongoClient().GetDatabase("messagedatastoretests");
+            _bucket = new GridFSBucket(db);
+
+            _now = DateTime.UtcNow;
+            SystemDateTime.Set(_now);
+
+            var fixture = new Fixture();
+
+            var resolver = new Mock<IMongoMessageUriResolver>();
+            resolver.Setup(x => x.Resolve(It.IsAny<ObjectId>()))
+                .Callback((ObjectId id) => _id = id);
+
+            var nameCreator = new Mock<IFileNameCreator>();
+            nameCreator.Setup(x => x.CreateFileName())
+                .Returns(fixture.Create<string>());
+            
+            var sut = new MongoMessageDataRepository(resolver.Object, _bucket, nameCreator.Object);
+            _expectedTtl = TimeSpan.FromHours(1);
+
+            using (var stream = new MemoryStream(fixture.Create<byte[]>()))
+            {
+                sut.Put(stream, _expectedTtl).GetAwaiter().GetResult();
+            }
+        }
+     
+        [Test]
+        public async Task ThenExpirationSetAsExpected()
+        {
+            var cursor = await _bucket.FindAsync(Builders<GridFSFileInfo>.Filter.Eq("_id", _id));
+            var list = await cursor.ToListAsync();
+            var doc = list.Single();
+
+            var expiration = doc.Metadata["expiration"].ToUniversalTime();
+            Assert.That(expiration, Is.EqualTo(_now.Add(_expectedTtl)).Within(1).Milliseconds);
+        }
+
+        [OneTimeTearDown]
+        public void Kill()
+        {
+            SystemDateTime.Reset();
+
+            _bucket.DropAsync().GetAwaiter().GetResult();
+        }
+    }
+}
