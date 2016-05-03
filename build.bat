dbea41b2d06dbc24f099da94269d7ae0caf2e743
Chris Patterson <chris@phatboyg.com>
2016-05-03 14:43:18 -0700

    Updated dependencies to all 4.5.2 packages, including Topshelf 4

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
 If Not Exist src\.nuget\nuget.exe msbuild src\.nuget\NuGet.targets -Target:RestorePackages
-If Not Exist src\packages\FAKE\tools\fake.exe src\.nuget\nuget.exe Install FAKE -OutputDirectory "src\packages" -ExcludeVersion
+If Not Exist src\packages\FAKE\tools\fake.exe src\.nuget\nuget.exe Install FAKE -Source "https://www.nuget.org/api/v2/" -OutputDirectory "src\packages" -ExcludeVersion
 src\packages\FAKE\tools\fake.exe build.fsx %*
--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("MassTransit")]
 [assembly: AssemblyDescriptionAttribute("MassTransit is a distributed application framework for .NET http://masstransit-project.com")]
 [assembly: AssemblyProductAttribute("MassTransit")]
-[assembly: AssemblyVersionAttribute("3.3.0.0")]
-[assembly: AssemblyFileVersionAttribute("3.3.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("3.3.0.0 (develop/918fe047)")]
+[assembly: AssemblyVersionAttribute("3.3.1.0")]
+[assembly: AssemblyFileVersionAttribute("3.3.1.0")]
+[assembly: AssemblyInformationalVersionAttribute("3.3.1.0 (develop/71cb3ebe)")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "3.3.0.0";
+        internal const string Version = "3.3.1.0";
     }
 }
