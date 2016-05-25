153b0215de9ec44477e30b0e5997c8f69399e5b8
Chris Patterson <chris@phatboyg.com>
2016-05-25 08:28:31 -0700

    Removed outdated lib folder no longer used.

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -5,8 +5,8 @@ using System.Reflection;
 [assembly: AssemblyDescriptionAttribute("MassTransit is a distributed application framework for .NET http://masstransit-project.com")]
 [assembly: AssemblyProductAttribute("MassTransit")]
 [assembly: AssemblyVersionAttribute("3.3.1.0")]
-[assembly: AssemblyFileVersionAttribute("3.3.2.0")]
-[assembly: AssemblyInformationalVersionAttribute("3.3.2.0 (develop/2b519cbb)")]
+[assembly: AssemblyFileVersionAttribute("3.3.4.0")]
+[assembly: AssemblyInformationalVersionAttribute("3.3.4.0 (develop/db849c5b)")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "3.3.1.0";
