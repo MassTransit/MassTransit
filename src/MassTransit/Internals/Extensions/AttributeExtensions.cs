137e851313484044bfa2268ca9b5ddcc5d912a7d
Chris Patterson <chris@phatboyg.com>
2016-08-30 11:46:37 -0700

    Fixed #637, uses proper detection and use of file version resource for BusHostInfo

---
+++ src/MassTransit/Internals/Extensions/AttributeExtensions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright 2007-2016 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.Internals.Extensions
+{
+    using System;
+    using System.Reflection;
+
+
+    public static class AttributeExtensions
+    {
+        public static T GetCustomAttribute<T>(this ICustomAttributeProvider provider)
+            where T : Attribute
+        {
+            object[] attributes = provider.GetCustomAttributes(typeof(T), true);
+            return attributes.Length > 0 ? attributes[0] as T : null;
+        }
+    }
+}
