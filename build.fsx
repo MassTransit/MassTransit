f78c47fa1d1d91a5ee1609e3d3f8c689d3d105c3
Chris Patterson <chris@phatboyg.com>
2017-01-12 07:41:02 -0800

    Bumped version

--- build.fsx
+++ build.fsx
@@ -13,7 +13,7 @@ let packagesPath = FullName "./src/packages"
 let keyFile = FullName "./MassTransit.snk"
 
 let assemblyVersion = "3.5.0.0"
-let baseVersion = "3.5.4"
+let baseVersion = "3.5.5"
 
 let semVersion : SemVerInfo = parse baseVersion
 
