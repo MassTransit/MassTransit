1fbdcc0889964076afd2f3601ba31624c8894874
Alexey Zimarev <alex@zimarev.com>
2017-02-09 21:19:30 +0100

    Added assembly scanning EF saga dbcontext

---
+++ src/Persistence/MassTransit.EntityFrameworkIntegration.Tests/AssemblyScanningSagaLocator_Specs.cs
@@ -0,0 +1,67 @@
+﻿namespace MassTransit.EntityFrameworkIntegration.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using MassTransit.Saga;
+    using MassTransit.Tests.Saga;
+    using MassTransit.Tests.Saga.Messages;
+    using NUnit.Framework;
+    using Saga;
+    using Shouldly;
+    using TestFramework;
+
+    [TestFixture, Category("Integration")]
+    public class Locating_an_existing_ef_saga_with_scanning :
+        InMemoryTestFixture
+    {
+        [Test]
+        public async Task A_correlated_message_should_find_the_correct_saga()
+        {
+            Guid sagaId = NewId.NextGuid();
+            var message = new InitiateSimpleSaga(sagaId);
+
+            await InputQueueSendEndpoint.Send(message);
+
+            Guid? foundId = await _sagaRepository.Value.ShouldContainSaga(message.CorrelationId, TestTimeout);
+
+            foundId.HasValue.ShouldBe(true);
+
+            var nextMessage = new CompleteSimpleSaga { CorrelationId = sagaId };
+
+            await InputQueueSendEndpoint.Send(nextMessage);
+
+            foundId = await _sagaRepository.Value.ShouldContainSaga(x => x.CorrelationId == sagaId && x.Completed, TestTimeout);
+
+            foundId.HasValue.ShouldBe(true);
+        }
+
+        [Test]
+        public async Task An_initiating_message_should_start_the_saga()
+        {
+            Guid sagaId = NewId.NextGuid();
+            var message = new InitiateSimpleSaga(sagaId);
+
+            await InputQueueSendEndpoint.Send(message);
+
+            Guid? foundId = await _sagaRepository.Value.ShouldContainSaga(message.CorrelationId, TestTimeout);
+
+            foundId.HasValue.ShouldBe(true);
+        }
+
+        readonly SagaDbContextFactory sagaDbContextFactory;
+        readonly Lazy<ISagaRepository<SimpleSaga>> _sagaRepository;
+
+        public Locating_an_existing_ef_saga_with_scanning()
+        {
+            sagaDbContextFactory = () => new AssemblyScanningSagaDbContext(typeof(SimpleSagaMap).Assembly,
+                SagaDbContextFactoryProvider.GetLocalDbConnectionString());
+            _sagaRepository = new Lazy<ISagaRepository<SimpleSaga>>(() => new EntityFrameworkSagaRepository<SimpleSaga>(sagaDbContextFactory));
+        }
+
+        protected override void ConfigureInMemoryReceiveEndpoint(IInMemoryReceiveEndpointConfigurator configurator)
+        {
+            configurator.Saga(_sagaRepository.Value);
+        }
+
+    }
+}
---
+++ src/Persistence/MassTransit.EntityFrameworkIntegration/AssemblyScanningSagaDbContext .cs
@@ -0,0 +1,70 @@
+﻿// Copyright 2007-2017 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.EntityFrameworkIntegration
+{
+    using System.Data.Common;
+    using System.Data.Entity;
+    using System.Data.Entity.Core.Objects;
+    using System.Data.Entity.Infrastructure;
+    using System.Reflection;
+
+
+    public class AssemblyScanningSagaDbContext : DbContext
+    {
+        readonly Assembly _mappingAssembly;
+
+        public AssemblyScanningSagaDbContext(Assembly mappingAssembly, string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+            _mappingAssembly = mappingAssembly;
+        }
+
+        public AssemblyScanningSagaDbContext(Assembly mappingAssembly, ObjectContext objectContext, bool dbContextOwnsObjectContext)
+            : base(objectContext, dbContextOwnsObjectContext)
+        {
+            _mappingAssembly = mappingAssembly;
+        }
+
+        public AssemblyScanningSagaDbContext(Assembly mappingAssembly, DbConnection existingConnection, bool contextOwnsConnection)
+            : base(existingConnection, contextOwnsConnection)
+        {
+            _mappingAssembly = mappingAssembly;
+        }
+
+        public AssemblyScanningSagaDbContext(Assembly mappingAssembly, string nameOrConnectionString, DbCompiledModel model)
+            : base(nameOrConnectionString, model)
+        {
+            _mappingAssembly = mappingAssembly;
+        }
+
+        public AssemblyScanningSagaDbContext(Assembly mappingAssembly, DbConnection existingConnection, DbCompiledModel model, bool contextOwnsConnection)
+            : base(existingConnection, model, contextOwnsConnection)
+        {
+            _mappingAssembly = mappingAssembly;
+        }
+
+        protected AssemblyScanningSagaDbContext(Assembly mappingAssembly)
+        {
+            _mappingAssembly = mappingAssembly;
+        }
+
+        protected AssemblyScanningSagaDbContext(Assembly mappingAssembly, DbCompiledModel model)
+            : base(model)
+        {
+            _mappingAssembly = mappingAssembly;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) =>
+            modelBuilder.Configurations.AddFromAssembly(_mappingAssembly);
+    }
+}
