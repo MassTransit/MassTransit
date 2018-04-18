4eddc46299ccc1cc6376da71c1e343446afd1641
Adem Çatamak <adem.catamak@trendyol.com>
2018-04-18 11:48:15 +0300

    RedeliverExtensions has callback (Action) parameter

--- src/MassTransit/RedeliverExtensions.cs
+++ src/MassTransit/RedeliverExtensions.cs
@@ -21,14 +21,15 @@ namespace MassTransit
     {
         /// <summary>
         /// Redeliver uses the message scheduler to deliver the message to the queue at a future
-        /// time. The delivery count is incremented. A message scheduler must be configured on the
-        /// bus for redelivery to be enabled.
+        /// time. The delivery count is incremented. Moreover, if you give custom callback action, it perform before sending message to queueu.
+        /// A message scheduler must be configured on the bus for redelivery to be enabled.
         /// </summary>
         /// <typeparam name="T">The message type</typeparam>
         /// <param name="context">The consume context of the message</param>
         /// <param name="delay">The delay before the message is delivered. It may take longer to receive the message if the queue is not empty.</param>
+        /// <param name="callback">Operation which is executed before the message is delivered.</param>
         /// <returns></returns>
-        public static Task Redeliver<T>(this ConsumeContext<T> context, TimeSpan delay)
+        public static Task Redeliver<T>(this ConsumeContext<T> context, TimeSpan delay, Action<ConsumeContext, SendContext> callback = null)
             where T : class
         {
             if (!context.TryGetPayload(out MessageSchedulerContext schedulerContext))
@@ -36,7 +37,7 @@ namespace MassTransit
 
             MessageRedeliveryContext redeliverContext = new ScheduleMessageRedeliveryContext<T>(context, schedulerContext);
 
-            return redeliverContext.ScheduleRedelivery(delay);
+            return redeliverContext.ScheduleRedelivery(delay, callback);
         }
     }
 }
