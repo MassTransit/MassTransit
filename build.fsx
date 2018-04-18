26fe19416848c5103fe3811ab3a295c79772cf38
Chris Patterson <chris@phatboyg.com>
2018-04-18 09:52:56 -0700

    Updated version

--- build.fsx
+++ build.fsx
@@ -11,7 +11,7 @@ let packagesPath = FullName "./src/packages"
 let keyFile = FullName "./MassTransit.snk"
 
 let assemblyVersion = "5.0.0.0"
-let baseVersion = "5.0.0"
+let baseVersion = "5.0.1"
 
 let envVersion = (environVarOrDefault "APPVEYOR_BUILD_VERSION" (baseVersion + ".0"))
 let buildVersion = (envVersion.Substring(0, envVersion.LastIndexOf('.')))
