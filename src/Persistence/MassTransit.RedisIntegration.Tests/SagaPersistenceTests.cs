465b6fb13b1d30031b9c99b5d643b539e01fce70
Chris Patterson <chris@phatboyg.com>
2018-04-20 19:07:38 -0700

    Added pessimistic tests for Redis

--- src/Persistence/MassTransit.RedisIntegration.Tests/SagaPersistenceTests.cs
+++ src/Persistence/MassTransit.RedisIntegration.Tests/SagaPersistenceTests.cs
@@ -79,4 +79,63 @@ namespace MassTransit.RedisIntegration.Tests
             configurator.Saga(_sagaRepository.Value);
         }
     }
+    
+    [TestFixture]
+    [Category("Integration")]
+    public class LocatingAnExistingSagaWithoutOptimism :
+        InMemoryTestFixture
+    {
+        [Test]
+        public async Task A_correlated_message_should_find_the_correct_saga()
+        {
+            var sagaId = NewId.NextGuid();
+            var message = new InitiateSimpleSaga(sagaId);
+
+            await InputQueueSendEndpoint.Send(message);
+
+            var found = await _sagaRepository.Value.ShouldContainSaga(message.CorrelationId, TestTimeout);
+
+            found.ShouldBeTrue();
+
+            var nextMessage = new CompleteSimpleSaga {CorrelationId = sagaId};
+
+            await InputQueueSendEndpoint.Send(nextMessage);
+
+            found = await _sagaRepository.Value.ShouldContainSaga(sagaId, x => x != null && x.Moved, TestTimeout);
+            found.ShouldBeTrue();
+
+            var retrieveRepository = _sagaRepository.Value as IRetrieveSagaFromRepository<SimpleSaga>;
+            var retrieved = await retrieveRepository.GetSaga(sagaId);
+            retrieved.ShouldNotBeNull();
+            retrieved.Moved.ShouldBeTrue();
+        }
+
+        [Test]
+        public async Task An_initiating_message_should_start_the_saga()
+        {
+            var sagaId = NewId.NextGuid();
+            var message = new InitiateSimpleSaga(sagaId);
+
+            await InputQueueSendEndpoint.Send(message);
+
+            var found = await _sagaRepository.Value.ShouldContainSaga(message.CorrelationId, TestTimeout);
+
+            found.ShouldBeTrue();
+        }
+
+        readonly Lazy<ISagaRepository<SimpleSaga>> _sagaRepository;
+
+        public LocatingAnExistingSagaWithoutOptimism()
+        {
+            var redis = ConnectionMultiplexer.Connect("127.0.0.1");
+            redis.PreserveAsyncOrder = false;
+
+            _sagaRepository = new Lazy<ISagaRepository<SimpleSaga>>(() => new RedisSagaRepository<SimpleSaga>(() => redis.GetDatabase(), optimistic: false));
+        }
+
+        protected override void ConfigureInMemoryReceiveEndpoint(IInMemoryReceiveEndpointConfigurator configurator)
+        {
+            configurator.Saga(_sagaRepository.Value);
+        }
+    }
 }
