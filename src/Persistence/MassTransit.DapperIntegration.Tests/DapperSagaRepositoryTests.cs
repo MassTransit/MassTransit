323bc010aefecf9f458275da68a9a5e8dc4339ed
John Strömblom <john.stromblom@outlook.com>
2018-06-01 19:58:40 +0200

    Added Dapper integration.

---
+++ src/Persistence/MassTransit.DapperIntegration.Tests/DapperSagaRepositoryTests.cs
@@ -0,0 +1,84 @@
+﻿namespace MassTransit.DapperIntegration.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using MassTransit.Tests.Saga.Messages;
+    using NUnit.Framework;
+    using Saga;
+    using Shouldly;
+    using TestFramework;
+    using Testing;
+
+
+    [TestFixture, Category("Integration")]
+    public class DapperSagaRepositoryTests :
+        InMemoryTestFixture
+    {
+        [Test]
+        public async Task A_correlated_message_should_find_the_correct_saga()
+        {
+            Guid sagaId = NewId.NextGuid();
+            var message = new InitiateSimpleSaga(sagaId);
+
+            await this.InputQueueSendEndpoint.Send(message);
+
+            Guid? foundId = await this._sagaRepository.Value.ShouldContainSaga(message.CorrelationId, this.TestTimeout);
+
+            foundId.HasValue.ShouldBe(true);
+
+            var nextMessage = new CompleteSimpleSaga { CorrelationId = sagaId };
+
+            await this.InputQueueSendEndpoint.Send(nextMessage);
+
+            foundId = await this._sagaRepository.Value.ShouldContainSaga(x => x.CorrelationId == sagaId && x.Completed, this.TestTimeout);
+
+            foundId.HasValue.ShouldBe(true);
+        }
+
+        [Test]
+        public async Task An_observed_message_should_find_and_update_the_correct_saga()
+        {
+            Guid sagaId = NewId.NextGuid();
+            var message = new InitiateSimpleSaga(sagaId) { Name = "MySimpleSaga" };
+
+            await InputQueueSendEndpoint.Send(message);
+
+            Guid? found = await _sagaRepository.Value.ShouldContainSaga(message.CorrelationId, TestTimeout);
+
+            found.ShouldBe(sagaId);
+
+            var nextMessage = new ObservableSagaMessage { Name = "MySimpleSaga" };
+
+            await InputQueueSendEndpoint.Send(nextMessage);
+
+            found = await _sagaRepository.Value.ShouldContainSaga(x => x.CorrelationId == sagaId && x.Observed, TestTimeout);
+            found.ShouldBe(sagaId);
+        }
+
+        [Test]
+        public async Task An_initiating_message_should_start_the_saga()
+        {
+            Guid sagaId = NewId.NextGuid();
+            var message = new InitiateSimpleSaga(sagaId);
+
+            await this.InputQueueSendEndpoint.Send(message);
+
+            Guid? foundId = await this._sagaRepository.Value.ShouldContainSaga(message.CorrelationId, this.TestTimeout);
+
+            foundId.HasValue.ShouldBe(true);
+        }
+
+        readonly Lazy<ISagaRepository<SimpleSaga>> _sagaRepository;
+
+        public DapperSagaRepositoryTests()
+        {
+            var connectionString = LocalDbConnectionStringProvider.GetLocalDbConnectionString();
+            this._sagaRepository = new Lazy<ISagaRepository<SimpleSaga>>(() => new DapperSagaRepository<SimpleSaga>(connectionString));
+        }
+
+        protected override void ConfigureInMemoryReceiveEndpoint(IInMemoryReceiveEndpointConfigurator configurator)
+        {
+            configurator.Saga(this._sagaRepository.Value);
+        }
+    }
+}
---
+++ src/Persistence/MassTransit.DapperIntegration/Sql/WhereStatementHelper.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2007-2018 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.DapperIntegration.Sql
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Text;
+    using Dapper;
+    using Saga;
+
+
+    public static class WhereStatementHelper
+    {
+        public static (string whereStatement, DynamicParameters parameters) GetWhereStatementAndParametersFromExpression<TSaga>(
+            Expression<Func<TSaga, bool>> expression)
+            where TSaga : class, ISaga
+        {
+            List<(string, object)> columnsAndValues = SqlExpressionVisitor.CreateFromExpression(expression);
+            var parameters = new DynamicParameters();
+
+            if (!columnsAndValues.Any())
+            {
+                return (string.Empty, parameters);
+            }
+
+            var sb = new StringBuilder();
+            sb.Append("WHERE");
+
+            var i = 0;
+            foreach (var (name, value) in columnsAndValues)
+            {
+                if (i > 0)
+                {
+                    sb.Append(" AND");
+                }
+
+                var valueName = $"@value{i}";
+                sb.Append($" {name} = {valueName}");
+                parameters.Add(valueName, value);
+                i++;
+            }
+
+            return (sb.ToString(), parameters);
+        }
+    }
+}
