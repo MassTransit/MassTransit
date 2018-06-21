fa4d20303250c0589f6ce17d6011c06e5633e0bf
James Woodley <wooderz@hotmail.com>
2018-06-21 12:20:30 +0100

    Use the StructureMap integration as a base and implement as Lamar.

---
+++ src/Containers/MassTransit.Containers.Tests/Lamar_Idiomatic.cs
@@ -0,0 +1,91 @@
+﻿namespace MassTransit.Containers.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Lamar;
+    using LamarIntegration;
+    using NUnit.Framework;
+    using Scenarios;
+    using Shouldly;
+    using TestFramework;
+    using Testing;
+
+
+    [TestFixture]
+    public class Lamar_Idiomatic :
+        AsyncTestFixture
+    {
+        [Test]
+        public async Task Should_work_with_the_registry()
+        {
+            var bus = _container.GetInstance<IBusControl>();
+
+            BusHandle busHandle = await bus.StartAsync();
+
+            await busHandle.Ready;
+
+            ISendEndpoint endpoint = await bus.GetSendEndpoint(new Uri("loopback://localhost/input_queue"));
+
+            const string name = "Joe";
+
+            await endpoint.Send(new SimpleMessageClass(name));
+
+            var lastConsumer = await SimpleConsumer.LastConsumer;
+            lastConsumer.ShouldNotBeNull();
+
+            var last = await lastConsumer.Last;
+            last.Name
+                .ShouldBe(name);
+
+            var wasDisposed = await lastConsumer.Dependency.WasDisposed;
+            wasDisposed
+                .ShouldBeTrue(); // Dependency was not disposed");
+
+            lastConsumer.Dependency.SomethingDone
+                .ShouldBeTrue(); // Dependency was disposed before consumer executed");
+        }
+
+        public Lamar_Idiomatic()
+            : base(new InMemoryTestHarness())
+        { }
+
+        Container _container;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _container = new Container(x =>
+            {
+                x.IncludeRegistry<BusServiceRegistry>();
+                x.IncludeRegistry<ConsumerServiceRegistry>();
+            });
+        }
+
+
+        class ConsumerServiceRegistry : ServiceRegistry
+        {
+            public ConsumerServiceRegistry()
+            {
+                ForConcreteType<SimpleConsumer>();
+                For<ISimpleConsumerDependency>().Use<SimpleConsumerDependency>();
+                For<AnotherMessageConsumer>().Use<AnotherMessageConsumerImpl>();
+            }
+        }
+
+
+        class BusServiceRegistry : ServiceRegistry
+        {
+            public BusServiceRegistry()
+            {
+                For<IBusControl>().Use(context => Bus.Factory.CreateUsingInMemory(x => x.ReceiveEndpoint("input_queue", e => e.LoadFrom(context)))).Singleton();
+            }
+        }
+
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _container.Dispose();
+        }
+    }
+}
