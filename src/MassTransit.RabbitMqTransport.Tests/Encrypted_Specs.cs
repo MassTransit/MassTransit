9843c6ca591440af93f1310d751b559acccf9600
kevbite <kev_bite@msn.com>
2018-07-08 22:57:24 +0100

    Adding high level end to end test for RabbitMQ and encrypted messages.

--- src/MassTransit.RabbitMqTransport.Tests/Encrypted_Specs.cs
+++ src/MassTransit.RabbitMqTransport.Tests/Encrypted_Specs.cs
@@ -20,6 +20,42 @@ namespace MassTransit.RabbitMqTransport.Tests
 
 
     [TestFixture]
+    public class Publishing_an_encrypted_message_to_an_endpoint_with_a_symmetric_key :
+        RabbitMqTestFixture
+    {
+        Task<ConsumeContext<PingMessage>> _handler;
+
+        protected override void ConfigureRabbitMqReceiveEndpoint(IRabbitMqReceiveEndpointConfigurator configurator)
+        {
+            _handler = Handled<PingMessage>(configurator);
+        }
+
+        protected override void ConfigureRabbitMqBus(IRabbitMqBusFactoryConfigurator configurator)
+        {
+            var key = new byte[]
+            {
+                31, 182, 254, 29, 98, 114, 85, 168, 176, 48, 113,
+                206, 198, 176, 181, 125, 106, 134, 98, 217, 113,
+                158, 88, 75, 118, 223, 117, 160, 224, 1, 47, 162
+            };
+
+            configurator.UseEncryption(key);
+
+            base.ConfigureRabbitMqBus(configurator);
+        }
+
+        [Test]
+        public async Task Should_succeed()
+        {
+            await Bus.Publish(new PingMessage());
+
+            ConsumeContext<PingMessage> received = await _handler;
+
+            Assert.AreEqual(EncryptedMessageSerializerV2.EncryptedContentType, received.ReceiveContext.ContentType);
+        }
+    }
+
+    [TestFixture]
     public class Publishing_an_encrypted_message_to_an_endpoint_with_a_specific_key :
         RabbitMqTestFixture
     {
