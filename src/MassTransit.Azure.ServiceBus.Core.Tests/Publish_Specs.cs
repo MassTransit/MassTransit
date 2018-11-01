e2cde124adae985f54508672270e14c21567ad69
Denys Kozhevnikov <noonamer@gmail.com>
2018-11-01 18:24:55 +0200

    - add test for RemoveSubscriptions on ASB Core

--- src/MassTransit.Azure.ServiceBus.Core.Tests/Publish_Specs.cs
+++ src/MassTransit.Azure.ServiceBus.Core.Tests/Publish_Specs.cs
@@ -1,14 +1,14 @@
 // Copyright 2007-2015 Chris Patterson, Dru Sellers, Travis Smith, et. al.
-//  
+//
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-// this file except in compliance with the License. You may obtain a copy of the 
-// License at 
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0 
-// 
+// this file except in compliance with the License. You may obtain a copy of the
+// License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.Azure.ServiceBus.Core.Tests
 {
@@ -20,6 +20,7 @@ namespace MassTransit.Azure.ServiceBus.Core.Tests
     using Serialization;
     using TestFramework;
     using TestFramework.Messages;
+    using Testing;
 
 
     [TestFixture]
@@ -116,6 +117,37 @@ namespace MassTransit.Azure.ServiceBus.Core.Tests
             ConsumeContext<PingMessage> received = await _handler;
 
             Assert.AreEqual(EncryptedMessageSerializer.EncryptedContentType, received.ReceiveContext.ContentType);
+        }
+    }
+
+
+    [TestFixture]
+    public class Publishing_message_to_an_endpoint_with_remove_subscriptions :
+        AzureServiceBusTestFixture
+    {
+        Task<ConsumeContext<PingMessage>> _handler;
+        readonly TimeSpan _timeout;
+
+        public Publishing_message_to_an_endpoint_with_remove_subscriptions()
+        {
+            _timeout = TimeSpan.FromSeconds(15);
+        }
+
+        protected override void ConfigureServiceBusBusHost(IServiceBusBusFactoryConfigurator configurator, IServiceBusHost host)
+        {
+            configurator.ReceiveEndpoint(host, c =>
+            {
+                c.RemoveSubscriptions = true;
+                _handler = HandledByConsumer<PingMessage>(c);
+            });
+        }
+
+        [Test]
+        public async Task Should_succeed()
+        {
+            await Bus.Publish(new PingMessage());
+
+            Assert.AreEqual(await Task.WhenAny(_handler, Task.Delay(_timeout)), _handler);
         }
     }
 }
