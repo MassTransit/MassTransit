7a0b0b254cf94bfffab1d91e4ebbcc1ffb4b0e94
hannan mumtaz <hannanmumtaz@hotmail.com>
2018-11-16 15:51:53 +1100

    Added Automatonymous StateMachineSagas integration for Castle Windsor.

---
+++ src/Containers/MassTransit.Automatonymous.WindsorIntegration/WindsorSagaScopeProvider.cs
@@ -0,0 +1,98 @@
+﻿namespace MassTransit.AutomatonymousWindsorIntegration
+{
+    using Castle.MicroKernel;
+    using Castle.MicroKernel.Lifestyle;
+    using Castle.MicroKernel.Registration;
+    using Context;
+    using GreenPipes;
+    using GreenPipes.Payloads;
+    using Saga;
+    using Scoping;
+    using Scoping.SagaContexts;
+    using System;
+    using System.Collections.Generic;
+
+
+    public class WindsorSagaScopeProvider<TSaga> :
+        ISagaScopeProvider<TSaga>
+        where TSaga : class, ISaga
+    {
+        readonly IKernel _container;
+        readonly IList<Action<ConsumeContext>> _scopeActions;
+
+        public WindsorSagaScopeProvider(IKernel container)
+        {
+            _container = container;
+            _scopeActions = new List<Action<ConsumeContext>>();
+        }
+
+        public void AddScopeAction(Action<ConsumeContext> action)
+        {
+            _scopeActions.Add(action);
+        }
+
+        public ISagaQueryScopeContext<TSaga, T> GetQueryScope<T>(SagaQueryConsumeContext<TSaga, T> context) where T : class
+        {
+            if (context.TryGetPayload<IKernel>(out _))
+                return new ExistingSagaQueryScopeContext<TSaga, T>(context);
+
+            var scope = _container.BeginScope();
+
+            _container.Register(Component.For<ConsumeContext, ConsumeContext<T>>().Instance(context));
+
+            try
+            {
+                var proxy = new SagaQueryConsumeContextProxy<TSaga, T>(context, new PayloadCacheScope(context), context.Query);
+
+                var consumerContainer = _container;
+                proxy.GetOrAddPayload(() => consumerContainer);
+                foreach (Action<ConsumeContext> scopeAction in _scopeActions)
+                    scopeAction(proxy);
+
+                return new CreatedSagaQueryScopeContext<IDisposable, TSaga, T>(scope, proxy);
+            }
+            catch
+            {
+                scope.Dispose();
+
+                throw;
+            }
+
+        }
+
+        public ISagaScopeContext<T> GetScope<T>(ConsumeContext<T> context) where T : class
+        {
+            if (context.TryGetPayload<IKernel>(out _))
+                return new ExistingSagaScopeContext<T>(context);
+
+            var scope = _container.BeginScope();
+
+            _container.Register(Component.For<ConsumeContext, ConsumeContext<T>>().Instance(context));
+
+            try
+            {
+                var proxy = new ConsumeContextProxy<T>(context, new PayloadCacheScope(context));
+
+                var consumerContainer = _container;
+
+                proxy.GetOrAddPayload(() => consumerContainer);
+                foreach (Action<ConsumeContext> scopeAction in _scopeActions)
+                    scopeAction(proxy);
+
+                return new CreatedSagaScopeContext<IDisposable, T>(scope, proxy);
+            }
+            catch
+            {
+                scope.Dispose();
+
+                throw;
+            }
+
+        }
+
+        public void Probe(ProbeContext context)
+        {
+            context.Add("provider", "windsor");
+        }
+    }
+}
---
+++ src/Containers/MassTransit.Automatonymous.WindsorIntegration/WindsorStateMachineRegisterSagaExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace MassTransit.AutomatonymousWindsorIntegration
+{
+    using System;
+    using System.Linq;
+    using Automatonymous;
+    using Castle.MicroKernel.Registration;
+    using Castle.Windsor;
+    using System.Reflection;
+    using Castle.MicroKernel;
+
+
+    public static class WindsorStateMachineRegisterSagaExtensions
+    {
+        /// <summary>
+        /// Register the state machine sagas found in the specified assemblies using the Container provided. The
+        /// machines are registered using their SagaStateMachine&lt;&gt; type, as well as the concrete type for use by
+        /// the application.
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="assemblies"></param>
+        public static void RegisterStateMachineSagas(this IWindsorContainer container, params Assembly[] assemblies)
+        {
+            if (container == null)
+                throw new ArgumentNullException(nameof(container));
+
+            RegisterStateMachineSagas(container.Kernel, assemblies);
+        }
+
+        /// <summary>
+        /// Register the state machine sagas found in the specified assemblies using the Container provided. The
+        /// machines are registered using their SagaStateMachine&lt;&gt; type, as well as the concrete type for use by
+        /// the application.
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="assemblies"></param>
+        public static void RegisterStateMachineSagas(this IKernel container, params Assembly[] assemblies)
+        {
+            if (container == null)
+                throw new ArgumentNullException(nameof(container));
+
+            if (assemblies == null)
+                throw new ArgumentNullException(nameof(assemblies));
+
+            foreach (var assembly in assemblies)
+            {
+                container.Register(Classes.FromAssembly(assembly)
+                    .BasedOn(typeof(SagaStateMachine<>))
+                    .WithServiceAllInterfaces()
+                    .WithServiceSelf()
+                    .LifestyleTransient()
+                );
+            }
+        }
+    }
+}
