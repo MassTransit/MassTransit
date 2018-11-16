38ca0e640e9d0864cb902041ca232ecb13042c1b
Denys Kozhevnikov <noonamer@gmail.com>
2018-11-16 11:18:28 +0200

    - fix remove subscriptions ASB bug

--- src/MassTransit.AzureServiceBusTransport.Tests/Publish_Specs.cs
+++ src/MassTransit.AzureServiceBusTransport.Tests/Publish_Specs.cs
@@ -1,14 +1,14 @@
 // Copyright 2007-2015 Chris Patterson, Dru Sellers, Travis Smith, et. al.
-//  
+//
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-// this file except in compliance with the License. You may obtain a copy of the 
-// License at 
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0 
-// 
+// this file except in compliance with the License. You may obtain a copy of the
+// License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 namespace MassTransit.AzureServiceBusTransport.Tests
 {
@@ -116,6 +116,32 @@ namespace MassTransit.AzureServiceBusTransport.Tests
             ConsumeContext<PingMessage> received = await _handler;
 
             Assert.AreEqual(EncryptedMessageSerializer.EncryptedContentType, received.ReceiveContext.ContentType);
+        }
+    }
+
+
+    [TestFixture]
+    public class Publishing_a_message_to_an_remove_subscriptions_endpoint :
+        AzureServiceBusTestFixture
+    {
+        Task<ConsumeContext<PingMessage>> _consumer;
+
+        protected override void ConfigureServiceBusBusHost(IServiceBusBusFactoryConfigurator configurator, IServiceBusHost host)
+        {
+            base.ConfigureServiceBusBusHost(configurator, host);
+            configurator.ReceiveEndpoint(host, e =>
+            {
+                e.RemoveSubscriptions = true;
+                _consumer = HandledByConsumer<PingMessage>(e);
+            });
+        }
+
+        [Test]
+        public async Task Should_succeed()
+        {
+            await Bus.Publish(new PingMessage());
+
+            await _consumer;
         }
     }
 }
--- src/MassTransit.AzureServiceBusTransport/Configuration/Configuration/ServiceBusReceiveEndpointConfiguration.cs
+++ src/MassTransit.AzureServiceBusTransport/Configuration/Configuration/ServiceBusReceiveEndpointConfiguration.cs
@@ -156,7 +156,7 @@
             var receiveEndpointContext = builder.CreateReceiveEndpointContext();
 
             NamespacePipeConfigurator.UseFilter(new ConfigureTopologyFilter<ReceiveSettings>(_settings, receiveEndpointContext.BrokerTopology,
-                _settings.RemoveSubscriptions));
+                _settings.RemoveSubscriptions, _hostConfiguration.Host.Stopping));
 
             return CreateReceiveEndpoint(receiveEndpointContext);
         }
