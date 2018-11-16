967343a282c7dfc1148bdffaee01afa2d89cc261
hannan mumtaz <hannanmumtaz@hotmail.com>
2018-11-16 15:51:53 +1100

    Added Automatonymous StateMachineSagas integration for Castle Windsor.

---
+++ src/Containers/MassTransit.Automatonymous.WindsorIntegration/WindsorStateMachineLoadSagaExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MassTransit.AutomatonymousWindsorIntegration
+{
+    using Automatonymous;
+    using Automatonymous.Scoping;
+    using Castle.MicroKernel;
+    using Castle.Windsor;
+    using GreenPipes.Internals.Extensions;
+
+
+    public static class WindsorStateMachineLoadSagaExtensions
+    {
+        /// <summary>
+        /// Specify that the service bus should load the StateMachineSagas from the container passed as argument
+        /// </summary>
+        /// <param name="configurator"></param>
+        /// <param name="container"></param>
+        public static void LoadStateMachineSagas(this IReceiveEndpointConfigurator configurator, IWindsorContainer container)
+        {
+            if (container == null)
+                throw new ArgumentNullException(nameof(container));
+
+            LoadStateMachineSagas(configurator, container.Kernel);
+        }
+
+        /// <summary>
+        /// Specify that the service bus should load the StateMachineSagas from the container passed as argument
+        /// </summary>
+        /// <param name="configurator"></param>
+        /// <param name="container"></param>
+        public static void LoadStateMachineSagas(this IReceiveEndpointConfigurator configurator, IKernel container)
+        {
+            if (configurator == null)
+                throw new ArgumentNullException(nameof(configurator));
+            if (container == null)
+                throw new ArgumentNullException(nameof(container));
+
+            IEnumerable<Type> sagaTypes = FindStateMachineSagaTypes(container);
+
+            var stateMachineFactory = new WindsorSagaStateMachineFactory(container);
+
+            var repositoryFactory = new WindsorStateMachineSagaRepositoryFactory(container);
+
+            foreach (var sagaType in sagaTypes)
+            {
+                StateMachineSagaConfiguratorCache.Configure(sagaType, configurator, stateMachineFactory, repositoryFactory);
+            }
+        }
+
+        static IEnumerable<Type> FindStateMachineSagaTypes(IKernel container)
+        {
+            var types = container.GetAssignableHandlers(typeof(StateMachine))
+                 .Where(x => x.HasInterface(typeof(SagaStateMachine<>)))
+                 .Select(x => x.ComponentModel.Implementation.GetClosingArguments(typeof(SagaStateMachine<>)).First())
+                 .Distinct()
+                 .ToList();
+
+            return types;
+        }
+
+        static bool HasInterface(this IHandler handler, Type type)
+        {
+            return handler.ComponentModel.Implementation.GetInterfaces()
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == type);
+        }
+    }
+}
---
+++ src/Containers/MassTransit.Containers.Tests/CastleWindsorStateMachineSaga_Specs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassTransit.Containers.Tests
+{
+    using Automatonymous;
+    using Castle.MicroKernel.Registration;
+    using Castle.Windsor;
+    using NUnit.Framework;
+    using Saga;
+    using Scenarios.StateMachines;
+    using TestFramework;
+    using AutomatonymousWindsorIntegration;
+
+    public class CastleWindsorStateMachineSaga_Specs : InMemoryTestFixture
+    {
+        [Test]
+        public async Task Should_receive_the_first_event_successfully()
+        {
+            Task<ConsumeContext<TestStarted>> started = ConnectPublishHandler<TestStarted>();
+            Task<ConsumeContext<TestUpdated>> updated = ConnectPublishHandler<TestUpdated>();
+
+            await InputQueueSendEndpoint.Send(new StartTest { CorrelationId = NewId.NextGuid(), TestKey = "Unique" });
+
+            await started;
+
+            await InputQueueSendEndpoint.Send(new UpdateTest { TestKey = "Unique" });
+
+            await updated;
+        }
+
+        IWindsorContainer _container;
+
+        protected override void ConfigureInMemoryReceiveEndpoint(IInMemoryReceiveEndpointConfigurator configurator)
+        {
+            _container = new WindsorContainer();
+
+            _container.Register(Component.For(typeof(ISagaRepository<>))
+                .ImplementedBy(typeof(InMemorySagaRepository<>)));
+
+            _container.Register(Component.For<PublishTestStartedActivity>()
+                .LifestyleTransient());
+
+            _container.RegisterStateMachineSagas(GetType().Assembly);
+
+            configurator.LoadStateMachineSagas(_container);
+        }
+
+        [OneTimeTearDown]
+        public void Close_container()
+        {
+            _container.Dispose();
+        }
+    }
+}
