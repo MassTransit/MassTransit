5992e4c818e826f34e935782093901fd85d74107
Dmitry Ushenko <dmitry.ushenko@redkassa.ru>
2018-11-20 12:49:34 +0300

    Async flow options for transaction scope

--- src/MassTransit/TransactionContextExtensions.cs
+++ src/MassTransit/TransactionContextExtensions.cs
@@ -45,5 +45,20 @@ namespace MassTransit
 
             return new TransactionScope(transactionContext.Transaction, scopeTimeout);
         }
+
+        /// <summary>
+        /// Create a transaction scope using the transaction context (added by the TransactionFilter),
+        /// to ensure that any transactions are carried between any threads.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="scopeTimeout">The timespan after which the scope times out and aborts the transaction</param>
+        /// <param name="asyncFlowOptions">Specifies whether transaction flow across thread continuations is enabled.</param>
+        /// <returns></returns>
+        public static TransactionScope CreateTransactionScope(this PipeContext context, TimeSpan scopeTimeout, TransactionScopeAsyncFlowOption asyncFlowOptions)
+        {
+            var transactionContext = context.GetPayload<TransactionContext>();
+
+            return new TransactionScope(transactionContext.Transaction, scopeTimeout, asyncFlowOptions);
+        }
     }
 }
