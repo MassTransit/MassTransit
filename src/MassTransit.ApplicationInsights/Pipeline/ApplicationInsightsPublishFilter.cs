a93a4b1d26c9c58053b220a99cfae11bf95eab44
Richard Dalziel <RichardDalziel@outlook.com>
2019-01-13 21:23:16 +0000

    Reverted AppInsights Filter changes

--- src/MassTransit.ApplicationInsights/Pipeline/ApplicationInsightsPublishFilter.cs
+++ src/MassTransit.ApplicationInsights/Pipeline/ApplicationInsightsPublishFilter.cs
@@ -10,8 +10,6 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
-
-#if NETSTANDARD
 namespace MassTransit.ApplicationInsights.Pipeline
 {
     using System;
@@ -21,6 +19,7 @@ namespace MassTransit.ApplicationInsights.Pipeline
     using Microsoft.ApplicationInsights.DataContracts;
     using Microsoft.ApplicationInsights.Extensibility;
     using Util;
+
 
     public class ApplicationInsightsPublishFilter<T> :
         IFilter<PublishContext<T>>
@@ -109,4 +108,3 @@ namespace MassTransit.ApplicationInsights.Pipeline
         }
     }
 }
-#endif
