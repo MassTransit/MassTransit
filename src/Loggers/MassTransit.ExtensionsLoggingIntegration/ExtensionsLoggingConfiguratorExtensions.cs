09fb91c666b9ecacadb0c2e20c50bcfe176bf04d
Chris Patterson <chris@phatboyg.com>
2019-06-03 12:35:49 -0500

    Working on #1461 - setting references to 2.0.0 where possible

--- src/Loggers/MassTransit.ExtensionsLoggingIntegration/ExtensionsLoggingConfiguratorExtensions.cs
+++ src/Loggers/MassTransit.ExtensionsLoggingIntegration/ExtensionsLoggingConfiguratorExtensions.cs
@@ -1,33 +1,24 @@
-// Copyright 2007-2016 Chris Patterson, Dru Sellers, Travis Smith, et. al.
-//  
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-// this file except in compliance with the License. You may obtain a copy of the 
-// License at 
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
-// specific language governing permissions and limitations under the License.
 namespace MassTransit
 {
     using System;
+    using ExtensionsLoggingIntegration;
     using Microsoft.Extensions.Logging;
+
 
     public static class ExtensionsLoggingConfiguratorExtensions
     {
         /// <summary> Configure the Mass Transit Service Bus to use Microsoft.Extensions.Logging. </summary>
-        /// <param name="configurator"> The configurator to act on. </param>
-        /// <param name="loggerFactory"> (Optional) The ILoggerFactory. If none supplied, will be created. </param>
-        public static ILoggerFactory UseExtensionsLogging(this IBusFactoryConfigurator configurator, ILoggerFactory loggerFactory = null)
+        /// <param name="configurator">The configurator to act on. </param>
+        /// <param name="loggerFactory">The ILoggerFactory. If none supplied, will be created.</param>
+        public static ILoggerFactory UseExtensionsLogging(this IBusFactoryConfigurator configurator, ILoggerFactory loggerFactory)
         {
             if (configurator == null)
-            {
-                throw new ArgumentNullException("configurator");
-            }
+                throw new ArgumentNullException(nameof(configurator));
 
-            return ExtensionsLoggingIntegration.ExtensionsLogger.Use(loggerFactory);
+            if (loggerFactory == null)
+                throw new ArgumentNullException(nameof(loggerFactory));
+
+            return ExtensionsLogger.Use(loggerFactory);
         }
     }
 }
