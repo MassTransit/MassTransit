cad182509b5c88e52704839d8b5ac63320c04568
Ivan Demchuk <ivan.demchuk@gmail.com>
2019-06-06 16:39:29 +0300

    Fix typo in DependencyInjectionCompensateActivityScopeProvider

--- src/Containers/MassTransit.ExtensionsDependencyInjectionIntegration/ScopeProviders/DependencyInjectionCompensateActivityScopeProvider.cs
+++ src/Containers/MassTransit.ExtensionsDependencyInjectionIntegration/ScopeProviders/DependencyInjectionCompensateActivityScopeProvider.cs
@@ -77,7 +77,7 @@ namespace MassTransit.ExtensionsDependencyInjectionIntegration.ScopeProviders
 
         public void Probe(ProbeContext context)
         {
-            context.Add("provider", "structureMap");
+            context.Add("provider", "dependencyInjection");
         }
     }
 }
