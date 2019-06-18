f322af74d98332fdc77337a238cb8c5398cf437f
Chris Patterson <chris@phatboyg.com>
2019-06-18 16:50:26 -0500

    Changed RabbitMQ send trasnport to use a pipe

--- src/MassTransit.RabbitMqTransport/Transport/RabbitMqSendTransport.cs
+++ src/MassTransit.RabbitMqTransport/Transport/RabbitMqSendTransport.cs
@@ -20,6 +20,7 @@ namespace MassTransit.RabbitMqTransport.Transport
     using Contexts;
     using GreenPipes;
     using GreenPipes.Agents;
+    using GreenPipes.Pipes;
     using Integration;
     using Internals.Extensions;
     using Logging;
@@ -55,72 +56,14 @@ namespace MassTransit.RabbitMqTransport.Transport
             return this.Stop("Disposed", cancellationToken);
         }
 
-        async Task ISendTransport.Send<T>(T message, IPipe<SendContext<T>> pipe, CancellationToken cancellationToken)
+        Task ISendTransport.Send<T>(T message, IPipe<SendContext<T>> pipe, CancellationToken cancellationToken)
         {
             if (IsStopped)
                 throw new TransportUnavailableException($"The RabbitMQ send transport is stopped: {_exchange}");
 
-            IPipe<ModelContext> modelPipe = Pipe.New<ModelContext>(p =>
-            {
-                p.UseFilter(_filter);
+            var sendPipe = new SendPipe<T>(_filter, _observers, _exchange, message, pipe, cancellationToken);
 
-                p.UseExecuteAsync(async modelContext =>
-                {
-                    var properties = modelContext.Model.CreateBasicProperties();
-
-                    var context = new BasicPublishRabbitMqSendContext<T>(properties, _exchange, message, cancellationToken);
-                    try
-                    {
-                        await pipe.Send(context).ConfigureAwait(false);
-
-                        byte[] body = context.Body;
-
-                        if (context.TryGetPayload(out PublishContext publishContext))
-                            context.Mandatory = context.Mandatory || publishContext.Mandatory;
-
-                        if (properties.Headers == null)
-                            properties.Headers = new Dictionary<string, object>();
-
-                        properties.ContentType = context.ContentType.MediaType;
-
-                        properties.Headers["Content-Type"] = context.ContentType.MediaType;
-
-                        properties.Headers.SetTextHeaders(context.Headers, (_, text) => text);
-
-                        properties.Persistent = context.Durable;
-
-                        if (context.MessageId.HasValue)
-                            properties.MessageId = context.MessageId.ToString();
-
-                        if (context.CorrelationId.HasValue)
-                            properties.CorrelationId = context.CorrelationId.ToString();
-
-                        if (context.TimeToLive.HasValue)
-                            properties.Expiration = context.TimeToLive.Value.TotalMilliseconds.ToString("F0", CultureInfo.InvariantCulture);
-
-                        await _observers.PreSend(context).ConfigureAwait(false);
-
-                        var publishTask = modelContext.BasicPublishAsync(context.Exchange, context.RoutingKey ?? "", context.Mandatory,
-                            context.BasicProperties, body, context.AwaitAck);
-
-                        await publishTask.WithCancellation(context.CancellationToken).ConfigureAwait(false);
-
-                        context.LogSent();
-
-                        await _observers.PostSend(context).ConfigureAwait(false);
-                    }
-                    catch (Exception ex)
-                    {
-                        context.LogFaulted(ex);
-
-                        await _observers.SendFault(context, ex).ConfigureAwait(false);
-
-                        throw;
-                    }
-                });
-            });
-
-            await _modelContextSupervisor.Send(modelPipe, cancellationToken).ConfigureAwait(false);
+            return _modelContextSupervisor.Send(sendPipe, cancellationToken);
         }
 
         public ConnectHandle ConnectSendObserver(ISendObserver observer)
@@ -134,6 +77,91 @@ namespace MassTransit.RabbitMqTransport.Transport
                 _log.DebugFormat("Stopping transport: {0}", _exchange);
 
             return base.StopSupervisor(context);
+        }
+
+
+        struct SendPipe<T> :
+            IPipe<ModelContext>
+            where T : class
+        {
+            readonly IFilter<ModelContext> _filter;
+            readonly SendObservable _observers;
+            readonly string _exchange;
+            readonly T _message;
+            readonly IPipe<SendContext<T>> _pipe;
+            readonly CancellationToken _cancellationToken;
+
+            public SendPipe(IFilter<ModelContext> filter, SendObservable observers, string exchange, T message, IPipe<SendContext<T>> pipe,
+                CancellationToken cancellationToken)
+            {
+                _filter = filter;
+                _observers = observers;
+                _exchange = exchange;
+                _message = message;
+                _pipe = pipe;
+                _cancellationToken = cancellationToken;
+            }
+
+            public async Task Send(ModelContext modelContext)
+            {
+                await _filter.Send(modelContext, Pipe.Empty<ModelContext>()).ConfigureAwait(false);
+
+                var properties = modelContext.Model.CreateBasicProperties();
+
+                var context = new BasicPublishRabbitMqSendContext<T>(properties, _exchange, _message, _cancellationToken);
+                try
+                {
+                    await _pipe.Send(context).ConfigureAwait(false);
+
+                    byte[] body = context.Body;
+
+                    if (context.TryGetPayload(out PublishContext publishContext))
+                        context.Mandatory = context.Mandatory || publishContext.Mandatory;
+
+                    if (properties.Headers == null)
+                        properties.Headers = new Dictionary<string, object>();
+
+                    properties.ContentType = context.ContentType.MediaType;
+
+                    properties.Headers["Content-Type"] = context.ContentType.MediaType;
+
+                    properties.Headers.SetTextHeaders(context.Headers, (_, text) => text);
+
+                    properties.Persistent = context.Durable;
+
+                    if (context.MessageId.HasValue)
+                        properties.MessageId = context.MessageId.ToString();
+
+                    if (context.CorrelationId.HasValue)
+                        properties.CorrelationId = context.CorrelationId.ToString();
+
+                    if (context.TimeToLive.HasValue)
+                        properties.Expiration = context.TimeToLive.Value.TotalMilliseconds.ToString("F0", CultureInfo.InvariantCulture);
+
+                    await _observers.PreSend(context).ConfigureAwait(false);
+
+                    var publishTask = modelContext.BasicPublishAsync(context.Exchange, context.RoutingKey ?? "", context.Mandatory,
+                        context.BasicProperties, body, context.AwaitAck);
+
+                    await publishTask.WithCancellation(context.CancellationToken).ConfigureAwait(false);
+
+                    context.LogSent();
+
+                    await _observers.PostSend(context).ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    context.LogFaulted(ex);
+
+                    await _observers.SendFault(context, ex).ConfigureAwait(false);
+
+                    throw;
+                }
+            }
+
+            public void Probe(ProbeContext context)
+            {
+            }
         }
     }
 }
