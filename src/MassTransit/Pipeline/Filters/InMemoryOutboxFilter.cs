5b881468d5eb89e1893399c48f527ab2e9a4e0c5
Chris Patterson <chris@phatboyg.com>
2019-09-24 19:26:51 -0500

    Converted to new Log syntax

--- src/MassTransit/Pipeline/Filters/InMemoryOutboxFilter.cs
+++ src/MassTransit/Pipeline/Filters/InMemoryOutboxFilter.cs
@@ -1,28 +1,14 @@
-﻿// Copyright 2007-2015 Chris Patterson, Dru Sellers, Travis Smith, et. al.
-//  
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-// this file except in compliance with the License. You may obtain a copy of the 
-// License at 
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
-// specific language governing permissions and limitations under the License.
-namespace MassTransit.Pipeline.Filters
+﻿namespace MassTransit.Pipeline.Filters
 {
     using System;
     using System.Threading.Tasks;
     using Context;
     using GreenPipes;
-    using MassTransit.Logging;
+
 
     public class InMemoryOutboxFilter :
         IFilter<ConsumeContext>
     {
-        static readonly ILog _log = Logger.Get<InMemoryOutboxFilter>();
-
         public async Task Send(ConsumeContext context, IPipe<ConsumeContext> next)
         {
             InMemoryOutboxMessageSchedulerContext outboxSchedulerContext = null;
@@ -48,14 +34,12 @@ namespace MassTransit.Pipeline.Filters
 
                 try
                 {
-                    if(outboxSchedulerContext != null)
+                    if (outboxSchedulerContext != null)
                         await outboxSchedulerContext.CancelAllScheduledMessages().ConfigureAwait(false);
                 }
                 catch (Exception e)
                 {
-                    // Failed to unschedule some messages
-                    if (_log.IsWarnEnabled)
-                        _log.Warn("Some or all of the outbox unschedule failed", e);
+                    LogContext.Warning?.Log(e, "One or more messages could not be unscheduled.", e);
                 }
 
                 throw;
@@ -74,8 +58,6 @@ namespace MassTransit.Pipeline.Filters
         IFilter<ConsumeContext<T>>
         where T : class
     {
-        static readonly ILog _log = Logger.Get<InMemoryOutboxFilter<T>>();
-
 
         public async Task Send(ConsumeContext<T> context, IPipe<ConsumeContext<T>> next)
         {
@@ -106,9 +88,7 @@ namespace MassTransit.Pipeline.Filters
                 }
                 catch (Exception e)
                 {
-                    // Failed to unschedule some messages
-                    if (_log.IsWarnEnabled)
-                        _log.Warn("Some or all of the outbox unschedule failed", e);
+                    LogContext.Warning?.Log(e, "One or more messages could not be unscheduled.", e);
                 }
 
                 throw;
