fc05aa07b490878c689de592f6372923765ffde3
Chris Patterson <chris@phatboyg.com>
2019-10-07 14:53:20 -0500

    Also related to #1563, some circular reference object handles

--- src/MassTransit/Context/PublishContextProxy.cs
+++ src/MassTransit/Context/PublishContextProxy.cs
@@ -1,16 +1,4 @@
-﻿// Copyright 2007-2017 Chris Patterson, Dru Sellers, Travis Smith, et. al.
-//  
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-// this file except in compliance with the License. You may obtain a copy of the 
-// License at 
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
-// specific language governing permissions and limitations under the License.
-namespace MassTransit.Context
+﻿namespace MassTransit.Context
 {
     public class PublishContextProxy :
         SendContextProxy,
@@ -22,8 +10,6 @@ namespace MassTransit.Context
             : base(context)
         {
             _context = context;
-
-            _context.GetOrAddPayload<PublishContext>(() => this);
         }
 
         bool PublishContext.Mandatory { get; set; }
@@ -46,9 +32,6 @@ namespace MassTransit.Context
             : base(context, message)
         {
             _context = context;
-
-            _context.GetOrAddPayload<PublishContext>(() => this);
-            _context.GetOrAddPayload<PublishContext<TMessage>>(() => this);
         }
 
         bool PublishContext.Mandatory { get; set; }
--- src/MassTransit/Context/SendContextProxy.cs
+++ src/MassTransit/Context/SendContextProxy.cs
@@ -1,20 +1,7 @@
-// Copyright 2007-2015 Chris Patterson, Dru Sellers, Travis Smith, et. al.
-//  
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-// this file except in compliance with the License. You may obtain a copy of the 
-// License at 
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
-// specific language governing permissions and limitations under the License.
 namespace MassTransit.Context
 {
     using System;
     using System.Net.Mime;
-    using System.Threading;
     using GreenPipes;
 
 
@@ -34,123 +21,148 @@ namespace MassTransit.Context
 
 
     public class SendContextProxy :
+        BasePipeContext,
         SendContext
     {
         readonly SendContext _context;
 
-        public SendContextProxy(SendContext context)
+        protected SendContextProxy(SendContext context)
+            : base(context)
         {
             _context = context;
         }
 
-        public CancellationToken CancellationToken => _context.CancellationToken;
-
-        public bool HasPayloadType(Type contextType)
+        public override bool HasPayloadType(Type payloadType)
         {
-            return _context.HasPayloadType(contextType);
+            if (base.HasPayloadType(payloadType))
+                return true;
+
+            return _context.HasPayloadType(payloadType);
         }
 
-        public bool TryGetPayload<TPayload>(out TPayload payload)
-            where TPayload : class
+        public override bool TryGetPayload<T>(out T payload)
         {
+            if (base.TryGetPayload(out payload))
+                return true;
+
             return _context.TryGetPayload(out payload);
         }
 
-        public TPayload GetOrAddPayload<TPayload>(PayloadFactory<TPayload> payloadFactory)
-            where TPayload : class
+        public override T GetOrAddPayload<T>(PayloadFactory<T> payloadFactory)
         {
-            return _context.GetOrAddPayload(payloadFactory);
+            if (base.TryGetPayload<T>(out var existing))
+                return existing;
+
+            if (_context.TryGetPayload(out existing))
+                return existing;
+
+            return base.GetOrAddPayload(payloadFactory);
         }
 
-        T PipeContext.AddOrUpdatePayload<T>(PayloadFactory<T> addFactory, UpdatePayloadFactory<T> updateFactory)
+        public override T AddOrUpdatePayload<T>(PayloadFactory<T> addFactory, UpdatePayloadFactory<T> updateFactory)
         {
-            return _context.AddOrUpdatePayload(addFactory, updateFactory);
+            if (base.TryGetPayload<T>(out var existing) || _context.TryGetPayload(out existing))
+            {
+                T Update(T _)
+                {
+                    return updateFactory(existing);
+                }
+
+                T Add()
+                {
+                    return updateFactory(existing);
+                }
+
+                return base.AddOrUpdatePayload(Add, Update);
+            }
+
+            return base.AddOrUpdatePayload(addFactory, updateFactory);
         }
 
         public Uri SourceAddress
         {
-            get { return _context.SourceAddress; }
-            set { _context.SourceAddress = value; }
+            get => _context.SourceAddress;
+            set => _context.SourceAddress = value;
         }
 
         public Uri DestinationAddress
         {
-            get { return _context.DestinationAddress; }
-            set { _context.DestinationAddress = value; }
+            get => _context.DestinationAddress;
+            set => _context.DestinationAddress = value;
         }
 
         public Uri ResponseAddress
         {
-            get { return _context.ResponseAddress; }
-            set { _context.ResponseAddress = value; }
+            get => _context.ResponseAddress;
+            set => _context.ResponseAddress = value;
         }
 
         public Uri FaultAddress
         {
-            get { return _context.FaultAddress; }
-            set { _context.FaultAddress = value; }
+            get => _context.FaultAddress;
+            set => _context.FaultAddress = value;
         }
 
         public Guid? RequestId
         {
-            get { return _context.RequestId; }
-            set { _context.RequestId = value; }
+            get => _context.RequestId;
+            set => _context.RequestId = value;
         }
 
         public Guid? MessageId
         {
-            get { return _context.MessageId; }
-            set { _context.MessageId = value; }
+            get => _context.MessageId;
+            set => _context.MessageId = value;
         }
 
         public Guid? CorrelationId
         {
-            get { return _context.CorrelationId; }
-            set { _context.CorrelationId = value; }
+            get => _context.CorrelationId;
+            set => _context.CorrelationId = value;
         }
 
         public Guid? ConversationId
         {
-            get { return _context.ConversationId; }
-            set { _context.ConversationId = value; }
+            get => _context.ConversationId;
+            set => _context.ConversationId = value;
         }
 
         public Guid? InitiatorId
         {
-            get { return _context.InitiatorId; }
-            set { _context.InitiatorId = value; }
+            get => _context.InitiatorId;
+            set => _context.InitiatorId = value;
         }
 
         public Guid? ScheduledMessageId
         {
-            get { return _context.ScheduledMessageId; }
-            set { _context.ScheduledMessageId = value; }
+            get => _context.ScheduledMessageId;
+            set => _context.ScheduledMessageId = value;
         }
 
         public SendHeaders Headers => _context.Headers;
 
         public TimeSpan? TimeToLive
         {
-            get { return _context.TimeToLive; }
-            set { _context.TimeToLive = value; }
+            get => _context.TimeToLive;
+            set => _context.TimeToLive = value;
         }
 
         public ContentType ContentType
         {
-            get { return _context.ContentType; }
-            set { _context.ContentType = value; }
+            get => _context.ContentType;
+            set => _context.ContentType = value;
         }
 
         public bool Durable
         {
-            get { return _context.Durable; }
-            set { _context.Durable = value; }
+            get => _context.Durable;
+            set => _context.Durable = value;
         }
 
         public IMessageSerializer Serializer
         {
-            get { return _context.Serializer; }
-            set { _context.Serializer = value; }
+            get => _context.Serializer;
+            set => _context.Serializer = value;
         }
 
         public SendContext<T> CreateProxy<T>(T message)
