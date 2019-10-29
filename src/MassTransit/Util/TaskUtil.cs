0357229f21fc9a4c92e5350b18774fd70be99a61
Chris Patterson <chris@phatboyg.com>
2019-10-29 15:06:20 -0500

    Complete stack upgrade for TaskCompletionSource recommendations

--- src/MassTransit/Util/TaskUtil.cs
+++ src/MassTransit/Util/TaskUtil.cs
@@ -46,7 +46,7 @@
         }
 
         /// <summary>
-        /// Creates a new <see cref="TaskCompletionSource{T}"/>, and ensures the <see cref="TaskCreationOptions.RunContinuationsAsynchronously"/>
+        /// Creates a new <see cref="TaskCompletionSource{T}"/>, and ensures the TaskCreationOptions.RunContinuationsAsynchronously
         /// flag is specified (if available).
         /// </summary>
         /// <param name="options"></param>
@@ -54,10 +54,72 @@
         /// <returns></returns>
         public static TaskCompletionSource<T> GetTask<T>(TaskCreationOptions options = TaskCreationOptions.None)
         {
-        #if NETCORE
+        #if NETSTD
             options |= TaskCreationOptions.RunContinuationsAsynchronously;
         #endif
             return new TaskCompletionSource<T>(options);
+        }
+
+        /// <summary>
+        /// Creates a new TaskCompletionSource and ensures the TaskCreationOptions.RunContinuationsAsynchronously
+        /// flag is specified (if available).
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public static TaskCompletionSource<bool> GetTask(TaskCreationOptions options = TaskCreationOptions.None)
+        {
+        #if NETSTD
+            options |= TaskCreationOptions.RunContinuationsAsynchronously;
+        #endif
+            return new TaskCompletionSource<bool>(options);
+        }
+
+        /// <summary>
+        /// Register a callback on the <paramref name="cancellationToken"/> which completes the resulting task.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <param name="cancelTask"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static CancellationTokenRegistration RegisterTask(this CancellationToken cancellationToken, out Task cancelTask)
+        {
+            if (!cancellationToken.CanBeCanceled)
+                throw new ArgumentException("The cancellationToken must support cancellation", nameof(cancellationToken));
+
+            var source = GetTask();
+
+            cancelTask = source.Task;
+
+            return cancellationToken.Register(SetCompleted, source);
+        }
+
+        static void SetCompleted(object obj)
+        {
+            if (obj is TaskCompletionSource<bool> source)
+                source.SetCompleted();
+        }
+
+        public static CancellationTokenRegistration RegisterIfCanBeCanceled(this CancellationToken cancellationToken, CancellationTokenSource source)
+        {
+            if (cancellationToken.CanBeCanceled)
+                return cancellationToken.Register(Cancel, source);
+
+            return default;
+        }
+
+        static void Cancel(object obj)
+        {
+            if (obj is CancellationTokenSource source)
+                source.Cancel();
+        }
+
+        /// <summary>
+        /// Sets the source to completed using TrySetResult
+        /// </summary>
+        /// <param name="source"></param>
+        public static void SetCompleted(this TaskCompletionSource<bool> source)
+        {
+            source.TrySetResult(true);
         }
 
         public static void Await(Func<Task> taskFactory, CancellationToken cancellationToken = default)
