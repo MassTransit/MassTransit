e09bf204ff79266feb315bcaf8369362ea3af613
Eduardo Monteiro de Barros <eduardo.mb@amwins.com>
2019-11-18 11:38:56 -0500

    Added migrations to EFCoreIntegration.Tests

--- src/Persistence/MassTransit.EntityFrameworkCoreIntegration.Tests/Migrations/Audit/20191115203752_audit_add_senttime.Designer.cs
+++ src/Persistence/MassTransit.EntityFrameworkCoreIntegration.Tests/Migrations/Audit/20191115203752_audit_add_senttime.Designer.cs
@@ -4,14 +4,16 @@ using MassTransit.EntityFrameworkCoreIntegration.Audit;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MassTransit.EntityFrameworkCoreIntegration.Tests.Migrations.Audit
 {
     [DbContext(typeof(AuditDbContext))]
-    partial class AuditDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191115203752_audit_add_senttime")]
+    partial class audit_add_senttime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
