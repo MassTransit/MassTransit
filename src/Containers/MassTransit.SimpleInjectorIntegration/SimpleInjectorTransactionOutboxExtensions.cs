3f64fd78adf07c4aefcae233cc345b9c31e72304
maldworth <m@maldworth.com>
2020-01-02 10:27:27 -0500

    Add Transaction Outbox

---
+++ src/Containers/MassTransit.SimpleInjectorIntegration/SimpleInjectorTransactionOutboxExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace MassTransit
+{
+    using MassTransit.Transactions;
+    using Microsoft.Extensions.Logging;
+    using SimpleInjector;
+    using SimpleInjectorIntegration;
+
+
+    public static class SimpleInjectorTransactionOutboxExtensions
+    {
+        /// <summary>
+        /// Decorates the IPublishEndpoint and ISendEndpointProvider with a Transaction Outbox. Messages will not be
+        /// released/sent until the ambient transaction is committed. This is only meant to be used outside of a consumer.
+        /// If you want an outbox for Consumers, it is recommended to use the InMemoryOutbox.
+        /// </summary>
+        public static void AddTransactionOutbox(this ISimpleInjectorConfigurator builder)
+        {
+            builder.Container
+                .RegisterSingleton(()=>
+                {
+                    var busControl = builder.Container.GetInstance<IBusControl>();
+
+                    return new TransactionOutbox(busControl, busControl, builder.Container.TryGetInstance<ILoggerFactory>());
+                });
+
+            builder.Container
+                .Register(() => (IPublishEndpoint)builder.Container.GetInstance<TransactionOutbox>(), Lifestyle.Singleton);
+
+            builder.Container
+                .Register(() => (ISendEndpointProvider)builder.Container.GetInstance<TransactionOutbox>(), Lifestyle.Singleton);
+        }
+    }
+}
---
+++ src/Containers/MassTransit.StructureMapIntegration/StructureMapTransactionOutboxExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace MassTransit
+{
+    using MassTransit.Transactions;
+    using Microsoft.Extensions.Logging;
+    using StructureMapIntegration;
+
+    public static class StructureMapTransactionOutboxExtensions
+    {
+        /// <summary>
+        /// Decorates the IPublishEndpoint and ISendEndpointProvider with a Transaction Outbox. Messages will not be
+        /// released/sent until the ambient transaction is committed. This is only meant to be used outside of a consumer.
+        /// If you want an outbox for Consumers, it is recommended to use the InMemoryOutbox.
+        /// </summary>
+        public static void AddTransactionOutbox(this IConfigurationExpressionConfigurator builder)
+        {
+            builder.Builder
+                .For<TransactionOutbox>()
+                .Use("Fetch Bus Control and Create TransactionOutbox", x =>
+                {
+                    var busControl = x.GetInstance<IBusControl>();
+
+                    return new TransactionOutbox(busControl, busControl, x.TryGetInstance<ILoggerFactory>());
+                })
+                .Singleton();
+
+            builder.Builder
+                .For<IPublishEndpoint>()
+                .Use(x => x.GetInstance<TransactionOutbox>())
+                .Singleton();
+
+            builder.Builder
+                .For<ISendEndpointProvider>()
+                .Use(x => x.GetInstance<TransactionOutbox>())
+                .Singleton();
+        }
+    }
+}
