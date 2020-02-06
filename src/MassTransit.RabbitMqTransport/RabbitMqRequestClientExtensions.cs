a5c784897874bbf36ad07209c107b570b5d6f2cf
Chris Patterson <chris@phatboyg.com>
2020-02-06 08:07:56 -0600

    Increase concurrent message limit for ReplyToClientFactory endpoint.

--- src/MassTransit.RabbitMqTransport/RabbitMqRequestClientExtensions.cs
+++ src/MassTransit.RabbitMqTransport/RabbitMqRequestClientExtensions.cs
@@ -18,7 +18,7 @@
         /// <returns></returns>
         public static Task<IClientFactory> CreateReplyToClientFactory(this IReceiveConnector connector, RequestTimeout timeout = default)
         {
-            var endpointDefinition = new ReplyToEndpointDefinition();
+            var endpointDefinition = new ReplyToEndpointDefinition(1000);
 
             var receiveEndpointHandle = connector.ConnectReceiveEndpoint(endpointDefinition, KebabCaseEndpointNameFormatter.Instance);
 
