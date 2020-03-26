2966508f7789643f97ba0d986f12c8b73d732624
Denys Kozhevnikov <noonamer@gmail.com>
2020-03-26 10:13:04 +0200

    - more tests for hangfire

---
+++ src/MassTransit.HangfireIntegration.Tests/HangfireInMemoryActivityTestFixture.cs
@@ -0,0 +1,57 @@
+﻿namespace MassTransit.HangfireIntegration.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Hangfire;
+    using Hangfire.MemoryStorage;
+    using NUnit.Framework;
+    using TestFramework;
+
+
+    public abstract class HangfireInMemoryActivityTestFixture :
+        InMemoryActivityTestFixture
+    {
+        readonly string _queueName;
+
+        static HangfireInMemoryActivityTestFixture()
+        {
+            GlobalConfiguration.Configuration
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseMemoryStorage();
+
+        }
+
+        protected HangfireInMemoryActivityTestFixture()
+        {
+            _queueName = "hangfire";
+            HangfireAddress = new Uri($"loopback://localhost/{_queueName}");
+        }
+
+        protected Uri HangfireAddress { get; }
+
+        protected ISendEndpoint HangfireEndpoint { get; private set; }
+
+        protected override void ConfigureInMemoryBus(IInMemoryBusFactoryConfigurator configurator)
+        {
+            configurator.UseHangfireScheduler(_queueName, cfg =>
+            {
+                cfg.ServerCheckInterval = TimeSpan.FromSeconds(1);
+                cfg.SchedulePollingInterval = TimeSpan.FromSeconds(1);
+            });
+
+            base.ConfigureInMemoryBus(configurator);
+        }
+
+        [OneTimeSetUp]
+        public async Task Setup_quartz_service()
+        {
+            HangfireEndpoint = await GetSendEndpoint(HangfireAddress);
+        }
+
+        [OneTimeTearDown]
+        public void Take_it_down()
+        {
+        }
+    }
+}
---
+++ src/MassTransit.HangfireIntegration.Tests/HangfirePublish_Specs.cs
@@ -0,0 +1,56 @@
+﻿// Copyright 2007-2015 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+namespace MassTransit.HangfireIntegration.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+
+    public class HangfirePublish_Specs :
+        HangfireInMemoryTestFixture
+    {
+        class SomeMessageConsumer :
+            IConsumer<SomeMessage>
+        {
+            public Task Consume(ConsumeContext<SomeMessage> context)
+            {
+                return Console.Out.WriteLineAsync(context.Message.GetType().Name + " - " + context.Message.SendDate + " - " + context.Message.Source);
+            }
+        }
+
+
+        class SomeMessage
+        {
+            public DateTime SendDate { get; set; }
+            public string Source { get; set; }
+        }
+
+
+        [Test]
+        public async Task Should_receive_the_message()
+        {
+            var handled = SubscribeHandler<SomeMessage>();
+
+            Bus.ConnectConsumer(() => new SomeMessageConsumer());
+
+            await Bus.ScheduleSend(Bus.Address, DateTime.Now, new SomeMessage
+            {
+                SendDate = DateTime.Now,
+                Source = "Schedule"
+            });
+
+            await handled;
+        }
+    }
+}
