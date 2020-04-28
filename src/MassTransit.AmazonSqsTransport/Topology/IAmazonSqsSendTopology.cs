9089fc8bc75acef3a477f312a54713d83419d317
Krzysztof Łabuś <krzysztof.labus@sinch.com>
2020-04-28 07:41:00 +0200

    Added ConfigureErrorSettings, ConfigureDeadLetterSettings to IAmazonSqsSendTopologyConfigurator

--- src/MassTransit.AmazonSqsTransport/Topology/IAmazonSqsSendTopology.cs
+++ src/MassTransit.AmazonSqsTransport/Topology/IAmazonSqsSendTopology.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright 2007-2018 Chris Patterson, Dru Sellers, Travis Smith, et. al.
-//  
+//
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file except in compliance with the License. You may obtain a copy of the
 // License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -15,11 +15,15 @@ namespace MassTransit.AmazonSqsTransport.Topology
     using System;
     using Configuration;
     using MassTransit.Topology;
+    using Settings;
 
 
     public interface IAmazonSqsSendTopology :
         ISendTopology
     {
+        Action<QueueErrorSettings> ConfigureErrorSettings { get; }
+        Action<QueueDeadLetterSettings> ConfigureDeadLetterSettings { get; }
+
         new IAmazonSqsMessageSendTopologyConfigurator<T> GetMessageTopology<T>()
             where T : class;
 
