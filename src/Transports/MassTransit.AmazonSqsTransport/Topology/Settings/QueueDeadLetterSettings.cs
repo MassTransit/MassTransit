86d163c38882c3d84eba3f86983f8f62b628aa67
Krzysztof Łabuś <krzysztof.labus@sinch.com>
2020-05-25 13:59:58 +0200

    Use copy of tags in error/dead-letter settings.

--- src/Transports/MassTransit.AmazonSqsTransport/Topology/Settings/QueueDeadLetterSettings.cs
+++ src/Transports/MassTransit.AmazonSqsTransport/Topology/Settings/QueueDeadLetterSettings.cs
@@ -1,5 +1,6 @@
 ﻿namespace MassTransit.AmazonSqsTransport.Topology.Settings
 {
+    using System.Linq;
     using Builders;
     using Configurators;
 
@@ -11,7 +12,7 @@
         public QueueDeadLetterSettings(EntitySettings source, string queueName)
             : base(queueName, source.Durable, source.AutoDelete)
         {
-            QueueTags = source.Tags;
+            QueueTags = source.Tags?.ToDictionary(x => x.Key, x => x.Value);
         }
 
         public BrokerTopology GetBrokerTopology()
