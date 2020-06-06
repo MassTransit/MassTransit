718bd126fb105960ab1d37a30e931313165bd1d0
Richard Dalziel <richard.dalziel@hymans.co.uk>
2020-06-06 16:17:26 +0100

    Added bring your own storage account

--- src/Persistence/MassTransit.Azure.Cosmos.Table/Configuration/AzureCosmosTableAuditStoreConfiguratorExtensions.cs
+++ src/Persistence/MassTransit.Azure.Cosmos.Table/Configuration/AzureCosmosTableAuditStoreConfiguratorExtensions.cs
@@ -2,17 +2,15 @@ namespace MassTransit.Azure.Cosmos.Table
 {
     using System;
 
-
     public static class AzureCosmosTableAuditStoreConfiguratorExtensions
     {
         public static void UseAzureCosmosTableAuditStore(this IBusFactoryConfigurator configurator,
                                                          Func<IAzureCosmosTableConfigurator, AzureCosmosTableAuditBusObserver> configure)
         {
             var builder = new AzureCosmosTableConfigurationBuilder();
-
             var auditBusObserver = configure.Invoke(builder);
 
-            configurator.ConnectBusObserver(auditBusObserver );
+            configurator.ConnectBusObserver(auditBusObserver);
         }
     }
 }
---
+++ tests/MassTransit.Azure.Cosmos.Table.Tests/Audit/AuditStore_Configure_Builder_With_StorageAccountSupplied.cs
@@ -0,0 +1,51 @@
+﻿namespace MassTransit.Azure.Cosmos.Table.Tests.Audit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos.Table;
+    using NUnit.Framework;
+    using Shouldly;
+
+
+    [TestFixture]
+    public class Configure_audit_store_bring_your_own_storage_account :
+        AzureCosmosTableInMemoryTestFixture
+    {
+        [OneTimeSetUp]
+        public async Task SetUp()
+        {
+            await InputQueueSendEndpoint.Send(new A());
+        }
+
+        [Test]
+        public async Task Should_have_send_audit_records()
+        {
+            IEnumerable<AuditRecord> consumeRecords = AzureTableHelpers.GetAuditRecords().Where(x => x.ContextType == "Send");
+            consumeRecords.Count().ShouldBe(1);
+        }
+
+        protected override void ConfigureInMemoryBus(IInMemoryBusFactoryConfigurator configurator)
+        {
+            var storageAccount = CloudStorageAccount.Parse(ConnectionString);
+            var tableClient = storageAccount.CreateCloudTableClient(new TableClientConfiguration());
+            var table = tableClient.GetTableReference(AuditTableName);
+            configurator.UseAzureCosmosTableAuditStore(configure => configure.WithTable(table)
+                                                                             .WithContextTypePartitionKeyStrategy()
+                                                                             .WithNoMessageFilter()
+                                                                             .Build());
+            base.ConfigureInMemoryBus(configurator);
+        }
+
+        protected override void ConfigureInMemoryReceiveEndpoint(IInMemoryReceiveEndpointConfigurator configurator)
+        {
+            EndpointConvention.Map<A>(new Uri($"{configurator.InputAddress}"));
+        }
+
+
+        class A
+        {
+        }
+    }
+}
