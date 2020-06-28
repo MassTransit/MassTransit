23848ac7b04a29ba7af372c4d9aac71abdd42192
Richard Dalziel <richard.dalziel@hymans.co.uk>
2020-06-28 22:44:32 +0100

    Added extension and tests

---
+++ src/Persistence/MassTransit.Azure.Table/Configuration/AzureTableJobServiceConfigurationExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace MassTransit.Azure.Table
+{
+    using System;
+    using JobService.Components.StateMachines;
+    using JobService.Configuration;
+    using Microsoft.Azure.Cosmos.Table;
+    using Saga;
+
+
+    public static class AzureTableJobServiceConfigurationExtensions
+    {
+        public static void UseAzureTableSagaRepository(this IJobServiceConfigurator configurator,
+            Func<CloudTable> contextFactory)
+        {
+            configurator.Repository = AzureTableSagaRepository<JobTypeSaga>.Create(contextFactory);
+
+            configurator.JobRepository = AzureTableSagaRepository<JobSaga>.Create(contextFactory);
+
+            configurator.JobAttemptRepository = AzureTableSagaRepository<JobAttemptSaga>.Create(contextFactory);
+        }
+    }
+}
