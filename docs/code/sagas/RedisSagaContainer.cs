206c8e1b430a1a406cbe7b527b6b3a3a8b41d182
Chris Patterson <chris@phatboyg.com>
2020-06-28 20:49:41 -0500

    Redis documentation updated

---
+++ docs/code/sagas/RedisSagaContainer.cs
@@ -0,0 +1,24 @@
+namespace RedisSagaContainer
+{
+    using System;
+    using MassTransit;
+    using MassTransit.MongoDbIntegration;
+    using Microsoft.Extensions.DependencyInjection;
+    using MongoDbSaga;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var services = new ServiceCollection();
+
+            services.AddMassTransit(x =>
+            {
+                const string configurationString = "127.0.0.1";
+
+                x.AddSagaStateMachine<OrderStateMachine, OrderState>()
+                    .RedisRepository(configurationString);
+            });
+        }
+    }
+}
---
+++ docs/code/sagas/RedisSagaContainerConfiguration.cs
@@ -0,0 +1,40 @@
+namespace RedisSagaContainerConfiguration
+{
+    using System;
+    using MassTransit;
+    using MassTransit.MongoDbIntegration;
+    using Microsoft.Extensions.DependencyInjection;
+    using MongoDbSaga;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var services = new ServiceCollection();
+
+            services.AddMassTransit(x =>
+            {
+                const string configurationString = "127.0.0.1";
+
+                x.AddSagaStateMachine<OrderStateMachine, OrderState>()
+                    .RedisRepository(r =>
+                    {
+                        r.DatabaseConfiguration(configurationString);
+
+                        // Default is Optimistic
+                        r.ConcurrencyMode = ConcurrentMode.Pessimistic;
+
+                        // Optional, prefix each saga instance key with the string specified
+                        // resulting dev:c6cfd285-80b2-4c12-bcd3-56a00d994736
+                        r.KeyPrefix = "dev";
+
+                        // Optional, to customize the lock key
+                        r.LockSuffix = "-lockage";
+
+                        // Optional, the default is 30 seconds
+                        r.LockTimeout = TimeSpan.FromSeconds(90);
+                    });;
+            });
+        }
+    }
+}
