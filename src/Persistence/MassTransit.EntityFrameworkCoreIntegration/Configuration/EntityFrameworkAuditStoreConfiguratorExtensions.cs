ff4f55bc037897e53b503869c0bf93eb40c30580
Richard Dalziel <richard.dalziel@hymans.co.uk>
2020-07-06 21:16:15 +0100

    Added simpler configuration for Audit store with Entity Framework Core

---
+++ src/Persistence/MassTransit.EntityFrameworkCoreIntegration/Configuration/EntityFrameworkAuditStoreConfiguratorExtensions.cs
@@ -0,0 +1,43 @@
+namespace MassTransit
+{
+    using System;
+    using BusConfigurators;
+    using EntityFrameworkCoreIntegration.Audit;
+    using Microsoft.EntityFrameworkCore;
+
+
+    public static class EntityFrameworkAuditStoreConfiguratorExtensions
+    {
+        /// <summary>
+        /// Configure Audit Store against Entity Framework Core
+        /// </summary>
+        /// <param name="configurator"></param>
+        /// <param name="dbContextOptions">The DB Context Options configuration builder</param>
+        /// <param name="auditTableName">Name of the table to store audit records.</param>
+        /// <param name="configureFilter">Configure messages to exclude or include from auditing.</param>
+        public static void UseEntityFrameworkCoreAuditStore(this IBusFactoryConfigurator configurator, DbContextOptionsBuilder dbContextOptions,
+            string auditTableName,
+            Action<IMessageFilterConfigurator> configureFilter)
+        {
+            ConfigureAuditStore(configurator, dbContextOptions, auditTableName, configureFilter);
+        }
+
+        /// <summary>
+        /// Configure Audit Store against Entity Framework Core
+        /// </summary>
+        /// <param name="configurator"></param>
+        /// <param name="dbContextOptions">The DB Context Options configuration builder</param>
+        /// <param name="auditTableName">Name of the table to store audit records.</param>
+        public static void UseEntityFrameworkCoreAuditStore(this IBusFactoryConfigurator configurator, DbContextOptionsBuilder dbContextOptions,
+            string auditTableName)
+        {
+            ConfigureAuditStore(configurator, dbContextOptions, auditTableName);
+        }
+
+        static void ConfigureAuditStore(IBusObserverConnector configurator, DbContextOptionsBuilder dbContextOptions, string auditTableName,
+            Action<IMessageFilterConfigurator> configureFilter = default)
+        {
+            configurator.ConnectBusObserver(new AuditBusObserver(dbContextOptions, auditTableName, configureFilter));
+        }
+    }
+}
