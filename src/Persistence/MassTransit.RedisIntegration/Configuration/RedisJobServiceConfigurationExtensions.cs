fbbcfe180f8fbe6405eba37f987e4393b7ce7d4b
Richard Dalziel <richarddalziel@outlook.com>
2020-07-11 19:00:12 +0100

    Added Job Service Tests and Configuration Helpers

---
+++ src/Persistence/MassTransit.RedisIntegration/Configuration/RedisJobServiceConfigurationExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace MassTransit.RedisIntegration.Configuration
+{
+    using JobService.Components.StateMachines;
+    using JobService.Configuration;
+    using StackExchange.Redis;
+
+
+    public static class RedisJobServiceConfigurationExtensions
+    {
+        public static void UseRedisSagaRepository(this IJobServiceConfigurator configurator, IDatabase redisDB)
+        {
+            configurator.Repository = RedisSagaRepository<JobTypeSaga>.Create(() => redisDB, true, keyPrefix: "jobtype");
+
+            configurator.JobRepository = RedisSagaRepository<JobSaga>.Create(() => redisDB, true, keyPrefix: "job");
+            
+            configurator.JobAttemptRepository = RedisSagaRepository<JobAttemptSaga>.Create(() => redisDB, true, keyPrefix: "jobattempt");
+        }
+    }
+}
---
+++ tests/MassTransit.RedisIntegration.Tests/Turnout/Faulted_Specs.cs
@@ -0,0 +1,116 @@
+namespace MassTransit.RedisIntegration.Tests.Turnout
+{
+    using System;
+    using System.Threading.Tasks;
+    using Conductor;
+    using Configuration;
+    using Contracts.JobService;
+    using Definition;
+    using JobService;
+    using NUnit.Framework;
+    using StackExchange.Redis;
+    using TestFramework;
+
+
+    public interface GrindTheGears
+    {
+        TimeSpan Duration { get; }
+    }
+
+    public class Submitting_a_job_to_turnout_that_faults :
+        InMemoryTestFixture
+    {
+        [Test]
+        [Order(1)]
+        public async Task Should_get_the_job_accepted()
+        {
+            var serviceClient = Bus.CreateServiceClient();
+
+            IRequestClient<SubmitJob<GrindTheGears>> requestClient = serviceClient.CreateRequestClient<SubmitJob<GrindTheGears>>();
+
+            Response<JobSubmissionAccepted> response = await requestClient.GetResponse<JobSubmissionAccepted>(new
+            {
+                JobId = _jobId,
+                Job = new {Duration = TimeSpan.FromSeconds(1)}
+            });
+
+            Assert.That(response.Message.JobId, Is.EqualTo(_jobId));
+
+            // just to capture all the test output in a single window
+            ConsumeContext<JobFaulted> faulted = await _faulted;
+        }
+
+        [Test]
+        [Order(4)]
+        public async Task Should_have_published_the_job_faulted_event()
+        {
+            ConsumeContext<JobFaulted> faulted = await _faulted;
+        }
+
+        [Test]
+        [Order(3)]
+        public async Task Should_have_published_the_job_started_event()
+        {
+            ConsumeContext<JobStarted> started = await _started;
+        }
+
+        [Test]
+        [Order(2)]
+        public async Task Should_have_published_the_job_submitted_event()
+        {
+            ConsumeContext<JobSubmitted> submitted = await _submitted;
+        }
+
+        readonly Guid _jobId = NewId.NextGuid();
+        Task<ConsumeContext<JobFaulted>> _faulted;
+        Task<ConsumeContext<JobSubmitted>> _submitted;
+        Task<ConsumeContext<JobStarted>> _started;
+
+        protected override void ConfigureInMemoryBus(IInMemoryBusFactoryConfigurator configurator)
+        {
+            base.ConfigureInMemoryBus(configurator);
+
+            var redis = ConnectionMultiplexer.Connect("127.0.0.1");
+            redis.PreserveAsyncOrder = false;
+
+            var options = new ServiceInstanceOptions()
+                .EnableInstanceEndpoint()
+                .SetEndpointNameFormatter(KebabCaseEndpointNameFormatter.Instance);
+
+            configurator.ServiceInstance(options, instance =>
+            {
+                instance.ConfigureJobServiceEndpoints(x =>
+                {
+                    x.UseRedisSagaRepository(redis.GetDatabase());
+                });
+
+                instance.ReceiveEndpoint(instance.EndpointNameFormatter.Message<GrindTheGears>(), e =>
+                {
+                    e.Consumer(() => new GrindTheGearsConsumer(), cfg =>
+                    {
+                        cfg.Options<JobOptions<GrindTheGears>>(jobOptions => jobOptions.SetJobTimeout(TimeSpan.FromSeconds(90)));
+                    });
+                });
+            });
+        }
+
+        protected override void ConfigureInMemoryReceiveEndpoint(IInMemoryReceiveEndpointConfigurator configurator)
+        {
+            _submitted = Handled<JobSubmitted>(configurator, context => context.Message.JobId == _jobId);
+            _started = Handled<JobStarted>(configurator, context => context.Message.JobId == _jobId);
+            _faulted = Handled<JobFaulted>(configurator, context => context.Message.JobId == _jobId);
+        }
+
+
+        public class GrindTheGearsConsumer :
+            IJobConsumer<GrindTheGears>
+        {
+            public async Task Run(JobContext<GrindTheGears> context)
+            {
+                await Task.Delay(context.Job.Duration);
+
+                throw new IntentionalTestException("Grinding gears, dropped the transmission");
+            }
+        }
+    }
+}
