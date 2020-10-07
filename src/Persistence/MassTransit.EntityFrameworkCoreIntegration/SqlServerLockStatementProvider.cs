a30a2e84ab7a5c274076e292cf63a605a18c638c
maldworth <m@maldworth.com>
2020-10-06 21:19:03 -0400

    Get EFCore Metadata for Column Name

--- src/Persistence/MassTransit.EntityFrameworkCoreIntegration/SqlServerLockStatementProvider.cs
+++ src/Persistence/MassTransit.EntityFrameworkCoreIntegration/SqlServerLockStatementProvider.cs
@@ -3,7 +3,7 @@
     public class SqlServerLockStatementProvider :
         SqlLockStatementProvider
     {
-        const string DefaultRowLockStatement = "SELECT * FROM {0}.{1} WITH (UPDLOCK, ROWLOCK) WHERE CorrelationId = @p0";
+        const string DefaultRowLockStatement = "SELECT * FROM {0}.{1} WITH (UPDLOCK, ROWLOCK) WHERE {2} = @p0";
         const string DefaultSchemaName = "dbo";
 
         public SqlServerLockStatementProvider(bool enableSchemaCaching = true)
