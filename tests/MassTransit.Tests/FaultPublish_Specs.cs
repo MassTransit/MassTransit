10fc65535946f5a52ccc9b65ff6f8004e685f24e
Chris Patterson <chris@phatboyg.com>
2020-11-06 11:05:11 -0600

    Two additional tests to verify that faults include the proper fault message types

--- tests/MassTransit.Tests/FaultPublish_Specs.cs
+++ tests/MassTransit.Tests/FaultPublish_Specs.cs
@@ -7,6 +7,7 @@
     using GreenPipes;
     using MassTransit.Testing;
     using MassTransit.Testing.MessageObservers;
+    using Metadata;
     using NUnit.Framework;
     using Shouldly;
     using TestFramework;
@@ -51,6 +52,145 @@
 
             public IReceivedMessageList<PingMessage> Messages => _messages;
             public IReceivedMessageList<Fault<PingMessage>> Faults => _faults;
+        }
+    }
+
+
+    [TestFixture]
+    public class A_fault_event :
+        InMemoryTestFixture
+    {
+        [Test]
+        public async Task Should_include_the_faulted_message_types()
+        {
+            await InputQueueSendEndpoint.Send<ActualMessageType>(new { });
+
+            _consumer.Faults.Select().Count().ShouldBe(1);
+
+            var fault = _consumer.Faults.Select().FirstOrDefault();
+            Assert.That(fault, Is.Not.Null);
+
+            await TestContext.Out.WriteLineAsync(string.Join(Environment.NewLine, fault.Context.Message.FaultMessageTypes));
+            await TestContext.Out.WriteLineAsync(string.Join(Environment.NewLine, fault.Context.SupportedMessageTypes));
+
+            Assert.That(fault.Context.Message.FaultMessageTypes.Contains(MessageUrn.ForTypeString<BaseMessageType>()));
+            Assert.That(fault.Context.Message.FaultMessageTypes.Contains(MessageUrn.ForTypeString<ActualMessageType>()));
+
+            Assert.That(fault.Context.SupportedMessageTypes.Contains(MessageUrn.ForTypeString<Fault<BaseMessageType>>()));
+        }
+
+        BaseMessageConsumer _consumer;
+
+        protected override void ConfigureInMemoryReceiveEndpoint(IInMemoryReceiveEndpointConfigurator configurator)
+        {
+            _consumer = new BaseMessageConsumer(TimeSpan.FromSeconds(5), InMemoryTestHarness.InactivityToken);
+
+            _consumer.Configure(configurator);
+        }
+
+
+        class BaseMessageConsumer :
+            MultiTestConsumer
+        {
+            readonly ReceivedMessageList<Fault<BaseMessageType>> _faults;
+
+            public BaseMessageConsumer(TimeSpan timeout, CancellationToken testCompleted)
+                : base(timeout, testCompleted)
+            {
+                Fault<BaseMessageType>();
+                _faults = Consume<Fault<BaseMessageType>>();
+            }
+
+            public IReceivedMessageList<Fault<BaseMessageType>> Faults => _faults;
+        }
+
+
+        public interface BaseMessageType
+        {
+        }
+
+
+        public interface ActualMessageType :
+            BaseMessageType
+        {
+        }
+    }
+
+
+    [TestFixture]
+    public class A_fault_event_using_try_get_message :
+        InMemoryTestFixture
+    {
+        [Test]
+        public async Task Should_include_the_faulted_message_types()
+        {
+            await InputQueueSendEndpoint.Send<ActualMessageType>(new { });
+
+            _consumer.Faults.Select().Count().ShouldBe(1);
+
+            var fault = _consumer.Faults.Select().FirstOrDefault();
+            Assert.That(fault, Is.Not.Null);
+
+            await TestContext.Out.WriteLineAsync(string.Join(Environment.NewLine, fault.Context.Message.FaultMessageTypes));
+            await TestContext.Out.WriteLineAsync(string.Join(Environment.NewLine, fault.Context.SupportedMessageTypes));
+
+            Assert.That(fault.Context.Message.FaultMessageTypes.Contains(MessageUrn.ForTypeString<BaseMessageType>()));
+            Assert.That(fault.Context.Message.FaultMessageTypes.Contains(MessageUrn.ForTypeString<ActualMessageType>()));
+
+            Assert.That(fault.Context.SupportedMessageTypes.Contains(MessageUrn.ForTypeString<Fault<BaseMessageType>>()));
+            Assert.That(fault.Context.SupportedMessageTypes.Contains(MessageUrn.ForTypeString<Fault<ActualMessageType>>()));
+        }
+
+        BaseMessageConsumer _consumer;
+
+        protected override void ConfigureInMemoryReceiveEndpoint(IInMemoryReceiveEndpointConfigurator configurator)
+        {
+            _consumer = new BaseMessageConsumer(TimeSpan.FromSeconds(5), InMemoryTestHarness.InactivityToken);
+
+            _consumer.Configure(configurator);
+
+            configurator.Consumer<FaultConsumer>();
+        }
+
+
+        class FaultConsumer :
+            IConsumer<BaseMessageType>
+        {
+            public Task Consume(ConsumeContext<BaseMessageType> context)
+            {
+                if (context.TryGetMessage<ActualMessageType>(out var actualContext))
+                {
+                    return actualContext.NotifyFaulted(TimeSpan.Zero, TypeMetadataCache<FaultConsumer>.ShortName, new IntentionalTestException());
+                }
+
+                throw new InvalidOperationException("This was not expected, but gets the job done");
+            }
+        }
+
+
+        class BaseMessageConsumer :
+            MultiTestConsumer
+        {
+            readonly ReceivedMessageList<Fault<BaseMessageType>> _faults;
+
+            public BaseMessageConsumer(TimeSpan timeout, CancellationToken testCompleted)
+                : base(timeout, testCompleted)
+            {
+                _faults = Consume<Fault<BaseMessageType>>();
+            }
+
+            public IReceivedMessageList<Fault<BaseMessageType>> Faults => _faults;
+        }
+
+
+        public interface BaseMessageType
+        {
+        }
+
+
+        public interface ActualMessageType :
+            BaseMessageType
+        {
         }
     }
 }
