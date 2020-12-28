07b832c317133721a5e2d278fffe3bd8d69e6b9c
Chris Patterson <chris@phatboyg.com>
2020-12-28 14:45:49 -0600

    Keep track of kill switches so that they aren't added multiple times.

--- src/MassTransit/Transports/Components/KillSwitchReceiveEndpointObserver.cs
+++ src/MassTransit/Transports/Components/KillSwitchReceiveEndpointObserver.cs
@@ -1,21 +1,36 @@
 namespace MassTransit.Transports.Components
 {
+    using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
 
     public class KillSwitchReceiveEndpointObserver :
         IReceiveEndpointObserver
     {
+        readonly IDictionary<Uri, KillSwitch> _killSwitches;
         readonly KillSwitchOptions _options;
 
         public KillSwitchReceiveEndpointObserver(KillSwitchOptions options)
         {
             _options = options;
+
+            _killSwitches = new Dictionary<Uri, KillSwitch>();
         }
 
         public Task Ready(ReceiveEndpointReady ready)
         {
-            ready.ReceiveEndpoint.ConnectConsumeObserver(new KillSwitch(_options, ready.ReceiveEndpoint));
+            lock (_killSwitches)
+            {
+                if (!_killSwitches.TryGetValue(ready.InputAddress, out var killSwitch))
+                {
+                    killSwitch = new KillSwitch(_options, ready.ReceiveEndpoint);
+
+                    _killSwitches.Add(ready.InputAddress, killSwitch);
+
+                    ready.ReceiveEndpoint.ConnectConsumeObserver(killSwitch);
+                }
+            }
 
             return Task.CompletedTask;
         }
