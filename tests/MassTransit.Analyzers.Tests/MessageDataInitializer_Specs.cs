dcb226645b5ea9d7f24f547822b419559be1c1ec
Alex Wardle <464713+awardle@users.noreply.github.com>
2021-01-12 13:10:42 +0000

    Updated analyser so it does not warn when using MessageData<Stream>
    * Added unit test to analyser to check for derived stream types

--- tests/MassTransit.Analyzers.Tests/MessageDataInitializer_Specs.cs
+++ tests/MassTransit.Analyzers.Tests/MessageDataInitializer_Specs.cs
@@ -17,6 +17,7 @@ namespace ConsoleApplication1
         Guid Id { get; }
         string CustomerId { get; }
         MessageData<string> Document { get; }
+        MessageData<Stream> Stream { get; }
     }
 }
 ";
@@ -25,6 +26,7 @@ namespace ConsoleApplication1
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using MassTransit;
 ";
 
@@ -51,10 +53,10 @@ namespace ConsoleApplication1
             {
                 Id = "MCA0003",
                 Message =
-                    "Anonymous type is missing properties that are in the message contract 'ProcessDocument'. The following properties are missing: Id, CustomerId, Document.",
+                    "Anonymous type is missing properties that are in the message contract 'ProcessDocument'. The following properties are missing: Id, CustomerId, Document, Stream.",
                 Severity = DiagnosticSeverity.Info,
                 Locations =
-                    new[] {new DiagnosticResultLocation("Test0.cs", 25, 48)}
+                    new[] {new DiagnosticResultLocation("Test0.cs", 27, 48)}
             };
 
             VerifyCSharpDiagnostic(test, expected);
@@ -76,7 +78,8 @@ namespace ConsoleApplication1
             {
                 InVar.Id,
                 CustomerId = ""53051996-AEEC-4EF1-BCFD-7835F17BA8E7"",
-                Document = 72
+                Document = 72,
+                Stream = 42
             });
         }
     }
@@ -86,10 +89,10 @@ namespace ConsoleApplication1
             {
                 Id = "MCA0001",
                 Message =
-                    "Anonymous type does not map to message contract 'ProcessDocument'. The following properties of the anonymous type are incompatible: Document.",
+                    "Anonymous type does not map to message contract 'ProcessDocument'. The following properties of the anonymous type are incompatible: Document, Stream.",
                 Severity = DiagnosticSeverity.Error,
                 Locations =
-                    new[] {new DiagnosticResultLocation("Test0.cs", 25, 48)}
+                    new[] {new DiagnosticResultLocation("Test0.cs", 27, 48)}
             };
 
             VerifyCSharpDiagnostic(test, expected);
@@ -106,12 +109,41 @@ namespace ConsoleApplication1
         static async Task Main()
         {
             var bus = Bus.Factory.CreateUsingInMemory(cfg => { });
+            Stream testStream = new MemoryStream();
 
             await bus.Publish<ProcessDocument>(new
             {
                 InVar.Id,
                 CustomerId = ""53051996-AEEC-4EF1-BCFD-7835F17BA8E7"",
-                Document = ""This would be a really big document typically""
+                Document = ""This would be a really big document typically"",
+                Stream = testStream
+            });
+        }
+    }
+}
+";
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [Test]
+        public void WhenTypesAreStructurallyCompatibleAndInherited_ShouldNotHaveDiagnostic()
+        {
+            var test = Usings + MessageContracts + @"
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var bus = Bus.Factory.CreateUsingInMemory(cfg => { });
+            MemoryStream testStream = new MemoryStream();
+
+            await bus.Publish<ProcessDocument>(new
+            {
+                InVar.Id,
+                CustomerId = ""53051996-AEEC-4EF1-BCFD-7835F17BA8E7"",
+                Document = ""This would be a really big document typically"",
+                Stream = testStream
             });
         }
     }
