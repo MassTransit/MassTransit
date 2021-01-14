b9f228fab534c40c72a88b1b2f860c96b45c9722
Chris Patterson <chris@phatboyg.com>
2021-01-14 12:42:19 -0600

    Package updates, including Automatonymous bug fix.

--- tests/MassTransit.Tests/AutomatonymousIntegration/CatchFault_Specs.cs
+++ tests/MassTransit.Tests/AutomatonymousIntegration/CatchFault_Specs.cs
@@ -3,88 +3,15 @@
     using System;
     using System.Threading.Tasks;
     using Automatonymous;
+    using FaultTestContracts;
     using MassTransit.Saga;
+    using MassTransit.Testing;
     using NUnit.Framework;
     using TestFramework;
 
 
-    [TestFixture]
-    public class Catching_a_fault :
-        InMemoryTestFixture
+    namespace FaultTestContracts
     {
-        [Test]
-        public async Task Should_receive_the_caught_fault_response()
-        {
-            var message = new Start();
-
-            Task<ConsumeContext<ServiceFaulted>> serviceFaulted = ConnectPublishHandler<ServiceFaulted>();
-
-            Response<StartFaulted> startFaulted = await Bus.Request<Start, StartFaulted>(InputQueueAddress, message, TestCancellationToken, TestTimeout);
-
-            Assert.AreEqual(message.CorrelationId, startFaulted.CorrelationId);
-
-            ConsumeContext<ServiceFaulted> context = await serviceFaulted;
-
-            Assert.AreEqual(message.CorrelationId, context.CorrelationId);
-        }
-
-        protected override void ConfigureInMemoryReceiveEndpoint(IInMemoryReceiveEndpointConfigurator configurator)
-        {
-            _machine = new TestStateMachine();
-            _repository = new InMemorySagaRepository<Instance>();
-
-            configurator.StateMachineSaga(_machine, _repository);
-        }
-
-        TestStateMachine _machine;
-        InMemorySagaRepository<Instance> _repository;
-
-
-        class Instance :
-            SagaStateMachineInstance
-        {
-            public Instance(Guid correlationId)
-            {
-                CorrelationId = correlationId;
-            }
-
-            protected Instance()
-            {
-            }
-
-            public State CurrentState { get; set; }
-            public Guid CorrelationId { get; set; }
-        }
-
-
-        class TestStateMachine :
-            MassTransitStateMachine<Instance>
-        {
-            public TestStateMachine()
-            {
-                InstanceState(x => x.CurrentState);
-
-                Initially(
-                    When(Started)
-                        .Then(context =>
-                        {
-                            throw new NotSupportedException("This is expected, but nonetheless exceptional");
-                        })
-                        .Publish(context => new Started(context.Instance.CorrelationId))
-                        .TransitionTo(Running)
-                        .Catch<NotSupportedException>(ex => ex
-                            .Respond(context => new StartFaulted(context.Instance.CorrelationId))
-                            .Publish(context => new ServiceFaulted(context.Instance.CorrelationId))
-                            .TransitionTo(FailedToStart)));
-            }
-
-            public State FailedToStart { get; private set; }
-            public State Running { get; private set; }
-
-            public Event<Start> Started { get; private set; }
-        }
-
-
         public class Start :
             CorrelatedBy<Guid>
         {
@@ -151,5 +78,161 @@
 
             public Guid CorrelationId { get; set; }
         }
+    }
+
+
+    [TestFixture]
+    public class Catching_a_fault :
+        InMemoryTestFixture
+    {
+        [Test]
+        public async Task Should_receive_the_caught_fault_response()
+        {
+            var message = new Start();
+
+            Task<ConsumeContext<ServiceFaulted>> serviceFaulted = ConnectPublishHandler<ServiceFaulted>();
+
+            Response<StartFaulted> startFaulted = await Bus.Request<Start, StartFaulted>(InputQueueAddress, message, TestCancellationToken, TestTimeout);
+
+            Assert.AreEqual(message.CorrelationId, startFaulted.CorrelationId);
+
+            ConsumeContext<ServiceFaulted> context = await serviceFaulted;
+
+            Assert.AreEqual(message.CorrelationId, context.CorrelationId);
+
+            Assert.That(await _repository.ShouldContainSagaInState(message.CorrelationId, _machine, x => x.FailedToStart, TestTimeout), Is.Not.Null);
+        }
+
+        protected override void ConfigureInMemoryReceiveEndpoint(IInMemoryReceiveEndpointConfigurator configurator)
+        {
+            _machine = new TestStateMachine();
+            _repository = new InMemorySagaRepository<Instance>();
+
+            configurator.StateMachineSaga(_machine, _repository);
+        }
+
+        TestStateMachine _machine;
+        InMemorySagaRepository<Instance> _repository;
+
+
+        class Instance :
+            SagaStateMachineInstance
+        {
+            public Instance(Guid correlationId)
+            {
+                CorrelationId = correlationId;
+            }
+
+            protected Instance()
+            {
+            }
+
+            public State CurrentState { get; set; }
+            public Guid CorrelationId { get; set; }
+        }
+
+
+        class TestStateMachine :
+            MassTransitStateMachine<Instance>
+        {
+            public TestStateMachine()
+            {
+                InstanceState(x => x.CurrentState);
+
+                Initially(
+                    When(Started)
+                        .Then(context => throw new NotSupportedException("This is expected, but nonetheless exceptional"))
+                        .Publish(context => new Started(context.Instance.CorrelationId))
+                        .TransitionTo(Running)
+                        .Catch<NotSupportedException>(ex => ex
+                            .Respond(context => new StartFaulted(context.Instance.CorrelationId))
+                            .Publish(context => new ServiceFaulted(context.Instance.CorrelationId))
+                            .TransitionTo(FailedToStart)));
+            }
+
+            public State FailedToStart { get; }
+            public State Running { get; }
+
+            public Event<Start> Started { get; }
+        }
+    }
+
+
+    [TestFixture]
+    public class Catching_a_fault_and_finalizing :
+        InMemoryTestFixture
+    {
+        [Test]
+        public async Task Should_receive_the_caught_fault_response()
+        {
+            var message = new Start();
+
+            Task<ConsumeContext<ServiceFaulted>> serviceFaulted = ConnectPublishHandler<ServiceFaulted>();
+
+            Response<StartFaulted> startFaulted = await Bus.Request<Start, StartFaulted>(InputQueueAddress, message, TestCancellationToken, TestTimeout);
+
+            Assert.AreEqual(message.CorrelationId, startFaulted.CorrelationId);
+
+            ConsumeContext<ServiceFaulted> context = await serviceFaulted;
+
+            Assert.AreEqual(message.CorrelationId, context.CorrelationId);
+
+            Assert.That(await _repository.ShouldNotContainSaga(message.CorrelationId, TestTimeout), Is.Null);
+        }
+
+        protected override void ConfigureInMemoryReceiveEndpoint(IInMemoryReceiveEndpointConfigurator configurator)
+        {
+            _machine = new TestStateMachine();
+            _repository = new InMemorySagaRepository<Instance>();
+
+            configurator.StateMachineSaga(_machine, _repository);
+        }
+
+        TestStateMachine _machine;
+        InMemorySagaRepository<Instance> _repository;
+
+
+        class Instance :
+            SagaStateMachineInstance
+        {
+            public Instance(Guid correlationId)
+            {
+                CorrelationId = correlationId;
+            }
+
+            protected Instance()
+            {
+            }
+
+            public State CurrentState { get; set; }
+            public Guid CorrelationId { get; set; }
+        }
+
+
+        class TestStateMachine :
+            MassTransitStateMachine<Instance>
+        {
+            public TestStateMachine()
+            {
+                InstanceState(x => x.CurrentState);
+
+                Initially(
+                    When(Started)
+                        .Then(context => throw new NotSupportedException("This is expected, but nonetheless exceptional"))
+                        .Publish(context => new Started(context.Instance.CorrelationId))
+                        .TransitionTo(Running)
+                        .Catch<NotSupportedException>(ex => ex
+                            .Respond(context => new StartFaulted(context.Instance.CorrelationId))
+                            .Publish(context => new ServiceFaulted(context.Instance.CorrelationId))
+                            .Finalize()));
+
+                SetCompletedWhenFinalized();
+            }
+
+            public State Running { get; }
+
+            public Event<Start> Started { get; }
+        }
+
     }
 }
