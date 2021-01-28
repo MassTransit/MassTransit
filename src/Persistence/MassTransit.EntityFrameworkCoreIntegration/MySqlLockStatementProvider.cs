4b06a0ede2102e3e6736c779bdeb13abe07fbefc
Joep van diesen <Joepvandiessen@gmail.com>
2021-01-28 21:02:12 +0100

    Added MySqlLockStateProvider

---
+++ src/Persistence/MassTransit.EntityFrameworkCoreIntegration/MySqlLockStatementProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MassTransit.EntityFrameworkCoreIntegration
+{
+    public class MySqlLockStatementProvider :
+        SqlLockStatementProvider
+    {
+        const string DefaultSchemaName = "public";
+        const string DefaultRowLockStatement = "SELECT * FROM {1} WHERE {2} = \"@p0\" FOR UPDATE";
+
+        public MySqlLockStatementProvider(bool enableSchemaCaching = true)
+            : base(DefaultSchemaName, DefaultRowLockStatement, enableSchemaCaching)
+        {
+        }
+    }
+}
