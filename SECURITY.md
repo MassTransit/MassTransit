0b3fe1dc3a05f6ca4f9830e8fbba06327d615fba
Chris Patterson <chris@phatboyg.com>
2021-01-31 21:44:26 -0600

    Added AVRO unit test for Kafka

---
+++ SECURITY.md
@@ -0,0 +1,14 @@
+# Security Policy
+
+## Supported Versions
+
+MassTransit supports the current major version only. If issues or vulnerabilities are encountered with previous versions for which there is no workaround, upgrading to the latest version if recommended.
+
+| Version | Supported          |
+| ------- | ------------------ |
+| 7.x     | :white_check_mark: |
+| < 7.0   | :x:                |
+
+## Reporting a Vulnerability
+
+Vulnerabilities can be reported by submitting an issue.
---
+++ tests/MassTransit.KafkaIntegration.Tests/Avro_Specs.cs
@@ -0,0 +1,132 @@
+namespace MassTransit.KafkaIntegration.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Confluent.Kafka;
+    using Confluent.Kafka.SyncOverAsync;
+    using Confluent.SchemaRegistry;
+    using Confluent.SchemaRegistry.Serdes;
+    using GreenPipes;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
+    using Microsoft.Extensions.Logging;
+    using NUnit.Framework;
+    using TestFramework;
+    using UnitTests;
+
+
+    public class Avro_Specs :
+        InMemoryTestFixture
+    {
+        const string Topic = "avrogie";
+
+        [Test]
+        public async Task Should_produce()
+        {
+            TaskCompletionSource<ConsumeContext<KafkaMessage>> taskCompletionSource = GetTask<ConsumeContext<KafkaMessage>>();
+            var services = new ServiceCollection();
+            services.AddSingleton(taskCompletionSource);
+
+            var schemaRegistryClient = new CachedSchemaRegistryClient(new Dictionary<string, string>
+            {
+                {"schema.registry.url", "localhost:8081"},
+            });
+
+            services.TryAddSingleton<ILoggerFactory>(LoggerFactory);
+            services.TryAddSingleton(typeof(ILogger<>), typeof(Logger<>));
+
+            services.AddMassTransit(x =>
+            {
+                x.UsingInMemory((context, cfg) => cfg.ConfigureEndpoints(context));
+                x.AddRider(rider =>
+                {
+                    rider.AddConsumer<KafkaMessageConsumer>();
+
+                    rider.AddProducer<string, KafkaMessage>(Topic, context => context.MessageId.ToString())
+                        .SetKeySerializer(new AvroSerializer<string>(schemaRegistryClient).AsSyncOverAsync())
+                        .SetValueSerializer(new AvroSerializer<KafkaMessage>(schemaRegistryClient).AsSyncOverAsync());
+
+                    rider.UsingKafka((context, k) =>
+                    {
+                        k.Host("localhost:9092");
+
+                        k.TopicEndpoint<string, KafkaMessage>(Topic, nameof(Producer_Specs), c =>
+                        {
+                            c.SetKeyDeserializer(new AvroDeserializer<string>(schemaRegistryClient).AsSyncOverAsync());
+                            c.SetValueDeserializer(new AvroDeserializer<KafkaMessage>(schemaRegistryClient).AsSyncOverAsync());
+                            c.AutoOffsetReset = AutoOffsetReset.Earliest;
+                            c.ConfigureConsumer<KafkaMessageConsumer>(context);
+
+                            c.CreateIfMissing(m =>
+                            {
+                                m.NumPartitions = 2;
+                            });
+                        });
+                    });
+                });
+            });
+
+            var provider = services.BuildServiceProvider(true);
+
+            var busControl = provider.GetRequiredService<IBusControl>();
+
+            await busControl.StartAsync(TestCancellationToken);
+
+            var serviceScope = provider.CreateScope();
+
+            var producer = serviceScope.ServiceProvider.GetRequiredService<ITopicProducer<KafkaMessage>>();
+
+            try
+            {
+                var correlationId = NewId.NextGuid();
+                var conversationId = NewId.NextGuid();
+                var initiatorId = NewId.NextGuid();
+                var messageId = NewId.NextGuid();
+                await producer.Produce(new {Test = "text"}, Pipe.Execute<SendContext>(context =>
+                    {
+                        context.CorrelationId = correlationId;
+                        context.MessageId = messageId;
+                        context.InitiatorId = initiatorId;
+                        context.ConversationId = conversationId;
+                    }),
+                    TestCancellationToken);
+
+                ConsumeContext<KafkaMessage> result = await taskCompletionSource.Task;
+
+                Assert.AreEqual("text", result.Message.Test);
+                Assert.That(result.SourceAddress, Is.EqualTo(new Uri("loopback://localhost/")));
+                Assert.That(result.DestinationAddress, Is.EqualTo(new Uri($"loopback://localhost/{KafkaTopicAddress.PathPrefix}/{Topic}")));
+                Assert.That(result.MessageId, Is.EqualTo(messageId));
+                Assert.That(result.CorrelationId, Is.EqualTo(correlationId));
+                Assert.That(result.InitiatorId, Is.EqualTo(initiatorId));
+                Assert.That(result.ConversationId, Is.EqualTo(conversationId));
+            }
+            finally
+            {
+                serviceScope.Dispose();
+
+                await busControl.StopAsync(TestCancellationToken);
+
+                await provider.DisposeAsync();
+            }
+        }
+
+
+        class KafkaMessageConsumer :
+            IConsumer<KafkaMessage>
+        {
+            readonly TaskCompletionSource<ConsumeContext<KafkaMessage>> _taskCompletionSource;
+
+            public KafkaMessageConsumer(TaskCompletionSource<ConsumeContext<KafkaMessage>> taskCompletionSource)
+            {
+                _taskCompletionSource = taskCompletionSource;
+            }
+
+            public async Task Consume(ConsumeContext<KafkaMessage> context)
+            {
+                _taskCompletionSource.TrySetResult(context);
+            }
+        }
+    }
+}
