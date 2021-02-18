2f99672ef1c7483c3682fc6374b82264b967af95
Chris Patterson <chris@phatboyg.com>
2021-02-18 11:32:08 -0600

    Fixed #2324 - applied to Redis as well.

--- src/Persistence/MassTransit.MongoDbIntegration/Configuration/Configurators/MongoDbSagaRepositoryRegistrationProvider.cs
+++ src/Persistence/MassTransit.MongoDbIntegration/Configuration/Configurators/MongoDbSagaRepositoryRegistrationProvider.cs
@@ -21,7 +21,7 @@ namespace MassTransit.MongoDbIntegration.Configurators
         {
             if (typeof(TSaga).HasInterface<ISagaVersion>())
             {
-                var proxy = (IProxy)Activator.CreateInstance(typeof(Proxy<>).MakeGenericType(typeof(TSaga)));
+                var proxy = (IProxy)Activator.CreateInstance(typeof(Proxy<>).MakeGenericType(typeof(TSaga)), configurator);
 
                 proxy.Configure(this);
             }
