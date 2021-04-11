ef9592d837916d3015c9be2c029c64231ab7197b
Chris Patterson <chris@phatboyg.com>
2021-04-10 19:59:53 -0500

    Wait for Kafka to be ready.

---
+++ tests/MassTransit.KafkaIntegration.Tests/FixtureSetUp.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+
+[assembly: Parallelizable(ParallelScope.None)]
+[assembly: LevelOfParallelism(1)]
+
+
+namespace MassTransit.KafkaIntegration.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Confluent.Kafka;
+    using Confluent.SchemaRegistry;
+    using Policies;
+
+
+    [SetUpFixture]
+    public class AzureServiceBusTests
+    {
+        [OneTimeSetUp]
+        public async Task Before_any()
+        {
+            await CheckBrokerReady();
+        }
+
+        Task CheckBrokerReady()
+        {
+            return Retry.Interval(10, 5000).Retry(async () =>
+            {
+                using var client = new CachedSchemaRegistryClient(new Dictionary<string, string>
+                {
+                    {"schema.registry.url", "localhost:8081"},
+                });
+
+                await client.GetAllSubjectsAsync();
+
+                var clientConfig = new ClientConfig {BootstrapServers = "localhost:9092"};
+                using var adminClient = new AdminClientBuilder(clientConfig).Build();
+
+                adminClient.GetMetadata(TimeSpan.FromSeconds(60));
+            });
+        }
+    }
+}
