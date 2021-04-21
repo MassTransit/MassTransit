e3669427932f15a35c290c660b61a8340cabab4e
Henk Edel <henk.edel@gmail.com>
2021-04-21 11:28:22 -0700

    Introduced selector for MessageData storage.

---
+++ src/MassTransit/Configuration/Configurators/MessageDataRepositorySelector.cs
@@ -0,0 +1,10 @@
+﻿namespace MassTransit.Configurators
+{
+    class MessageDataRepositorySelector :
+        IMessageDataRepositorySelector
+    {
+        public MessageDataRepositorySelector(IBusFactoryConfigurator configurator) => Configurator = configurator;
+
+        public IBusFactoryConfigurator Configurator { get; }
+    }
+}
---
+++ src/MassTransit/IMessageDataRepositorySelector.cs
@@ -0,0 +1,13 @@
+﻿namespace MassTransit
+{
+    using MessageData;
+
+    /// <summary>
+    /// Use one of the selector extension methods to create a <see cref="IMessageDataRepository"/> instance for the
+    /// selected repository implementation.
+    /// </summary>
+    public interface IMessageDataRepositorySelector
+    {
+        IBusFactoryConfigurator Configurator { get; }
+    }
+}
