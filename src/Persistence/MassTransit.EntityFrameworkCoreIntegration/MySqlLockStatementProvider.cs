8b086359cde668784f08b243b31aa4abd3934136
tgdraugr <tiagogabrielsilva0@gmail.com>
2021-05-04 21:53:44 +0100

    MySql Lock statement corrected

--- src/Persistence/MassTransit.EntityFrameworkCoreIntegration/MySqlLockStatementProvider.cs
+++ src/Persistence/MassTransit.EntityFrameworkCoreIntegration/MySqlLockStatementProvider.cs
@@ -3,11 +3,10 @@
     public class MySqlLockStatementProvider :
         SqlLockStatementProvider
     {
-        const string DefaultSchemaName = "public";
-        const string DefaultRowLockStatement = "SELECT * FROM {1} WHERE {2} = \"@p0\" FOR UPDATE";
+        const string DefaultRowLockStatement = "SELECT * FROM `{1}` WHERE `{2}` = @p0 FOR UPDATE";
 
         public MySqlLockStatementProvider(bool enableSchemaCaching = true)
-            : base(DefaultSchemaName, DefaultRowLockStatement, enableSchemaCaching)
+            : base(string.Empty, DefaultRowLockStatement, enableSchemaCaching)
         {
         }
     }
