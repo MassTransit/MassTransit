095c8a1f507b4e91808bf1a0a9234688b4c0e128
Jakub Januszkiewicz <jakub@januszkiewicz.net>
2021-06-21 11:29:22 +0200

    improve documentation of GetRetryAttempt and GetRetryCount
    Improve the documentation:
     - RetryContextExtensions.GetRetryAttempt - made it clear that it returns the number of the current attempt
     - RetryContextExtensions.GetRetryCount - corrected the incorrect description of when it returns zero

--- src/MassTransit/RetryContextExtensions.cs
+++ src/MassTransit/RetryContextExtensions.cs
@@ -6,8 +6,8 @@
     public static class RetryContextExtensions
     {
         /// <summary>
-        /// If within a retry attempt, the return value is greater than zero and indicates the number of retry attempts
-        /// that have occurred.
+        /// If within a retry attempt, the return value is greater than zero and indicates the number of the retry attempt
+        /// in progress.
         /// </summary>
         /// <param name="context"></param>
         /// <returns>The retry attempt number, 0 = first time, >= 1 = retry</returns>
@@ -17,11 +17,10 @@
         }
 
         /// <summary>
-        /// If within a retry attempt, the return value is greater than zero and indicates the number of retry attempts
-        /// that have occurred.
+        /// If within a retry attempt, the return value indicates the number of retry attempts that have already occurred.
         /// </summary>
         /// <param name="context"></param>
-        /// <returns>The retry attempt number, 0 = first time, >= 1 = retry</returns>
+        /// <returns>The number of retries that have already been attempted, 0 = first time, 0 = first retry, >= 1 = subsequent retry</returns>
         public static int GetRetryCount(this ConsumeContext context)
         {
             return context.TryGetPayload(out ConsumeRetryContext retryContext) ? retryContext.RetryCount : 0;
