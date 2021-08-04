a33f9dbf2cae93922f2c3af452ca4bf8960c4832
Denys Kozhevnikov <noonamer@gmail.com>
2021-08-04 19:42:38 +0100

    Cancel producer flush if error occurred

--- src/Transports/MassTransit.KafkaIntegration/Contexts/KafkaProducerContext.cs
+++ src/Transports/MassTransit.KafkaIntegration/Contexts/KafkaProducerContext.cs
@@ -4,7 +4,9 @@ namespace MassTransit.KafkaIntegration.Contexts
     using System.Threading;
     using System.Threading.Tasks;
     using Confluent.Kafka;
+    using Context;
     using GreenPipes;
+    using Logging;
     using Serializers;
     using Transport;
 
@@ -14,12 +16,19 @@ namespace MassTransit.KafkaIntegration.Contexts
         ProducerContext<TKey, TValue>
         where TValue : class
     {
+        readonly CancellationTokenSource _cancellationTokenSource;
+        readonly ILogContext _logContext;
         readonly IProducer<TKey, TValue> _producer;
 
-        public KafkaProducerContext(ProducerBuilder<TKey, TValue> producerBuilder, IHeadersSerializer headersSerializer, CancellationToken cancellationToken)
+        public KafkaProducerContext(ProducerBuilder<TKey, TValue> producerBuilder, IHeadersSerializer headersSerializer, ILogContext logContext,
+            CancellationToken cancellationToken)
             : base(cancellationToken)
         {
-            _producer = producerBuilder.Build();
+            _logContext = logContext;
+            _producer = producerBuilder
+                .SetErrorHandler(OnError)
+                .Build();
+            _cancellationTokenSource = new CancellationTokenSource();
             HeadersSerializer = headersSerializer;
         }
 
@@ -32,10 +41,34 @@ namespace MassTransit.KafkaIntegration.Contexts
 
         public void Dispose()
         {
-            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
-            _producer.Flush(cts.Token);
+            var timeoutTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+            var cts = CancellationTokenSource.CreateLinkedTokenSource(timeoutTokenSource.Token, _cancellationTokenSource.Token);
+            try
+            {
+                _producer.Flush(cts.Token);
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            catch (Exception e)
+            {
+                _logContext.Error?.Log(e, "Failed to dispose producer: {producerName}", _producer.Name);
+            }
+            finally
+            {
+                timeoutTokenSource.Dispose();
+                _cancellationTokenSource.Dispose();
+                _producer.Dispose();
+            }
+        }
 
-            _producer.Dispose();
+        void OnError(IProducer<TKey, TValue> producer, Error error)
+        {
+            EnabledLogger? logger = error.IsFatal ? _logContext.Error : _logContext.Warning;
+            logger?.Log("Error ({code}): {reason} on producer: {producerName}", error.Code, error.Reason, producer.Name);
+
+            if (!_cancellationTokenSource.IsCancellationRequested && error.IsLocalError)
+                _cancellationTokenSource.Cancel();
         }
     }
 }
