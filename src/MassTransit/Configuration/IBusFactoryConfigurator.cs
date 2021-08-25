497a10146a94a4db105fae6d1a5c7ed52632ec88
Michael Aldworth <aldworth@ocas.ca>
2021-08-25 09:34:17 -0400

    Fix XML Comment Tag

--- src/MassTransit/Configuration/IBusFactoryConfigurator.cs
+++ src/MassTransit/Configuration/IBusFactoryConfigurator.cs
@@ -45,7 +45,8 @@ namespace MassTransit
         /// Specify the number of concurrent messages that can be consumed (separate from prefetch count)
         /// </summary>
         int? ConcurrentMessageLimit { set; }
-		
+
+        /// <summary>
 		/// Set to true if you want to use the relational db outbox
         /// </summary>
         bool UseOutboxTransport { set; }
