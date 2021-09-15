d11e8dd07232c3bb163a3ed20b80c969b4dfa3b6
AhmedKhalil777 <progeng_ahmed_khalil@outlook.com>
2021-09-15 16:50:32 +0200

    Add WIP

---
+++ src/Tests/SendActiveMQTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using MassTransit.ActiveMqTransport;
+using MassTransit.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SendActiveMQTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var _bus = Bus.Factory.CreateUsingActiveMq(config =>
+            {
+                config.Host("localhost", 61616, h =>
+                {
+                    h.Username("admin");
+                    h.Password("admin");
+                });
+                config.ClearMessageDeserializers();
+                config.UseRawXmlSerialzier(RawXmlSerializerOptions.Default);
+
+            });
+            EndpointConvention.Map<LoadProfile>(new Uri($"queue:TestQueue"));
+         
+            await _bus.Send(new LoadProfile { Message ="Hello"}, new Dictionary<string, string> { ["Type"] = "Text" });
+        }
+    }
+
+    public class LoadProfile{
+        public string Message { get; set; }
+    }
+}
