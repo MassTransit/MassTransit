ac2f633f220ea6edc0590be3b76b3c6babe9d8da
Chris Patterson <chris@phatboyg.com>
2021-09-26 18:50:52 -0500

    Added UsePartitioner methods for consumer/saga pipelines with shared partitions across all message types #2855

--- src/MassTransit/Configuration/MassTransitPartitionerConfigurationExtensions.cs
+++ src/MassTransit/Configuration/MassTransitPartitionerConfigurationExtensions.cs
@@ -1,7 +1,10 @@
 namespace GreenPipes
 {
     using System;
+    using System.Text;
     using MassTransit;
+    using MassTransit.PipeConfigurators;
+    using MassTransit.Saga;
     using Partitioning;
     using Specifications;
 
@@ -32,6 +35,140 @@ namespace GreenPipes
             }
 
             var specification = new PartitionerPipeSpecification<ConsumeContext<T>>(Provider, partitioner);
+
+            configurator.AddPipeSpecification(specification);
+        }
+
+        /// <summary>
+        /// Specify a concurrency limit for tasks executing through the filter. No more than the specified
+        /// number of tasks will be allowed to execute concurrently.
+        /// </summary>
+        /// <typeparam name="TConsumer"></typeparam>
+        /// <param name="configurator"></param>
+        /// <param name="partitionCount">The number of partitions to use when distributing message delivery</param>
+        /// <param name="keyProvider">Provides the key from the message</param>
+        public static void UsePartitioner<TConsumer>(this IPipeConfigurator<ConsumerConsumeContext<TConsumer>> configurator, int partitionCount,
+            Func<ConsumerConsumeContext<TConsumer>, Guid> keyProvider)
+            where TConsumer : class
+        {
+            if (configurator == null)
+                throw new ArgumentNullException(nameof(configurator));
+            if (keyProvider == null)
+                throw new ArgumentNullException(nameof(keyProvider));
+
+            byte[] PartitionKeyProvider(ConsumerConsumeContext<TConsumer> context)
+            {
+                return keyProvider(context).ToByteArray();
+            }
+
+            UseConsumerPartitioner(configurator, partitionCount, PartitionKeyProvider);
+        }
+
+        /// <summary>
+        /// Specify a concurrency limit for tasks executing through the filter. No more than the specified
+        /// number of tasks will be allowed to execute concurrently.
+        /// </summary>
+        /// <typeparam name="TConsumer"></typeparam>
+        /// <param name="configurator"></param>
+        /// <param name="partitionCount">The number of partitions to use when distributing message delivery</param>
+        /// <param name="keyProvider">Provides the key from the message</param>
+        /// <param name="encoding"></param>
+        public static void UsePartitioner<TConsumer>(this IPipeConfigurator<ConsumerConsumeContext<TConsumer>> configurator, int partitionCount,
+            Func<ConsumerConsumeContext<TConsumer>, string> keyProvider, Encoding encoding = null)
+            where TConsumer : class
+        {
+            if (configurator == null)
+                throw new ArgumentNullException(nameof(configurator));
+            if (keyProvider == null)
+                throw new ArgumentNullException(nameof(keyProvider));
+
+            var textEncoding = encoding ?? Encoding.UTF8;
+
+            byte[] PartitionKeyProvider(ConsumerConsumeContext<TConsumer> context)
+            {
+                var key = keyProvider(context);
+                return key == null
+                    ? Array.Empty<byte>()
+                    : textEncoding.GetBytes(key);
+            }
+
+            UseConsumerPartitioner(configurator, partitionCount, PartitionKeyProvider);
+        }
+
+        static void UseConsumerPartitioner<TConsumer>(IPipeConfigurator<ConsumerConsumeContext<TConsumer>> configurator, int partitionCount,
+            PartitionKeyProvider<ConsumerConsumeContext<TConsumer>> keyProvider)
+            where TConsumer : class
+        {
+            var partitioner = new Partitioner(partitionCount, new Murmur3UnsafeHashGenerator());
+
+            var specification = new PartitionConsumerSpecification<TConsumer>(partitioner, keyProvider);
+
+            configurator.AddPipeSpecification(specification);
+        }
+
+        /// <summary>
+        /// Specify a concurrency limit for tasks executing through the filter. No more than the specified
+        /// number of tasks will be allowed to execute concurrently.
+        /// </summary>
+        /// <typeparam name="TSaga"></typeparam>
+        /// <param name="configurator"></param>
+        /// <param name="partitionCount">The number of partitions to use when distributing message delivery</param>
+        /// <param name="keyProvider">Provides the key from the message</param>
+        public static void UsePartitioner<TSaga>(this IPipeConfigurator<SagaConsumeContext<TSaga>> configurator, int partitionCount,
+            Func<SagaConsumeContext<TSaga>, Guid> keyProvider)
+            where TSaga : class, ISaga
+        {
+            if (configurator == null)
+                throw new ArgumentNullException(nameof(configurator));
+            if (keyProvider == null)
+                throw new ArgumentNullException(nameof(keyProvider));
+
+            byte[] PartitionKeyProvider(SagaConsumeContext<TSaga> context)
+            {
+                return keyProvider(context).ToByteArray();
+            }
+
+            UseSagaPartitioner(configurator, partitionCount, PartitionKeyProvider);
+        }
+
+        /// <summary>
+        /// Specify a concurrency limit for tasks executing through the filter. No more than the specified
+        /// number of tasks will be allowed to execute concurrently.
+        /// </summary>
+        /// <typeparam name="TSaga"></typeparam>
+        /// <param name="configurator"></param>
+        /// <param name="partitionCount">The number of partitions to use when distributing message delivery</param>
+        /// <param name="keyProvider">Provides the key from the message</param>
+        /// <param name="encoding"></param>
+        public static void UsePartitioner<TSaga>(this IPipeConfigurator<SagaConsumeContext<TSaga>> configurator, int partitionCount,
+            Func<SagaConsumeContext<TSaga>, string> keyProvider, Encoding encoding = null)
+            where TSaga : class, ISaga
+        {
+            if (configurator == null)
+                throw new ArgumentNullException(nameof(configurator));
+            if (keyProvider == null)
+                throw new ArgumentNullException(nameof(keyProvider));
+
+            var textEncoding = encoding ?? Encoding.UTF8;
+
+            byte[] PartitionKeyProvider(SagaConsumeContext<TSaga> context)
+            {
+                var key = keyProvider(context);
+                return key == null
+                    ? Array.Empty<byte>()
+                    : textEncoding.GetBytes(key);
+            }
+
+            UseSagaPartitioner(configurator, partitionCount, PartitionKeyProvider);
+        }
+
+        static void UseSagaPartitioner<TSaga>(IPipeConfigurator<SagaConsumeContext<TSaga>> configurator, int partitionCount,
+            PartitionKeyProvider<SagaConsumeContext<TSaga>> keyProvider)
+            where TSaga : class, ISaga
+        {
+            var partitioner = new Partitioner(partitionCount, new Murmur3UnsafeHashGenerator());
+
+            var specification = new PartitionSagaSpecification<TSaga>(partitioner, keyProvider);
 
             configurator.AddPipeSpecification(specification);
         }
---
+++ src/MassTransit/Configuration/PipeConfigurators/PartitionConsumerSpecification.cs
@@ -0,0 +1,39 @@
+namespace MassTransit.PipeConfigurators
+{
+    using System;
+    using System.Collections.Generic;
+    using GreenPipes;
+    using GreenPipes.Filters;
+    using GreenPipes.Partitioning;
+
+
+    public class PartitionConsumerSpecification<TConsumer> :
+        IPipeSpecification<ConsumerConsumeContext<TConsumer>>
+        where TConsumer : class
+    {
+        readonly PartitionKeyProvider<ConsumerConsumeContext<TConsumer>> _keyProvider;
+        readonly IPartitioner _partitioner;
+
+        public PartitionConsumerSpecification(IPartitioner partitioner, PartitionKeyProvider<ConsumerConsumeContext<TConsumer>> keyProvider)
+        {
+            if (partitioner == null)
+                throw new ArgumentNullException(nameof(partitioner));
+            if (keyProvider == null)
+                throw new ArgumentNullException(nameof(keyProvider));
+
+            _partitioner = partitioner;
+            _keyProvider = keyProvider;
+        }
+
+        public void Apply(IPipeBuilder<ConsumerConsumeContext<TConsumer>> builder)
+        {
+            builder.AddFilter(new PartitionFilter<ConsumerConsumeContext<TConsumer>>(_keyProvider, _partitioner));
+        }
+
+        public IEnumerable<ValidationResult> Validate()
+        {
+            if (_keyProvider == null)
+                yield return this.Failure("KeyProvider", "must not be null");
+        }
+    }
+}
