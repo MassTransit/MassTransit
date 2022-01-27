209feed463e82161a6f4ed7a42e77253d47d2d41
Anthony <anthony@boutonanthony.be>
2022-01-27 19:09:00 +0100

    add the ability to run the tests from anywhere

--- tests/MassTransit.DynamoDb.Tests/SagaPersistenceTests.cs
+++ tests/MassTransit.DynamoDb.Tests/SagaPersistenceTests.cs
@@ -1,9 +1,12 @@
 ﻿namespace MassTransit.DynamoDb.Tests
 {
     using System;
+    using System.Collections.Generic;
+    using System.Threading;
     using System.Threading.Tasks;
     using Amazon.DynamoDBv2;
     using Amazon.DynamoDBv2.DataModel;
+    using Amazon.DynamoDBv2.Model;
     using NUnit.Framework;
     using Saga;
     using Shouldly;
@@ -17,11 +20,64 @@
     {
         readonly ISagaRepository<SimpleSaga> _sagaRepository;
 
+        [SetUp]
+        public async Task CreateTableIfNotExists()
+        {
+            try
+            {
+                await DynamoDbClient.DescribeTableAsync(nameof(LocatingAnExistingSaga), CancellationToken.None);
+                await ClearTable();
+            }
+            catch (ResourceNotFoundException e)
+            {
+                await DynamoDbClient.CreateTableAsync(new CreateTableRequest
+                {
+                    BillingMode = BillingMode.PAY_PER_REQUEST,
+                    KeySchema = new List<KeySchemaElement>
+                    {
+                        new KeySchemaElement("PK", KeyType.HASH),
+                        new KeySchemaElement("SK", KeyType.RANGE)
+                    },
+                    TableName = nameof(LocatingAnExistingSaga),
+                    AttributeDefinitions = new List<AttributeDefinition>
+                    {
+                        new AttributeDefinition("PK", ScalarAttributeType.S),
+                        new AttributeDefinition("SK", ScalarAttributeType.S)
+                    }
+                });
+            }
+        }
+
+        [TearDown]
+        public async Task ClearTable()
+        {
+            var response = await DynamoDbClient.ScanAsync(nameof(LocatingAnExistingSaga), new List<string>
+            {
+                "PK",
+                "SK"
+            }, CancellationToken.None);
+
+            foreach (var item in response.Items)
+            {
+                await DynamoDbClient.DeleteItemAsync(nameof(LocatingAnExistingSaga),
+                    new Dictionary<string, AttributeValue>
+                    {
+                        {"PK", new AttributeValue(item["PK"].S)},
+                        {"SK", new AttributeValue(item["SK"].S)}
+                    });
+            }
+        }
+
         public LocatingAnExistingSaga()
         {
-            var dynamoDb = new AmazonDynamoDBClient(new AmazonDynamoDBConfig {ServiceURL = "http://localhost:8000"});
-            _sagaRepository = DynamoDbSagaRepository<SimpleSaga>.Create(() => new DynamoDBContext(dynamoDb), "Mass", TimeSpan.FromDays(1));
+            DynamoDbClient = new AmazonDynamoDBClient(new AmazonDynamoDBConfig {ServiceURL = "http://localhost:8000"});
+            DynamoDbContext = new DynamoDBContext(DynamoDbClient);
+            _sagaRepository = DynamoDbSagaRepository<SimpleSaga>.Create(() => DynamoDbContext, nameof(LocatingAnExistingSaga), TimeSpan.FromDays(1));
         }
+
+        public DynamoDBContext DynamoDbContext { get; }
+
+        public AmazonDynamoDBClient DynamoDbClient { get; }
 
         [Test]
         public async Task A_correlated_message_should_find_the_correct_saga()
