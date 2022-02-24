0996798d16c5ff50870b692b4393d7be607b1e59
chris van de steeg <chris@careconnections.nl>
2022-02-24 13:39:17 +0100

    Fixues issue #3188

--- src/MassTransit/Transactions/TransactionalBusSendEndpoint.cs
+++ src/MassTransit/Transactions/TransactionalBusSendEndpoint.cs
@@ -25,67 +25,57 @@
         public Task Send<T>(T message, CancellationToken cancellationToken = default)
             where T : class
         {
-            _outboxBus.Add(() => _sendEndpoint.Send(message, cancellationToken));
-            return Task.CompletedTask;
+            return _outboxBus.Add(() => _sendEndpoint.Send(message, cancellationToken));
         }
 
         public Task Send<T>(T message, IPipe<SendContext<T>> pipe, CancellationToken cancellationToken = default)
             where T : class
         {
-            _outboxBus.Add(() => _sendEndpoint.Send(message, pipe, cancellationToken));
-            return Task.CompletedTask;
+            return _outboxBus.Add(() => _sendEndpoint.Send(message, pipe, cancellationToken));
         }
 
         public Task Send<T>(T message, IPipe<SendContext> pipe, CancellationToken cancellationToken = default)
             where T : class
         {
-            _outboxBus.Add(() => _sendEndpoint.Send(message, pipe, cancellationToken));
-            return Task.CompletedTask;
+            return _outboxBus.Add(() => _sendEndpoint.Send(message, pipe, cancellationToken));
         }
 
         public Task Send(object message, CancellationToken cancellationToken = default)
         {
-            _outboxBus.Add(() => _sendEndpoint.Send(message, cancellationToken));
-            return Task.CompletedTask;
+            return _outboxBus.Add(() => _sendEndpoint.Send(message, cancellationToken));
         }
 
         public Task Send(object message, Type messageType, CancellationToken cancellationToken = default)
         {
-            _outboxBus.Add(() => _sendEndpoint.Send(message, messageType, cancellationToken));
-            return Task.CompletedTask;
+            return _outboxBus.Add(() => _sendEndpoint.Send(message, messageType, cancellationToken));
         }
 
         public Task Send(object message, IPipe<SendContext> pipe, CancellationToken cancellationToken = default)
         {
-            _outboxBus.Add(() => _sendEndpoint.Send(message, pipe, cancellationToken));
-            return Task.CompletedTask;
+            return _outboxBus.Add(() => _sendEndpoint.Send(message, pipe, cancellationToken));
         }
 
         public Task Send(object message, Type messageType, IPipe<SendContext> pipe, CancellationToken cancellationToken = default)
         {
-            _outboxBus.Add(() => _sendEndpoint.Send(message, messageType, pipe, cancellationToken));
-            return Task.CompletedTask;
+            return _outboxBus.Add(() => _sendEndpoint.Send(message, messageType, pipe, cancellationToken));
         }
 
         public Task Send<T>(object values, CancellationToken cancellationToken = default)
             where T : class
         {
-            _outboxBus.Add(() => _sendEndpoint.Send<T>(values, cancellationToken));
-            return Task.CompletedTask;
+            return _outboxBus.Add(() => _sendEndpoint.Send<T>(values, cancellationToken));
         }
 
         public Task Send<T>(object values, IPipe<SendContext<T>> pipe, CancellationToken cancellationToken = default)
             where T : class
         {
-            _outboxBus.Add(() => _sendEndpoint.Send(values, pipe, cancellationToken));
-            return Task.CompletedTask;
+            return _outboxBus.Add(() => _sendEndpoint.Send(values, pipe, cancellationToken));
         }
 
         public Task Send<T>(object values, IPipe<SendContext> pipe, CancellationToken cancellationToken = default)
             where T : class
         {
-            _outboxBus.Add(() => _sendEndpoint.Send<T>(values, pipe, cancellationToken));
-            return Task.CompletedTask;
+            return _outboxBus.Add(() => _sendEndpoint.Send<T>(values, pipe, cancellationToken));
         }
     }
 }
