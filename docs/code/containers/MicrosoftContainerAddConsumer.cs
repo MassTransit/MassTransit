c61d25ce7e7b45cb451992123ff41c5ed5e617e4
Dru Sellers <dru@drusellers.com>
2022-03-24 13:19:44 -0500

    Azure Docs

--- docs/code/containers/MicrosoftContainerAddConsumer.cs
+++ docs/code/containers/MicrosoftContainerAddConsumer.cs
@@ -5,27 +5,32 @@ namespace MicrosoftContainerAddConsumer
     using ContainerConsumers;
     using MassTransit;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
 
     public class Program
     {
-        public static async Task Main()
+        public static async Task Main(string[] args)
         {
-            var services = new ServiceCollection();
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddMassTransit(x =>
+                    {
+                        // Add a single consumer
+                        x.AddConsumer<SubmitOrderConsumer>(typeof(SubmitOrderConsumerDefinition));
 
-            services.AddMassTransit(x =>
-            {
-                // Add a single consumer
-                x.AddConsumer<SubmitOrderConsumer>(typeof(SubmitOrderConsumerDefinition));
+                        // Add a single consumer by type
+                        x.AddConsumer(typeof(SubmitOrderConsumer), typeof(SubmitOrderConsumerDefinition));
 
-                // Add a single consumer by type
-                x.AddConsumer(typeof(SubmitOrderConsumer), typeof(SubmitOrderConsumerDefinition));
+                        // Add all consumers in the specified assembly
+                        x.AddConsumers(typeof(SubmitOrderConsumer).Assembly);
 
-                // Add all consumers in the specified assembly
-                x.AddConsumers(typeof(SubmitOrderConsumer).Assembly);
-
-                // Add all consumers in the namespace containing the specified type
-                x.AddConsumersFromNamespaceContaining<SubmitOrderConsumer>();
-            });
+                        // Add all consumers in the namespace containing the specified type
+                        x.AddConsumersFromNamespaceContaining<SubmitOrderConsumer>();
+                    });
+                })
+                .Build()
+                .RunAsync();
         }
     }
 }
---
+++ docs/usage/faas/README.md
@@ -0,0 +1,3 @@
+# Functions as a Service
+
+MassTransit can also be run with FaaS models like [Azure Functions](/usage/faas/azure-functions) and [AWS Lambda](/usage/faas/aws-lambda)
