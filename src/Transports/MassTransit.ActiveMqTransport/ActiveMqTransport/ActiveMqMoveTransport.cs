cdabe04ff475060618c2f1dc8e24c9c6c0c0373d
Chris Patterson <chris@phatboyg.com>
2022-06-22 09:31:43 -0500

    Fixed #3448 - Unsupported message types will be moved to the error queue properly

--- src/Transports/MassTransit.ActiveMqTransport/ActiveMqTransport/ActiveMqMoveTransport.cs
+++ src/Transports/MassTransit.ActiveMqTransport/ActiveMqTransport/ActiveMqMoveTransport.cs
@@ -21,31 +21,43 @@
             if (!context.TryGetPayload(out SessionContext sessionContext))
                 throw new ArgumentException("The ReceiveContext must contain a SessionContext", nameof(context));
 
+            if (!context.TryGetPayload(out ActiveMqMessageContext messageContext))
+                throw new ArgumentException("The ActiveMqMessageContext was not present", nameof(context));
+
             await _topologyFilter.Send(sessionContext, Pipe.Empty<SessionContext>()).ConfigureAwait(false);
 
             var queue = await sessionContext.GetQueue(_destination).ConfigureAwait(false);
 
             var producer = await sessionContext.CreateMessageProducer(queue).ConfigureAwait(false);
-            var body = context.GetBody();
 
-            var message = producer.CreateBytesMessage();
-
-            if (context.TryGetPayload(out ActiveMqMessageContext messageContext))
+            var message = messageContext.TransportMessage switch
             {
-                foreach (string key in messageContext.Properties.Keys)
-                    message.Properties[key] = messageContext.Properties[key];
-            }
+                IBytesMessage _ => producer.CreateBytesMessage(context.Body.GetBytes()),
+                ITextMessage _ => producer.CreateTextMessage(context.Body.GetString()),
+                _ => producer.CreateMessage(),
+            };
+
+            CloneMessage(message, messageContext.TransportMessage, preSend);
+
+            var task = Task.Run(() => producer.Send(message));
+            context.AddReceiveTask(task);
+        }
+
+        static void CloneMessage(IMessage message, IMessage source, Action<IMessage, SendHeaders> preSend)
+        {
+            message.NMSReplyTo = source.NMSReplyTo;
+            message.NMSDeliveryMode = source.NMSDeliveryMode;
+            message.NMSCorrelationID = source.NMSCorrelationID;
+            message.NMSPriority = source.NMSPriority;
+
+            foreach (string key in source.Properties.Keys)
+                message.Properties[key] = source.Properties[key];
 
             SendHeaders headers = new PrimitiveMapHeaders(message.Properties);
 
             headers.SetHostHeaders();
 
             preSend(message, headers);
-
-            message.Content = body;
-
-            var task = Task.Run(() => producer.Send(message));
-            context.AddReceiveTask(task);
         }
     }
 }
---
+++ tests/MassTransit.ActiveMqTransport.Tests/InvalidMessage_Specs.cs
@@ -0,0 +1,63 @@
+namespace MassTransit.ActiveMqTransport.Tests
+{
+    using System.Threading.Tasks;
+    using Apache.NMS;
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class InvalidMessage_Specs :
+        ActiveMqTestFixture
+    {
+        [Test]
+        public async Task Should_fault()
+        {
+            Task<ConsumeContext<ReceiveFault>> receiveFault = await ConnectPublishHandler<ReceiveFault>();
+
+            await ProduceInvalidMessage();
+
+            await receiveFault;
+        }
+
+        protected override void ConfigureActiveMqReceiveEndpoint(IActiveMqReceiveEndpointConfigurator configurator)
+        {
+            Handled<SubmitOrder>(configurator);
+        }
+
+        async Task ProduceInvalidMessage()
+        {
+            var options = new ActiveMqTransportOptions();
+
+            var brokerAddress = $"activemq:tcp://{options.Host}:{options.Port}";
+
+            var factory = new NMSConnectionFactory(brokerAddress);
+
+            var connection = factory.ConnectionFactory.CreateConnection(options.User, options.Pass);
+            try
+            {
+                var session = connection.CreateSession(AcknowledgementMode.ClientAcknowledge);
+
+                var producer = session.CreateProducer(session.GetQueue(ActiveMqTestHarness.InputQueueName));
+
+                var message = session.CreateMessage();
+
+                message.NMSCorrelationID = "AB76E632-8550-49B9-A119-BBEB84D53355";
+
+                producer.Send(message);
+
+                producer.Close();
+                session.Close();
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+
+        class SubmitOrder
+        {
+            public string OrderId { get; set; }
+        }
+    }
+}
