1060802d168dd6732eef2ae8d46578d0728a0f1e
Daniel Reis <danreis.spark@gmail.com>
2022-07-29 14:01:39 -0300

    Add method to record activity exceptions

--- src/MassTransit/Logging/StartedActivity.cs
+++ src/MassTransit/Logging/StartedActivity.cs
@@ -1,6 +1,7 @@
 #nullable enable
 namespace MassTransit.Logging
 {
+    using System;
     using System.Diagnostics;
 
 
@@ -26,6 +27,22 @@ namespace MassTransit.Logging
         {
             if (context.BodyLength.HasValue)
                 AddTag(DiagnosticHeaders.Messaging.BodyLength, context.BodyLength.Value.ToString());
+        }
+
+        public void RecordException(Exception exception, bool escaped)
+        {
+            var tags = new ActivityTagsCollection
+            {
+                { DiagnosticHeaders.Exceptions.Escaped, escaped },
+                { DiagnosticHeaders.Exceptions.Message, exception.Message },
+                { DiagnosticHeaders.Exceptions.Type, exception.GetType().Name },
+                { DiagnosticHeaders.Exceptions.Stacktrace, exception.ToString() }
+            };
+
+            var activityEvent = new ActivityEvent(DiagnosticHeaders.Exceptions.EventName, DateTimeOffset.UtcNow, tags);
+
+            Activity.AddEvent(activityEvent);
+            Activity.SetStatus(ActivityStatusCode.Error);
         }
 
         public void Stop()
