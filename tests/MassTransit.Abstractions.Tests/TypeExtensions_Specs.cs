b10a0a12bb937def85232964d1042470895b1ff6
Ard Kuijpers <ardkuijpers@mp.nl>
2022-08-23 21:55:52 +0200

    Fixed bug when inspecting the properties of a type with multiple this operators

---
+++ tests/MassTransit.Abstractions.Tests/TypeExtensions_Specs.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace MassTransit.Abstractions.Tests
+{
+    [TestFixture]
+    public class Using_the_TypeExtensions
+    {
+        [Test]
+        public void Should_accept_multiple_this_operators()
+        {
+            var typeInfo = typeof(TypeWithMultipleThisOperators).GetTypeInfo();
+            Assert.That(() => Internals.TypeExtensions.GetAllProperties(typeInfo).ToList(), Throws.Nothing);
+        }
+    }
+
+    internal class TypeWithMultipleThisOperators
+    {
+        public string FirstValue { get; } = "aValue";
+        public string SecondValue { get; } = "anotherValue";
+
+        public string this[int index] => index switch
+        {
+            0 => FirstValue,
+            1 => SecondValue,
+            _ => throw new ArgumentOutOfRangeException(nameof(index))
+        };
+
+        public string this[string name] => name.ToLower() switch
+        {
+            "first" => FirstValue,
+            "second" => SecondValue,
+            _ => throw new ArgumentOutOfRangeException(nameof(name))
+        };
+
+
+    }
+}
