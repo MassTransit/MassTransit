ea6c1303a52fab41f83f8c4700dc2d4b7892c13c
Chris Patterson <chris@phatboyg.com>
2022-10-07 15:44:00 -0500

    Added MediatorRequestHandler base types for created mediator-specific Request<T> handlers

---
+++ src/MassTransit.Abstractions/Mediator/MediatorRequestHandler.cs
@@ -0,0 +1,45 @@
+namespace MassTransit.Mediator
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+
+    /// <summary>
+    /// A Mediator request handler base class, that provides a simplified overridable method with
+    /// a Task (void) return type
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    public abstract class MediatorRequestHandler<TRequest> :
+        IConsumer<TRequest>
+        where TRequest : class
+    {
+        public Task Consume(ConsumeContext<TRequest> context)
+        {
+            return Handle(context.Message, context.CancellationToken);
+        }
+
+        protected abstract Task Handle(TRequest request, CancellationToken cancellationToken);
+    }
+
+
+    /// <summary>
+    /// A Mediator request handler base class, that provides a simplified overridable method with
+    /// a Task&lt;typeparamref name="TResponse"/>&gt; return type
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    public abstract class MediatorRequestHandler<TRequest, TResponse> :
+        IConsumer<TRequest>
+        where TRequest : class, Request<TResponse>
+        where TResponse : class
+    {
+        public async Task Consume(ConsumeContext<TRequest> context)
+        {
+            var response = await Handle(context.Message, context.CancellationToken).ConfigureAwait(false);
+
+            await context.RespondAsync(response).ConfigureAwait(false);
+        }
+
+        protected abstract Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
+    }
+}
