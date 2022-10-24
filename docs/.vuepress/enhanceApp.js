2b38474b532877fcfacd6321b535c69f1fc02840
Chris Patterson <chris@phatboyg.com>
2022-10-24 09:13:20 -0500

    Added resolver to deal with production redirection for VuePress

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,5 +1,14 @@
 export default ({ router }) => {
   router.addRoutes([
     { path: '/support', redirect: '/learn/support.html' }
-  ])
+  ]);
+
+  router.beforeResolve((to, _from, next) => {
+    const browserWindow = typeof window !== "undefined" ? window : null;
+    if (browserWindow && to.matched.length && to.matched[0].path !== '*' && to.redirectedFrom) {
+      browserWindow.location.href = to.fullPath;
+    } else {
+      next();
+    }
+  });
 }
