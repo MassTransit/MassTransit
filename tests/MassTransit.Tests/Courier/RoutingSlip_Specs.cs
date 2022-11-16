6955be6798bab10faa38c7c108b580878e3c9315
ushenkodmitry <ushenko.dmitry@gmail.com>
2022-11-16 17:11:38 +0300

    Failing unit test for dictionary variable

--- tests/MassTransit.Tests/Courier/RoutingSlip_Specs.cs
+++ tests/MassTransit.Tests/Courier/RoutingSlip_Specs.cs
@@ -1,6 +1,7 @@
 ﻿namespace MassTransit.Tests.Courier
 {
     using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using MassTransit.Courier.Contracts;
     using NUnit.Framework;
@@ -20,6 +21,24 @@
             await InputQueueSendEndpoint.Send(builder.Build());
 
             await _received;
+        }
+
+        [Test]
+        public async Task Should_be_properly_serialized_as_a_message_with_dictionary_variable()
+        {
+            var result = new Dictionary<Guid, string> { [Guid.Empty] = null };
+
+            var builder = new RoutingSlipBuilder(NewId.NextGuid());
+            builder.AddVariable("Result", result);
+            builder.AddActivity("test", new Uri("loopback://localhost/execute_testactivity"), new { });
+
+            await InputQueueSendEndpoint.Send(builder.Build());
+
+            var received = await _received;
+
+            var result1 = (Dictionary<string, object>)received.Message.Variables["Result"];
+
+            Assert.IsNotEmpty(result1);
         }
 
         Task<ConsumeContext<RoutingSlip>> _received;
