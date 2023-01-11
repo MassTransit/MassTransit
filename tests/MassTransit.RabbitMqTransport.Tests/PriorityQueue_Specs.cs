a5fc2f864ff611824184195e1c5954e4dda87899
Chris Patterson <chris@phatboyg.com>
2023-01-11 06:52:57 -0600

    Fixed #4002 - values converted to long when stored couldn't be loaded as byte, added common numeric type loader (since they're all longs, apparently)

--- tests/MassTransit.RabbitMqTransport.Tests/PriorityQueue_Specs.cs
+++ tests/MassTransit.RabbitMqTransport.Tests/PriorityQueue_Specs.cs
@@ -1,7 +1,10 @@
 ﻿namespace MassTransit.RabbitMqTransport.Tests
 {
     using System;
+    using System.Configuration;
     using System.Threading.Tasks;
+    using MassTransit.Testing;
+    using Microsoft.Extensions.DependencyInjection;
     using NUnit.Framework;
     using RabbitMQ.Client;
     using TestFramework.Messages;
@@ -14,10 +17,11 @@
         [Test]
         public async Task Should_allow_priority_to_be_specified()
         {
-            var response = await Bus.Request<PingMessage, PongMessage>(_endpointAddress, new PingMessage(), TestCancellationToken, TestTimeout, x =>
-            {
-                x.SetPriority(2);
-            });
+            Response<PongMessage> response = await Bus.Request<PingMessage, PongMessage>(_endpointAddress, new PingMessage(), TestCancellationToken,
+                TestTimeout, x =>
+                {
+                    x.SetPriority(2);
+                });
 
             Assert.That(response.Headers.Get<string>("Received-Priority"), Is.EqualTo("2"));
         }
@@ -48,6 +52,96 @@
         {
             model.ExchangeDelete("priority_input_queue");
             model.QueueDelete("priority_input_queue");
+        }
+    }
+
+
+    [TestFixture]
+    public class Using_priority_queues_with_outbox
+    {
+        [Test]
+        public async Task Should_retain_the_priority()
+        {
+            await using var provider = new ServiceCollection()
+                .ConfigureRabbitMqTestOptions(options =>
+                {
+                    options.CleanVirtualHost = true;
+                    options.CreateVirtualHostIfNotExists = true;
+                })
+                .AddMassTransitTestHarness(x =>
+                {
+                    x.AddOptions<RabbitMqTransportOptions>()
+                        .Configure(options => options.VHost = "test");
+
+                    x.AddInMemoryInboxOutbox();
+
+                    x.AddHandler((BusMessage _) => Task.CompletedTask)
+                        .Endpoint(e => e.Name = "observer-priority");
+
+                    x.AddConsumer<MessageHandler>()
+                        .Endpoint(e => e.Name = "outbox-priority");
+
+                    x.SetTestTimeouts(testInactivityTimeout: TimeSpan.FromSeconds(10));
+
+                    x.AddConfigureEndpointsCallback((provider, name, cfg) =>
+                    {
+                        if (cfg is IRabbitMqReceiveEndpointConfigurator rmq)
+                            rmq.EnablePriority(5);
+
+                        cfg.UseInMemoryInboxOutbox(provider);
+                    });
+
+                    x.UsingRabbitMq((context, cfg) =>
+                    {
+                        cfg.ConfigureEndpoints(context);
+                    });
+                }).BuildServiceProvider();
+
+            var harness = provider.GetTestHarness();
+
+            await harness.Start();
+
+            var correlationId = NewId.NextGuid();
+            var conversationId = NewId.NextGuid();
+            var initiatorId = NewId.NextGuid();
+            var messageId = NewId.NextGuid();
+            await harness.Bus.Publish(new PingMessage(), Pipe.Execute<SendContext>(context =>
+            {
+                context.SetPriority(2);
+
+                context.CorrelationId = correlationId;
+                context.MessageId = messageId;
+                context.InitiatorId = initiatorId;
+                context.ConversationId = conversationId;
+            }), harness.CancellationToken);
+
+            IReceivedMessage<BusMessage> message = await harness.Consumed.SelectAsync<BusMessage>().FirstOrDefault();
+
+            Assert.That(message, Is.Not.Null);
+
+            Assert.That(message.Context.TryGetPayload<RabbitMqBasicConsumeContext>(out var rmqContext), Is.True);
+            Assert.That(rmqContext.Properties.Priority, Is.EqualTo(3));
+        }
+
+
+        class MessageHandler :
+            IConsumer<PingMessage>
+        {
+            public Task Consume(ConsumeContext<PingMessage> context)
+            {
+                return context.Publish(new BusMessage
+                {
+                    OriginalMessageId = context.MessageId,
+                    OriginalCorrelationId = context.CorrelationId
+                }, sendContext => sendContext.SetPriority(3));
+            }
+        }
+
+
+        class BusMessage
+        {
+            public Guid? OriginalMessageId { get; set; }
+            public Guid? OriginalCorrelationId { get; set; }
         }
     }
 }
