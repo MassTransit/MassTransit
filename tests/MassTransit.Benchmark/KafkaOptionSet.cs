864d694901f89b57ed235de843260ca93139b4ef
denys.kozhevnikov <github@mail.noonamer.com>
2023-02-14 10:21:46 +0000

    Simplify concurrency for riders. Improve benchmark

--- tests/MassTransit.Benchmark/KafkaOptionSet.cs
+++ tests/MassTransit.Benchmark/KafkaOptionSet.cs
@@ -11,19 +11,29 @@ public class KafkaOptionSet :
     {
         Add<string>("h|host:", "The host name of the broker", x => Host = x);
         Add<ushort>("pc|partitions:", "The number of partitions for the topic", x => PartitionCount = x);
+        Add<ushort>("kc|keys:", "The number of keys for the topic", x => KeysCount = x);
+        Add<ushort>("cc|consumers:", "The number of concurrent kafka consumers", x => ConcurrentConsumerLimit = x);
         Add<string>("t|topic:", "The name of the topic to use", x => Topic = x);
 
         Host = "localhost:9092";
         Topic = "benchmark-topic";
         PartitionCount = 2;
+        KeysCount = 20;
+        ConcurrentConsumerLimit = 1;
     }
 
     public string Host { get; set; }
     public string Topic { get; set; }
     public ushort PartitionCount { get; set; }
+    public ushort KeysCount { get; set; }
+    public ushort ConcurrentConsumerLimit { get; set; }
 
     public void ShowOptions()
     {
         Console.WriteLine("Host: {0}", Host);
+        Console.WriteLine("Topic: {0}", Topic);
+        Console.WriteLine("Partitions Count: {0}", PartitionCount);
+        Console.WriteLine("Keys Count: {0}", KeysCount);
+        Console.WriteLine("Concurrent Consumers: {0}", ConcurrentConsumerLimit);
     }
 }
--- tests/MassTransit.Benchmark/Latency/KafkaMessageLatencyTransport.cs
+++ tests/MassTransit.Benchmark/Latency/KafkaMessageLatencyTransport.cs
@@ -4,14 +4,22 @@ using System;
 using System.Threading.Tasks;
 using BusOutbox;
 using Confluent.Kafka;
+using Confluent.Kafka.Admin;
 using MassTransit;
+using MassTransit.KafkaIntegration;
+using MassTransit.Middleware;
 using Microsoft.Extensions.DependencyInjection;
 
 
 public class KafkaMessageLatencyTransport :
     IMessageLatencyTransport
 {
+    readonly IAdminClient _adminClient;
+    readonly ClientConfig _clientConfig;
+    readonly Murmur3UnsafeHashGenerator _hashGenerator;
+    readonly KafkaKeyResolver<int, LatencyTestMessage> _keyResolver;
     readonly KafkaOptionSet _options;
+    readonly IPipe<KafkaSendContext<LatencyTestMessage>> _partitionPipe;
     readonly IMessageLatencySettings _settings;
     ITopicProducer<LatencyTestMessage> _producer;
     ServiceProvider _provider;
@@ -21,15 +29,24 @@ public class KafkaMessageLatencyTransport :
     {
         _options = options;
         _settings = settings;
+        _partitionPipe = GetPartitionPipe();
+        _keyResolver = GetKeyResolver();
+        _hashGenerator = new Murmur3UnsafeHashGenerator();
+        _clientConfig = new ClientConfig { BootstrapServers = _options.Host };
+
+        _adminClient = new AdminClientBuilder(new AdminClientConfig(_clientConfig))
+            .Build();
     }
 
     public Task Send(LatencyTestMessage message)
     {
-        return _producer.Produce(message);
+        return _producer.Produce(message, _partitionPipe);
     }
 
     public async Task Start(Action<IReceiveEndpointConfigurator> callback, IReportConsumerMetric reportConsumerMetric)
     {
+        await CreateTopic();
+
         _provider = new ServiceCollection()
             .AddTextLogger(Console.Out)
             .AddSingleton(reportConsumerMetric)
@@ -41,20 +58,17 @@ public class KafkaMessageLatencyTransport :
                 {
                     r.AddConsumer<MessageLatencyConsumer>();
 
-                    r.AddProducer<LatencyTestMessage>(_options.Topic);
+                    r.AddProducer(_options.Topic, _keyResolver);
 
-                    r.UsingKafka((context, k) =>
+                    r.UsingKafka(_clientConfig, (context, k) =>
                     {
-                        k.Host("localhost:9092");
-
                         k.TopicEndpoint<LatencyTestMessage>(_options.Topic, nameof(KafkaMessageLatencyTransport), e =>
                         {
                             e.PrefetchCount = _settings.PrefetchCount;
                             e.AutoOffsetReset = AutoOffsetReset.Earliest;
-                            e.CreateIfMissing(p =>
-                            {
-                                p.NumPartitions = _options.PartitionCount;
-                            });
+
+                            if (_options.ConcurrentConsumerLimit > 0)
+                                e.ConcurrentConsumerLimit = _options.ConcurrentConsumerLimit;
 
                             if (_settings.ConcurrencyLimit > 0)
                                 e.ConcurrentMessageLimit = _settings.ConcurrencyLimit;
@@ -77,5 +91,72 @@ public class KafkaMessageLatencyTransport :
         await _scope.DisposeAsync();
 
         await _provider.StopHostedServices();
+
+        await DeleteTopic();
+
+        _adminClient.Dispose();
+    }
+
+    async Task DeleteTopic()
+    {
+        try
+        {
+            await _adminClient.DeleteTopicsAsync(new[] { _options.Topic });
+        }
+        catch (DeleteTopicsException)
+        {
+        }
+    }
+
+    async Task CreateTopic()
+    {
+        try
+        {
+            await _adminClient.CreateTopicsAsync(new[]
+            {
+                new TopicSpecification
+                {
+                    Name = _options.Topic,
+                    NumPartitions = _options.PartitionCount,
+                    ReplicationFactor = 1
+                }
+            });
+        }
+        catch (CreateTopicsException)
+        {
+        }
+
+        // Adding some delays for kafka to settle
+        await Task.Delay(TimeSpan.FromMilliseconds(100));
+    }
+
+    KafkaKeyResolver<int, LatencyTestMessage> GetKeyResolver()
+    {
+        if (_options.KeysCount > 1)
+        {
+            return context =>
+            {
+                var partitionKey = context.Message.CorrelationId.ToByteArray();
+                var hash = partitionKey?.Length > 0 ? _hashGenerator.Hash(partitionKey) : 0;
+                return (int)(hash % _options.KeysCount);
+            };
+        }
+
+        return _ => 1;
+    }
+
+    IPipe<KafkaSendContext<LatencyTestMessage>> GetPartitionPipe()
+    {
+        if (_options.PartitionCount > 1)
+        {
+            return Pipe.Execute<KafkaSendContext<LatencyTestMessage>>(ctx =>
+            {
+                var partitionKey = ctx.Message.CorrelationId.ToByteArray();
+                var hash = partitionKey?.Length > 0 ? _hashGenerator.Hash(partitionKey) : 0;
+                ctx.Partition = (int)(hash % _options.PartitionCount);
+            });
+        }
+
+        return Pipe.Empty<KafkaSendContext<LatencyTestMessage>>();
     }
 }
