08d308f7a9f137e32209b6750e736e5ed05e39ac
denys.kozhevnikov <github@mail.noonamer.com>
2023-02-27 20:32:52 +0000

    Add Test for Redelivery and ConsumerStopTimeout

--- tests/MassTransit.Containers.Tests/Stop_Specs.cs
+++ tests/MassTransit.Containers.Tests/Stop_Specs.cs
@@ -25,7 +25,7 @@ namespace MassTransit.Containers.Tests
                 configurator.AddConsumer<StuckConsumer>();
             });
 
-            var provider = collection.BuildServiceProvider(true);
+            await using var provider = collection.BuildServiceProvider(true);
 
             var harness = provider.GetTestHarness();
             await harness.Start();
@@ -55,6 +55,73 @@ namespace MassTransit.Containers.Tests
             {
                 _taskCompletionSource.TrySetResult(context);
                 return Task.Delay(TimeSpan.FromMinutes(10), context.CancellationToken);
+            }
+        }
+    }
+
+
+    [TestFixture]
+    public class Stop_Retry_Specs :
+        InMemoryTestFixture
+    {
+        [Test]
+        public async Task Should_respect_ConsumerStopTimeout()
+        {
+            var collection = new ServiceCollection();
+            collection.AddOptions<MassTransitHostOptions>().Configure(options => options.ConsumerStopTimeout = TimeSpan.FromSeconds(1));
+            collection.AddMassTransitTestHarness(configurator =>
+            {
+                configurator.AddTaskCompletionSource<ConsumeContext<PingMessage>>();
+                configurator.AddConsumer<StuckConsumer, StuckConsumerDefinition>();
+            });
+
+            await using var provider = collection.BuildServiceProvider(true);
+
+            var harness = provider.GetTestHarness();
+            await harness.Start();
+
+            await harness.Bus.Publish(new PingMessage(), harness.CancellationToken);
+
+            await provider.GetTask<ConsumeContext<PingMessage>>();
+
+            await harness.Stop(harness.CancellationToken);
+
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(3));
+            Assert.That(await harness.Consumed.Any<PingMessage>(x => x.Exception is OperationCanceledException, cts.Token), Is.True);
+        }
+
+
+        class StuckConsumer :
+            IConsumer<PingMessage>
+        {
+            readonly TaskCompletionSource<ConsumeContext<PingMessage>> _taskCompletionSource;
+
+            public StuckConsumer(TaskCompletionSource<ConsumeContext<PingMessage>> taskCompletionSource)
+            {
+                _taskCompletionSource = taskCompletionSource;
+            }
+
+            public Task Consume(ConsumeContext<PingMessage> context)
+            {
+                try
+                {
+                    throw new ArgumentException("Expected error, causing retries");
+                }
+                finally
+                {
+                    _taskCompletionSource.TrySetResult(context);
+                }
+            }
+        }
+
+
+        class StuckConsumerDefinition :
+            ConsumerDefinition<StuckConsumer>
+        {
+            protected override void ConfigureConsumer(IReceiveEndpointConfigurator endpointConfigurator,
+                IConsumerConfigurator<StuckConsumer> consumerConfigurator)
+            {
+                endpointConfigurator.UseMessageRetry(r => r.Interval(10, TimeSpan.FromMinutes(1)));
             }
         }
     }
