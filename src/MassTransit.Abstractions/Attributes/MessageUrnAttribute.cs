529922d1dc538c64466f3487319a3a35f4f67a2d
Kieranties <Kieran@Kieranties.com>
2023-03-29 21:11:09 +0100

    feat: add MessageUrnAttribute to support customization of message urn resolution
    tests: add expectations for results of MessageUrnAttribute usage
    
    feat: add handling of MessageUrnAttribute to MessageUrn
    
    chore: whitespace clean up
    
    refactor: move MessageUrnAttribute to MassTransit namespace
    
    tests: add validation of urn for MessageUrnAttribute
    
    feat: enable full override of urn value with supported tests
    
    feat: extend MessageUrnAttribute to target interfaces
    
    docs: add documentation for MessageUrnAttribute
    
    tests: correct MessageUrn tests with symbols
    
    docs: add links and additional section for ExcludeFromImplementedTypes

---
+++ src/MassTransit.Abstractions/Attributes/MessageUrnAttribute.cs
@@ -0,0 +1,45 @@
+﻿namespace MassTransit
+{
+    using System;
+
+    /// <summary>
+    /// Specify the URN used for this message contract
+    /// if configured.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
+    public class MessageUrnAttribute :
+        Attribute
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="urn">The urn value to use for this message type.</param>
+        /// <param name="useDefaultPrefix">Prefixes with default scheme and namespace if true.</param>
+        public MessageUrnAttribute(string urn, bool useDefaultPrefix = true)
+        {
+            Urn = GetValidUrn(urn, useDefaultPrefix);
+        }
+
+        public Uri Urn { get; }
+
+        private static Uri GetValidUrn(string urn, bool useDefaultPrefix)
+        {
+            if (urn == null)
+                throw new ArgumentNullException(nameof(urn));
+
+            if (string.IsNullOrWhiteSpace(urn))
+                throw new ArgumentException("Value cannot be empty or whitespace only string.", nameof(urn));
+
+            if (urn.StartsWith(MessageUrn.Prefix))
+                throw new ArgumentException($"Value should not contain the default prefix '{MessageUrn.Prefix}'.", nameof(urn));
+
+            var fullValue = useDefaultPrefix ? MessageUrn.Prefix + urn : urn;
+
+
+            if (Uri.TryCreate(fullValue, UriKind.Absolute, out var uri))
+                return uri;
+
+            throw new UriFormatException($"'{fullValue}' is not a valid URI.");
+        }
+    }
+}
--- tests/MassTransit.Tests/MessageUrnSpecs.cs
+++ tests/MassTransit.Tests/MessageUrnSpecs.cs
@@ -36,6 +36,72 @@ namespace MassTransit.Tests
             Assert.AreEqual(urn.AbsolutePath, "message:MassTransit.TestFramework.Messages:PingMessage");
         }
 
+        [Test]
+        public void AttributedMessage()
+        {
+            var urn = MessageUrn.ForType(typeof(Attributed));
+            Assert.AreEqual(urn.AbsolutePath, "message:MyCustomName");
+        }
+
+        [Test]
+        public void AttributedMessage_with_symbols()
+        {
+            var urn = MessageUrn.ForType(typeof(AttributedSymbols));
+            Assert.AreEqual(urn, "urn:message:\\|,./<>?;'#:@~[]{}¬!\"£$%25^&*()_+`¦€");
+        }
+
+        [Test]
+        public void AttributedMessage_with_null_throws_error()
+        {
+            Assert.That(
+                () => MessageUrn.ForType(typeof(AttributedNull)),
+                Throws.TypeOf<TypeInitializationException>()
+                .And.InnerException.TypeOf<ArgumentNullException>()
+                .And.InnerException.Message.EqualTo("Value cannot be null. (Parameter 'urn')"));
+        }
+
+        [Test]
+        public void AttributedMessage_with_empty_throws_error()
+        {
+            Assert.That(() => MessageUrn.ForType(typeof(AttributedEmpty)),
+                Throws.TypeOf<TypeInitializationException>()
+                .And.InnerException.TypeOf<ArgumentException>()
+                .And.InnerException.Message.EqualTo("Value cannot be empty or whitespace only string. (Parameter 'urn')"));
+        }
+
+        [Test]
+        public void AttributedMessage_with_whitespace_throws_error()
+        {
+            Assert.That(() => MessageUrn.ForType(typeof(AttributedWhitespace)),
+                Throws.TypeOf<TypeInitializationException>()
+                .And.InnerException.TypeOf<ArgumentException>()
+                .And.InnerException.Message.EqualTo("Value cannot be empty or whitespace only string. (Parameter 'urn')"));
+        }
+
+        [Test]
+        public void AttributedMessage_with_default_prefix_throws_error()
+        {
+            Assert.That(() => MessageUrn.ForType(typeof(AttributedKnownPrefix)),
+                Throws.TypeOf<TypeInitializationException>()
+                .And.InnerException.TypeOf<ArgumentException>()
+                .And.InnerException.Message.EqualTo("Value should not contain the default prefix 'urn:message:'. (Parameter 'urn')"));
+        }
+
+        [Test]
+        public void AttributedMessage_without_default_prefix()
+        {
+            var urn = MessageUrn.ForType(typeof(AttributedNoDefaults));
+            Assert.AreEqual(urn, "scheme:identifier");
+        }
+
+        [Test]
+        public void AttributedMessage_without_default_prefix_and_invalid_urn_throws_error()
+        {
+            Assert.That(() => MessageUrn.ForType(typeof(AttributedNoDefaultsInvalidUrn)),
+                Throws.TypeOf<TypeInitializationException>()
+                .And.InnerException.TypeOf<UriFormatException>()
+                .And.InnerException.Message.EqualTo("'scheme' is not a valid URI."));
+        }
 
         class X
         {
@@ -45,5 +111,53 @@ namespace MassTransit.Tests
 
     public class G<T>
     {
+    }
+
+    [MessageUrn("MyCustomName")]
+    public class Attributed
+    {
+
+    }
+
+    [MessageUrn(null)]
+    public class AttributedNull
+    {
+
+    }
+
+    [MessageUrn("")]
+    public class AttributedEmpty
+    {
+
+    }
+
+    [MessageUrn("\t\t   ")]
+    public class AttributedWhitespace
+    {
+
+    }
+
+    [MessageUrn("urn:message:MyCustomName")]
+    public class AttributedKnownPrefix
+    {
+
+    }
+
+    [MessageUrn("\\|,./<>?;'#:@~[]{}¬!\"£$%^&*()_+`¦€")]
+    public class AttributedSymbols
+    {
+
+    }
+
+    [MessageUrn("scheme:identifier", useDefaultPrefix: false)]
+    public class AttributedNoDefaults
+    {
+
+    }
+
+    [MessageUrn("scheme", useDefaultPrefix: false)]
+    public class AttributedNoDefaultsInvalidUrn
+    {
+
     }
 }
