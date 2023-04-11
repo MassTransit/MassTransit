b99f434ecf9736728b60e9c0cb6269c08ebfeea8
Patrick van Ek <patrick.vanek@infosupport.com>
2023-04-11 14:01:01 +0200

    Added Encrypted Deserializer (V2) with fallback to support Key Rotation

---
+++ tests/MassTransit.Tests/Encryption/TestMessage.cs
@@ -0,0 +1,44 @@
+﻿namespace MassTransit.Tests.Encryption
+{
+    using System;
+
+    public class TestMessage
+    {
+        public Guid Id { get; }
+        public string TestValue { get; }
+
+        public TestMessage(Guid id, string testValue)
+        {
+            Id = id;
+            TestValue = testValue;
+        }
+
+    #region Equality members
+
+        protected bool Equals(TestMessage other)
+        {
+            return Id.Equals(other.Id) && TestValue == other.TestValue;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+            return Equals((TestMessage)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Id.GetHashCode() * 397) ^ TestValue.GetHashCode();
+            }
+        }
+
+    #endregion
+    }
+}
