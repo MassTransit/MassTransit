1ccaa0c669786f9c47e4d2eaecc80efa974ee56b
Chris Patterson <chris@phatboyg.com>
2023-09-08 14:35:28 -0500

    Fixed #4634 - defensive programming for a bug in localstack (I mean, seriously, how many more bugs can there be in localstack).

--- src/Transports/MassTransit.AmazonSqsTransport/AmazonSqsTransport/AmazonWebServiceResponseExtensions.cs
+++ src/Transports/MassTransit.AmazonSqsTransport/AmazonSqsTransport/AmazonWebServiceResponseExtensions.cs
@@ -11,10 +11,11 @@ namespace MassTransit.AmazonSqsTransport
             const string documentationUri = "https://aws.amazon.com/blogs/developer/logging-with-the-aws-sdk-for-net/";
 
             var statusCode = response.HttpStatusCode;
-            var requestId = response.ResponseMetadata.RequestId;
 
             if (statusCode >= HttpStatusCode.OK && statusCode < HttpStatusCode.MultipleChoices)
                 return;
+
+            var requestId = response.ResponseMetadata?.RequestId ?? "[Missing RequestId]";
 
             throw new AmazonSqsTransportException(
                 $"Received unsuccessful response ({statusCode}) from AWS endpoint. See AWS SDK logs ({requestId}) for more details: {documentationUri}");
