c1daada8536377bb3776c88b28f29d144dd9cb74
Nicolai Ringbæk <nicolai.ringbaek@outlook.com>
2024-02-23 00:28:22 +0100

    Added Mermaid StateMachine visualizer

--- src/MassTransit.StateMachineVisualizer/StateMachineGraphvizGenerator.cs
+++ src/MassTransit.StateMachineVisualizer/StateMachineGraphvizGenerator.cs
@@ -1,27 +1,22 @@
 ﻿namespace MassTransit.Visualizer
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using Internals;
+    using MassTransit.Visualizer.Abstractions;
     using QuikGraph;
     using QuikGraph.Graphviz;
     using QuikGraph.Graphviz.Dot;
     using SagaStateMachine;
 
-
-    public class StateMachineGraphvizGenerator
+    public class StateMachineGraphvizGenerator : StateMachineGenerator
     {
-        readonly AdjacencyGraph<Vertex, Edge<Vertex>> _graph;
-
-        public StateMachineGraphvizGenerator(StateMachineGraph data)
+        public StateMachineGraphvizGenerator(StateMachineGraph data) : base(data)
         {
-            _graph = CreateAdjacencyGraph(data);
         }
 
         public string CreateDotFile()
         {
-            var algorithm = new GraphvizAlgorithm<Vertex, Edge<Vertex>>(_graph);
+            var algorithm = new GraphvizAlgorithm<Vertex, Edge<Vertex>>(Graph);
             algorithm.FormatVertex += VertexStyler;
             return algorithm.Generate();
         }
@@ -63,21 +58,6 @@
 
                 args.VertexFormat.Shape = GraphvizVertexShape.Ellipse;
             }
-        }
-
-        static AdjacencyGraph<Vertex, Edge<Vertex>> CreateAdjacencyGraph(StateMachineGraph data)
-        {
-            var graph = new AdjacencyGraph<Vertex, Edge<Vertex>>();
-
-            List<Vertex> compositeTargets = data.Edges.Where(x => x.From.IsComposite).Select(x => x.To).ToList();
-
-            List<Vertex> targets = data.Edges.Select(x => x.To).ToList();
-            List<Vertex> vertices = data.Vertices.Where(v => targets.Contains(v) || v.Title == "Initial").ToList();
-            List<Edge> edges = data.Edges.Where(x => vertices.Contains(x.From) && !compositeTargets.Contains(x.From)).ToList();
-
-            graph.AddVertexRange(vertices);
-            graph.AddEdgeRange(edges.Select(x => new Edge<Vertex>(x.From, x.To)));
-            return graph;
         }
     }
 }
