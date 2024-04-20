651ef2d6c4eb25f53fc4278c3e58f6439165bec4
Mike Mao <23351416+zyofeng@users.noreply.github.com>
2024-04-20 16:46:09 +1200

    clean up

--- src/MassTransit/Futures/FutureStateExtensions.cs
+++ src/MassTransit/Futures/FutureStateExtensions.cs
@@ -4,7 +4,6 @@ namespace MassTransit
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
-    using Futures;
 
 
     public static class FutureStateExtensions
