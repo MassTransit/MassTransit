f36c14faa4f7d2a04f38998c6b270c01867dd6f4
Chris Patterson <chris@phatboyg.com>
2024-04-26 09:43:40 -0500

    Cleaned up warnings on RabbitMQ ConfigurationHostSettings

--- src/Transports/MassTransit.RabbitMqTransport/RabbitMqTransport/Configuration/ConfigurationHostSettings.cs
+++ src/Transports/MassTransit.RabbitMqTransport/RabbitMqTransport/Configuration/ConfigurationHostSettings.cs
@@ -37,25 +37,25 @@ namespace MassTransit.RabbitMqTransport.Configuration
             _hostAddress = new Lazy<Uri>(FormatHostAddress);
         }
 
-        public RefreshConnectionFactoryCallback OnRefreshConnectionFactory { get; set; }
+        public RefreshConnectionFactoryCallback? OnRefreshConnectionFactory { get; set; }
 
-        public string Host { get; set; }
+        public string? Host { get; set; }
         public int Port { get; set; }
-        public string VirtualHost { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string? VirtualHost { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
         public TimeSpan Heartbeat { get; set; }
         public bool Ssl { get; set; }
         public SslProtocols SslProtocol { get; set; }
-        public string SslServerName { get; set; }
+        public string? SslServerName { get; set; }
         public SslPolicyErrors AcceptablePolicyErrors { get; set; }
-        public string ClientCertificatePath { get; set; }
-        public string ClientCertificatePassphrase { get; set; }
-        public X509Certificate ClientCertificate { get; set; }
+        public string? ClientCertificatePath { get; set; }
+        public string? ClientCertificatePassphrase { get; set; }
+        public X509Certificate? ClientCertificate { get; set; }
         public bool UseClientCertificateAsAuthenticationIdentity { get; set; }
-        public LocalCertificateSelectionCallback CertificateSelectionCallback { get; set; }
-        public RemoteCertificateValidationCallback CertificateValidationCallback { get; set; }
-        public IRabbitMqEndpointResolver EndpointResolver { get; set; }
+        public LocalCertificateSelectionCallback? CertificateSelectionCallback { get; set; }
+        public RemoteCertificateValidationCallback? CertificateValidationCallback { get; set; }
+        public IRabbitMqEndpointResolver? EndpointResolver { get; set; }
         public string? ClientProvidedName { get; set; }
         public bool PublisherConfirmation { get; set; }
         public Uri HostAddress => _hostAddress.Value;
