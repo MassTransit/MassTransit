c76611b8ea24e3e8f0d99b211daf631ae27ac1a6
denys.kozhevnikov <github@mail.noonamer.com>
2024-05-24 17:51:17 +0100

    Add Azure Storage tests

--- tests/MassTransit.Azure.Table.Tests/Configuration.cs
+++ tests/MassTransit.Azure.Table.Tests/Configuration.cs
@@ -1,15 +1,7 @@
 namespace MassTransit.Azure.Table.Tests
 {
-    using System;
-    using NUnit.Framework;
-
-
     public static class Configuration
     {
-        public static string StorageAccount =>
-            TestContext.Parameters.Exists(nameof(StorageAccount))
-                ? TestContext.Parameters.Get(nameof(StorageAccount))
-                : Environment.GetEnvironmentVariable("MT_AZURE_STORAGE_ACCOUNT")
-                ?? "DefaultEndpointsProtocol=http;AccountName=localhost;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;TableEndpoint=https://localhost:8081/;";
+        public static string StorageAccount => "UseDevelopmentStorage=true";
     }
 }
