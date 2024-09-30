8a39bd775df477c3054b1d8a6c4b5f14039dc484
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2024-09-30 14:46:05 +0200

    Reintroduce outbox.Property(p => p.MessageType);

--- src/Persistence/MassTransit.EntityFrameworkCoreIntegration/Configuration/EntityFrameworkOutboxConfigurationExtensions.cs
+++ src/Persistence/MassTransit.EntityFrameworkCoreIntegration/Configuration/EntityFrameworkOutboxConfigurationExtensions.cs
@@ -353,6 +353,7 @@ namespace MassTransit
 
             outbox.Property(p => p.ContentType)
                 .HasMaxLength(256);
+            outbox.Property(p => p.MessageType);
 
             outbox.Property(p => p.Body);
         }
