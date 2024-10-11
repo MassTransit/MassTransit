874c16d34a468b4f3a1b59f153ea058d38d87bd8
Chris Patterson <chris@phatboyg.com>
2024-10-11 09:01:43 -0500

    Optimizing the flaky batch test that always fails.

--- tests/MassTransit.Tests/Batch_Specs.cs
+++ tests/MassTransit.Tests/Batch_Specs.cs
@@ -109,8 +109,6 @@
             await InputQueueSendEndpoint.Send(new PingMessage(), Pipe.Execute<SendContext>(ctx => ctx.CorrelationId = correlation2));
             await InputQueueSendEndpoint.Send(new PingMessage(), Pipe.Execute<SendContext>(ctx => ctx.CorrelationId = correlation2));
 
-            await InactivityTask;
-
             var count = await BusTestHarness.Consumed.SelectAsync<PingMessage>().Take(6).Count();
 
             Assert.Multiple(() =>
@@ -155,8 +153,6 @@
             await InputQueueSendEndpoint.Send(new PingMessage(), Pipe.Execute<SendContext>(ctx => ctx.CorrelationId = correlation2));
             await InputQueueSendEndpoint.Send(new PingMessage(), Pipe.Execute<SendContext>(ctx => ctx.CorrelationId = correlation2));
 
-            await InactivityTask;
-
             var count = await BusTestHarness.Consumed.SelectAsync<PingMessage>().Take(6).Count();
 
             Assert.Multiple(() =>
@@ -173,12 +169,14 @@
             configurator.ConcurrentMessageLimit = 10;
 
             configurator.Consumer(() =>
-            {
-                TaskCompletionSource<Batch<PingMessage>> tcs = GetTask<Batch<PingMessage>>();
-                tcs.Task.ContinueWith(t => _batches.Add(t.Result));
-                var consumer = new TestBatchConsumer(tcs);
-                return consumer;
-            }, cc => cc.Options<BatchOptions>(x => x.GroupBy<PingMessage, string>(ctx => ctx.CorrelationId?.ToString("D"))));
+                {
+                    TaskCompletionSource<Batch<PingMessage>> tcs = GetTask<Batch<PingMessage>>();
+                    tcs.Task.ContinueWith(t => _batches.Add(t.Result));
+                    var consumer = new TestBatchConsumer(tcs);
+                    return consumer;
+                },
+                cc => cc.Options<BatchOptions>(x =>
+                    x.SetTimeLimit(TimeSpan.FromMilliseconds(500)).GroupBy<PingMessage, string>(ctx => ctx.CorrelationId?.ToString("D"))));
         }
     }
 
