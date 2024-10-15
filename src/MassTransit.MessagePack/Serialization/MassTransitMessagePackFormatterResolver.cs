96f6cbd976b4e5cee6c6be812ab8876f8f548fc0
Marcus Christensen <mail@marcuslc.com>
2024-10-15 23:23:37 +0200

    Optimized the map lookup of MassTransitMessagePackFormatterResolver from O(n) to O(1), where 'n' is the number of registered type mappers.

--- src/MassTransit.MessagePack/Serialization/MassTransitMessagePackFormatterResolver.cs
+++ src/MassTransit.MessagePack/Serialization/MassTransitMessagePackFormatterResolver.cs
@@ -4,11 +4,11 @@ namespace MassTransit.Serialization;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Contracts.JobService;
 using Courier.Contracts;
 using Courier.Messages;
 using Events;
-using Internals;
 using JobService.Messages;
 using MessagePack;
 using MessagePack.Formatters;
@@ -22,80 +22,100 @@ class MassTransitMessagePackFormatterResolver :
 {
     public static MassTransitMessagePackFormatterResolver Instance { get; } = new();
 
-    readonly IReadOnlyCollection<KeyValuePair<Type, Type>> _mappedFormatterByType;
+    readonly Dictionary<Type, Type> _mappedNonGenericTypes;
+    readonly Dictionary<Type, Type> _mappedGenericTypes;
+
     readonly ConcurrentDictionary<Type, IMessagePackFormatter> _cachedFormatters;
 
     MassTransitMessagePackFormatterResolver()
     {
-        _mappedFormatterByType = new List<KeyValuePair<Type, Type>>
+        _mappedGenericTypes = new Dictionary<Type, Type>
         {
-            new(typeof(MessageData<>), typeof(MessageDataFormatter<>)),
+            // May only contain open generic types.
+            { typeof(MessageData<>), typeof(MessageDataFormatter<>) },
+        };
+
+        _mappedNonGenericTypes = new Dictionary<Type, Type>
+        {
+            // May only contain non-open generic types.
         #if USE_CONCRETE_MAPPERS
-            new(typeof(Fault), typeof(InterfaceConcreteMapFormatter<Fault, FaultEvent>)),
-            new(typeof(ReceiveFault), typeof(InterfaceConcreteMapFormatter<ReceiveFault, ReceiveFaultEvent>)),
-            new(typeof(ExceptionInfo), typeof(InterfaceConcreteMapFormatter<ExceptionInfo, FaultExceptionInfo>)),
-            new(typeof(HostInfo), typeof(InterfaceConcreteMapFormatter<HostInfo, BusHostInfo>)),
-            new(typeof(ScheduleMessage), typeof(InterfaceConcreteMapFormatter<ScheduleMessage, ScheduleMessageCommand>)),
-            new(typeof(ScheduleRecurringMessage), typeof(InterfaceConcreteMapFormatter<ScheduleRecurringMessage, ScheduleRecurringMessageCommand>)),
-            new(typeof(CancelScheduledMessage), typeof(InterfaceConcreteMapFormatter<CancelScheduledMessage, CancelScheduledMessageCommand>)),
-            new(typeof(CancelScheduledRecurringMessage),
-                typeof(InterfaceConcreteMapFormatter<CancelScheduledRecurringMessage, CancelScheduledRecurringMessageCommand>)),
-            new(typeof(PauseScheduledRecurringMessage),
-                typeof(InterfaceConcreteMapFormatter<PauseScheduledRecurringMessage, PauseScheduledRecurringMessageCommand>)),
-            new(typeof(ResumeScheduledRecurringMessage),
-                typeof(InterfaceConcreteMapFormatter<ResumeScheduledRecurringMessage, ResumeScheduledRecurringMessageCommand>)),
-            new(typeof(RoutingSlip), typeof(InterfaceConcreteMapFormatter<RoutingSlip, RoutingSlipRoutingSlip>)),
-            new(typeof(Activity), typeof(InterfaceConcreteMapFormatter<Activity, RoutingSlipActivity>)),
-            new(typeof(ActivityLog), typeof(InterfaceConcreteMapFormatter<ActivityLog, RoutingSlipActivityLog>)),
-            new(typeof(CompensateLog), typeof(InterfaceConcreteMapFormatter<CompensateLog, RoutingSlipCompensateLog>)),
-            new(typeof(ActivityException), typeof(InterfaceConcreteMapFormatter<ActivityException, RoutingSlipActivityException>)),
-            new(typeof(Subscription), typeof(InterfaceConcreteMapFormatter<Subscription, RoutingSlipSubscription>)),
-            new(typeof(RoutingSlipCompleted), typeof(InterfaceConcreteMapFormatter<RoutingSlipCompleted, RoutingSlipCompletedMessage>)),
-            new(typeof(RoutingSlipFaulted), typeof(InterfaceConcreteMapFormatter<RoutingSlipFaulted, RoutingSlipFaultedMessage>)),
-            new(typeof(RoutingSlipActivityCompleted), typeof(InterfaceConcreteMapFormatter<RoutingSlipActivityCompleted, RoutingSlipActivityCompletedMessage>)),
-            new(typeof(RoutingSlipActivityFaulted), typeof(InterfaceConcreteMapFormatter<RoutingSlipActivityFaulted, RoutingSlipActivityFaultedMessage>)),
-            new(typeof(RoutingSlipActivityCompensated),
-                typeof(InterfaceConcreteMapFormatter<RoutingSlipActivityCompensated, RoutingSlipActivityCompensatedMessage>)),
-            new(typeof(RoutingSlipActivityCompensationFailed),
-                typeof(InterfaceConcreteMapFormatter<RoutingSlipActivityCompensationFailed, RoutingSlipActivityCompensationFailedMessage>)),
-            new(typeof(RoutingSlipCompensationFailed),
-                typeof(InterfaceConcreteMapFormatter<RoutingSlipCompensationFailed, RoutingSlipCompensationFailedMessage>)),
-            new(typeof(RoutingSlipTerminated), typeof(InterfaceConcreteMapFormatter<RoutingSlipTerminated, RoutingSlipTerminatedMessage>)),
-            new(typeof(RoutingSlipRevised), typeof(InterfaceConcreteMapFormatter<RoutingSlipRevised, RoutingSlipRevisedMessage>)),
-            new(typeof(RecurringJobSchedule), typeof(InterfaceConcreteMapFormatter<RecurringJobSchedule, RecurringJobScheduleInfo>)),
-            new(typeof(AllocateJobSlot), typeof(InterfaceConcreteMapFormatter<AllocateJobSlot, AllocateJobSlotCommand>)),
-            new(typeof(CancelJob), typeof(InterfaceConcreteMapFormatter<CancelJob, CancelJobCommand>)),
-            new(typeof(CancelJobAttempt), typeof(InterfaceConcreteMapFormatter<CancelJobAttempt, CancelJobAttemptCommand>)),
-            new(typeof(CompleteJob), typeof(InterfaceConcreteMapFormatter<CompleteJob, CompleteJobCommand>)),
-            new(typeof(FaultJob), typeof(InterfaceConcreteMapFormatter<FaultJob, FaultJobCommand>)),
-            new(typeof(FinalizeJob), typeof(InterfaceConcreteMapFormatter<FinalizeJob, FinalizeJobCommand>)),
-            new(typeof(FinalizeJobAttempt), typeof(InterfaceConcreteMapFormatter<FinalizeJobAttempt, FinalizeJobAttemptCommand>)),
-            new(typeof(GetJobAttemptStatus), typeof(InterfaceConcreteMapFormatter<GetJobAttemptStatus, GetJobAttemptStatusRequest>)),
-            new(typeof(GetJobState), typeof(InterfaceConcreteMapFormatter<GetJobState, GetJobStateRequest>)),
-            new(typeof(JobAttemptCanceled), typeof(InterfaceConcreteMapFormatter<JobAttemptCanceled, JobAttemptCanceledEvent>)),
-            new(typeof(JobAttemptCompleted), typeof(InterfaceConcreteMapFormatter<JobAttemptCompleted, JobAttemptCompletedEvent>)),
-            new(typeof(JobAttemptFaulted), typeof(InterfaceConcreteMapFormatter<JobAttemptFaulted, JobAttemptFaultedEvent>)),
-            new(typeof(JobAttemptStarted), typeof(InterfaceConcreteMapFormatter<JobAttemptStarted, JobAttemptStartedEvent>)),
-            new(typeof(JobCanceled), typeof(InterfaceConcreteMapFormatter<JobCanceled, JobCanceledEvent>)),
-            new(typeof(JobCompleted), typeof(InterfaceConcreteMapFormatter<JobCompleted, JobCompletedEvent>)),
-            new(typeof(JobFaulted), typeof(InterfaceConcreteMapFormatter<JobFaulted, JobFaultedEvent>)),
-            new(typeof(JobRetryDelayElapsed), typeof(InterfaceConcreteMapFormatter<JobRetryDelayElapsed, JobRetryDelayElapsedEvent>)),
-            new(typeof(JobSlotAllocated), typeof(InterfaceConcreteMapFormatter<JobSlotAllocated, JobSlotAllocatedResponse>)),
-            new(typeof(JobSlotReleased), typeof(InterfaceConcreteMapFormatter<JobSlotReleased, JobSlotReleasedEvent>)),
-            new(typeof(JobSlotUnavailable), typeof(InterfaceConcreteMapFormatter<JobSlotUnavailable, JobSlotUnavailableResponse>)),
-            new(typeof(JobSlotWaitElapsed), typeof(InterfaceConcreteMapFormatter<JobSlotWaitElapsed, JobSlotWaitElapsedEvent>)),
-            new(typeof(JobState), typeof(InterfaceConcreteMapFormatter<JobState, JobStateResponse>)),
-            new(typeof(JobStarted), typeof(InterfaceConcreteMapFormatter<JobStarted, JobStartedEvent>)),
-            new(typeof(JobStatusCheckRequested), typeof(InterfaceConcreteMapFormatter<JobStatusCheckRequested, JobStatusCheckRequestedEvent>)),
-            new(typeof(JobSubmissionAccepted), typeof(InterfaceConcreteMapFormatter<JobSubmissionAccepted, JobSubmissionAcceptedResponse>)),
-            new(typeof(JobSubmitted), typeof(InterfaceConcreteMapFormatter<JobSubmitted, JobSubmittedEvent>)),
-            new(typeof(RetryJob), typeof(InterfaceConcreteMapFormatter<RetryJob, RetryJobCommand>)),
-            new(typeof(RunJob), typeof(InterfaceConcreteMapFormatter<RunJob, RunJobCommand>)),
-            new(typeof(SaveJobState), typeof(InterfaceConcreteMapFormatter<SaveJobState, SaveJobStateCommand>)),
-            new(typeof(SetConcurrentJobLimit), typeof(InterfaceConcreteMapFormatter<SetConcurrentJobLimit, SetConcurrentJobLimitCommand>)),
-            new(typeof(SetJobProgress), typeof(InterfaceConcreteMapFormatter<SetJobProgress, SetJobProgressCommand>)),
-            new(typeof(StartJob), typeof(InterfaceConcreteMapFormatter<StartJob, StartJobCommand>)),
-            new(typeof(StartJobAttempt), typeof(InterfaceConcreteMapFormatter<StartJobAttempt, StartJobAttemptCommand>))
+            { typeof(Fault), typeof(InterfaceConcreteMapFormatter<Fault, FaultEvent>) },
+            { typeof(ReceiveFault), typeof(InterfaceConcreteMapFormatter<ReceiveFault, ReceiveFaultEvent>) },
+            { typeof(ExceptionInfo), typeof(InterfaceConcreteMapFormatter<ExceptionInfo, FaultExceptionInfo>) },
+            { typeof(HostInfo), typeof(InterfaceConcreteMapFormatter<HostInfo, BusHostInfo>) },
+            { typeof(ScheduleMessage), typeof(InterfaceConcreteMapFormatter<ScheduleMessage, ScheduleMessageCommand>) },
+            { typeof(ScheduleRecurringMessage), typeof(InterfaceConcreteMapFormatter<ScheduleRecurringMessage, ScheduleRecurringMessageCommand>) },
+            { typeof(CancelScheduledMessage), typeof(InterfaceConcreteMapFormatter<CancelScheduledMessage, CancelScheduledMessageCommand>) },
+            {
+                typeof(CancelScheduledRecurringMessage),
+                typeof(InterfaceConcreteMapFormatter<CancelScheduledRecurringMessage, CancelScheduledRecurringMessageCommand>)
+            },
+            {
+                typeof(PauseScheduledRecurringMessage),
+                typeof(InterfaceConcreteMapFormatter<PauseScheduledRecurringMessage, PauseScheduledRecurringMessageCommand>)
+            },
+            {
+                typeof(ResumeScheduledRecurringMessage),
+                typeof(InterfaceConcreteMapFormatter<ResumeScheduledRecurringMessage, ResumeScheduledRecurringMessageCommand>)
+            },
+            { typeof(RoutingSlip), typeof(InterfaceConcreteMapFormatter<RoutingSlip, RoutingSlipRoutingSlip>) },
+            { typeof(Activity), typeof(InterfaceConcreteMapFormatter<Activity, RoutingSlipActivity>) },
+            { typeof(ActivityLog), typeof(InterfaceConcreteMapFormatter<ActivityLog, RoutingSlipActivityLog>) },
+            { typeof(CompensateLog), typeof(InterfaceConcreteMapFormatter<CompensateLog, RoutingSlipCompensateLog>) },
+            { typeof(ActivityException), typeof(InterfaceConcreteMapFormatter<ActivityException, RoutingSlipActivityException>) },
+            { typeof(Subscription), typeof(InterfaceConcreteMapFormatter<Subscription, RoutingSlipSubscription>) },
+            { typeof(RoutingSlipCompleted), typeof(InterfaceConcreteMapFormatter<RoutingSlipCompleted, RoutingSlipCompletedMessage>) },
+            { typeof(RoutingSlipFaulted), typeof(InterfaceConcreteMapFormatter<RoutingSlipFaulted, RoutingSlipFaultedMessage>) },
+            { typeof(RoutingSlipActivityCompleted), typeof(InterfaceConcreteMapFormatter<RoutingSlipActivityCompleted, RoutingSlipActivityCompletedMessage>) },
+            { typeof(RoutingSlipActivityFaulted), typeof(InterfaceConcreteMapFormatter<RoutingSlipActivityFaulted, RoutingSlipActivityFaultedMessage>) },
+            {
+                typeof(RoutingSlipActivityCompensated),
+                typeof(InterfaceConcreteMapFormatter<RoutingSlipActivityCompensated, RoutingSlipActivityCompensatedMessage>)
+            },
+            {
+                typeof(RoutingSlipActivityCompensationFailed),
+                typeof(InterfaceConcreteMapFormatter<RoutingSlipActivityCompensationFailed, RoutingSlipActivityCompensationFailedMessage>)
+            },
+            {
+                typeof(RoutingSlipCompensationFailed),
+                typeof(InterfaceConcreteMapFormatter<RoutingSlipCompensationFailed, RoutingSlipCompensationFailedMessage>)
+            },
+            { typeof(RoutingSlipTerminated), typeof(InterfaceConcreteMapFormatter<RoutingSlipTerminated, RoutingSlipTerminatedMessage>) },
+            { typeof(RoutingSlipRevised), typeof(InterfaceConcreteMapFormatter<RoutingSlipRevised, RoutingSlipRevisedMessage>) },
+            { typeof(RecurringJobSchedule), typeof(InterfaceConcreteMapFormatter<RecurringJobSchedule, RecurringJobScheduleInfo>) },
+            { typeof(AllocateJobSlot), typeof(InterfaceConcreteMapFormatter<AllocateJobSlot, AllocateJobSlotCommand>) },
+            { typeof(CancelJob), typeof(InterfaceConcreteMapFormatter<CancelJob, CancelJobCommand>) },
+            { typeof(CancelJobAttempt), typeof(InterfaceConcreteMapFormatter<CancelJobAttempt, CancelJobAttemptCommand>) },
+            { typeof(CompleteJob), typeof(InterfaceConcreteMapFormatter<CompleteJob, CompleteJobCommand>) },
+            { typeof(FaultJob), typeof(InterfaceConcreteMapFormatter<FaultJob, FaultJobCommand>) },
+            { typeof(FinalizeJob), typeof(InterfaceConcreteMapFormatter<FinalizeJob, FinalizeJobCommand>) },
+            { typeof(FinalizeJobAttempt), typeof(InterfaceConcreteMapFormatter<FinalizeJobAttempt, FinalizeJobAttemptCommand>) },
+            { typeof(GetJobAttemptStatus), typeof(InterfaceConcreteMapFormatter<GetJobAttemptStatus, GetJobAttemptStatusRequest>) },
+            { typeof(GetJobState), typeof(InterfaceConcreteMapFormatter<GetJobState, GetJobStateRequest>) },
+            { typeof(JobAttemptCanceled), typeof(InterfaceConcreteMapFormatter<JobAttemptCanceled, JobAttemptCanceledEvent>) },
+            { typeof(JobAttemptCompleted), typeof(InterfaceConcreteMapFormatter<JobAttemptCompleted, JobAttemptCompletedEvent>) },
+            { typeof(JobAttemptFaulted), typeof(InterfaceConcreteMapFormatter<JobAttemptFaulted, JobAttemptFaultedEvent>) },
+            { typeof(JobAttemptStarted), typeof(InterfaceConcreteMapFormatter<JobAttemptStarted, JobAttemptStartedEvent>) },
+            { typeof(JobCanceled), typeof(InterfaceConcreteMapFormatter<JobCanceled, JobCanceledEvent>) },
+            { typeof(JobCompleted), typeof(InterfaceConcreteMapFormatter<JobCompleted, JobCompletedEvent>) },
+            { typeof(JobFaulted), typeof(InterfaceConcreteMapFormatter<JobFaulted, JobFaultedEvent>) },
+            { typeof(JobRetryDelayElapsed), typeof(InterfaceConcreteMapFormatter<JobRetryDelayElapsed, JobRetryDelayElapsedEvent>) },
+            { typeof(JobSlotAllocated), typeof(InterfaceConcreteMapFormatter<JobSlotAllocated, JobSlotAllocatedResponse>) },
+            { typeof(JobSlotReleased), typeof(InterfaceConcreteMapFormatter<JobSlotReleased, JobSlotReleasedEvent>) },
+            { typeof(JobSlotUnavailable), typeof(InterfaceConcreteMapFormatter<JobSlotUnavailable, JobSlotUnavailableResponse>) },
+            { typeof(JobSlotWaitElapsed), typeof(InterfaceConcreteMapFormatter<JobSlotWaitElapsed, JobSlotWaitElapsedEvent>) },
+            { typeof(JobState), typeof(InterfaceConcreteMapFormatter<JobState, JobStateResponse>) },
+            { typeof(JobStarted), typeof(InterfaceConcreteMapFormatter<JobStarted, JobStartedEvent>) },
+            { typeof(JobStatusCheckRequested), typeof(InterfaceConcreteMapFormatter<JobStatusCheckRequested, JobStatusCheckRequestedEvent>) },
+            { typeof(JobSubmissionAccepted), typeof(InterfaceConcreteMapFormatter<JobSubmissionAccepted, JobSubmissionAcceptedResponse>) },
+            { typeof(JobSubmitted), typeof(InterfaceConcreteMapFormatter<JobSubmitted, JobSubmittedEvent>) },
+            { typeof(RetryJob), typeof(InterfaceConcreteMapFormatter<RetryJob, RetryJobCommand>) },
+            { typeof(RunJob), typeof(InterfaceConcreteMapFormatter<RunJob, RunJobCommand>) },
+            { typeof(SaveJobState), typeof(InterfaceConcreteMapFormatter<SaveJobState, SaveJobStateCommand>) },
+            { typeof(SetConcurrentJobLimit), typeof(InterfaceConcreteMapFormatter<SetConcurrentJobLimit, SetConcurrentJobLimitCommand>) },
+            { typeof(SetJobProgress), typeof(InterfaceConcreteMapFormatter<SetJobProgress, SetJobProgressCommand>) },
+            { typeof(StartJob), typeof(InterfaceConcreteMapFormatter<StartJob, StartJobCommand>) },
+            { typeof(StartJobAttempt), typeof(InterfaceConcreteMapFormatter<StartJobAttempt, StartJobAttemptCommand>) }
         #endif
         };
 
@@ -109,12 +129,8 @@ class MassTransitMessagePackFormatterResolver :
         if (_cachedFormatters.TryGetValue(tType, out var cachedFormatter))
             return (IMessagePackFormatter<T>)cachedFormatter;
 
-        if (TryGetMappedType(tType, out KeyValuePair<Type, Type> mappedPair))
+        if (TryGetMappedType(tType, out Type? formatterType))
         {
-            var formatterType = mappedPair.Value;
-
-            EnsureFormatterTypeHasGenericParametersOfOriginalType(tType, ref formatterType);
-
             var createdFormatterInstance = Activator.CreateInstance(formatterType);
 
             if (createdFormatterInstance is null)
@@ -137,30 +153,30 @@ class MassTransitMessagePackFormatterResolver :
         return createdConcreteFormatter;
     }
 
-    static void EnsureFormatterTypeHasGenericParametersOfOriginalType(Type tType, ref Type formatterType)
-    {
-        if (!formatterType.IsGenericTypeDefinition)
-            return;
 
-        formatterType = formatterType
-            .MakeGenericType(tType.GenericTypeArguments);
+    bool TryGetMappedType(Type originType, [NotNullWhen(true)] out Type? mappedTargetType)
+    {
+        // If the type is not generic, or it is a generic type definition, we use the non-generic mapping.
+        return !originType.IsGenericType || originType.IsGenericTypeDefinition
+            ? TryGetNonGenericMappedType(originType, out mappedTargetType)
+            : TryGetOpenGenericMappedType(originType, out mappedTargetType);
     }
 
-    bool TryGetMappedType(Type originType, out KeyValuePair<Type, Type> mappedPair)
+    bool TryGetNonGenericMappedType(Type originType, out Type? mappedTargetType)
     {
-        foreach (KeyValuePair<Type, Type> mapPair in _mappedFormatterByType)
-        {
-            /*
-             * If the mapped type is not an open generic, and the origin type is not the same as the mapped type, skip.
-             */
-            if ((!mapPair.Key.IsOpenGeneric() || !originType.ClosesType(mapPair.Key)) && originType != mapPair.Key)
-                continue;
+        return _mappedNonGenericTypes.TryGetValue(originType, out mappedTargetType);
+    }
 
-            mappedPair = mapPair;
-            return true;
+    bool TryGetOpenGenericMappedType(Type originType, out Type? mappedTargetType)
+    {
+        var genericTypeDefinition = originType.GetGenericTypeDefinition();
+        if (!_mappedGenericTypes.TryGetValue(genericTypeDefinition, out Type? openGenericMappedType))
+        {
+            mappedTargetType = null;
+            return false;
         }
 
-        mappedPair = default;
-        return false;
+        mappedTargetType = openGenericMappedType.MakeGenericType(originType.GenericTypeArguments);
+        return true;
     }
 }
