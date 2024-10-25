1dc9d1a5700ba7f4aa0c6eff27932138dab3d50f
Michelle Li <michellli@microsoft.com>
2024-10-25 15:06:54 +0000

    upgrade from Microsoft.Azure.Cosmos.Table to Azure.Data.Tables in AuditStore

--- src/Persistence/MassTransit.Azure.Table/AzureTable/AzureTableAuditStore.cs
+++ src/Persistence/MassTransit.Azure.Table/AzureTable/AzureTableAuditStore.cs
@@ -1,17 +1,18 @@
 ﻿namespace MassTransit.AzureTable
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using Audit;
-    using Microsoft.Azure.Cosmos.Table;
+    using Azure.Data.Tables;
 
 
     public class AzureTableAuditStore :
         IMessageAuditStore
     {
         readonly IPartitionKeyFormatter _partitionKeyFormatter;
-        readonly CloudTable _table;
+        readonly TableClient _table;
 
-        public AzureTableAuditStore(CloudTable table, IPartitionKeyFormatter partitionKeyFormatter)
+        public AzureTableAuditStore(TableClient table, IPartitionKeyFormatter partitionKeyFormatter)
         {
             _table = table;
             _partitionKeyFormatter = partitionKeyFormatter;
@@ -20,8 +21,10 @@
         Task IMessageAuditStore.StoreMessage<T>(T message, MessageAuditMetadata metadata)
         {
             var auditRecord = AuditRecord.Create(message, metadata, _partitionKeyFormatter);
-            var insertOrMergeOperation = TableOperation.InsertOrMerge(auditRecord);
-            return _table.ExecuteAsync(insertOrMergeOperation);
+            var p = new Dictionary<string, object>();
+            p.Add(auditRecord.PartitionKey, auditRecord);
+            TableEntity entity = new TableEntity(p);
+            return _table.UpsertEntityAsync(entity);
         }
     }
 }
