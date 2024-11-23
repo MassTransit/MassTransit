6d5f5ae861b26c15bbc4da5c5a1a82681422590c
qrzychu <qrzychu@gmail.com>
2024-11-23 17:30:16 +0100

    Throw configuration exception if bus was not added (#5680)
    * throw configuration exception if bus was not added
    
    * use type name in exception message
    
    ---------
    
    Co-authored-by: Krzysztof Skowronek <krzysztof.skowronek@gentwo.com>

---
+++ tests/MassTransit.Tests/ContainerTests/EmptyBodyConfiguration_Specs.cs
@@ -0,0 +1,31 @@
+﻿namespace MassTransit.Tests.ContainerTests;
+
+using System;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+
+[TestFixture]
+public class EmptyBodyConfiguration_Specs
+{
+    [Test]
+    public void Should_throw_when_no_bus_is_configured()
+    {
+        var services = new ServiceCollection();
+
+        Assert.Throws<ConfigurationException>(() =>
+            services.AddMassTransit(x => { }));
+    }
+
+    [Test]
+    public void Should_throw_when_no_bus_is_configured_multibus()
+    {
+        var services = new ServiceCollection();
+        Assert.Throws(Is.InstanceOf<TargetInvocationException>().And.InnerException.InstanceOf<ConfigurationException>(),
+            () => services.AddMassTransit<ISecondBus>(x => { }));
+    }
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public interface ISecondBus : IBus;
+}
