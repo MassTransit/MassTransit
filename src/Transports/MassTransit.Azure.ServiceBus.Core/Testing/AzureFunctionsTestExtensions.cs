9376d1bac83d4f89e0e239412022e0f93b901e91
Chris Patterson <chris@phatboyg.com>
2025-06-25 11:03:30 -0500

    Simple clean up for Azure Function configuration extension and methods

--- src/Transports/MassTransit.Azure.ServiceBus.Core/Testing/AzureFunctionsTestExtensions.cs
+++ src/Transports/MassTransit.Azure.ServiceBus.Core/Testing/AzureFunctionsTestExtensions.cs
@@ -1,55 +1,54 @@
-namespace MassTransit.Testing
+namespace MassTransit.Testing;
+
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Azure.Core.Amqp;
+using Azure.Messaging.ServiceBus;
+using AzureServiceBusTransport;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Serialization;
+
+
+public static class AzureFunctionsTestExtensions
 {
-    using System;
-    using System.Reflection;
-    using System.Threading.Tasks;
-    using Azure.Core.Amqp;
-    using Azure.Messaging.ServiceBus;
-    using AzureServiceBusTransport;
-    using Microsoft.Extensions.DependencyInjection;
-    using Serialization;
-
-
-    public static class AzureFunctionsTestExtensions
+    public static IBusRegistrationConfigurator AddAzureFunctionsTestComponents(this IBusRegistrationConfigurator configurator)
     {
-        public static IBusRegistrationConfigurator AddAzureFunctionsTestComponents(this IBusRegistrationConfigurator configurator)
+        configurator.TryAddSingleton<IAsyncBusHandle, AsyncBusHandle>();
+        configurator.TryAddSingleton<IMessageReceiver, MessageReceiver>();
+
+        return configurator;
+    }
+
+    /// <summary>
+    /// Handle the Azure Service Bus message using the specified consumer
+    /// </summary>
+    /// <param name="harness"></param>
+    /// <param name="message"></param>
+    /// <typeparam name="TConsumer"></typeparam>
+    public static Task HandleConsumer<TConsumer>(this ITestHarness harness, object message)
+        where TConsumer : class, IConsumer
+    {
+        var body = SystemTextJsonMessageSerializer.Instance.SerializeObject(message);
+
+        var messageBody = new AmqpMessageBody([new BinaryData(body.GetBytes()).ToMemory()]);
+        var annotatedMessage = new AmqpAnnotatedMessage(messageBody)
         {
-            configurator.AddSingleton<IAsyncBusHandle, AsyncBusHandle>()
-                .AddSingleton<IMessageReceiver, MessageReceiver>();
-
-            return configurator;
-        }
-
-        /// <summary>
-        /// Handle the Azure Service Bus message using the specified consumer
-        /// </summary>
-        /// <param name="harness"></param>
-        /// <param name="message"></param>
-        /// <typeparam name="TConsumer"></typeparam>
-        public static Task HandleConsumer<TConsumer>(this ITestHarness harness, object message)
-            where TConsumer : class, IConsumer
-        {
-            var body = SystemTextJsonMessageSerializer.Instance.SerializeObject(message);
-
-            var messageBody = new AmqpMessageBody(new[] { new BinaryData(body.GetBytes()).ToMemory() });
-            var annotatedMessage = new AmqpAnnotatedMessage(messageBody)
+            Header = { DeliveryCount = 1 },
+            Properties =
             {
-                Header = { DeliveryCount = 1 },
-                Properties =
-                {
-                    MessageId = new AmqpMessageId(NewId.NextGuid().ToString()),
-                    ContentType = SystemTextJsonRawMessageSerializer.JsonContentType.MediaType
-                }
-            };
+                MessageId = new AmqpMessageId(NewId.NextGuid().ToString()),
+                ContentType = SystemTextJsonRawMessageSerializer.JsonContentType.MediaType
+            }
+        };
 
-            var receivedMessage = (ServiceBusReceivedMessage)typeof(ServiceBusReceivedMessage).GetConstructor(
-                BindingFlags.NonPublic | BindingFlags.Instance,
-                null, new[] { typeof(AmqpAnnotatedMessage) }, null).Invoke(new object[] { annotatedMessage });
+        var receivedMessage = (ServiceBusReceivedMessage)typeof(ServiceBusReceivedMessage).GetConstructor(
+            BindingFlags.NonPublic | BindingFlags.Instance, null, [typeof(AmqpAnnotatedMessage)], null).Invoke([annotatedMessage]);
 
-            var receiver = harness.Scope.ServiceProvider.GetRequiredService<IMessageReceiver>();
-            var formatter = harness.Scope.ServiceProvider.GetService<IEndpointNameFormatter>() ?? DefaultEndpointNameFormatter.Instance;
+        var receiver = harness.Scope.ServiceProvider.GetRequiredService<IMessageReceiver>();
+        var formatter = harness.Scope.ServiceProvider.GetService<IEndpointNameFormatter>() ?? DefaultEndpointNameFormatter.Instance;
 
-            return receiver.HandleConsumer<TConsumer>(formatter.Consumer<TConsumer>(), receivedMessage, harness.CancellationToken);
-        }
+        return receiver.HandleConsumer<TConsumer>(formatter.Consumer<TConsumer>(), receivedMessage, harness.CancellationToken);
     }
 }
