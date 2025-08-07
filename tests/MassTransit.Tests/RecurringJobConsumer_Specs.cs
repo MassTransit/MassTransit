96adb565f7cc311488d1df104549f924203be760
Chris Patterson <chris@phatboyg.com>
2025-08-07 12:31:55 -0500

    Valid multiple job types with different names work together without issues.

--- tests/MassTransit.Tests/RecurringJobConsumer_Specs.cs
+++ tests/MassTransit.Tests/RecurringJobConsumer_Specs.cs
@@ -13,40 +13,6 @@ using NUnit.Framework;
 public class Configuring_a_recurring_job_consumer
 {
     [Test]
-    public async Task Should_support_configuration_of_the_job()
-    {
-        await using var provider = new ServiceCollection()
-            .AddMassTransitTestHarness(x =>
-            {
-                x.SetKebabCaseEndpointNameFormatter();
-
-                x.AddConsumer<RecurringJobConsumer>();
-
-                x.AddJobSagaStateMachines(options => options.SlotWaitTime = TimeSpan.FromSeconds(1));
-                x.SetTestTimeouts(testInactivityTimeout: TimeSpan.FromSeconds(15));
-
-                x.UsingInMemory((context, cfg) =>
-                {
-                    cfg.UseDelayedMessageScheduler();
-
-                    cfg.ConfigureEndpoints(context);
-                });
-            })
-            .BuildServiceProvider(true);
-
-        var harness = await provider.StartTestHarness();
-
-        var client = harness.GetRequestClient<SubmitJob<RecurringJobMessage>>();
-
-        var jobId = await client.AddOrUpdateRecurringJob(nameof(RecurringJobConsumer), new RecurringJobMessage(), x => x.Every(seconds: 5),
-            harness.CancellationToken);
-
-        Assert.That(await harness.Published.SelectAsync<JobCompleted<RecurringJobMessage>>().Take(2).Count(), Is.EqualTo(2));
-
-        await harness.Stop();
-    }
-
-    [Test]
     public async Task Should_support_cancellation_and_continuation_of_the_job()
     {
         await using var provider = new ServiceCollection()
@@ -71,7 +37,7 @@ public class Configuring_a_recurring_job_consumer
         var harness = await provider.StartTestHarness();
         try
         {
-            var client = harness.GetRequestClient<SubmitJob<RecurringJobMessage>>();
+            IRequestClient<SubmitJob<RecurringJobMessage>> client = harness.GetRequestClient<SubmitJob<RecurringJobMessage>>();
 
             var jobId = await client.AddOrUpdateRecurringJob(nameof(RecurringJobConsumer), new RecurringJobMessage(), x => x.Every(seconds: 5),
                 harness.CancellationToken);
@@ -91,6 +57,77 @@ public class Configuring_a_recurring_job_consumer
         {
             await harness.Stop();
         }
+    }
+
+    [Test]
+    public async Task Should_support_configuration_of_the_job()
+    {
+        await using var provider = new ServiceCollection()
+            .AddMassTransitTestHarness(x =>
+            {
+                x.SetKebabCaseEndpointNameFormatter();
+
+                x.AddConsumer<RecurringJobConsumer>();
+
+                x.AddJobSagaStateMachines(options => options.SlotWaitTime = TimeSpan.FromSeconds(1));
+                x.SetTestTimeouts(testInactivityTimeout: TimeSpan.FromSeconds(15));
+
+                x.UsingInMemory((context, cfg) =>
+                {
+                    cfg.UseDelayedMessageScheduler();
+
+                    cfg.ConfigureEndpoints(context);
+                });
+            })
+            .BuildServiceProvider(true);
+
+        var harness = await provider.StartTestHarness();
+
+        IRequestClient<SubmitJob<RecurringJobMessage>> client = harness.GetRequestClient<SubmitJob<RecurringJobMessage>>();
+
+        var jobId = await client.AddOrUpdateRecurringJob(nameof(RecurringJobConsumer), new RecurringJobMessage(), x => x.Every(seconds: 5),
+            harness.CancellationToken);
+
+        Assert.That(await harness.Published.SelectAsync<JobCompleted<RecurringJobMessage>>().Take(2).Count(), Is.EqualTo(2));
+
+        await harness.Stop();
+    }
+
+    [Test]
+    [Explicit]
+    public async Task Should_support_multiple_jobs_of_the_same_type_with_different_names()
+    {
+        await using var provider = new ServiceCollection()
+            .AddMassTransitTestHarness(x =>
+            {
+                x.SetKebabCaseEndpointNameFormatter();
+
+                x.AddConsumer<MaintenanceJobConsumer>();
+
+                x.AddJobSagaStateMachines(options => options.SlotWaitTime = TimeSpan.FromSeconds(1));
+                x.SetTestTimeouts(testInactivityTimeout: TimeSpan.FromSeconds(15), testTimeout: TimeSpan.FromMinutes(5));
+
+                x.UsingInMemory((context, cfg) =>
+                {
+                    cfg.UseDelayedMessageScheduler();
+
+                    cfg.ConfigureEndpoints(context);
+                });
+            })
+            .BuildServiceProvider(true);
+
+        var harness = await provider.StartTestHarness();
+
+        IRequestClient<SubmitJob<MaintenanceTask>> client = harness.GetRequestClient<SubmitJob<MaintenanceTask>>();
+
+        var job1Id = await client.AddOrUpdateRecurringJob("One", new MaintenanceTask { Name = "One" }, x => x.Every(seconds: 4), harness.CancellationToken);
+        var job2Id = await client.AddOrUpdateRecurringJob("Two", new MaintenanceTask { Name = "Two" }, x => x.Every(seconds: 8), harness.CancellationToken);
+        var job3Id = await client.AddOrUpdateRecurringJob("Tree", new MaintenanceTask { Name = "Tree" }, x => x.Every(seconds: 10), harness.CancellationToken);
+        var job4Id = await client.AddOrUpdateRecurringJob("Four", new MaintenanceTask { Name = "Four" }, x => x.Every(seconds: 15), harness.CancellationToken);
+
+        Assert.That(await harness.Published.SelectAsync<JobCompleted<MaintenanceTask>>().Take(30).Count(), Is.EqualTo(30));
+
+        await harness.Stop();
     }
 
     [Test]
@@ -117,7 +154,7 @@ public class Configuring_a_recurring_job_consumer
 
         var harness = await provider.StartTestHarness();
 
-        var client = harness.GetRequestClient<SubmitJob<RecurringJobMessage>>();
+        IRequestClient<SubmitJob<RecurringJobMessage>> client = harness.GetRequestClient<SubmitJob<RecurringJobMessage>>();
 
         var jobId = await client.AddOrUpdateRecurringJob(nameof(RecurringJobConsumer), new RecurringJobMessage(), "*/5 * * * * ?",
             harness.CancellationToken);
@@ -156,7 +193,7 @@ public class Configuring_a_recurring_job_consumer
 
         var harness = await provider.StartTestHarness();
 
-        var client = harness.GetRequestClient<SubmitJob<RecurringJobMessage>>();
+        IRequestClient<SubmitJob<RecurringJobMessage>> client = harness.GetRequestClient<SubmitJob<RecurringJobMessage>>();
 
         var jobId = await client.AddOrUpdateRecurringJob(nameof(RecurringJobConsumer), new RecurringJobMessage(), "*/5 * * * * ?",
             harness.CancellationToken);
@@ -195,7 +232,7 @@ public class Configuring_a_recurring_job_consumer
 
         var harness = await provider.StartTestHarness();
 
-        var client = harness.GetRequestClient<SubmitJob<RecurringJobMessage>>();
+        IRequestClient<SubmitJob<RecurringJobMessage>> client = harness.GetRequestClient<SubmitJob<RecurringJobMessage>>();
 
         var jobId = await client.ScheduleJob(DateTimeOffset.UtcNow.AddSeconds(3), new RecurringJobMessage(), harness.CancellationToken);
 
@@ -205,7 +242,7 @@ public class Configuring_a_recurring_job_consumer
     }
 
 
-    public record RecurringJobConsumer :
+    public class RecurringJobConsumer :
         IJobConsumer<RecurringJobMessage>
     {
         readonly ILogger<RecurringJobConsumer> _logger;
@@ -222,7 +259,51 @@ public class Configuring_a_recurring_job_consumer
             return Task.CompletedTask;
         }
     }
+
+
+    public class MaintenanceJobConsumer :
+        IJobConsumer<MaintenanceTask>
+    {
+        readonly ILogger<MaintenanceJobConsumer> _logger;
+
+        public MaintenanceJobConsumer(ILogger<MaintenanceJobConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Run(JobContext<MaintenanceTask> context)
+        {
+            _logger.LogInformation("Running MaintenanceTask: {Id} {Name}", context.JobId, context.Job.Name);
+
+            try
+            {
+                await Task.Delay(4000, context.CancellationToken);
+
+                _logger.LogInformation("MaintenanceTask completed: {Id} {Name}", context.JobId, context.Job.Name);
+            }
+            catch (OperationCanceledException) when (context.CancellationToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("MaintenanceTask exception: {Id} {Name}", context.JobId, context.Job.Name);
+
+                await context.SaveJobState(new MaintenanceTaskState { Variance = 10000 });
+
+                throw;
+            }
+        }
+    }
 }
 
 
 public record RecurringJobMessage;
+
+
+public record MaintenanceTask
+{
+    public string Name { get; set; }
+}
+
+
+public record MaintenanceTaskState
+{
+    public int Variance { get; set; }
+}
