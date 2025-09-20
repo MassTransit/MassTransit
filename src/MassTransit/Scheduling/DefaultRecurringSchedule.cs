1a9c435a4da629e691d45ff8deb2ddb3550cef84
Chris Patterson <chris@phatboyg.com>
2025-09-20 09:56:48 -0500

    Fixed #6017 - made ScheduleId and ScheduleGroup settable

--- src/MassTransit/Scheduling/DefaultRecurringSchedule.cs
+++ src/MassTransit/Scheduling/DefaultRecurringSchedule.cs
@@ -1,27 +1,26 @@
-﻿namespace MassTransit.Scheduling
+﻿namespace MassTransit.Scheduling;
+
+using System;
+
+
+public abstract class DefaultRecurringSchedule :
+    RecurringSchedule
 {
-    using System;
-
-
-    public abstract class DefaultRecurringSchedule :
-        RecurringSchedule
+    protected DefaultRecurringSchedule()
     {
-        protected DefaultRecurringSchedule()
-        {
-            ScheduleId = TypeCache.GetShortName(GetType());
-            ScheduleGroup = GetType().Assembly.FullName.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[0];
+        ScheduleId = TypeCache.GetShortName(GetType());
+        ScheduleGroup = GetType().Assembly.FullName.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[0];
 
-            TimeZoneId = TimeZoneInfo.Local.Id;
-            StartTime = DateTime.Now;
-        }
-
-        public MissedEventPolicy MisfirePolicy { get; protected set; }
-        public string TimeZoneId { get; protected set; }
-        public DateTimeOffset StartTime { get; protected set; }
-        public DateTimeOffset? EndTime { get; protected set; }
-        public string ScheduleId { get; private set; }
-        public string ScheduleGroup { get; private set; }
-        public string CronExpression { get; protected set; }
-        public string Description { get; protected set; }
+        TimeZoneId = TimeZoneInfo.Local.Id;
+        StartTime = DateTime.Now;
     }
+
+    public MissedEventPolicy MisfirePolicy { get; protected set; }
+    public string TimeZoneId { get; protected set; }
+    public DateTimeOffset StartTime { get; protected set; }
+    public DateTimeOffset? EndTime { get; protected set; }
+    public string ScheduleId { get; protected set; }
+    public string ScheduleGroup { get; protected set; }
+    public string CronExpression { get; protected set; }
+    public string Description { get; protected set; }
 }
