d964e8ff82e756e2562a77f3e924b45a3c4cc0b8
Chris Patterson <chris@phatboyg.com>
2025-12-01 10:34:35 -0600

    Added a wrapped around the Nuxt content pointing to the new documentation site

---
+++ doc/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="bg-blue-600 text-white py-2 px-4 text-center">
+      <p class="font-medium">
+          Massient, Inc. is now behind MassTransit! Preview the new documentation at
+        <a href="https://masstransit.massient.com/" class="font-bold underline">
+          https://masstransit.massient.com/
+        </a>
+      </p>
+    </div>
+    <AppLayout>
+      <NuxtPage />
+    </AppLayout>
+  </div>
+</template>
